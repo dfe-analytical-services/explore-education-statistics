495591b981934d402ebe5c66f06775cd3e3ed39e
Martin Smith <msmith@hiveit.co.uk>
2018-11-20 10:22:38 +0000

    Basic gov.uk prototyping kit
    initial prototype. gov.uk prototyping kit

---
+++ govuk-prototype-kit-8.3.0/gulp/clean.js
@@ -0,0 +1,17 @@
+/*
+  clean.js
+  ===========
+  removes folders:
+    - public
+*/
+
+const gulp = require('gulp')
+const clean = require('gulp-clean')
+
+const config = require('./config.json')
+
+gulp.task('clean', function () {
+  return gulp.src([config.paths.public + '/*',
+    '.port.tmp'], {read: false})
+    .pipe(clean())
+})
