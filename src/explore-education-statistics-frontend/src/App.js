6d6533f74c6e5508e8dfaf43cf01486e7b7996f9
James Cutts <james@hiveit.co.uk>
2018-12-18 11:34:49 +0000

    [DFE-26] UPDATED skeleton frontend react application

--- src/explore-education-statistics-frontend/src/App.js
+++ src/explore-education-statistics-frontend/src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
+import Cookies from './pages/cookies'
+import Privacy from './pages/privacy'
+import NotFound from './pages/notfound'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/cookies" component={Cookies} />
+            <Route exact path="/privacy-policy" component={Privacy} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="govuk-heading-xl">Explore education statistics</h1>
       </div>
     );
   }
---
+++ src/explore-education-statistics-frontend/src/pages/privacy.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Privacy extends Component {
+    render() {
+      return (
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds">
+            <h1 className="govuk-heading-xl">Privacy policy</h1>
+          </div>
+        </div>
+  
+      );
+    }
+  }
+
+  export default Privacy;
