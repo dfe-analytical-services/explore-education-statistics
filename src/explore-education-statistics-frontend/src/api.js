e7b47e4de169c455cf84cf175fe52418e6c35e6a
Ben Outram <ben@hiveit.co.uk>
2019-01-02 14:39:23 +0000

    Removing url scheme

--- src/explore-education-statistics-frontend/src/api.js
+++ src/explore-education-statistics-frontend/src/api.js
@@ -4,9 +4,9 @@ const hostname = window && window.location && window.location.hostname;
 let baseURL;
 
 if(hostname === 'educationstatisticstest.z6.web.core.windows.net') {
-    baseURL = 'http://content-explore-education-statistics-test.azurewebsites.net';
+    baseURL = '//content-explore-education-statistics-test.azurewebsites.net';
   } else {
-    baseURL = 'http://localhost:5010';
+    baseURL = '//localhost:5010';
   }
 
 export default axios.create({
