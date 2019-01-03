f81e9865acdca86f29f9385dc43c5331b186fb55
James Cutts <james@hiveit.co.uk>
2019-01-03 20:35:42 +0000

    linting formatting

--- src/explore-education-statistics-frontend/src/api.ts
+++ src/explore-education-statistics-frontend/src/api.ts
@@ -5,11 +5,9 @@ let baseURL;
 
 if (hostname === 'educationstatisticstest.z6.web.core.windows.net') {
   baseURL = '//content-explore-education-statistics-test.azurewebsites.net';
-} 
-else if (hostname === 'educationstatisticsstage.z6.web.core.windows.net') {
+} else if (hostname === 'educationstatisticsstage.z6.web.core.windows.net') {
   baseURL = '//content-explore-education-statistics-stage.azurewebsites.net';
-}
-else if (hostname === 'educationstatistics.z6.web.core.windows.net') {
+} else if (hostname === 'educationstatistics.z6.web.core.windows.net') {
   baseURL = '//content-explore-education-statistics-live.azurewebsites.net';
 } else {
   baseURL = '//localhost:5010';
