00e9979c02d74bc4c3c2998c972e77b2ee736b6d
James Cutts <james@hiveit.co.uk>
2019-01-03 20:24:06 +0000

    Update api location check

--- src/explore-education-statistics-frontend/src/api.ts
+++ src/explore-education-statistics-frontend/src/api.ts
@@ -5,6 +5,12 @@ let baseURL;
 
 if (hostname === 'educationstatisticstest.z6.web.core.windows.net') {
   baseURL = '//content-explore-education-statistics-test.azurewebsites.net';
+} 
+else if (hostname === 'educationstatisticsstage.z6.web.core.windows.net') {
+  baseURL = '//content-explore-education-statistics-stage.azurewebsites.net';
+}
+else if (hostname === 'educationstatistics.z6.web.core.windows.net') {
+  baseURL = '//content-explore-education-statistics-live.azurewebsites.net';
 } else {
   baseURL = '//localhost:5010';
 }
