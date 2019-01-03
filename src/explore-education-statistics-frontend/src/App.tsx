37b933bbadf2b531c0f7327e9ad22804dee299e9
James Cutts <james@hiveit.co.uk>
2019-01-03 21:46:52 +0000

    Frontend: fixed up breadcrumb positioning inside react app

--- src/explore-education-statistics-frontend/src/App.tsx
+++ src/explore-education-statistics-frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Breadcrumbs from './components/Breadcrumbs';
 import CookiesPage from './pages/CookiesPage';
 import FeedbackPage from './pages/FeedbackPage';
 import HomePage from './pages/HomePage';
@@ -18,40 +24,71 @@ class App extends Component {
   public render() {
     return (
       <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/cookies" component={CookiesPage} />
-            <Route exact path="/privacy-policy" component={PrivacyPage} />
-            <Route exact path="/feedback" component={FeedbackPage} />
+        <HashRouter>
+          <div>
+            <div className="govuk-phase-banner">
+              <p className="govuk-phase-banner__content">
+                <strong className="govuk-tag govuk-phase-banner__content__tag ">
+                  alpha
+                </strong>
+                <span className="govuk-phase-banner__text">
+                  This is a new service – your
+                  <a className="govuk-link" href="/feedback">
+                    feedback
+                  </a>{' '}
+                  will help us to improve it.
+                </span>
+              </p>
+            </div>
+            <Breadcrumbs current={'page'} />
+            <main
+              className="govuk-main-wrapper app-main-class"
+              id="main-content"
+              role="main"
+            >
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/cookies" component={CookiesPage} />
+                <Route exact path="/privacy-policy" component={PrivacyPage} />
+                <Route exact path="/feedback" component={FeedbackPage} />
 
-            <Route exact path="/themes" component={ThemesPage} />
-            <Route exact path="/themes/:theme" component={ThemePage} />
-            <Route exact path="/themes/:theme/:topic" component={TopicPage} />
-            <Route
-              exact
-              path="/themes/:theme/:topic/:publication"
-              component={PublicationPage}
-            />
+                <Route exact path="/themes" component={ThemesPage} />
+                <Route exact path="/themes/:theme" component={ThemePage} />
+                <Route
+                  exact
+                  path="/themes/:theme/:topic"
+                  component={TopicPage}
+                />
+                <Route
+                  exact
+                  path="/themes/:theme/:topic/:publication"
+                  component={PublicationPage}
+                />
 
-            <Route exact path="/topics" component={TopicsPage} />
-            <Route exact path="/topics/:topic" component={TopicPage} />
-            <Route
-              exact
-              path="/topics/:topic/:publication"
-              component={PublicationPage}
-            />
+                <Route exact path="/topics" component={TopicsPage} />
+                <Route exact path="/topics/:topic" component={TopicPage} />
+                <Route
+                  exact
+                  path="/topics/:topic/:publication"
+                  component={PublicationPage}
+                />
 
-            <Route exact path="/publications/" component={PublicationsPage} />
-            <Route
-              exact
-              path="/publications/:publication"
-              component={PublicationPage}
-            />
+                <Route
+                  exact
+                  path="/publications/"
+                  component={PublicationsPage}
+                />
+                <Route
+                  exact
+                  path="/publications/:publication"
+                  component={PublicationPage}
+                />
 
-            <Route component={NotFoundPage} />
-          </Switch>
-        </Router>
+                <Route component={NotFoundPage} />
+              </Switch>
+            </main>
+          </div>
+        </HashRouter>
       </div>
     );
   }
--- src/explore-education-statistics-frontend/src/index.tsx
+++ src/explore-education-statistics-frontend/src/index.tsx
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('main-content'));
+ReactDOM.render(<App />, document.getElementById('application'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
