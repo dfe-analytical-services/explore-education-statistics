815aab762abd45cdb40395fdf374bd509e67cecb
Nicholas Tsim <nick@hiveit.co.uk>
2019-01-03 17:25:47 +0000

    frontend: Remove index.scss

--- src/explore-education-statistics-frontend/src/index.tsx
+++ src/explore-education-statistics-frontend/src/index.tsx
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import './index.scss';
-
 ReactDOM.render(<App />, document.getElementById('application'));
 
 // If you want your app to work offline and load faster, you can change
