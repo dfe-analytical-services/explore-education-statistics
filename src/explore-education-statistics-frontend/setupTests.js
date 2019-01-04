63a1c3b13f3b69df70177486ad80735946da78b8
Nicholas Tsim <nick@hiveit.co.uk>
2019-01-04 12:01:46 +0000

    frontend: Add Jest test dependencies and `classnames`

---
+++ src/explore-education-statistics-frontend/setupTests.js
@@ -0,0 +1,9 @@
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
