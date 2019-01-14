ccbf8a8c11878b35fc3ec67c4512cf5b3476188a
Martin Waite <martin@hiveit.co.uk>
2019-01-14 16:31:10 +0000

    PROT - updating template strings for linting

--- src/explore-education-statistics-frontend/src/prototypes/components/PrototypeDataSample.tsx
+++ src/explore-education-statistics-frontend/src/prototypes/components/PrototypeDataSample.tsx
@@ -15,31 +15,31 @@ const PrototypeDataSample = ({ sectionId, chartTitle }: Props) => {
           <li className="govuk-tabs__list-item">
             <a
               className="govuk-tabs__tab govuk-tabs__tab--selected"
-              href={'#' + sectionId + 'ChartData'}
+              href={`#${sectionId}ChartData`}
             >
               Summary
             </a>
           </li>
           <li className="govuk-tabs__list-item">
-            <a className="govuk-tabs__tab" href={'#' + sectionId + 'TableData'}>
+            <a className="govuk-tabs__tab" href={`#${sectionId}TableData`}>
               Data tables
             </a>
           </li>
           <li className="govuk-tabs__list-item">
-            <a className="govuk-tabs__tab" href={'#' + sectionId + 'Downloads'}>
+            <a className="govuk-tabs__tab" href={`#${sectionId}Downloads`}>
               Data downloads
             </a>
           </li>
         </ul>
-        <section className="govuk-tabs__panel" id={sectionId + 'ChartData'}>
-          <h2 className="govuk-heading-s">{'Chart showing ' + chartTitle}</h2>
+        <section className="govuk-tabs__panel" id={`${sectionId}ChartData`}>
+          <h2 className="govuk-heading-s">{`Chart showing ${chartTitle}`}</h2>
           <PrototypeChartSample />
         </section>
         <section
           className="govuk-tabs__panel govuk-tabs__panel--hidden"
-          id={sectionId + 'TableData'}
+          id={`${sectionId}TableData`}
         >
-          <h2 className="govuk-heading-s">{'Table showing ' + chartTitle}</h2>
+          <h2 className="govuk-heading-s">{`Table showing${chartTitle}`}</h2>
           <PrototypeTableSample />
         </section>
       </div>
