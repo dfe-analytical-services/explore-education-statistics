eb1ec179942db940672d3a538c942cda140ec2cf
Mark Youngman <mark@hiveit.co.uk>
2019-01-15 11:10:50 +0000

    Test: Increase implicit wait/timeout for pipeline tests. Use pabot when running tests in pipeline.

--- tests/robot-tests/scripts/pipeline-run-rf-tests.sh
+++ tests/robot-tests/scripts/pipeline-run-rf-tests.sh
@@ -2,4 +2,5 @@
 
 cd tests/robot-tests
 pipenv install
-pipenv run robot --outputdir test-results/ --xunit xunit -v headless:1 -v env:test -v browser:chrome -v timeout:5 -v implicit_wait:5 tests/
+#pipenv run robot --outputdir test-results/ --xunit xunit -v headless:1 -v env:test -v browser:chrome -v timeout:10 -v implicit_wait:10 tests/
+pipenv run pabot --outputdir test-results/ --xunit xunit -v headless:1 -v env:test -v browser:chrome -v timeout:10 -v implicit_wait:10 tests/
