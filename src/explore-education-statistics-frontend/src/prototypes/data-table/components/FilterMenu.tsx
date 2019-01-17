b207e98b7d837c08e3f862c760e1a9557d6f16fd
Nicholas Tsim <nick@hiveit.co.uk>
2019-01-17 15:09:18 +0000

    DFE-121 Add some additional themes to FilterMenu

--- src/explore-education-statistics-frontend/src/prototypes/data-table/components/FilterMenu.tsx
+++ src/explore-education-statistics-frontend/src/prototypes/data-table/components/FilterMenu.tsx
@@ -15,33 +15,51 @@ const FilterMenu: FunctionComponent<Props> = ({ beforeMenu, onChange }) => {
     <div className={styles.filterMenu}>
       {beforeMenu}
 
-      <MenuDetails summary="School statistics (under 16)" open>
-        <MenuDetails summary="Absence and exclusions" open>
-          <FormCheckboxGroup
-            name="absenceAndExclusions"
-            onChange={onChange}
-            options={[
-              {
-                checked: true,
-                id: 'pupilAbsence',
-                label: 'Pupil absence',
-                value: 'PUPIL_ABSENCE',
-              },
-              {
-                checked: true,
-                id: 'exclusions',
-                label: 'Exclusions',
-                value: 'EXCLUSIONS',
-              },
-            ]}
-          />
+      <div>
+        <MenuDetails summary="Schools (under 16)" open>
+          <MenuDetails summary="Absence and exclusions" open>
+            <FormCheckboxGroup
+              name="absenceAndExclusions"
+              onChange={onChange}
+              options={[
+                {
+                  checked: true,
+                  id: 'pupilAbsence',
+                  label: 'Pupil absence',
+                  value: 'PUPIL_ABSENCE',
+                },
+                {
+                  checked: true,
+                  id: 'exclusions',
+                  label: 'Exclusions',
+                  value: 'EXCLUSIONS',
+                },
+              ]}
+            />
+          </MenuDetails>
+          <MenuDetails summary="Capacity and admissions" />
+          <MenuDetails summary="Results" />
+          <MenuDetails summary="School and pupil numbers" />
+          <MenuDetails summary="School finance" />
+          <MenuDetails summary="Teacher numbers" />
         </MenuDetails>
-        <MenuDetails summary="Capacity and admissions" />
-        <MenuDetails summary="Results" />
-        <MenuDetails summary="School and pupil numbers" />
-        <MenuDetails summary="School finance" />
-        <MenuDetails summary="Teacher numbers" />
-      </MenuDetails>
+        <MenuDetails summary="16+ education">
+          <MenuDetails summary="Absence and exclusions" />
+          <MenuDetails summary="Capacity and admissions" />
+          <MenuDetails summary="Results" />
+          <MenuDetails summary="School and pupil numbers" />
+          <MenuDetails summary="School finance" />
+          <MenuDetails summary="Teacher numbers" />
+        </MenuDetails>
+        <MenuDetails summary="Social care">
+          <MenuDetails summary="Absence and exclusions" />
+          <MenuDetails summary="Capacity and admissions" />
+          <MenuDetails summary="Results" />
+          <MenuDetails summary="School and pupil numbers" />
+          <MenuDetails summary="School finance" />
+          <MenuDetails summary="Teacher numbers" />
+        </MenuDetails>
+      </div>
     </div>
   );
 };
