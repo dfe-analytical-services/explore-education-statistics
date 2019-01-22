ab9fe7e7d19ef3bcbab49893f82d3f257fbcedcf
Nicholas Tsim <nick@hiveit.co.uk>
2019-01-22 10:50:42 +0000

    frontend: Fix Details unit test

--- src/explore-education-statistics-frontend/src/components/__tests__/Details.test.tsx
+++ src/explore-education-statistics-frontend/src/components/__tests__/Details.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { fireEvent, render } from 'react-testing-library';
 import Details from '../Details';
 
 describe('Details', () => {
   test('renders correctly', () => {
     const { container } = render(
-      <Details summary="Test summary">
-        <p>Test details</p>
+      <Details summary="Test summary" id="test-details">
+        Test content
       </Details>,
     );
 
@@ -14,13 +14,36 @@ describe('Details', () => {
   });
 
   test('renders correctly with `open` prop set to true', () => {
-    const { container } = render(
-      <Details summary="Test summary" open>
-        <p>Test details</p>
+    const { getByText, container } = render(
+      <Details summary="Test summary" id="test-details" open>
+        Test content
       </Details>,
     );
 
     expect(container.querySelector('[open]')).not.toBeNull();
+
+    expect(container.querySelector('summary')).toHaveAttribute('aria-expanded', 'true');
+    expect(getByText('Test content')).toHaveAttribute('aria-hidden', 'false');
+
     expect(container.innerHTML).toMatchSnapshot();
+  });
+
+  test('clicking the summary reveals the content', () => {
+    const { container, getByText } = render(
+      <Details summary="Test summary" id="test-details">
+        Test content
+      </Details>,
+    );
+
+    const content = getByText('Test content');
+    const summary = container.querySelector('summary') as HTMLElement;
+
+    expect(summary).toHaveAttribute('aria-expanded', 'false');
+    expect(content).toHaveAttribute('aria-hidden', 'true');
+
+    fireEvent.click(summary);
+
+    expect(summary).toHaveAttribute('aria-expanded', 'true');
+    expect(content).toHaveAttribute('aria-hidden', 'false');
   });
 });
