10a9f82618aa9e050bf8a19dd140a7d6be9f58dd
Nicholas Tsim <nick@hiveit.co.uk>
2019-01-25 15:31:53 +0000

    frontend: Add FormTextInput component

---
+++ src/explore-education-statistics-frontend/src/components/__tests__/FormTextInput.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import { FormTextInput } from '../FormTextInput';
+
+describe('FormTextInput', () => {
+  test('renders correctly with required props', () => {
+    const { container, getByLabelText } = render(
+      <FormTextInput id="test-input" label="Test input" name="testInput" />,
+    );
+
+    expect(getByLabelText('Test input')).toBeDefined();
+    expect(container.innerHTML).toMatchSnapshot();
+  });
+
+  test('renders correctly with hint', () => {
+    const { container, getByText } = render(
+      <FormTextInput
+        id="test-input"
+        label="Test input"
+        name="testInput"
+        hint="Fill me in"
+      />,
+    );
+
+    expect(getByText('Fill me in')).toBeDefined();
+    expect(container.innerHTML).toMatchSnapshot();
+  });
+});
