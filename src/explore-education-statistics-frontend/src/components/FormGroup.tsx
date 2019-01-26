da637c82c40bd23e28b84318c47ec20c643f5e1f
Nicholas Tsim <nicholastsim@gmail.com>
2019-01-26 18:53:48 +0000

    frontend: Add FormGroup

---
+++ src/explore-education-statistics-frontend/src/components/FormGroup.tsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames';
+import React, { FunctionComponent, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+  hasError?: boolean;
+}
+
+export const FormGroup: FunctionComponent<Props> = ({
+  children,
+  hasError = false,
+}) => {
+  return (
+    <div
+      className={classNames('govuk-form-group', {
+        'govuk-form-group--error': hasError,
+      })}
+    >
+      {children}
+    </div>
+  );
+};
