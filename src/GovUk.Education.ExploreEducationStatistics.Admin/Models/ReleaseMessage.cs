2b7d7bd8302537a4c9a63dc037b3345fcd9576ec
James Cutts <james@hiveit.co.uk>
2019-01-28 13:59:05 +0000

    ADMIN-APP: Add files into publication and release folders including filename

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Models/ReleaseMessage.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Models/ReleaseMessage.cs
@@ -8,14 +8,14 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Models
         {
         }
 
-        public ReleaseMessage(Guid publication, Guid release, DateTime uploadedOn)
+        public ReleaseMessage(string publication, Guid release, DateTime uploadedOn)
         {
             Publication = publication;
             Release = release;
             UploadedOn = uploadedOn;
         }
 
-        public Guid Publication { get; set; }
+        public string Publication { get; set; }
         public Guid Release { get; set; }
         public DateTime UploadedOn { get; set; }
     }
