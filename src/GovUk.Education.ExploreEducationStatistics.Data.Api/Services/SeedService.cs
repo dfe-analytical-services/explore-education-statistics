63eb1ca1f07e5e3b679f3ed321565d92befc4ea9
James Cutts <james@hiveit.co.uk>
2019-01-29 17:24:20 +0000

    DATA-API: Updated seeding

--- src/GovUk.Education.ExploreEducationStatistics.Data.Api/Services/SeedService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/Services/SeedService.cs
@@ -1,26 +1,27 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using GovUk.Education.ExploreEducationStatistics.Data.Api.Importer;
 using GovUk.Education.ExploreEducationStatistics.Data.Api.Models;
 using GovUk.Education.ExploreEducationStatistics.Data.Api.Models.Meta;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Services
 {
     public class SeedService
     {
+        private readonly ILogger _logger;
         private readonly IMongoDatabase _database;
         private readonly CsvImporterFactory _csvImporterFactory = new CsvImporterFactory();
 
-        public SeedService(IConfiguration config)
+        public SeedService(IConfiguration config, ILogger<SeedService> logger)
         {
             var client = new MongoClient(config.GetConnectionString("StatisticsDb"));
             _database = client.GetDatabase("education-statistics");
-        }
-
-        public bool CanSeed()
-        {
-            return !_database.ListCollectionNames().Any();
+            _logger = logger;
         }
 
         public void DropAllCollections()
@@ -28,50 +29,96 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Services
             _database.ListCollectionNames().ForEachAsync(collection => _database.DropCollection(collection));
         }
 
-        public int Seed()
+        public async Task<int> Seed()
         {
-            return SamplePublications.Publications.Values.Sum(Seed);
+            var count = 0;
+            foreach (var p in SamplePublications.Publications.Values)
+            {
+                count = count + await Seed(p);
+            }
+                
+            return count;
         }
 
-        private int Seed(Publication publication)
+        private async Task<int> Seed(Publication publication)
         {
-            Seed(publication, publication.AttributeMetas);
-            Seed(publication, publication.CharacteristicMetas);
-            return publication.Releases.Sum(Seed);
+            var count = 0;
+
+            await SeedAttribute(publication, publication.AttributeMetas);
+
+            await SeedCharacteristics(publication, publication.CharacteristicMetas);
+
+            foreach (var release in publication.Releases)
+            {
+                count = count + await SeedPublication(release);
+            }
+            
+            return count; 
         }
 
-        private int Seed(Release release)
+        private async Task<int> SeedPublication(Release release)
         {
             var count = 0;
             foreach (var dataCsvFilename in release.Filenames)
             {
                 var importer = _csvImporterFactory.Importer(dataCsvFilename);
-                var data = importer.Data(dataCsvFilename, release.PublicationId, release.ReleaseId,
-                    release.ReleaseDate);
-                _database.GetCollection<TidyData>(release.PublicationId.ToString()).InsertManyAsync(data);
+                var data = importer.Data(dataCsvFilename, release.PublicationId, release.ReleaseId,release.ReleaseDate);
+                
+                await _database.GetCollection<TidyData>(release.PublicationId.ToString()).InsertManyAsync(data);
                 count += data.Count;
             }
 
             return count;
         }
 
-        private void Seed(Publication publication, AttributeMeta[] attributeMetas)
+        private async Task SeedAttribute(Publication publication, ICollection<AttributeMeta> attributeMetas)
         {
-            foreach (var attributeMeta in attributeMetas)
-            {
-                attributeMeta.PublicationId = publication.PublicationId;
-            }
+            _logger.LogInformation("Seeding AttributeMeta");
 
-            _database.GetCollection<AttributeMeta>("AttributeMeta").InsertManyAsync(attributeMetas);
+            try
+            {
+                var collection = _database.GetCollection<AttributeMeta>("AttributeMeta");
+                
+                foreach (var attributeMeta in attributeMetas)
+                {
+                    attributeMeta.PublicationId = publication.PublicationId;
+                }
+
+                await collection.InsertManyAsync(attributeMetas);
+                
+                _logger.LogInformation("Seeded " + attributeMetas.Count + " items in AttributeMeta");
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Unable to seed AttributeMeta");
+
+                Console.WriteLine(e);
+            }
         }
 
-        private void Seed(Publication publication, CharacteristicMeta[] characteristicMetas)
+        private async Task SeedCharacteristics(Publication publication, ICollection<CharacteristicMeta> characteristicMetas)
         {
-            foreach (var characteristicMeta in characteristicMetas)
+            _logger.LogInformation("Seeding CharacteristicMeta");
+            
+            try
             {
-                characteristicMeta.PublicationId = publication.PublicationId;
+                foreach (var characteristicMeta in characteristicMetas)
+                {
+                    characteristicMeta.PublicationId = publication.PublicationId;
+                }
+                
+                await _database.GetCollection<CharacteristicMeta>("CharacteristicMeta").InsertManyAsync(characteristicMetas);
+                
+                _logger.LogInformation("Seeded " + characteristicMetas.Count + " items in CharacteristicMeta");
+
             }
-            _database.GetCollection<CharacteristicMeta>("CharacteristicMeta").InsertManyAsync(characteristicMetas);
+            catch (Exception e)
+            {
+                _logger.LogError("Unable to seed CharacteristicMeta");
+
+                Console.WriteLine(e);
+            }
         }
     }
 }
