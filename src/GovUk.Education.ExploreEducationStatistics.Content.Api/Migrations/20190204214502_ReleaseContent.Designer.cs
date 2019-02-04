37122e990649d660836e1a714ae07eda100aaca6
James Cutts <james@jamescutt.co.uk>
2019-02-04 21:46:44 +0000

    CONTENT-API: add content sections to releases

--- src/GovUk.Education.ExploreEducationStatistics.Content.Api/Migrations/20190204214502_ReleaseContent.Designer.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Migrations/20190204214502_ReleaseContent.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GovUk.Education.ExploreEducationStatistics.Content.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190204214502_ReleaseContent")]
+    partial class ReleaseContent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Models/ContentSection.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Models
+{
+    public class ContentSection
+    {
+        public string Heading { get; set; }
+        
+        public string Caption { get; set; }
+    }
+}
