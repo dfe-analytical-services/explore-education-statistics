9439fae8ef333f6c6ab18a0fb495da063a62a661
Nicholas Tsim <nick@hiveit.co.uk>
2019-02-04 15:23:27 +0000

    frontend: Add missing exports from form module index

--- src/explore-education-statistics-frontend/src/components/form/index.ts
+++ src/explore-education-statistics-frontend/src/components/form/index.ts
@@ -1,6 +1,8 @@
 export { default as FormCheckbox } from './FormCheckbox';
 export { default as FormCheckboxGroup } from './FormCheckboxGroup';
+export { default as FormFieldSet } from './FormFieldSet';
 export { default as FormGroup } from './FormGroup';
 export { default as FormRadio } from './FormRadio';
 export { default as FormRadioGroup } from './FormRadioGroup';
+export { default as FormSelect } from './FormSelect';
 export { default as FormTextInput } from './FormTextInput';
