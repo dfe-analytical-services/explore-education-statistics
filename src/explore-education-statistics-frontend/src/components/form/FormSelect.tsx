840c6e32088af7959f8128b1d2e1b585a90f288a
Nicholas Tsim <nick@hiveit.co.uk>
2019-02-04 15:14:55 +0000

    frontend: Add FormSelect component

---
+++ src/explore-education-statistics-frontend/src/components/form/FormSelect.tsx
@@ -0,0 +1,54 @@
+import classNames from 'classnames';
+import React, { ChangeEventHandler, ReactNode } from 'react';
+import ErrorMessage from '../ErrorMessage';
+
+export interface SelectOption {
+  text: string;
+  value: string | number;
+}
+
+interface Props {
+  error?: string;
+  id: string;
+  label: ReactNode | string;
+  name: string;
+  onChange?: ChangeEventHandler<any>;
+  options: SelectOption[];
+  value?: string | number;
+}
+
+const FormSelect = ({
+  error,
+  id,
+  label,
+  name,
+  onChange,
+  options,
+  value,
+}: Props) => {
+  return (
+    <>
+      <label className="govuk-label" htmlFor={id}>
+        {label}
+      </label>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      <select
+        className={classNames('govuk-select', {
+          'govuk-select--error': !!error,
+        })}
+        id={id}
+        name={name}
+        onChange={onChange}
+        value={value}
+      >
+        {options.map(option => (
+          <option value={option.value} key={`${option.value}-${option.text}`}>
+            {option.text}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};
+
+export default FormSelect;
