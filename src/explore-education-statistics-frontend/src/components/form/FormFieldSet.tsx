f126a2e954fd02cdd3d62657a418fbd0aff372b8
Nicholas Tsim <nick@hiveit.co.uk>
2019-02-04 15:21:41 +0000

    frontend: Add explicit type to FormFieldSet props

--- src/explore-education-statistics-frontend/src/components/form/FormFieldSet.tsx
+++ src/explore-education-statistics-frontend/src/components/form/FormFieldSet.tsx
@@ -5,6 +5,7 @@ import FormGroup from './FormGroup';
 import createDescribedBy from './util/createDescribedBy';
 
 export interface FieldSetProps {
+  children?: ReactNode;
   error?: string;
   hint?: string;
   id: string;
@@ -19,7 +20,7 @@ const FormFieldSet: FunctionComponent<FieldSetProps> = ({
   id,
   legend,
   legendSize = 'm',
-}) => {
+}: FieldSetProps) => {
   return (
     <FormGroup hasError={!!error}>
       <fieldset
