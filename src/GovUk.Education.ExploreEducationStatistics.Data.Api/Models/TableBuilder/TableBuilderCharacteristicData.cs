d48ae91092f11a11d59febc85ea3b2a19d73c388
Ben Outram <ben@hiveit.co.uk>
2019-02-11 15:21:18 +0000

    DFE-114 Adding missing JsonConverter to SchoolType.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Api/Models/TableBuilder/TableBuilderCharacteristicData.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/Models/TableBuilder/TableBuilderCharacteristicData.cs
@@ -1,13 +1,18 @@
 using System.Collections.Generic;
 using GovUk.Education.ExploreEducationStatistics.Data.Api.Models.Query;
 using GovUk.Education.ExploreEducationStatistics.Data.Api.ViewModels;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Models.TableBuilder
 {
     public class TableBuilderCharacteristicData : ITableBuilderData
     {
         public int Year { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
         public SchoolType SchoolType { get; set; }
+
         public CharacteristicViewModel Characteristic { get; set; }
         public Dictionary<string, string> Attributes { get; set; }
     }
