73a81a7c9e53f5521965c6f479fa3ee5e671cce4
James Cutts <james@hiveit.co.uk>
2019-02-25 16:22:48 +0000

    CONTENT-API: adding initial unit tests Check the the content block converter correctly identifies content block types. Updates Content api to dotnet core 2.2

---
+++ src/GovUk.Education.ExploreStatistics.Content.Api.Tests/Converters/ContentBlockConverterTests.cs
@@ -0,0 +1,45 @@
+using GovUk.Education.ExploreEducationStatistics.Content.Api.Converters;
+using GovUk.Education.ExploreEducationStatistics.Content.Api.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace GovUk.Education.ExploreStatistics.Content.Api.Tests.Converters
+{
+    public class ContentBlockConverterTests
+    {
+        [Fact]
+        public void ContentBlockConverterReturnsMarkdownBlock()
+        {
+            const string testString = "{\"Type\":\"MarkDownBlock\",\"Body\":\"Markdown text\"}";
+            
+            var serializerSettings = new JsonSerializerSettings();
+            serializerSettings.Converters.Add(new ContentBlockConverter());
+
+            var serializer = JsonSerializer.Create(serializerSettings);
+
+            var jObject = JObject.Parse(testString);
+            var result = jObject.ToObject<MarkDownBlock>(serializer);
+            
+            Assert.Equal(result.Body, "Markdown text");
+        }
+        
+        [Fact]
+        public void ContentBlockConverterReturnsInsetTextBlock()
+        {
+            const string testString = "{\"Type\":\"InsetTextBlock\", \"Heading\": \"Heading text\",\"Body\":\"heading body text\"}";
+            
+            var serializerSettings = new JsonSerializerSettings();
+            serializerSettings.Converters.Add(new ContentBlockConverter());
+
+            var serializer = JsonSerializer.Create(serializerSettings);
+
+            var jObject = JObject.Parse(testString);
+            
+            var result = jObject.ToObject<InsetTextBlock>(serializer);
+            
+            Assert.Equal(result.Heading, "Heading text");
+            Assert.Equal(result.Body, "heading body text");
+        }
+    }
+}
