a766d8976d2d62d95b437f4bbad5e9e32e900bf6
Nicholas Tsim <nick@hiveit.co.uk>
2019-03-06 11:13:35 +0000

    frontend: Add Sass modules (with .module.scss pattern) to Next config

--- src/explore-education-statistics-frontend/src/next.config.js
+++ src/explore-education-statistics-frontend/src/next.config.js
@@ -1,12 +1,56 @@
+const compose = require('lodash/fp/compose');
 const withTypescript = require('@zeit/next-typescript');
+const cssLoaderConfig = require('@zeit/next-css/css-loader-config');
 const withSass = require('@zeit/next-sass');
 const ForkTsCheckerPlugin = require('fork-ts-checker-webpack-plugin');
+const withImages = require('next-images');
 const path = require('path');
 
+const withSassModules = (nextConfig = {}) => {
+  return withSass(
+    Object.assign({}, nextConfig, {
+      webpack(config, options) {
+        const { dev, isServer } = options;
+
+        const sassModuleLoader = cssLoaderConfig(config, {
+          extensions: ['scss', 'sass'],
+          cssModules: true,
+          dev,
+          isServer,
+          loaders: [
+            {
+              loader: 'sass-loader',
+              options: {},
+            },
+          ],
+        });
+
+        config.module.rules.push(
+          {
+            test: /\.module.scss$/,
+            use: sassModuleLoader,
+          },
+          {
+            test: /\.module.sass$/,
+            use: sassModuleLoader,
+          },
+        );
+
+        if (typeof nextConfig.webpack === 'function') {
+          return nextConfig.webpack(config, options);
+        }
+
+        return config;
+      },
+    }),
+  );
+};
+
 const config = {
-  cssModules: true,
   webpack(config, options) {
-    if (options.isServer) {
+    const { isServer } = options;
+
+    if (isServer) {
       config.plugins.push(
         new ForkTsCheckerPlugin({
           tsconfig: path.resolve(__dirname, '../tsconfig.json'),
@@ -14,8 +58,14 @@ const config = {
       );
     }
 
+    config.resolve.alias.src = path.resolve(__dirname);
+
     return config;
   },
 };
 
-module.exports = withSass(withTypescript(config));
+module.exports = compose(
+  withImages,
+  withSassModules,
+  withTypescript,
+)(config);
