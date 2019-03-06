944925e0724e4e64d51e59b793ec0d995a405133
Nicholas Tsim <nick@hiveit.co.uk>
2019-03-06 12:04:50 +0000

    DFE-168 Refactor next.config with createPlugin function

--- src/explore-education-statistics-frontend/src/next.config.js
+++ src/explore-education-statistics-frontend/src/next.config.js
@@ -6,35 +6,11 @@ const ForkTsCheckerPlugin = require('fork-ts-checker-webpack-plugin');
 const withImages = require('next-images');
 const path = require('path');
 
-const withSassModules = (nextConfig = {}) => {
-  return withSass(
+const createPlugin = plugin => {
+  return (nextConfig = {}) =>
     Object.assign({}, nextConfig, {
       webpack(config, options) {
-        const { dev, isServer } = options;
-
-        const sassModuleLoader = cssLoaderConfig(config, {
-          extensions: ['scss', 'sass'],
-          cssModules: true,
-          dev,
-          isServer,
-          loaders: [
-            {
-              loader: 'sass-loader',
-              options: {},
-            },
-          ],
-        });
-
-        config.module.rules.push(
-          {
-            test: /\.module.scss$/,
-            use: sassModuleLoader,
-          },
-          {
-            test: /\.module.sass$/,
-            use: sassModuleLoader,
-          },
-        );
+        plugin(config, options, nextConfig);
 
         if (typeof nextConfig.webpack === 'function') {
           return nextConfig.webpack(config, options);
@@ -42,9 +18,60 @@ const withSassModules = (nextConfig = {}) => {
 
         return config;
       },
-    }),
-  );
+    });
 };
+
+const withFonts = createPlugin((config, options) => {
+  const { isServer } = options;
+
+  config.module.rules.push({
+    test: /\.(woff|woff2|eot|ttf|otf)$/,
+    use: [
+      {
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          fallback: 'file-loader',
+          publicPath: '/_next/static/fonts/',
+          outputPath: `${isServer ? '../' : ''}static/fonts/`,
+          name: '[name]-[hash].[ext]',
+        },
+      },
+    ],
+  });
+
+  return config;
+});
+
+const withSassModules = createPlugin((config, options) => {
+  const { dev, isServer } = options;
+
+  const sassModuleLoader = cssLoaderConfig(config, {
+    extensions: ['scss', 'sass'],
+    cssModules: true,
+    dev,
+    isServer,
+    loaders: [
+      {
+        loader: 'sass-loader',
+        options: {},
+      },
+    ],
+  });
+
+  config.module.rules.push(
+    {
+      test: /\.module.scss$/,
+      use: sassModuleLoader,
+    },
+    {
+      test: /\.module.sass$/,
+      use: sassModuleLoader,
+    },
+  );
+
+  return config;
+});
 
 const config = {
   webpack(config, options) {
@@ -65,7 +92,9 @@ const config = {
 };
 
 module.exports = compose(
+  withFonts,
   withImages,
+  withSass,
   withSassModules,
   withTypescript,
 )(config);
