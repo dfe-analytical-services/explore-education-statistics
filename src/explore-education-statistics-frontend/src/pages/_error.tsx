7c6e5b1792384ec8aaa4a83b3cb2a45018e26afe
Nicholas Tsim <nick@hiveit.co.uk>
2019-03-06 13:03:42 +0000

    DFE-168 Fix error page not showing 404s correctly

--- src/explore-education-statistics-frontend/src/pages/_error.tsx
+++ src/explore-education-statistics-frontend/src/pages/_error.tsx
@@ -1,75 +1,74 @@
 import { NextContext } from 'next';
-import React, { Component, ReactNode } from 'react';
+import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 
 interface Props {
-  errorPage?: ReactNode;
+  errorMessage: string;
+  statusCode: number;
 }
 
-const statusCodeTitles: { [code: number]: string } = {
-  404: 'Page not found',
-  500: 'Sorry, there is a problem with the service',
-};
-
-const statusCodePages: { [code: number]: ReactNode } = {
-  404: () => (
-    <>
-      <h1>{statusCodeTitles[404]}</h1>
-
-      <p>If you typed the web address, check it is correct.</p>
-      <p>If you pasted the web address, check you copied the entire address.</p>
-    </>
-  ),
-  500: () => (
-    <>
-      <h1>{statusCodeTitles[500]}</h1>
-
-      <p>Please try again later.</p>
-    </>
-  ),
-};
-
 class ErrorPage extends Component<Props> {
-  public static getInitialProps({ res, err }: NextContext) {
+  private static statusCodeTitles: { [code: number]: string } = {
+    404: 'Page not found',
+    500: 'Sorry, there is a problem with the service',
+  };
+
+  public static getInitialProps({ res, err }: NextContext): Props {
     const statusCode = res ? res.statusCode : 500;
     const errorMessage = err ? err.message : '';
 
-    if (errorMessage) {
-      return {
-        errorPage: (
-          <>
-            <h1>There was a problem</h1>
-
-            <p>{errorMessage}</p>
-          </>
-        ),
-        pageTitle: statusCodeTitles[statusCode] || statusCodeTitles[500],
-      };
-    }
-
     return {
-      errorPage: statusCodePages[statusCode],
-      pageTitle: statusCodeTitles[statusCode],
+      errorMessage,
+      statusCode,
     };
   }
 
+  private getStatusCodePage() {
+    switch (this.props.statusCode) {
+      case 404:
+        return (
+          <>
+            <h1>{ErrorPage.statusCodeTitles[404]}</h1>
+
+            <p>If you typed the web address, check it is correct.</p>
+            <p>
+              If you pasted the web address, check you copied the entire
+              address.
+            </p>
+          </>
+        );
+      default:
+        return (
+          <>
+            <h1>{ErrorPage.statusCodeTitles[500]}</h1>
+
+            <p>Please try again later.</p>
+          </>
+        );
+    }
+  }
+
   public render() {
-    const { errorPage } = this.props;
+    const { errorMessage, statusCode } = this.props;
+    const pageTitle = errorMessage
+      ? ErrorPage.statusCodeTitles[500]
+      : ErrorPage.statusCodeTitles[statusCode];
 
     return (
       <div className="govuk-grid-row">
         <Helmet>
-          <title>Page Not Found - GOV.UK</title>
+          <title>{pageTitle} - GOV.UK</title>
         </Helmet>
-        <div className="govuk-grid-column-two-thirds">
-          {errorPage ? (
-            errorPage
-          ) : (
-            <>
-              <h1>An error occurred</h1>
 
-              <p>Something went wrong</p>
+        <div className="govuk-grid-column-two-thirds">
+          {errorMessage ? (
+            <>
+              <h1>There was a problem</h1>
+
+              <p>{errorMessage}</p>
             </>
+          ) : (
+            this.getStatusCodePage()
           )}
         </div>
       </div>
