af5de6c347db99c77f0825816e447098af18d292
Nicholas Tsim <nick@hiveit.co.uk>
2019-03-08 11:40:42 +0000

    DFE-168 Move react-app-env type declarations

--- src/explore-education-statistics-frontend/src/types/react-app-env.d.ts
+++ src/explore-education-statistics-frontend/src/types/react-app-env.d.ts
@@ -52,11 +52,6 @@ declare module '*.svg' {
   export default src;
 }
 
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
 declare module '*.module.scss' {
   const classes: { [key: string]: string };
   export default classes;
