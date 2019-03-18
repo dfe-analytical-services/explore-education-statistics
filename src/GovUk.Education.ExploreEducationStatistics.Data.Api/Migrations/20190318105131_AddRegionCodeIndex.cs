535a94c213122325c166c9b8424b5afc810ce7e9
Ben Outram <ben@hiveit.co.uk>
2019-03-18 10:56:42 +0000

    DFE-141 Adding more missing indexes.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190318105131_AddRegionCodeIndex.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Migrations
+{
+    public partial class AddRegionCodeIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegionCode",
+                table: "GeographicData",
+                nullable: true,
+                computedColumnSql: "JSON_VALUE(Region, '$.region_code')",
+                oldClrType: typeof(string),
+                oldNullable: true,
+                oldComputedColumnSql: "JSON_VALUE(Region, '$.region_code')");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RegionCode",
+                table: "CharacteristicDataLa",
+                nullable: true,
+                computedColumnSql: "JSON_VALUE(Region, '$.region_code')",
+                oldClrType: typeof(string),
+                oldNullable: true,
+                oldComputedColumnSql: "JSON_VALUE(Region, '$.region_code')");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeographicData_RegionCode",
+                table: "GeographicData",
+                column: "RegionCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacteristicDataLa_RegionCode",
+                table: "CharacteristicDataLa",
+                column: "RegionCode");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_GeographicData_RegionCode",
+                table: "GeographicData");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CharacteristicDataLa_RegionCode",
+                table: "CharacteristicDataLa");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RegionCode",
+                table: "GeographicData",
+                nullable: true,
+                computedColumnSql: "JSON_VALUE(Region, '$.region_code')",
+                oldClrType: typeof(string),
+                oldNullable: true,
+                oldComputedColumnSql: "JSON_VALUE(Region, '$.region_code')");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RegionCode",
+                table: "CharacteristicDataLa",
+                nullable: true,
+                computedColumnSql: "JSON_VALUE(Region, '$.region_code')",
+                oldClrType: typeof(string),
+                oldNullable: true,
+                oldComputedColumnSql: "JSON_VALUE(Region, '$.region_code')");
+        }
+    }
+}
