867b22db8a6c1477a589f98c2913a8061b1e71e7
Fred Brenton <fredbrentonltd@gmail.com>
2019-03-19 23:47:25 +0000

    Added intial content to privacy policy page.

--- src/explore-education-statistics-frontend/src/pages/privacy-policy.tsx
+++ src/explore-education-statistics-frontend/src/pages/privacy-policy.tsx
@@ -6,7 +6,22 @@ class PrivacyPolicyPage extends Component {
   public render() {
     return (
       <Page breadcrumbs={[{ name: 'Privacy Policy' }]}>
-        <PageTitle title="Feedback" />
+        <PageTitle title="Explore education statistics privacy policy" />
+
+        <div>
+          <h3>Who we are</h3>
+          <p>
+            The Explore education statistics service is operated by the
+            Department for Education ('we' or 'us'). For the purpose of data
+            protection legislation, DfE is the data controller for the personal
+            data processed as part of the Explore education statistics service.
+          </p>
+          <p>
+            This privacy policy sets out how we collect and process your
+            personal data through the service. This policy applies to all users
+            of the service ('you').
+          </p>
+        </div>
       </Page>
     );
   }
