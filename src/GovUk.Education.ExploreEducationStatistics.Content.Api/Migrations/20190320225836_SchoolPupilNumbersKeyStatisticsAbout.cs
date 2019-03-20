b69d879726b4e443d3195be8c7dd9d23615b70b1
James Cutts <james@jamescutts.co.uk>
2019-03-20 22:59:15 +0000

    CONTENT-API: Added about this release to school and pupil numbers

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Migrations/20190320225836_SchoolPupilNumbersKeyStatisticsAbout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Migrations
+{
+    public partial class SchoolPupilNumbersKeyStatisticsAbout : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Releases",
+                keyColumn: "Id",
+                keyValue: new Guid("e3288537-9adb-431d-adfb-9bc3ef7be48c"),
+                column: "Content",
+                value: "[{\"Order\":1,\"Heading\":\"About this release\",\"Caption\":\"\",\"Content\":[{\"Type\":\"MarkDownBlock\",\"Body\":\"This statistical publication provides the number of schools and pupils in schools in England, using data from the January 2018School Census.\\n\\n Breakdowns are given for school types as well as for pupil characteristics including free school meal eligibility, English as an additional languageand ethnicity.This release also contains information about average class sizes.\\n\\n SEN tables previously provided in thispublication will be published in the statistical publication ‘Special educational needs in England: January 2018’ scheduled for release on 26July 2018.\\n\\n Cross border movement tables will be added to this publication later this year.\"}]}]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Releases",
+                keyColumn: "Id",
+                keyValue: new Guid("e3288537-9adb-431d-adfb-9bc3ef7be48c"),
+                column: "Content",
+                value: "[{\"Order\":1,\"Heading\":\"About this release\",\"Caption\":\"\",\"Content\":null}]");
+        }
+    }
+}
