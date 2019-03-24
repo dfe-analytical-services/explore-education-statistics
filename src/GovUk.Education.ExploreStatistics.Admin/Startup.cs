85b9cfdc82e4f293d6b47b2fdb4fc3e133a5b1a6
James Cutts <james@jamescutts.co.uk>
2019-03-24 19:00:46 +0000

    ADMIN-APP: Set HSTS Header to 365 days and subdomains

--- src/GovUk.Education.ExploreStatistics.Admin/Startup.cs
+++ src/GovUk.Education.ExploreStatistics.Admin/Startup.cs
@@ -66,8 +66,7 @@ namespace GovUk.Education.ExploreStatistics.Admin
             {
                 app.UseExceptionHandler("/Home/Error");
                 
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains());
             }
             
             app.UseHttpsRedirection();
