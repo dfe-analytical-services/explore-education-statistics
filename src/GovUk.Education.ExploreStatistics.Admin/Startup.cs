21bf187534e2e3df92a4d56fcd3eb9fcfcc7f130
James Cutts <james@jamescutts.co.uk>
2019-03-24 18:48:49 +0000

    DFE-294: Add X-Content-Type-Options Header to Admin Application

--- src/GovUk.Education.ExploreStatistics.Admin/Startup.cs
+++ src/GovUk.Education.ExploreStatistics.Admin/Startup.cs
@@ -65,11 +65,15 @@ namespace GovUk.Education.ExploreStatistics.Admin
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             app.UseHttpsRedirection();
+            app.UseXContentTypeOptions();
+            
+            
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
