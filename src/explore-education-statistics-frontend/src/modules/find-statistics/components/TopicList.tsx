dc865488cc023697a4a9234ee89cc11744f96846
jamie chatterton <negatron99@gmail.com>
2019-03-25 16:49:09 +0000

    DFE-199 - Making the find statistics page use the hierarchy instead of directly calling each API in turn

--- src/explore-education-statistics-frontend/src/modules/find-statistics/components/TopicList.tsx
+++ src/explore-education-statistics-frontend/src/modules/find-statistics/components/TopicList.tsx
@@ -2,46 +2,33 @@ import React, { Component } from 'react';
 import Accordion from 'src/components/Accordion';
 import AccordionSection from 'src/components/AccordionSection';
 import { contentApi } from 'src/services/api';
-import PublicationList from '../components/PublicationList';
+import PublicationList, { Publication } from '../components/PublicationList';
+
+export interface Topic {
+  id: string;
+  slug: string;
+  summary: string;
+  title: string;
+  publications: Publication[];
+}
 
 interface Props {
   theme: string;
+  topics: Topic[];
 }
 
-interface State {
-  topics: {
-    id: string;
-    slug: string;
-    summary: string;
-    title: string;
-  }[];
-}
-
-class TopicList extends Component<Props, State> {
-  public state = {
-    topics: [],
-  };
-
-  public componentDidMount() {
-    const { theme } = this.props;
-
-    contentApi
-      .get(`theme/${theme}/topics`)
-      .then(topics => this.setState({ topics }));
-  }
-
+class TopicList extends Component<Props> {
   public render() {
-    const { theme } = this.props;
-    const { topics } = this.state;
+    const { theme, topics } = this.props;
 
     return (
       <>
         {topics.length > 0 ? (
           <Accordion id={theme}>
-            {topics.map(({ id, slug, title, summary }) => (
+            {topics.map(({ id, slug, title, summary, publications }) => (
               <AccordionSection heading={title} caption={summary} key={id}>
                 <ul className="govuk-!-margin-top-0 govuk-!-padding-top-0">
-                  <PublicationList topic={slug} />
+                  <PublicationList publications={publications} topic={slug} />
                 </ul>
               </AccordionSection>
             ))}
