8bb8079bf0d31652a8edc5468cd5612db6414cbc
jamie chatterton <negatron99@gmail.com>
2019-03-28 16:53:36 +0000

    Trying to add links back in

--- src/explore-education-statistics-admin/src/components/Link.tsx
+++ src/explore-education-statistics-admin/src/components/Link.tsx
@@ -1,43 +1,46 @@
 import classNames from 'classnames';
-import React, { AnchorHTMLAttributes, ReactNode } from 'react';
+import React, {AnchorHTMLAttributes, ReactNode} from 'react';
+import {Link as ReactLink} from 'react-router-dom';
 
 type Props = {
-  as?: string ;
-  children: ReactNode;
-  className?: string;
-  prefetch?: boolean;
-  to?: string;
-  unvisited?: boolean;
+    as?: string;
+    children: ReactNode;
+    className?: string;
+    prefetch?: boolean;
+    to?: string;
+    unvisited?: boolean;
 } & AnchorHTMLAttributes<HTMLAnchorElement>;
 
 const Link = ({
-  as,
-  children,
-  className,
-  prefetch,
-  to,
-  unvisited = false,
-  ...props
-}: Props) => {
-  // We support href and to for backwards
-  // compatibility with react-router.
-  const href = props.href || to;
+                  as,
+                  children,
+                  className,
+                  prefetch,
+                  to,
+                  unvisited = false,
+                  ...props
+              }: Props) => {
+    // We support href and to for backwards
+    // compatibility with react-router.
+    const href = props.href || to;
 
-  return (
-      <a
-        {...props}
-          href={ href }
-        className={classNames(
-          'govuk-link',
-          {
-            'govuk-link--no-visited-state': unvisited,
-          },
-          className,
-        )}
-      >
-        {children}
-      </a>
-  );
+    return (
+        <ReactLink to={to || ""}>
+            <a
+                {...props}
+                href={href}
+                className={classNames(
+                    'govuk-link',
+                    {
+                        'govuk-link--no-visited-state': unvisited,
+                    },
+                    className,
+                )}
+            >
+                {children}
+            </a>
+        </ReactLink>
+    );
 };
 
 export default Link;
