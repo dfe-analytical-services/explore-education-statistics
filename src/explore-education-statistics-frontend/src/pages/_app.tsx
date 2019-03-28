26a0ea801fa4e2b0f5179d4d0725d4e800dbb996
James Cutts <james@jamescutts.co.uk>
2019-03-28 18:38:16 +0000

    DFE-178: GA Tracking is based on enviroment variable being set

--- src/explore-education-statistics-frontend/src/pages/_app.tsx
+++ src/explore-education-statistics-frontend/src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { Container, default as BaseApp } from 'next/app';
-import Router from 'next/router'
+import Router from 'next/router';
 import React from 'react';
 import Helmet from 'react-helmet';
-import { initGA, logPageView } from '../utils/analytics'
+import { initGA, logPageView } from '../utils/analytics';
 import './_app.scss';
 
 class App extends BaseApp {
@@ -17,12 +17,13 @@ class App extends BaseApp {
   }
 
   public componentDidMount(): void {
-    initGA();
-    logPageView();
+    if (process.env.GA_TRACKING_ID !== undefined) {
+      initGA();
+      logPageView();
 
-    if(Router.router !== null)
-    {
-      Router.router.events.on('routeChangeComplete', logPageView);
+      if (Router.router !== null) {
+        Router.router.events.on('routeChangeComplete', logPageView);
+      }
     }
 
     document.body.classList.add('js-enabled');
--- src/explore-education-statistics-frontend/src/utils/analytics.tsx
+++ src/explore-education-statistics-frontend/src/utils/analytics.tsx
@@ -1,26 +1,30 @@
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga';
 
 export const initGA = () => {
+  const trackingId = process.env.GA_TRACKING_ID;
+
+  if (trackingId !== undefined) {
     // tslint:disable-next-line:no-console
-    console.log('GA init')
-    ReactGA.initialize('UA-xxxxxxxxx-1')
+    console.log('GA init');
+    ReactGA.initialize(trackingId);
   }
-  
-  export const logPageView = () => {
-    // tslint:disable-next-line:no-console
-    console.log(`Logging pageview for ${window.location.pathname}`)
-    ReactGA.set({ page: window.location.pathname })
-    ReactGA.pageview(window.location.pathname)
+};
+
+export const logPageView = () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Logging pageview for ${window.location.pathname}`);
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
+
+export const logEvent = (category = '', action = '') => {
+  if (category && action) {
+    ReactGA.event({ category, action });
   }
-  
-  export const logEvent = (category = '', action = '') => {
-    if (category && action) {
-      ReactGA.event({ category, action })
-    }
+};
+
+export const logException = (description = '', fatal = false) => {
+  if (description) {
+    ReactGA.exception({ description, fatal });
   }
-  
-  export const logException = (description = '', fatal = false) => {
-    if (description) {
-      ReactGA.exception({ description, fatal })
-    }
-  }
+};
