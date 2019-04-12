ac292b66f72670e950e61e84feb0c17e5298568e
Si Shakes <si@hiveit.co.uk>
2019-04-12 09:11:04 +0100

    DFE-391 Correct queue functions & move table methods into service.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Notifier/Services/IStorageTableService.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace GovUk.Education.ExploreEducationStatistics.Notifier.Services
+{
+    public interface IStorageTableService
+    {
+        Task UpdateSubscriber(CloudTable table, SubscriptionEntity subscription);
+
+        Task RemoveSubscriber(CloudTable table, SubscriptionEntity subscription);
+
+        Task<SubscriptionEntity> RetrieveSubscriber(CloudTable table,
+            SubscriptionEntity subscription);
+
+        Task<CloudTable> GetTable(IConfiguration config, string connectionStr,
+            string storageTableName);
+    }
+}
