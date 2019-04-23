d7b71f89e8e8f774354da626c7de173bc00bab9d
jamie chatterton <negatron99@gmail.com>
2019-04-23 14:12:00 +0100

    DFE-471 DFE-538 - Adding ability to jump to results inside a tab container

--- src/explore-education-statistics-frontend/src/prototypes/components/PrototypeSearchForm.tsx
+++ src/explore-education-statistics-frontend/src/prototypes/components/PrototypeSearchForm.tsx
@@ -86,46 +86,42 @@ class PrototypeSearchForm extends Component<{}, State> {
     const searchResults: SearchResult[] = elements.map(element => {
       let scrollIntoView: () => void;
 
-      const collapsedContainer = PrototypeSearchForm.parentUntilClassname(
+      const accordionContainer = PrototypeSearchForm.parentUntilClassname(
         element,
         'govuk-accordion__section',
+      );
+
+      const detailsContainer = PrototypeSearchForm.parentUntilClassname(
+        element,
         'govuk-details',
+      );
+
+      const tabContainer = PrototypeSearchForm.parentUntilClassname(
+        element,
+        'govuk-tabs',
       );
 
       const location = PrototypeSearchForm.calculateLocationOfElement(
         element,
-        collapsedContainer,
+        accordionContainer,
       );
 
-      if (collapsedContainer.classList.contains('govuk-accordion__section')) {
-        scrollIntoView = () => {
-          PrototypeSearchForm.openAccordion(collapsedContainer);
-          this.resetSearch();
-          element.scrollIntoView();
-        };
-      } else if (collapsedContainer.classList.contains('govuk-details')) {
-        scrollIntoView = () => {
-          collapsedContainer.setAttribute('open', '');
-          const textEl = collapsedContainer.querySelector(
-            '.govuk-details__text',
-          );
-          if (textEl) {
-            textEl.setAttribute('aria-hidden', 'false');
-            textEl.setAttribute('style', '');
-          }
-          const summary = collapsedContainer.querySelector('summary');
-          if (summary) {
-            summary.setAttribute('aria-expanded', 'true');
-          }
-          this.resetSearch();
-          element.scrollIntoView();
-        };
-      } else {
-        scrollIntoView = () => {
-          this.resetSearch();
-          element.scrollIntoView();
-        };
-      }
+      scrollIntoView = () => {
+        if (accordionContainer.classList.contains('govuk-accordion__section')) {
+          PrototypeSearchForm.openAccordion(accordionContainer);
+        }
+
+        if (detailsContainer.classList.contains('govuk-details')) {
+          PrototypeSearchForm.openDetails(accordionContainer);
+        }
+
+        if (tabContainer.classList.contains('govuk-tabs')) {
+          PrototypeSearchForm.openTab(element, tabContainer);
+        }
+
+        this.resetSearch();
+        element.scrollIntoView();
+      };
 
       return {
         element,
@@ -136,6 +132,80 @@ class PrototypeSearchForm extends Component<{}, State> {
     });
 
     this.setState({ searchResults });
+  }
+
+  private static openTab(target: Element, container: Element) {
+    // find the panel in which the element lives
+    const panel = PrototypeSearchForm.parentUntilClassname(
+      target,
+      'govuk-tabs__panel',
+    );
+
+    // find all the sections within the container
+    const allSections = container.querySelectorAll('.govuk-tabs__panel');
+
+    // get the index of the panel within the container
+    const selectedPanel = Array.from(allSections).indexOf(panel);
+
+    // only proceed if all this is valid
+    if (selectedPanel >= 0) {
+      // find the selected anchor
+      const anchor = container
+        .querySelectorAll('ul.govuk-tabs__list > li.govuk-tabs__list-item')
+        .item(selectedPanel)
+        .querySelector('a');
+
+      if (anchor) {
+        // Deselect all the selected tabs, make sure we found the anchor first
+        container
+          .querySelectorAll(
+            'ul.govuk-tabs__list > li.govuk-tabs__list-item a.govuk-tabs__tab--selected',
+          )
+          .forEach(_ => {
+            _.classList.remove('govuk-tabs__tab--selected');
+            _.setAttribute('aria-selected', 'false');
+            _.setAttribute('tabIndex', '-1');
+          });
+
+        // make the selected tab selected
+        anchor.classList.add('govuk-tabs__tab--selected');
+        anchor.setAttribute('aria-selected', 'true');
+        anchor.removeAttribute('tabIndex');
+
+        // hide all the panels
+        Array.from(
+          container.querySelectorAll(
+            '.govuk-tabs__panel:not(.govuk-tabs__panel--hidden)',
+          ),
+        ).forEach(_ => _.classList.add('govuk-tabs__panel--hidden'));
+
+        // show the selected panel
+        panel.classList.remove('govuk-tabs__panel--hidden');
+
+        // click the anchor to get its component to update any internal state
+        anchor.dispatchEvent(
+          new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+            buttons: 1,
+          }),
+        );
+      }
+    }
+  }
+
+  private static openDetails(collapsedContainer: Element) {
+    collapsedContainer.setAttribute('open', '');
+    const textEl = collapsedContainer.querySelector('.govuk-details__text');
+    if (textEl) {
+      textEl.setAttribute('aria-hidden', 'false');
+      textEl.setAttribute('style', '');
+    }
+    const summary = collapsedContainer.querySelector('summary');
+    if (summary) {
+      summary.setAttribute('aria-expanded', 'true');
+    }
   }
 
   private static openAccordion(potentialAccordion: HTMLElement) {
