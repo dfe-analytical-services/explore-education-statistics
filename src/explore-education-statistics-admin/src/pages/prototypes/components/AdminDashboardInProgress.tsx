81505b11dc1ad669657dafe82e0a92766ad5d08a
Martin Waite <martin@hiveit.co.uk>
2019-04-29 14:20:38 +0100

    DFE-524 PROT Dashboard updates

--- src/explore-education-statistics-admin/src/pages/prototypes/components/AdminDashboardInProgress.tsx
+++ src/explore-education-statistics-admin/src/pages/prototypes/components/AdminDashboardInProgress.tsx
@@ -1,5 +1,5 @@
+import DashboardRelease from '@admin/pages/prototypes/components/DashboardRelease';
 import React from 'react';
-import Link from '@admin/components/Link';
 
 const AdminDashboardInProgress = () => {
   return (
@@ -9,40 +9,28 @@ const AdminDashboardInProgress = () => {
           <h2 className="govuk-heading-m">New releases in progress</h2>
           <ul className="govuk-list-bullet  govuk-!-margin-bottom-9">
             <li>
-              {' '}
-              <h4 className="govuk-heading-s govuk-!-margin-bottom-0">
-                Pupil absence statistics
-              </h4>
-              <dl className="dfe-meta-content govuk-!-margin-0">
-                <dt className="govuk-caption-m">Last edited:</dt>
-                <dd>
-                  20 March 2019 at 17:37 by <a href="#">me</a>
-                </dd>
-              </dl>
-              <div className="govuk-!-margin-top-0">
-                <Link to="/prototypes/publication-create-new-absence-config">
-                  Edit release
-                </Link>
-              </div>
+              <DashboardRelease
+                title="Pupil absense statistics and data for schools in England"
+                years="2017 to 2018"
+                editing={window.location.search === '?status=editNewRelease'}
+                isNew
+                lastEdited={new Date('2019-03-20 17:37')}
+                lastEditor={{ id: 'me', name: 'me', permissions: [] }}
+              />
             </li>
           </ul>
         </>
       )}
       <h2 className="govuk-heading-m">Editing current releases</h2>
       <ul className="govuk-list-bullet">
-        <li className="govuk-!-margin-top-6">
-          <h4 className="govuk-heading-s govuk-!-margin-bottom-0">
-            Permanent and fixed-period exclusions statistics
-          </h4>
-          <dl className="dfe-meta-content govuk-!-margin-0">
-            <dt className="govuk-caption-m">Last edited:</dt>
-            <dd>
-              24 April 2019 at 10:37 <a href="#">me</a>
-            </dd>
-          </dl>
-          <div className="govuk-!-margin-top-0">
-            <Link to="#">Edit</Link>
-          </div>
+        <li>
+          <DashboardRelease
+            title="Permanent and fixed-period exclusions statistics"
+            years="2017 to 2018"
+            editing
+            lastEdited={new Date('2019-03-20 17:37')}
+            lastEditor={{ id: 'me', name: 'me', permissions: [] }}
+          />
         </li>
       </ul>
     </>
