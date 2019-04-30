dcb39d440ca9de1d5a5c1278c5fb9ba763fa2ae8
Fred Brenton <fredbrentonltd@gmail.com>
2019-04-30 12:27:59 +0100

    Updated contact content for methodo

--- src/explore-education-statistics-frontend/src/prototypes/methodology/absence/PrototypeSection7.tsx
+++ src/explore-education-statistics-frontend/src/prototypes/methodology/absence/PrototypeSection7.tsx
@@ -3,14 +3,30 @@ import React from 'react';
 const PrototypeMethodologySection = () => {
   return (
     <>
-      <h4>Email</h4>
-      <p>
-        <a href="#">schools.statistics@education.gov.uk</a>
+      <h3>School absence and exclusions team</h3>
+      <p className="govuk-body-l">
+        If you have a specific enquiry about absence and exclusion statistics
+        and data:
       </p>
+      <ul className="govuk-body-l">
+        <li>
+          Email: <a href="#">schools.statistics@education.gov.uk</a>
+        </li>
+        <li>Telephone Mark Pearson: 0114 274 2585</li>
+      </ul>
       <h4>Public enquiries</h4>
-      <p>037 0000 2288 </p>
+      <p className="govuk-body-m">
+        If you have a general enquiry about education contact:
+      </p>
+      <ul className="govuk-body-m">
+        <li>
+          Email: <a href="#">schools.statistics@education.gov.uk</a>
+        </li>
+        <li>Telephone: 037 0000 2288</li>
+      </ul>
       <h4>Press office</h4>
-      <p>020 7925 6789 </p>
+      <p className="govuk-body-m">If you have a media enquiry:</p>
+      <p>Telephone: 020 7925 6789 </p>
     </>
   );
 };
