294909191b22589313a3fc2d15306d70a366d3c7
Nicholas Tsim <nicholastsim@gmail.com>
2019-05-02 10:30:42 +0100

    tests: Add flatMap polyfill to setupTests in each sub-project

--- src/explore-education-statistics-common/test/setupTests.js
+++ src/explore-education-statistics-common/test/setupTests.js
@@ -1,5 +1,6 @@
 import 'react-testing-library/cleanup-after-each';
 import 'jest-dom/extend-expect';
+import 'core-js/fn/array/virtual/flat-map';
 
 if (typeof window !== 'undefined') {
   // fetch polyfill for making API calls.
