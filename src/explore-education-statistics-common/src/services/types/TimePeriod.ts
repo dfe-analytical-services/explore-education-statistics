4de3db25af84596c324c47c5e7d938d702317115
Nicholas Tsim <nick@hiveit.co.uk>
2019-05-09 12:07:06 +0100

    DFE-628 Add HT5 time period code

--- src/explore-education-statistics-common/src/services/types/TimePeriod.ts
+++ src/explore-education-statistics-common/src/services/types/TimePeriod.ts
@@ -1,8 +1,8 @@
 import { Comparison } from '@common/types/util';
 
-type TimePeriodCode = 'AY' | 'HT6';
+type TimePeriodCode = 'AY' | 'HT6' | 'HT5';
 
-const allowedTimePeriodCodes: TimePeriodCode[] = ['AY', 'HT6'];
+const allowedTimePeriodCodes: TimePeriodCode[] = ['AY', 'HT6', 'HT5'];
 
 class TimePeriod {
   public readonly year: number;
@@ -47,7 +47,8 @@ class TimePeriod {
   public get label(): string {
     switch (this.code) {
       case 'AY':
-      case 'HT6': {
+      case 'HT6':
+      case 'HT5': {
         const yearString = this.year.toString();
         return `${yearString}/${Number(yearString.substring(2, 4)) + 1}`;
       }
@@ -64,6 +65,7 @@ class TimePeriod {
     switch (this.code) {
       case 'AY':
       case 'HT6':
+      case 'HT5':
         return new TimePeriod(this.year - 1, this.code);
       default:
         throw new Error('Could not parse previous time period');
@@ -74,6 +76,7 @@ class TimePeriod {
     switch (this.code) {
       case 'AY':
       case 'HT6':
+      case 'HT5':
         return new TimePeriod(this.year + 1, this.code);
       default:
         throw new Error('Could not parse next time period');
