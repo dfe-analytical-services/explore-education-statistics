36373d3bf3b693a7e877cbe9581ccff7fcd7e1ea
Mark Youngman <mark@hiveit.co.uk>
2019-05-09 11:13:26 +0100

    Remove unnecessary files

--- src/GovUK.Education.ExploreEducationStatistics.Data.Processor/services/ISeedService.cs
+++
@@ -1,9 +0,0 @@
-namespace GovUk.Education.ExploreEducationStatistics.Data.Processor.Services
-{
-    using GovUk.Education.ExploreEducationStatistics.Data.Processor.Models;
-
-    public interface ISeedService
-    {
-        void SeedRelease(Release release);
-    }
-}
