8f402d16bc725fda15be9087a63afe6ecb6a7f63
Simon Shakespeare <si.shakes@gmail.com>
2019-05-13 17:34:18 +0100

    DFE-574 Regenerate migrations.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190513162753_AddLocationLevels.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Migrations
+{
+    public partial class AddLocationLevels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
