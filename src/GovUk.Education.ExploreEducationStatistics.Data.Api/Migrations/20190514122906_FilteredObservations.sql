319df7ce5ad48b978b10700ab6f2573b8ced3f01
Simon Shakespeare <si.shakes@gmail.com>
2019-05-14 17:40:20 +0100

    DFE-574 Correct stored proc.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190514122906_FilteredObservations.sql
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190514122906_FilteredObservations.sql
@@ -21,14 +21,14 @@ DECLARE
     @countriesCount int = (SELECT count(id) FROM @countriesList),
     @regionsCount int = (SELECT count(id) FROM @regionsList),
     @localAuthoritiesCount int = (SELECT count(id) FROM @localAuthorityList),
-    @localAuthorityDistrictCount int = (SELECT count(id) FROM @localAuthorityDistrictList)
-    @localEnterprisePartnershipCount int = (SELECT count(id) FROM @localEnterprisePartnershipsList)
-    @institutionCount int = (SELECT count(id) FROM @institutionsList)
-    @matCount int = (SELECT count(id) FROM @matsList)
-    @mayoralCombinedAuthorityCount int = (SELECT count(id) FROM @mayoralCombinedAuthoritiesList)
-    @opportunityAreaCount int = (SELECT count(id) FROM @opportunityAreasList)
-    @parliamentaryConstituencyCount int = (SELECT count(id) FROM @parliamentaryConstituenciesList)
-    @providerCount int = (SELECT count(id) FROM @providersList)
+    @localAuthorityDistrictCount int = (SELECT count(id) FROM @localAuthorityDistrictList),
+    @localEnterprisePartnershipCount int = (SELECT count(id) FROM @localEnterprisePartnershipsList),
+    @institutionCount int = (SELECT count(id) FROM @institutionsList),
+    @matCount int = (SELECT count(id) FROM @matsList),
+    @mayoralCombinedAuthorityCount int = (SELECT count(id) FROM @mayoralCombinedAuthoritiesList),
+    @opportunityAreaCount int = (SELECT count(id) FROM @opportunityAreasList),
+    @parliamentaryConstituencyCount int = (SELECT count(id) FROM @parliamentaryConstituenciesList),
+    @providerCount int = (SELECT count(id) FROM @providersList),
     @wardCount int = (SELECT count(id) FROM @wardsList)
 SELECT o.id
 FROM Observation o
@@ -42,14 +42,14 @@ AND (@countriesCount = 0 OR l.Country_Code IN (SELECT id from @countriesList))
 AND (@regionsCount = 0 OR l.Region_Code IN (SELECT id from @regionsList))
 AND (@localAuthoritiesCount = 0 OR l.LocalAuthority_Code IN (SELECT id from @localAuthorityList))
 AND (@localAuthorityDistrictCount = 0 OR l.LocalAuthorityDistrict_Code IN (SELECT id from @localAuthorityDistrictList))
-AND (@localEnterprisePartnershipCount = 0 OR l.LocalAuthorityDistrict_Code IN (SELECT id from @localEnterprisePartnershipsList))
-AND (@institutionCount = 0 OR l.LocalAuthorityDistrict_Code IN (SELECT id from @institutionsList))
-AND (@matCount = 0 OR l.LocalAuthorityDistrict_Code IN (SELECT id from @matsList))
-AND (@mayoralCombinedAuthorityCount = 0 OR l.LocalAuthorityDistrict_Code IN (SELECT id from @mayoralCombinedAuthoritiesList))
-AND (@opportunityAreaCount = 0 OR l.LocalAuthorityDistrict_Code IN (SELECT id from @opportunityAreasList))
-AND (@parliamentaryConstituencyCount = 0 OR l.LocalAuthorityDistrict_Code IN (SELECT id from @parliamentaryConstituenciesList))
-AND (@providerCount = 0 OR l.LocalAuthorityDistrict_Code IN (SELECT id from @providersList))
-AND (@wardCount = 0 OR l.LocalAuthorityDistrict_Code IN (SELECT id from @wardsList))
+AND (@localEnterprisePartnershipCount = 0 OR l.LocalEnterprisePartnership_Code IN (SELECT id from @localEnterprisePartnershipsList))
+AND (@institutionCount = 0 OR l.Institution_Code IN (SELECT id from @institutionsList))
+AND (@matCount = 0 OR l.Mat_Code IN (SELECT id from @matsList))
+AND (@mayoralCombinedAuthorityCount = 0 OR l.MayoralCombinedAuthority_Code IN (SELECT id from @mayoralCombinedAuthoritiesList))
+AND (@opportunityAreaCount = 0 OR l.OpportunityArea_Code IN (SELECT id from @opportunityAreasList))
+AND (@parliamentaryConstituencyCount = 0 OR l.ParliamentaryConstituency_Code IN (SELECT id from @parliamentaryConstituenciesList))
+AND (@providerCount = 0 OR l.Provider_Code IN (SELECT id from @providersList))
+AND (@wardCount = 0 OR l.Ward_Code IN (SELECT id from @wardsList))
 GROUP BY o.Id
 HAVING COUNT(DISTINCT ofi.FilterItemId) = (SELECT COUNT(DISTINCT f.id) AS filterCount
 FROM
