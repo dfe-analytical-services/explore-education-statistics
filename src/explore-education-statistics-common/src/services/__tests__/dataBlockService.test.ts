12ef4eee9e51b6d67fdd74ac5204239092188d2c
negatron99 <negatron99@gmail.com>
2019-05-16 09:49:42 +0100

    DFE-351 - adding some more tests for the datablock service

--- src/explore-education-statistics-common/src/services/__tests__/dataBlockService.test.ts
+++ src/explore-education-statistics-common/src/services/__tests__/dataBlockService.test.ts
@@ -5,7 +5,6 @@ import dataBlockService, {
 import testData from '@common/modules/find-statistics/components/charts/__tests__/__data__/testBlockData';
 
 import { dataApi } from '@common/services/api';
-import { AxiosRequestConfig } from 'axios';
 
 jest.mock('@common/services/api', () => {
   return {
@@ -17,6 +16,18 @@ jest.mock('@common/services/api', () => {
 });
 
 describe('dataBlockService', () => {
+  beforeEach(() => {
+    // @ts-ignore
+    dataApi.get.mockImplementation(() => {
+      return Promise.resolve(testData.testBlockMetaData);
+    });
+
+    // @ts-ignore
+    dataApi.post.mockImplementation(() => {
+      return Promise.resolve(testData.testBlockData);
+    });
+  });
+
   const dataBlockRequest: DataBlockRequest = {
     subjectId: 1,
     geographicLevel: GeographicLevel.National,
@@ -27,16 +38,6 @@ describe('dataBlockService', () => {
   };
 
   test('Call datablock service', async () => {
-    // @ts-ignore
-    dataApi.get.mockImplementation(() => {
-      return Promise.resolve(testData.testBlockMetaData);
-    });
-
-    // @ts-ignore
-    dataApi.post.mockImplementation(() => {
-      return Promise.resolve(testData.testBlockData);
-    });
-
     const result = await dataBlockService.getDataBlockForSubject(
       dataBlockRequest,
     );
@@ -45,5 +46,18 @@ describe('dataBlockService', () => {
     expect(dataApi.post).toBeCalledWith('/tablebuilder', dataBlockRequest);
 
     expect(result).toMatchSnapshot(result);
+  });
+
+  test('DataBlock service returns geojson data', async () => {
+    const result = await dataBlockService.getDataBlockForSubject(
+      dataBlockRequest,
+    );
+
+    expect(dataApi.get).toBeCalledWith('/meta/subject/1');
+    expect(dataApi.post).toBeCalledWith('/tablebuilder', dataBlockRequest);
+
+    expect(result.metaData.locations).not.toBeUndefined();
+
+    expect(result.metaData.locations.E92000001).not.toBeUndefined();
   });
 });
