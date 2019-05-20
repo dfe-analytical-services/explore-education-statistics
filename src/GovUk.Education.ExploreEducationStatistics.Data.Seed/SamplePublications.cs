4da264004359e0521674f712e1b4cc3116a81db3
Ben Outram <ben@hiveit.co.uk>
2019-05-20 11:42:11 +0100

    DFE-656 Preparing to see exlcusions data.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Seed/SamplePublications.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Seed/SamplePublications.cs
@@ -12,7 +12,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Seed
                 "absence", new Publication
                 {
                     PublicationId = new Guid("cbbd299f-8297-44bc-92ac-558bcf51f8ad"),
-                    Name = "Pupil absence statistics and data for schools in England",
+                    Name = "Pupil absence data and statistics for schools in England",
                     Releases = new[]
                     {
                         new Release
@@ -56,6 +56,55 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Seed
                                 {
                                     Filename = DataCsvFilename.absence_rate_percent_bands,
                                     Name = "Absence rate percent bands"
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                "exclusions", new Publication
+                {
+                    PublicationId = new Guid("8345e27a-7a32-4b20-a056-309163bdf9c4"),
+                    Name = "Permanent and fixed period exclusions",
+                    Releases = new[]
+                    {
+                        new Release
+                        {
+                            PublicationId = new Guid("22ca9a1e-4b6c-496f-b025-edd2dcf49e7a"),
+                            ReleaseDate = new DateTime(2018, 7, 19),
+                            Name = "2016/17",
+                            Subjects = new[]
+                            {
+                                new Subject
+                                {
+                                    Filename = DataCsvFilename.exclusions_by_characteristic,
+                                    Name = "Exclusions by characteristic"
+                                },
+                                new Subject
+                                {
+                                    Filename = DataCsvFilename.exclusions_by_geographic_level,
+                                    Name = "Exclusions by geographic level"
+                                },
+                                new Subject
+                                {
+                                    Filename = DataCsvFilename.exclusions_by_reason,
+                                    Name = "Exclusions by reason"
+                                },
+                                new Subject
+                                {
+                                    Filename = DataCsvFilename.exclusions_duration_of_fixed_exclusions,
+                                    Name = "Duration of fixed exclusions"
+                                },
+                                new Subject
+                                {
+                                    Filename = DataCsvFilename.exclusions_number_of_fixed_exclusions,
+                                    Name = "Number of fixed exclusions"
+                                },
+                                new Subject
+                                {
+                                    Filename = DataCsvFilename.exclusions_total_days_missed_fixed_exclusions,
+                                    Name = "Total days missed due to fixed period exclusions"
                                 }
                             }
                         }
