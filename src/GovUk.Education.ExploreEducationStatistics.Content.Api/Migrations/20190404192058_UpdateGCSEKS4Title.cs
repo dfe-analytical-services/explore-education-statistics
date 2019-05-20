8adc0e8fecc72021175cd0ab8c426cf5738624a7
James Cutts <james@jamescutts.co.uk>
2019-05-20 20:30:50 +0100

    DFE-666: Exclude content api migrations from code coverage

--- src/GovUk.Education.ExploreEducationStatistics.Content.Api/Migrations/20190404192058_UpdateGCSEKS4Title.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Migrations/20190404192058_UpdateGCSEKS4Title.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Migrations
 {
+    [ExcludeFromCodeCoverage]
     public partial class UpdateGCSEKS4Title : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
