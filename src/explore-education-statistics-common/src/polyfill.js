46eb41af8e0478d6d746bdcbf1d813b64f697738
Nicholas Tsim <nick@hiveit.co.uk>
2019-05-28 17:44:07 +0100

    DFE-595 Remove MediaQueryList.add/removeEventListener polyfill

--- src/explore-education-statistics-common/src/polyfill.js
+++ src/explore-education-statistics-common/src/polyfill.js
@@ -36,17 +36,6 @@ if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
 
-// Alias addListener/removeListener (as these are deprecated)
-if (
-  typeof MediaQueryList.prototype.addEventListener === 'undefined' ||
-  typeof MediaQueryList.prototype.removeEventListener === 'undefined'
-) {
-  MediaQueryList.prototype.addEventListener =
-    MediaQueryList.prototype.addListener;
-  MediaQueryList.prototype.removeEventListener =
-    MediaQueryList.prototype.removeListener;
-}
-
 // For IE11
 
 if (!Element.prototype.matches) {
