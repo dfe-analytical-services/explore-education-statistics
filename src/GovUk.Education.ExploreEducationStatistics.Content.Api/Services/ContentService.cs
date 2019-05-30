018f23a754be8d9a2fba403d5729d7d3c5b4d335
James Cutts <james@jamescutts.co.uk>
2019-05-30 20:23:10 +0100

    DFE-717: Content Api content tree includes legacy url if applicable

--- src/GovUk.Education.ExploreEducationStatistics.Content.Api/Services/ContentService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Services/ContentService.cs
@@ -9,7 +9,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Services
     public class ContentService : IContentService
     {
         private readonly ApplicationDbContext _context;
-        
+
         public ContentService(
             ApplicationDbContext context)
         {
@@ -26,7 +26,13 @@ namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Services
                     Id = x.Id, Title = x.Title, Summary = x.Summary,
                     Publications = x.Publications
                         .Select(p => new PublicationTree
-                            {Id = p.Id, Title = p.Title, Summary = p.Summary, Slug = p.Slug}).OrderBy(publication => publication.Title).ToList()
+                        {
+                            Id = p.Id,
+                            Title = p.Title,
+                            Summary = p.Summary,
+                            Slug = p.Slug,
+                            LegacyPublicationUrl = p.LegacyPublicationUrl != null ? p.LegacyPublicationUrl.ToString() : null
+                        }).OrderBy(publication => publication.Title).ToList()
                 }).OrderBy(topic => topic.Title).ToList()
             }).OrderBy(theme => theme.Title).ToList();
 
