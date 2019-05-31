6f1537081f803e6d1f5e8a0bdec567b7bd65eda7
Nicholas Tsim <nick@hiveit.co.uk>
2019-05-31 11:31:39 +0100

    common: Remove data-testid from Details summary

--- src/explore-education-statistics-common/src/components/Details.tsx
+++ src/explore-education-statistics-common/src/components/Details.tsx
@@ -45,7 +45,6 @@ const Details = ({
       open={open}
       ref={ref}
       role={onMounted('group')}
-      data-testid={summary}
     >
       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
       <summary
