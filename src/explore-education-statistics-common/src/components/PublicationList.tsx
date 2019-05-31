c13fdc3946d4084f71d72b6042335b1e9da8b4e2
James Cutts <james@hiveit.co.uk>
2019-05-31 10:27:06 +0100

    DFE-717: Adds legacy release link

--- src/explore-education-statistics-common/src/components/PublicationList.tsx
+++ src/explore-education-statistics-common/src/components/PublicationList.tsx
@@ -1,9 +1,9 @@
-import Details from '@common/components/Details';
 import Link from '@frontend/components/Link';
 import React from 'react';
 
 export interface Publication {
   id: string;
+  legacyPublicationUrl: string | null;
   slug: string;
   summary: string;
   title: string;
@@ -17,30 +17,37 @@ function PublicationList({ publications }: Props) {
   return (
     <>
       {publications.length > 0 ? (
-        publications.map(({ id, slug, summary, title }) => (
+        publications.map(({ id, legacyPublicationUrl, slug, title }) => (
           <li key={id}>
             <strong>{title}</strong>
-            <div className="govuk-!-margin-top-0 govuk-!-margin-bottom-4">
-              <div className="govuk-grid-column-one-third">
-                <Link
-                  className="govuk-link govuk-!-margin-right-9"
-                  to={`/statistics/publication?publication=${slug}`}
-                  as={`/statistics/${slug}`}
-                  data-testid={`view-stats-${slug}`}
-                >
-                  View statistics and data
-                </Link>
+            {legacyPublicationUrl ? (
+              <span>
+                - currently available via{' '}
+                <a href={legacyPublicationUrl}>Statistics at DfE</a>
+              </span>
+            ) : (
+              <div className="govuk-grid-row govuk-!-margin-top-0 govuk-!-margin-bottom-4">
+                <div className="govuk-grid-column-one-third">
+                  <Link
+                    className="govuk-link govuk-!-margin-right-9"
+                    to={`/statistics/publication?publication=${slug}`}
+                    as={`/statistics/${slug}`}
+                    data-testid={`view-stats-${slug}`}
+                  >
+                    View statistics and data
+                  </Link>
+                </div>
+                <div className="govuk-grid-column-one-third">
+                  <Link
+                    className="govuk-link"
+                    to={`/table-tool/${slug}`}
+                    data-testid={`create-table-${slug}`}
+                  >
+                    Create your own tables online
+                  </Link>
+                </div>
               </div>
-              <div className="govuk-grid-column-one-third">
-                <Link
-                  className="govuk-link"
-                  to={`/table-tool/${slug}`}
-                  data-testid={`create-table-${slug}`}
-                >
-                  Create your own tables online
-                </Link>
-              </div>
-            </div>
+            )}
           </li>
         ))
       ) : (
