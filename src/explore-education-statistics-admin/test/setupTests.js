8c75f87633f0be26d68651ae7aed81a237eba19e
Nicholas Tsim <nicholastsim@gmail.com>
2019-06-02 02:07:12 +0100

    common: Refactor all setupTests into common

--- src/explore-education-statistics-admin/test/setupTests.js
+++ src/explore-education-statistics-admin/test/setupTests.js
@@ -1,31 +1,9 @@
 import 'react-testing-library/cleanup-after-each';
 import 'jest-dom/extend-expect';
 import 'core-js/fn/array/virtual/flat-map';
+import '@common-test/setupGlobals';
 
 if (typeof window !== 'undefined') {
   // fetch polyfill for making API calls.
   require('cross-fetch');
 }
-
-const localStorageMock = {
-  clear: jest.fn(),
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-};
-global.localStorage = localStorageMock;
-
-Element.prototype.scrollIntoView = jest.fn();
-
-beforeEach(() => {
-  window.matchMedia = jest.fn(() => {
-    return {
-      addListener: jest.fn(),
-      removeListener: jest.fn(),
-      matches: true,
-    };
-  });
-});
-
-afterEach(() => {
-  window.location.hash = '';
-});
