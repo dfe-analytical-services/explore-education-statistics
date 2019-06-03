a11d0f45d80bc29b49e2f10a38a176ef03bba125
Nicholas Tsim <nick@hiveit.co.uk>
2019-06-03 16:14:02 +0100

    tests: Add `toHaveScrollIntoView` assertion

--- src/explore-education-statistics-admin/test/setupTests.js
+++ src/explore-education-statistics-admin/test/setupTests.js
@@ -2,6 +2,7 @@ import 'react-testing-library/cleanup-after-each';
 import 'jest-dom/extend-expect';
 import 'core-js/fn/array/virtual/flat-map';
 import '@common-test/setupGlobals';
+import '@common-test/extend-expect';
 
 if (typeof window !== 'undefined') {
   // fetch polyfill for making API calls.
