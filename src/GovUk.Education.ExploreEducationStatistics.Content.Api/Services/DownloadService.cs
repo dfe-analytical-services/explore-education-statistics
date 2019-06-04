498bca68cecb7050600613d04432485365adf71c
James Cutts <james@hiveit.co.uk>
2019-06-04 17:06:19 +0100

    dFE-684: download content api

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Services/DownloadService.cs
@@ -0,0 +1,48 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Content.Api.Services.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Content.Api.ViewModels;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Services
+{
+    public class DownloadService : IDownloadService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DownloadService(
+            ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // TODO: Include the list of downloadable files
+        public List<ThemeTree> GetTree()
+        {
+            var tree = _context.Themes.Select(t => new ThemeTree
+            {
+                Id = t.Id,
+                Title = t.Title,
+                Topics = t.Topics.Select(x => new TopicTree
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Summary = x.Summary,
+                    Publications = x.Publications.Where(p => p.Releases.Any())
+                        .Select(p => new PublicationTree
+                        {
+                            Id = p.Id,
+                            Title = p.Title,
+                            Summary = p.Summary,
+                            Slug = p.Slug
+                        })
+                        .OrderBy(publication => publication.Title).ToList()
+                }).Where(topic => topic.Publications.Any()).OrderBy(topic => topic.Title).ToList()
+            }).Where(theme => theme.Topics.Any()).OrderBy(theme => theme.Title).ToList();
+
+            return tree;
+        }
+    }
+}
---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Services/Interfaces/IDownloadService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using GovUk.Education.ExploreEducationStatistics.Content.Api.ViewModels;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Services.Interfaces
+{
+    public interface IDownloadService
+    {
+        List<ThemeTree> GetTree();
+    }
+}
