ea3943a0cb36977fd49a7bc018d415d8b3603658
Martin Waite <martin@hiveit.co.uk>
2019-06-05 15:44:51 +0100

    DFE-684 Download data single page

--- src/explore-education-statistics-frontend/src/modules/download/DownloadIndexPage.tsx
+++ src/explore-education-statistics-frontend/src/modules/download/DownloadIndexPage.tsx
@@ -2,14 +2,20 @@ import { contentApi } from '@common/services/api';
 import Link from '@frontend/components/Link';
 import Page from '@frontend/components/Page';
 import PageTitle from '@frontend/components/PageTitle';
-import { Topic } from '@frontend/modules/find-statistics/components/TopicList';
 import React, { Component } from 'react';
+import Accordion from '@common/components/Accordion';
+import AccordionSection from '@common/components/AccordionSection';
+import Details from '@common/components/Details';
+import SearchForm from '@common/components/SearchForm';
+import PublicationDownloadList from './components/PublicationDownloadList';
+import { Topic } from './components/TopicList';
 
 interface Props {
   themes: {
     id: string;
     slug: string;
     title: string;
+    summary: string;
     topics: Topic[];
   }[];
 }
@@ -25,6 +31,7 @@ class DownloadIndexPage extends Component<Props> {
   }
 
   public render() {
+    const { themes } = this.props;
     return (
       <Page
         breadcrumbs={[
@@ -41,6 +48,7 @@ class DownloadIndexPage extends Component<Props> {
               Find the data files behind our range of national and regional
               statistics for your own analysis.
             </p>
+            <SearchForm />
           </div>
           <div className="govuk-grid-column-one-third">
             <aside className="app-related-items">
@@ -61,7 +69,40 @@ class DownloadIndexPage extends Component<Props> {
           </div>
         </div>
 
-        <h2 className="govuk-heading-l">Choose a theme</h2>
+        {themes.length > 0 ? (
+          <Accordion id="themes">
+            {themes.map(
+              ({
+                id: themeId,
+                title: themeTitle,
+                summary: themeSummary,
+                topics,
+              }) => (
+                <AccordionSection
+                  key={themeId}
+                  heading={themeTitle}
+                  caption={themeSummary}
+                >
+                  {topics.map(
+                    ({ id: topicId, title: topicTitle, publications }) => (
+                      <Details key={topicId} summary={topicTitle}>
+                        <div className="govuk-!-margin-top-0 govuk-!-padding-top-0">
+                          <ul className="govuk-bulllet-list govuk-!-margin-bottom-9">
+                            <PublicationDownloadList
+                              publications={publications}
+                            />
+                          </ul>
+                        </div>
+                      </Details>
+                    ),
+                  )}
+                </AccordionSection>
+              ),
+            )}
+          </Accordion>
+        ) : (
+          <div className="govuk-inset-text">No data currently published.</div>
+        )}
 
         {/* {themes.length > 0 ? (
           <>
