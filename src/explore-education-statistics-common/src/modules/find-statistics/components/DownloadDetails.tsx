063cfdb21fbb5224a3ae2d0496b75cd281bf9de8
Nicholas Tsim <nick@hiveit.co.uk>
2019-06-06 12:31:16 +0100

    DFE-693 Comment out DownloadDetails component

--- src/explore-education-statistics-common/src/modules/find-statistics/components/DownloadDetails.tsx
+++ src/explore-education-statistics-common/src/modules/find-statistics/components/DownloadDetails.tsx
@@ -1,25 +1,24 @@
-import Details from '@common/components/Details';
-import React from 'react';
-
 const DownloadDetails = () => {
-  return (
-    <Details summary="Download this data">
-      <ul className="govuk-list">
-        <li>
-          <a href="#">Download as .csv</a>
-        </li>
-        <li>
-          <a href="#">Download as .json</a>
-        </li>
-        <li>
-          <a href="#">Download as Excel</a>
-        </li>
-        <li>
-          <a href="#">Access API</a> - <a href="#">What is an API?</a>
-        </li>
-      </ul>
-    </Details>
-  );
+  // TODO: Implement Downloads properly in later ticket
+  return null;
+  // return (
+  //   <Details summary="Download this data">
+  //     <ul className="govuk-list">
+  //       <li>
+  //         <a href="#">Download as .csv</a>
+  //       </li>
+  //       <li>
+  //         <a href="#">Download as .json</a>
+  //       </li>
+  //       <li>
+  //         <a href="#">Download as Excel</a>
+  //       </li>
+  //       <li>
+  //         <a href="#">Access API</a> - <a href="#">What is an API?</a>
+  //       </li>
+  //     </ul>
+  //   </Details>
+  // );
 };
 
 export default DownloadDetails;
