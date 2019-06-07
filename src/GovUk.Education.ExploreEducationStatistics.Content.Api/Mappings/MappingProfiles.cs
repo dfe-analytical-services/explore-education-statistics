5567fb90576e16ef885faca7f6eaac6decfe6161
Ben Outram <ben@hiveit.co.uk>
2019-06-07 10:10:25 +0100

    DFE-551 Download controller to list Themes/Topics/Publications and data files for the Download data files page.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Mappings/MappingProfiles.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Mappings
+{
+    /**
+     * AutoMapper Profile which is configured by AutoMapper.Extensions.Microsoft.DependencyInjection.
+     */
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+        }
+    }
+}
