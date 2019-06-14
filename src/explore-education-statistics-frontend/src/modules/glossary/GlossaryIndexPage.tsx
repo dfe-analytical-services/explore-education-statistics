9c0d42ce78304f89bbabbd61fca26869d8d6e8d1
Fred Brenton <fredbrentonltd@gmail.com>
2019-06-14 06:50:37 +0100

    Updated glossary content.

--- src/explore-education-statistics-frontend/src/modules/glossary/GlossaryIndexPage.tsx
+++ src/explore-education-statistics-frontend/src/modules/glossary/GlossaryIndexPage.tsx
@@ -150,19 +150,14 @@ function GlossaryIndexPage() {
           <p>INSERT DEFINITION - TBC.</p>
         </AccordionSection>
         <AccordionSection heading="G">
-          <h3 id="gcse-gcses">GCSE, GCSEs</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="grammar-school">grammar school</h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="ggg-ggg">TBC</h3>
         </AccordionSection>
         <AccordionSection heading="H">
-          <h3 id="headteacher">headteacher</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="higher-education-he">higher education (HE)</h3>
+          <h3 id="higher-education-he">Higher education (HE)</h3>
           <p>INSERT DEFINITION - TBC.</p>
         </AccordionSection>
         <AccordionSection heading="I">
-          <h3 id="independent-review-panel">independent review panel</h3>
+          <h3 id="independent-review-panel">Independent review panel</h3>
           <p>
             Parents (and pupils if aged over 18 years) can request a review of a
             permanent exclusion.
@@ -177,29 +172,17 @@ function GlossaryIndexPage() {
             and have regard to the interests of other pupils and people working
             at the school.
           </p>
-          <h3 id="individual-schools-budget">individual schools budget</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="initial-teacher-training">initial teacher training</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="inset-day">inset day</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="international-baccalaureate">International Baccalaureate</h3>
-          <p>INSERT DEFINITION - TBC.</p>
         </AccordionSection>
         <AccordionSection heading="J">
-          <h3 id="jjj-jjj">SOMETHING BEGINNING WITH J</h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="jjj-jjj">TBC</h3>
         </AccordionSection>
         <AccordionSection heading="K">
-          <h3 id="key-stage">key stage</h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="key-stage">TBC</h3>
         </AccordionSection>
         <AccordionSection heading="L">
-          <h3 id="local-authority">local authority</h3>
+          <h3 id="local-authority">Local authority (LA)</h3>
           <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="looked-after-children">looked-after children</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="lunchtime-exclusion">lunchtime exclusion</h3>
+          <h3 id="lunchtime-exclusion">Lunchtime exclusion</h3>
           <p>
             When a pupil is not allowed to attend (or is excluded from) a school
             for the duration of the school's lunchtime period.
@@ -210,33 +193,10 @@ function GlossaryIndexPage() {
           </p>
         </AccordionSection>
         <AccordionSection heading="M">
-          <h3 id="mainstream-schools">mainstream schools</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="maintained-schools-maintained-nursery-schools">
-            maintained schools, maintained nursery schools
-          </h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="middle-deemed-primary-school-middle-deemed-secondary-school">
-            middle-deemed primary school, middle-deemed secondary school
-          </h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="mixed-sex-schools">mixed-sex schools</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="multi-academy-trust">multi-academy trust</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="multi-ethnic">multi-ethnic</h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="mmm-mmm">TBC</h3>
         </AccordionSection>
         <AccordionSection heading="N">
-          <h3 id="national-curriculum">national curriculum</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="national-curriculum-tests">national curriculum tests</h3>
-          <p>Not SATs - INSERT DEFINITION - TBC.</p>
           <h3 id="national-offer-day">National Offer Day</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="national-pupil-database">national pupil database</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="national-scholarship-fund">national scholarship fund</h3>
           <p>INSERT DEFINITION - TBC.</p>
           <h3 id="national-scholarship-fund">National Statistician</h3>
           <p>
@@ -248,23 +208,18 @@ function GlossaryIndexPage() {
             <a href="https://www.gov.uk/government/people/john-pullinger">
               John Pullinger
             </a>
-            .
           </p>
-          <h3 id="newly-qualified-teacher">newly qualified teacher</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="nursery-school">nursery school</h3>
-          <p>INSERT DEFINITION - TBC.</p>
         </AccordionSection>
         <AccordionSection heading="O">
           <h3 id="one-or-more-fixed-period-exclusion">
-            one or more fixed-period exclusion
+            One or more fixed-period exclusion
           </h3>
           <p>
             Pupils who have had at least 1 fixed-period excluions across a full
             academic year.
           </p>
           <p>Includes those with repeated fixed-period exclusions.</p>
-          <h3 id="overall-absence">overall absence</h3>
+          <h3 id="overall-absence">Overall absence</h3>
           <p>
             The total number of all authorised and unauthorised absences from
             possible school sessions for all pupils.
@@ -276,26 +231,15 @@ function GlossaryIndexPage() {
           <p>
             Counted in sessions, where 1 session is equivalent to half-a-day.
           </p>
-          <h3 id="ofsted-judgements">Ofsted judgements</h3>
-          <p>INCLUDE DEFINITION IN HERE....TBC</p>
-          <p>There are 4 Ofsted grades:</p>
-          <ul>
-            <li>outstanding (or grade 1)</li>
-            <li>good (or grade 2)</li>
-            <li>requires improvement (or grade 3)</li>
-            <li>inadequate (or grade 4)</li>
-          </ul>
-          <h3 id="one-year-on">one-year-on</h3>
-          <p>If used adjectivally, hyphenate and use one rather than 1.</p>
         </AccordionSection>
         <AccordionSection heading="P">
-          <h3 id="permanent-exclusion">permanent exclusion</h3>
+          <h3 id="permanent-exclusion">Permanent exclusion</h3>
           <p>
             When a pupil is not allowed to attend (or is excluded from) a school
             and cannot go back to that specific school unless their exclusion is
             overturned.
           </p>
-          <h3 id="persistent-absence">persistent absence</h3>
+          <h3 id="persistent-absence">Persistent absence</h3>
           <p>
             When a pupil misses (or is absent from) 10% or more possible school
             sessions.
@@ -313,13 +257,13 @@ function GlossaryIndexPage() {
             </a>{' '}
             guidance.
           </p>
-          <h3 id="possible-school-session">possible school session</h3>
+          <h3 id="possible-school-session">Possible school session</h3>
           <p>
             Schools are required to provide 2 possible sessions per day - 1
             session in the morning and 1 in the afternoon.
           </p>
           <p>This is why a session is equivalent to half-a-day.</p>
-          <h3 id="pupil-enrolment">pupil enrolment</h3>
+          <h3 id="pupil-enrolment">Pupil enrolment</h3>
           <p>
             A way to refer to a 'pupil' at a school. The number of pupil
             enrolments presented includes pupils on the school roll for at least
@@ -330,88 +274,65 @@ function GlossaryIndexPage() {
             moved schools during the academic year or are registered in more
             than 1 school.
           </p>
-          <h3 id="performance-tables">performance tables</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="pupil-referral-unit">pupil referral unit</h3>
+          <h3 id="pupil-referral-unit">Pupil referral unit</h3>
           <p>INSERT DEFINITION - TBC</p>
           <h3 id="pupils-with-1-or-more-fixed-period-exclusion">
-            pupils with 1 or more fixed-period exclusion
+            Pupils with one or more fixed-period exclusion
           </h3>
           <p>
-            Pupil who have had at least 1 fixed period exclusion across a full
+            Pupil who have had at least one fixed-period exclusion across a full
             academic year.
           </p>
         </AccordionSection>
         <AccordionSection heading="Q">
-          <h3 id="qualified-teacher-status">qualified teacher status</h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="qqq-qqq">TBC</h3>
         </AccordionSection>
         <AccordionSection heading="R">
-          <h3 id="rrr-rrr">SOMETHING BEGINNING WITH R</h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="rrr-rrr">TBC</h3>
         </AccordionSection>
         <AccordionSection heading="S">
-          <h3 id="same-sex-schools">same-sex schools</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="sats">SATs</h3>
+          <h3 id="school-census">School census</h3>
           <p>
-            <a href="#national-curriculum-tests">
-              See national curriculum tests
-            </a>
-            .
+            Statutory termly data collection for all of the following
+            educational organisations in England:
           </p>
-          <h3 id="school-admissions-code">School Admissions Code</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="school-and-college-performance-tables">
-            school and college performance tables
-          </h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="school-census">school census</h3>
-          <p>
-            Statutory termly data collection for all maintained nursery,
-            primary, secondary, middle-deemed primary, middle-deemed secondary,
-            local authority maintained special and non-maintained special
-            schools, academies including free schools, studio schools and
-            university technical colleges and city technology colleges in
-            England.
-          </p>
-          <h3 id="school-improvement-plan">school improvement plan</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="school-session">school session</h3>
+          <ul>
+            <li>academies - including free schools and studio schools</li>
+            <li>
+              colleges - including city technology and university technical
+              colleges
+            </li>
+            <li>
+              maintained schools - including nurseries, middle-deemed primary
+              and secondary schools, primary schools and secondary schools
+            </li>
+            <li>
+              special schools - including local authority maintained and
+              non-maintained special schools
+            </li>
+          </ul>
+          <h3 id="school-session">School session</h3>
           <p>
             See{' '}
             <a href="/glossary#possible-school-session">
               possible school session
             </a>
-            .
           </p>
-          <h3 id="schools-workforce">schools workforce</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="sixth-form-college">sixth-form college</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="sole-registered-pupils">sole registered pupils</h3>
-          <p>Pupils who are on the roll of 1 school only.</p>
-          <h3 id="special-educational-needs-code-of-practice">
-            Special Educational Needs Code of Practice
-          </h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="special-measures">special measures</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="summer-school">summer school</h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="school-session">School year</h3>
+          <p>
+            See <a href="/glossary#academic-year">academic year</a>
+          </p>
+          <h3 id="sole-registered-pupils">Sole registered pupils</h3>
+          <p>Pupils who are on the roll of only 1 school.</p>
         </AccordionSection>
         <AccordionSection heading="T">
-          <h3 id="techbacc">TechBacc</h3>
-          <p>INSERT DEFINITION - TBC.</p>
-          <h3 id="trust-school">trust school</h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="ttt-ttt">TBC</h3>
         </AccordionSection>
         <AccordionSection heading="U">
-          <h3 id="unauthorised-absence">unauthorised absence</h3>
+          <h3 id="unauthorised-absence">Unauthorised absence</h3>
           <p>
-            When a pupil misses (or is from) at least 1 possible school session
-            without the permission of a teacher or other authorised school
-            representative.
+            When a pupil misses at least 1 possible school session without the
+            permission of a teacher or other authorised school representative.
           </p>
           <p>
             Includes all unexplained or unjustified absences and late arrivals.
@@ -419,33 +340,23 @@ function GlossaryIndexPage() {
           <p>
             Counted in sessions, where 1 session is equivalent to half-a-day.
           </p>
-          <h3 id="university-technical-college">
-            university technical college
-          </h3>
-          <p>INSERT DEFINITION - TBC.</p>
         </AccordionSection>
         <AccordionSection heading="V">
-          <h3 id="voluntary-aided-schools-voluntary-controlled-schools">
-            voluntary-aided schools, voluntary-controlled schools
-          </h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="vvv-vvv">TBC</h3>
         </AccordionSection>
         <AccordionSection heading="W">
           <h3 id="what-is-an-api">What is an API?</h3>
           <p>INSERT DEFINITION - TBC.</p>
         </AccordionSection>
         <AccordionSection heading="X">
-          <h3 id="xxx-xxx">SOMETHING BEGINNIG WITH X</h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="xxx-xxx">TBC</h3>
         </AccordionSection>
         <AccordionSection heading="Y">
-          <h3 id="year-1-year-2">year 1, year 2</h3>
-          <p>INSERT DEFINITION - TBC.</p>
+          <h3 id="yyy-yyy">TBC</h3>
         </AccordionSection>
         <AccordionSection heading="Z">
           <div>
-            <h3 id="zzz-zzz">SOMETHING BEGINNING WITH Z</h3>
-            <p>INSERT DEFINITION - TBC.</p>
+            <h3 id="zzz-zzz">TBC</h3>
           </div>
         </AccordionSection>
       </Accordion>
