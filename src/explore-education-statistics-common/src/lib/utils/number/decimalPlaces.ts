e993aab9fa44eae834e6fe5c9ec3d109588588a9
Nicholas Tsim <nick@hiveit.co.uk>
2019-06-25 14:04:00 +0100

    DFE-759 Remove redundant decimalPlaces function

--- src/explore-education-statistics-common/src/lib/utils/number/decimalPlaces.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Get the number of decimal places for a {@param value}.
- */
-export default function decimalPlaces(value: number) {
-  const [_, decimals] = value.toString().split('.');
-  return decimals ? decimals.length : 0;
-}
