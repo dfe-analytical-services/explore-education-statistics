fecf78b28110ebaefe6eb7f506216ff517071d8d
James Cutts <james@hiveit.co.uk>
2019-06-26 13:53:29 +0100

    DFE-767: Add last update methodology migration

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Migrations/20190626125108_AddMethodologyLastUpdatedDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Migrations
+{
+    public partial class AddMethodologyLastUpdatedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Methodologies",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Methodologies",
+                keyColumn: "Id",
+                keyValue: new Guid("caa8e56f-41d2-4129-a5c3-53b051134bd7"),
+                column: "LastUpdated",
+                value: new DateTime(2019, 6, 26, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "Methodologies");
+        }
+    }
+}
