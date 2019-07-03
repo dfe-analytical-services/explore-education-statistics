47edfd7f4427369d5f2b5898a958b3c6233c60e4
Si Shakes <si@hiveit.co.uk>
2019-07-03 14:19:10 +0100

    DFE-754 Change to use int.MaxValue

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/FileUploadController.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/FileUploadController.cs
@@ -34,7 +34,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Controllers
         }
 
         [HttpPost]
-        [RequestSizeLimit(2047 * 1024 * 1024)]
+        [RequestSizeLimit(int.MaxValue)]
         [RequestFormLimits(ValueLengthLimit = int.MaxValue, MultipartBodyLengthLimit = int.MaxValue)]
         public async Task<IActionResult> Post(Guid releaseId, string name, IFormFile file, IFormFile metaFile)
         {
