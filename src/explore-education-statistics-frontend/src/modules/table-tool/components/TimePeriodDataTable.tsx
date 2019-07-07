0363c129fcca384ecd8ebd34c2859d534d541293
Nicholas Tsim <nicholastsim@gmail.com>
2019-07-07 22:11:36 +0100

    DFE-887 Wrap all filter options with Filter types and add initial location filtering

--- src/explore-education-statistics-frontend/src/modules/table-tool/components/TimePeriodDataTable.tsx
+++ src/explore-education-statistics-frontend/src/modules/table-tool/components/TimePeriodDataTable.tsx
@@ -1,30 +1,34 @@
 import cartesian from '@common/lib/utils/cartesian';
 import formatPretty from '@common/lib/utils/number/formatPretty';
 import {
-  FilterOption,
   IndicatorOption,
   TableData,
 } from '@common/services/tableBuilderService';
 import TimePeriod from '@common/services/types/TimePeriod';
 import { Dictionary } from '@common/types/util';
 import DataTableCaption from '@frontend/modules/table-tool/components/DataTableCaption';
+import {
+  CategoryFilter,
+  Indicator,
+  LocationFilter,
+} from '@frontend/modules/table-tool/components/types/filters';
 import sortBy from 'lodash/sortBy';
 import React, { memo, useEffect, useRef, useState } from 'react';
 import FixedMultiHeaderDataTable from './FixedMultiHeaderDataTable';
 import TableHeadersForm, { TableHeadersFormValues } from './TableHeadersForm';
 
 interface Props {
-  indicators: IndicatorOption[];
-  filters: Dictionary<FilterOption[]>;
+  indicators: Indicator[];
+  filters: Dictionary<CategoryFilter[]>;
   timePeriods: TimePeriod[];
   publicationName: string;
   subjectName: string;
-  locations: Dictionary<FilterOption[]>;
+  locations: Dictionary<LocationFilter[]>;
   results: TableData['result'];
 }
 
 const TimePeriodDataTable = (props: Props) => {
-  const { filters, timePeriods, indicators, results } = props;
+  const { filters, timePeriods, locations, indicators, results } = props;
 
   const dataTableRef = useRef<HTMLTableElement>(null);
 
@@ -36,9 +40,13 @@ const TimePeriodDataTable = (props: Props) => {
   });
 
   useEffect(() => {
-    const sortedFilters = sortBy(Object.values(filters), [
-      options => options.length,
-    ]);
+    const sortedFilters = sortBy(
+      Object.values({
+        ...filters,
+        locations: Object.values(locations).flat(),
+      }),
+      [options => options.length],
+    );
 
     const halfwayIndex = Math.floor(sortedFilters.length / 2);
     const columnGroups = sortedFilters.slice(0, halfwayIndex);
@@ -50,7 +58,7 @@ const TimePeriodDataTable = (props: Props) => {
       columns: timePeriods,
       rows: indicators,
     });
-  }, [filters, timePeriods, indicators]);
+  }, [filters, timePeriods, locations, indicators]);
 
   const columnHeaders: string[][] = [
     ...tableHeaders.columnGroups.map(colGroup =>
@@ -86,18 +94,29 @@ const TimePeriodDataTable = (props: Props) => {
         columnFilterCombination.length - 1
       ] as TimePeriod;
 
-      const aggregateFilters = [
+      const combinationFilters: LocationFilter[] | CategoryFilter[] = [
         ...rowFilterCombination.slice(0, -1),
         ...columnFilterCombination.slice(0, -1),
       ];
 
+      const categoryFilters = combinationFilters.filter(
+        filter => filter instanceof CategoryFilter,
+      );
+
+      const locationFilters = combinationFilters.filter(
+        filter => filter instanceof LocationFilter,
+      ) as LocationFilter[];
+
       const matchingResult = results.find(result => {
         return (
-          aggregateFilters.every(filter => {
-            return result.filters.includes(filter.value);
-          }) &&
+          categoryFilters.every(filter =>
+            result.filters.includes(filter.value),
+          ) &&
           result.timeIdentifier === time.code &&
-          result.year === time.year
+          result.year === time.year &&
+          locationFilters.every(
+            filter => result.location[filter.level].code === filter.value,
+          )
         );
       });
 
