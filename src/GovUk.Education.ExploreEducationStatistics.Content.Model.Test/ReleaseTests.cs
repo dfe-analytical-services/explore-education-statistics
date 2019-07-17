827bfa3c7b69f9fc2fc535ad72677632243924ee
richard <richard@hiveit.co.uk>
2019-07-17 11:24:13 +0100

    DFE-1021 addressing pull comments

--- src/GovUk.Education.ExploreEducationStatistics.Content.Model.Test/ReleaseTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model.Test/ReleaseTests.cs
@@ -6,20 +6,23 @@ namespace GovUk.Education.ExploreEducationStatistics.Content.Model.Test
     public class ReleaseTests
     {
         [Fact]
-        public void Release_Not_Live()
+        public void Release_Not_Live_Published_Null()
         {
-            {
-                var releaseNoPublishedDate = new Release {Published = null};
-                Assert.False(releaseNoPublishedDate.Live);
-            }
-            {
-                // Note that this should not happen, but we test the edge case.
-                var releasePublishedDateInFuture = new Release {Published = DateTime.Now.AddDays(1)};
-                Assert.False(releasePublishedDateInFuture.Live);
-            }
+
+            var releaseNoPublishedDate = new Release {Published = null};
+            Assert.False(releaseNoPublishedDate.Live);
         }
 
         [Fact]
+        public void Release_Not_Live_Published_In_Future()
+        {
+            // Note that this should not happen, but we test the edge case.
+            var releasePublishedDateInFuture = new Release {Published = DateTime.Now.AddDays(1)};
+            Assert.False(releasePublishedDateInFuture.Live);
+        }
+    
+
+    [Fact]
         public void Release_Live()
         {
             var releasePublished = new Release {Published = DateTime.Now.AddDays(-1)};
