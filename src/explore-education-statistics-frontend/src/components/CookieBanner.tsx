382126f08291016a13c3021e13b653d1be7a9190
Stuart Bennett <bennettstuart92@gmail.com>
2019-07-17 16:46:42 +0100

    Cookie Banner moved component to @frontend from common, and implemented a cookie package, got basic banner functionality in place

---
+++ src/explore-education-statistics-frontend/src/components/CookieBanner.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useCookies } from 'react-cookie';
+import ButtonText from '@common/components/ButtonText';
+import styles from './CookieBanner.module.scss';
+
+export const cookieBannerSeenName = 'dfe_seen_cookie_message';
+
+const CookieBanner = () => {
+  const [cookies, setCookie] = useCookies([cookieBannerSeenName]);
+
+  const dateToday = new Date();
+  const oneMonthFromNow = new Date(
+    dateToday.setMonth(dateToday.getMonth() + 1),
+  );
+  function acceptCookies() {
+    setCookie(cookieBannerSeenName, true, {
+      expires: oneMonthFromNow,
+    });
+  }
+
+  return !cookies[cookieBannerSeenName] ? (
+    <div className={styles.container}>
+      <p>
+        <span>GOV.UK uses cookies to make the site simpler.</span>{' '}
+        <ButtonText
+          type="button"
+          className={styles.button}
+          onClick={() => {
+            acceptCookies();
+          }}
+        >
+          Accept Cookies
+        </ButtonText>{' '}
+        or <a href="/cookies">find out more about cookies</a>.
+      </p>
+    </div>
+  ) : null;
+};
+
+export default CookieBanner;
