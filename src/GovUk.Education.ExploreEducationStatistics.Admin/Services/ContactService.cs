498b3cc7c5237068298864ae6596039deed74d92
richard <richard@hiveit.co.uk>
2019-07-18 16:50:50 +0100

    DFE-1022 api to return contacts

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/ContactService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public List<Contact> GetContacts()
+        {
+            return _context.Contacts.ToList();
+        }
+    }
+}
