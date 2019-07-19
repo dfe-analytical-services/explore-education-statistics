7f6dcf56f99ccdd8e40440038eb3f016daaf6364
Ben Outram <ben@hiveit.co.uk>
2019-07-19 17:38:48 +0100

    DFE-892 Adding IndicatorFootnote data script.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/Services/FootnoteService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Services/FootnoteService.cs
@@ -14,11 +14,14 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Services
 
         public IEnumerable<Footnote> GetFootnotes(long subjectId, IEnumerable<long> indicators)
         {
-            return from f in _context.Footnote
-                join s in _context.SubjectFootnote on f.Id equals s.FootnoteId
-                join i in _context.IndicatorFootnote on f.Id equals i.FootnoteId
-                where s.SubjectId == subjectId && indicators.Contains(i.IndicatorId)
-                select f;
+            return (from f in _context.Footnote
+                    join s in _context.SubjectFootnote on f.Id equals s.FootnoteId
+                    join i in _context.IndicatorFootnote on f.Id equals i.FootnoteId
+                    where s.SubjectId == subjectId && indicators.Contains(i.IndicatorId)
+                    select f)
+                .Distinct()
+                .OrderBy(footnote => footnote.Id)
+                .ToList();
         }
     }
 }
