72770dd2aa3c85949ff0faf781f040b53860adcf
Mark Youngman <mark@hiveit.co.uk>
2019-07-29 15:19:25 +0100

    Adding code to read and execute 20190729134031_AddNewLADBoundaryLevelApril2019.sql and added ExcludeFromCodeCoverage annotation

--- src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190729134031_AddNewLADBoundaryLevelApril2019.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190729134031_AddNewLADBoundaryLevelApril2019.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Migrations
 {
+    [ExcludeFromCodeCoverage]
     public partial class AddNewLADBoundaryLevelApril2019 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             // Add new LAD boundary level data
-            ExecuteFile(migrationBuilder, "Migrations/20190729134031_AddNewLADBoundaryLevelApril2019.sql");
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "Migrations/20190729134031_AddNewLADBoundaryLevelApril2019.sql");
+            migrationBuilder.Sql(File.ReadAllText(file));
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
