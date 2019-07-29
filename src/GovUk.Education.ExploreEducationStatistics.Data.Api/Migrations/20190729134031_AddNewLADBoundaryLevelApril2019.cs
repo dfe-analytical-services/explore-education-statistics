4f0b41f99432bb72bfc3b6b2dccadd7911b04710
Mark Youngman <mark@hiveit.co.uk>
2019-07-29 14:35:42 +0100

    Adding new LAD boundary level

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190729134031_AddNewLADBoundaryLevelApril2019.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Migrations
+{
+    public partial class AddNewLADBoundaryLevelApril2019 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Add new LAD boundary level data
+            ExecuteFile(migrationBuilder, "Migrations/20190729134031_AddNewLADBoundaryLevelApril2019.sql");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Drop new LAD boundary level
+            migrationBuilder.Sql("DELETE FROM BoundaryLevel WHERE Id=8;");
+        }
+    }
+}
