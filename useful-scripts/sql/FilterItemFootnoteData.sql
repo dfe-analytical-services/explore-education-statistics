56446e7bfce0ef654bbfa3f33807bc803fa03a25
Ben Outram <ben@hiveit.co.uk>
2019-07-29 17:30:00 +0100

    DFE-1127 Adding useful script to insert Filter footnote data

---
+++ useful-scripts/sql/FilterItemFootnoteData.sql
@@ -0,0 +1,62 @@
+-- State-funded primary 
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (71, 1);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (86, 1);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (89, 1);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (94, 1);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (98, 1);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (149, 1);
+-- State-funded secondary
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (75, 1);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (87, 1);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (90, 1);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (92, 1);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (99, 1);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (151, 1);
+
+-- State-funded primary
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (71, 2);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (86, 2);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (89, 2);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (94, 2);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (98, 2);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (149, 2);
+
+-- State-funded secondary
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (75, 3);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (87, 3);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (90, 3);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (92, 3);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (99, 3);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (151, 3);
+
+-- Special
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (74, 4);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (85, 4);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (91, 4);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (93, 4);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (100, 4);
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (150, 4);
+
+-- Characteristic
+INSERT INTO FilterFootnote (FilterId, FootnoteId) VALUES (1, 16);
+
+-- Characteristic
+INSERT INTO FilterFootnote (FilterId, FootnoteId) VALUES (1, 17);
+
+-- SEN provision
+INSERT INTO FilterGroupFootnote (FilterGroupId, FootnoteId) VALUES (10, 18);
+
+-- SEN provision
+INSERT INTO FilterGroupFootnote (FilterGroupId, FootnoteId) VALUES (10, 19);
+
+-- SEN provision
+INSERT INTO FilterGroupFootnote (FilterGroupId, FootnoteId) VALUES (10, 20);
+
+-- Ethnic group minor
+INSERT INTO FilterGroupFootnote (FilterGroupId, FootnoteId) VALUES (6, 21);
+
+-- SEN primary need
+INSERT INTO FilterGroupFootnote (FilterGroupId, FootnoteId) VALUES (11, 22);
+
+-- SEN primary need Social emotional and mental health
+INSERT INTO FilterItemFootnote (FilterItemId, FootnoteId) VALUES (58, 23);
