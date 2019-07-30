ce11c45b1b08328ea337e0c3a56f99e4ded7896d
Duncan Watson <duncan@hiveit.co.uk>
2019-07-30 15:35:03 +0100

    DFE-1145 - triggering a user validation / details retrieval check each time a protected route is hit, and more importantly, lazily now!

--- src/explore-education-statistics-admin/src/components/ProtectedRoute.tsx
+++ src/explore-education-statistics-admin/src/components/ProtectedRoute.tsx
@@ -3,6 +3,42 @@ import loginService, {Authentication} from "@admin/services/sign-in/service";
 import React, {useEffect, useState} from 'react';
 import {Redirect, Route, RouteProps} from 'react-router';
 
+const AuthenticationCheckingComponent = ({component, location, ...props}: RouteProps) => {
+
+  const redirect = () => (
+    <Redirect
+      to={{
+        pathname: '/sign-in',
+        state: { from: location },
+      }}
+    />
+  );
+
+  const [authentication, setAuthentication] = useState<Authentication>();
+
+  useEffect(() => {
+    loginService.
+    getUserDetails().
+    then(user => setAuthentication({user})).
+    catch(_ => setAuthentication({}))
+  }, []);
+
+  if (!authentication) {
+    return null;
+  }
+
+  if (!authentication.user || !component) {
+    return redirect();
+  }
+
+  return (
+    <LoginContext.Provider value={authentication}>
+      {React.createElement(component, props)}
+    </LoginContext.Provider>
+  );
+};
+
+
 /**
  * Creates a <Route> that firstly checks the user's authentication
  * status and then renders the protected component if the user has been
@@ -16,41 +52,13 @@ import {Redirect, Route, RouteProps} from 'react-router';
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const ProtectedRoute = ({component, location, ...rest}: RouteProps) => {
 
-  const [authentication, setAuthentication] = useState<Authentication>();
-
-  useEffect(() => {
-    loginService.
-    getUserDetails().
-    then(user => setAuthentication({user})).
-    catch(_ => setAuthentication({}))
-  }, []);
-
-
-  const redirect = () => (
-    <Redirect
-      to={{
-        pathname: '/sign-in',
-        state: { from: location },
-      }}
+  const routeComponent = (props: any) => (
+    <AuthenticationCheckingComponent
+      component={component}
+      location={location}
+      {...props}
     />
   );
-
-  const routeComponent = (props: any) => {
-
-    if (!authentication) {
-      return null;
-    }
-
-    if (!authentication.user || !component) {
-      return redirect();
-    }
-
-    return (
-      <LoginContext.Provider value={authentication}>
-        {React.createElement(component, props)}
-      </LoginContext.Provider>
-    );
-  };
 
   return (
     <Route
