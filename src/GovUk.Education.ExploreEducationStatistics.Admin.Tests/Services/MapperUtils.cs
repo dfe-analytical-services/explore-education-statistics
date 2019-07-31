0a61e052135d062df42218f0cb4eaa042e9c7a4b
richard <richard@hiveit.co.uk>
2019-07-31 18:29:39 +0100

    DFE-1167 refactor

--- src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/MapperUtils.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/MapperUtils.cs
@@ -5,11 +5,8 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services
     public static class MapperUtils
     {
         public static IMapper MapperForProfile<T>() where T : Profile, new()
-        {
-            var profile = new T();
-            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(profile));
-            var mapper = new Mapper(configuration);
-            return mapper;
+        {   
+            return new MapperConfiguration(cfg => cfg.AddProfile<T>()).CreateMapper();
         }   
     }
 }
--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/MethodologyService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/MethodologyService.cs
@@ -6,7 +6,6 @@ using GovUk.Education.ExploreEducationStatistics.Admin.Models.Api;
 using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
 using Microsoft.EntityFrameworkCore;
-using static GovUk.Education.ExploreEducationStatistics.Admin.Services.ModelMappers;
 using TopicId = System.Guid;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Services
