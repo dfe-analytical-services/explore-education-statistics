51232632d029991fdf61dba8a63707344f5867e9
Si Shakes <si@hiveit.co.uk>
2019-08-02 10:09:57 +0100

    DFE-1060 Oddly I'd removed the save of the actual observations.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Processor/Processor.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Processor/Processor.cs
@@ -87,6 +87,8 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Processor
                 logger.LogInformation($"{GetType().Name} function STARTED for : Batch: {message.BatchNo} of {message.BatchSize} with Datafile: {message.DataFileName}");
                 
                 _fileImportService.ImportObservations(message);
+                
+                _context.SaveChanges();
 
                 // If the batch size is > 1 i.e. The file was split into batches
                 // then delete each split batch processed
