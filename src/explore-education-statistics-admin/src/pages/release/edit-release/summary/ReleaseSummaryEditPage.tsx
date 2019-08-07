30400f860bb765ba1a112397e3928f90b5b7cf5a
Duncan Watson <duncan@hiveit.co.uk>
2019-08-07 15:35:34 +0100

    DFE-1254 - removed unnecessary MatchProps

--- src/explore-education-statistics-admin/src/pages/release/edit-release/summary/ReleaseSummaryEditPage.tsx
+++ src/explore-education-statistics-admin/src/pages/release/edit-release/summary/ReleaseSummaryEditPage.tsx
@@ -1,23 +1,29 @@
-import {TimePeriodCoverageGroup} from '@admin/pages/DummyReferenceData';
-import ManageReleaseContext, {ManageRelease} from "@admin/pages/release/ManageReleaseContext";
-import ReleaseSummaryForm, {EditFormValues,} from '@admin/pages/release/summary/ReleaseSummaryForm';
-import {assembleUpdateReleaseSummaryRequestFromForm} from '@admin/pages/release/util/releaseSummaryUtil';
-import {summaryRoute} from '@admin/routes/edit-release/routes';
-import {dateToDayMonthYear, dayMonthYearValuesToInputs,} from '@admin/services/common/types';
+import { TimePeriodCoverageGroup } from '@admin/pages/DummyReferenceData';
+import ManageReleaseContext, {
+  ManageRelease,
+} from '@admin/pages/release/ManageReleaseContext';
+import ReleaseSummaryForm, {
+  EditFormValues,
+} from '@admin/pages/release/summary/ReleaseSummaryForm';
+import { assembleUpdateReleaseSummaryRequestFromForm } from '@admin/pages/release/util/releaseSummaryUtil';
+import { summaryRoute } from '@admin/routes/edit-release/routes';
+import {
+  dateToDayMonthYear,
+  dayMonthYearValuesToInputs,
+} from '@admin/services/common/types';
 import service from '@admin/services/release/edit-release/summary/service';
-import {ReleaseSummaryDetails} from '@admin/services/release/types';
-import React, {useContext, useEffect, useState} from 'react';
-import {RouteComponentProps} from "react-router";
+import { ReleaseSummaryDetails } from '@admin/services/release/types';
+import React, { useContext, useEffect, useState } from 'react';
+import { RouteComponentProps } from 'react-router';
 
-const ReleaseSummaryEditPage = ({
-  history,
-}: RouteComponentProps) => {
-
+const ReleaseSummaryEditPage = ({ history }: RouteComponentProps) => {
   const [releaseSummaryDetails, setReleaseSummaryDetails] = useState<
     ReleaseSummaryDetails
   >();
 
-  const {releaseId, publication} = useContext(ManageReleaseContext) as ManageRelease;
+  const { releaseId, publication } = useContext(
+    ManageReleaseContext,
+  ) as ManageRelease;
 
   useEffect(() => {
     service.getReleaseSummaryDetails(releaseId).then(release => {
