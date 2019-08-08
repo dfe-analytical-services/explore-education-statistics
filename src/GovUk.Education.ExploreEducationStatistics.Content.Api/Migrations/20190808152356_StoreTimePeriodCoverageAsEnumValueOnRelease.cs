01a615b7061acb682ec13e4839197ef3fe96361f
richard <richard@hiveit.co.uk>
2019-08-08 16:57:35 +0100

    DFE-1183 native sql up and down for time identifier type on release

--- src/GovUk.Education.ExploreEducationStatistics.Content.Api/Migrations/20190808152356_StoreTimePeriodCoverageAsEnumValueOnRelease.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Migrations/20190808152356_StoreTimePeriodCoverageAsEnumValueOnRelease.cs
@@ -1,84 +1,41 @@
 ﻿using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Migrations
 {
     public partial class StoreTimePeriodCoverageAsEnumValueOnRelease : Migration
     {
+        private const string MigrationsPath = "Migrations/";
+        
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            
+            
             migrationBuilder.AlterColumn<string>(
                 name: "TimePeriodCoverage",
                 table: "Releases",
                 maxLength: 6,
                 nullable: false,
                 oldClrType: typeof(int));
-
-            migrationBuilder.UpdateData(
-                table: "Releases",
-                keyColumn: "Id",
-                keyValue: new Guid("4fa4fe8e-9a15-46bb-823f-49bf8e0cdec5"),
-                column: "TimePeriodCoverage",
-                value: "AY");
-
-            migrationBuilder.UpdateData(
-                table: "Releases",
-                keyColumn: "Id",
-                keyValue: new Guid("63227211-7cb3-408c-b5c2-40d3d7cb2717"),
-                column: "TimePeriodCoverage",
-                value: "AY");
-
-            migrationBuilder.UpdateData(
-                table: "Releases",
-                keyColumn: "Id",
-                keyValue: new Guid("e7774a74-1f62-4b76-b9b5-84f14dac7278"),
-                column: "TimePeriodCoverage",
-                value: "AY");
-
-            migrationBuilder.UpdateData(
-                table: "Releases",
-                keyColumn: "Id",
-                keyValue: new Guid("f75bc75e-ae58-4bc4-9b14-305ad5e4ff7d"),
-                column: "TimePeriodCoverage",
-                value: "AY");
+            ExecuteFile(migrationBuilder, MigrationsPath + "20190808152356_StoreTimePeriodCoverageAsEnumValueOnReleaseUp.sql");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            ExecuteFile(migrationBuilder, MigrationsPath + "20190808152356_StoreTimePeriodCoverageAsEnumValueOnReleaseDown.sql");
             migrationBuilder.AlterColumn<int>(
                 name: "TimePeriodCoverage",
                 table: "Releases",
                 nullable: false,
                 oldClrType: typeof(string),
                 oldMaxLength: 6);
+        }
 
-            migrationBuilder.UpdateData(
-                table: "Releases",
-                keyColumn: "Id",
-                keyValue: new Guid("4fa4fe8e-9a15-46bb-823f-49bf8e0cdec5"),
-                column: "TimePeriodCoverage",
-                value: 0);
-
-            migrationBuilder.UpdateData(
-                table: "Releases",
-                keyColumn: "Id",
-                keyValue: new Guid("63227211-7cb3-408c-b5c2-40d3d7cb2717"),
-                column: "TimePeriodCoverage",
-                value: 0);
-
-            migrationBuilder.UpdateData(
-                table: "Releases",
-                keyColumn: "Id",
-                keyValue: new Guid("e7774a74-1f62-4b76-b9b5-84f14dac7278"),
-                column: "TimePeriodCoverage",
-                value: 0);
-
-            migrationBuilder.UpdateData(
-                table: "Releases",
-                keyColumn: "Id",
-                keyValue: new Guid("f75bc75e-ae58-4bc4-9b14-305ad5e4ff7d"),
-                column: "TimePeriodCoverage",
-                value: 0);
+        private static void ExecuteFile(MigrationBuilder migrationBuilder, string filename)
+        {
+            var file = Path.Combine(Directory.GetCurrentDirectory(), filename);
+            migrationBuilder.Sql(File.ReadAllText(file));
         }
     }
 }
