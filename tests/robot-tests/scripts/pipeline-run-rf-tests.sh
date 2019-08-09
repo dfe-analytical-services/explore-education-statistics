5b50962a875ed17561f154b3ea9425451b66a07e
Mark Youngman <mark@hiveit.co.uk>
2019-08-09 09:20:05 +0100

    UI Tests: Update CI chromedriver version

--- tests/robot-tests/scripts/pipeline-run-rf-tests.sh
+++ tests/robot-tests/scripts/pipeline-run-rf-tests.sh
@@ -3,4 +3,4 @@
 python -m pip install --upgrade pip
 pip install pipenv
 pipenv install
-pipenv run python run_tests.py -e ci --chromedriver 74.0.3729.6
+pipenv run python run_tests.py -e ci --chromedriver 75.0.3770.140
