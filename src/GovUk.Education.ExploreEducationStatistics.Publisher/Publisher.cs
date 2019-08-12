241334735ba6b77b1e3ede050b1bec3a6170e4a2
James Cutts <james@jamescutts.co.uk>
2019-08-12 22:25:42 +0100

    wip content cache function

--- src/GovUk.Education.ExploreEducationStatistics.Publisher/Publisher.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher/Publisher.cs
@@ -17,13 +17,31 @@ namespace GovUk.Education.ExploreEducationStatistics.Publisher
 
         [FunctionName("Publisher")]
         // ReSharper disable once UnusedMember.Global
-        public async void Run(
+        public async void PublishReleaseData(
             [QueueTrigger("publish-release-data", Connection = "")]
             PublishReleaseDataMessage message,
             ILogger logger)
         {
             logger.LogInformation($"{GetType().Name} function triggered: {message}");
             await _publishingService.PublishReleaseData(message);
+            logger.LogInformation($"{GetType().Name} function completed");
+        }
+        
+        [FunctionName("RebuildContentCache")]
+        // ReSharper disable once UnusedMember.Global
+        public async void RebuildContentCacheTrees(
+            [QueueTrigger("content-cache", Connection = "")]
+            // TODO: create the cache message object
+            dynamic message,
+            ILogger logger)
+        {
+            logger.LogInformation($"{GetType().Name} function triggered: {message}");
+            
+            // TODO: switch actions based on message content
+            
+            // TODO: Add content cache generation service
+
+            
             logger.LogInformation($"{GetType().Name} function completed");
         }
     }
