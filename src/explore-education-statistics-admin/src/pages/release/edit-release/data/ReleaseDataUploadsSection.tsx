398fa41c070e1d13a57281a3c3a1e2d0861babad
Duncan Watson <duncan@hiveit.co.uk>
2019-08-13 20:30:17 +0100

    DFE-1184 - tweak to structure of forms on data and file management tabs

--- src/explore-education-statistics-admin/src/pages/release/edit-release/data/ReleaseDataUploadsSection.tsx
+++ src/explore-education-statistics-admin/src/pages/release/edit-release/data/ReleaseDataUploadsSection.tsx
@@ -59,135 +59,133 @@ const ReleaseDataUploadsSection = ({ publicationId, releaseId }: Props) => {
   );
 
   return (
-    <>
-      <Formik<FormValues>
-        enableReinitialize
-        initialValues={{
-          subjectTitle: '',
-          dataFile: null,
-          metadataFile: null,
-        }}
-        onSubmit={async (values: FormValues, actions) => {
-          await service.uploadDataFiles(releaseId, {
-            subjectTitle: values.subjectTitle,
-            dataFile: values.dataFile as File,
-            metadataFile: values.metadataFile as File,
-          });
+    <Formik<FormValues>
+      enableReinitialize
+      initialValues={{
+        subjectTitle: '',
+        dataFile: null,
+        metadataFile: null,
+      }}
+      onSubmit={async (values: FormValues, actions) => {
+        await service.uploadDataFiles(releaseId, {
+          subjectTitle: values.subjectTitle,
+          dataFile: values.dataFile as File,
+          metadataFile: values.metadataFile as File,
+        });
 
-          await resetPage(actions);
-        }}
-        validationSchema={Yup.object<FormValues>({
-          subjectTitle: Yup.string().required('Enter a subject title'),
-          dataFile: Yup.mixed().required('Choose a data file'),
-          metadataFile: Yup.mixed().required('Choose a metadata file'),
-        })}
-        render={(form: FormikProps<FormValues>) => {
-          return (
-            <Form id={formId} submitValidationHandler={handleServerValidation}>
-              {dataFiles &&
-                dataFiles.map(dataFile => (
-                  <SummaryList key={dataFile.file.fileName}>
-                    <SummaryListItem term="Subject title">
-                      {dataFile.title}
-                    </SummaryListItem>
-                    <SummaryListItem term="Data file">
-                      <a
-                        href={service.createDownloadDataFileLink(
-                          releaseId,
-                          dataFile.file.fileName,
-                        )}
+        await resetPage(actions);
+      }}
+      validationSchema={Yup.object<FormValues>({
+        subjectTitle: Yup.string().required('Enter a subject title'),
+        dataFile: Yup.mixed().required('Choose a data file'),
+        metadataFile: Yup.mixed().required('Choose a metadata file'),
+      })}
+      render={(form: FormikProps<FormValues>) => {
+        return (
+          <Form id={formId} submitValidationHandler={handleServerValidation}>
+            {dataFiles &&
+              dataFiles.map(dataFile => (
+                <SummaryList key={dataFile.file.fileName}>
+                  <SummaryListItem term="Subject title">
+                    {dataFile.title}
+                  </SummaryListItem>
+                  <SummaryListItem term="Data file">
+                    <a
+                      href={service.createDownloadDataFileLink(
+                        releaseId,
+                        dataFile.file.fileName,
+                      )}
+                    >
+                      {dataFile.file.fileName}
+                    </a>
+                  </SummaryListItem>
+                  <SummaryListItem term="Filesize">
+                    {dataFile.fileSize.size.toLocaleString()}{' '}
+                    {dataFile.fileSize.unit}
+                  </SummaryListItem>
+                  <SummaryListItem term="Number of rows">
+                    {dataFile.numberOfRows.toLocaleString()}
+                  </SummaryListItem>
+                  <SummaryListItem term="Metadata file">
+                    <a
+                      href={service.createDownloadDataMetadataFileLink(
+                        releaseId,
+                        dataFile.file.fileName,
+                      )}
+                    >
+                      {dataFile.metadataFile.fileName}
+                    </a>
+                  </SummaryListItem>
+                  <SummaryListItem
+                    term="Actions"
+                    actions={
+                      <Link
+                        to="#"
+                        onClick={() =>
+                          setDeleteFileName(dataFile.file.fileName)
+                        }
                       >
-                        {dataFile.file.fileName}
-                      </a>
-                    </SummaryListItem>
-                    <SummaryListItem term="Filesize">
-                      {dataFile.fileSize.size.toLocaleString()}{' '}
-                      {dataFile.fileSize.unit}
-                    </SummaryListItem>
-                    <SummaryListItem term="Number of rows">
-                      {dataFile.numberOfRows.toLocaleString()}
-                    </SummaryListItem>
-                    <SummaryListItem term="Metadata file">
-                      <a
-                        href={service.createDownloadDataMetadataFileLink(
-                          releaseId,
-                          dataFile.file.fileName,
-                        )}
-                      >
-                        {dataFile.metadataFile.fileName}
-                      </a>
-                    </SummaryListItem>
-                    <SummaryListItem
-                      term="Actions"
-                      actions={
-                        <Link
-                          to="#"
-                          onClick={() =>
-                            setDeleteFileName(dataFile.file.fileName)
-                          }
-                        >
-                          Delete files
-                        </Link>
-                      }
-                    />
-                  </SummaryList>
-                ))}
-              <FormFieldset
-                id={`${formId}-allFieldsFieldset`}
-                legend="Add new data to release"
-              >
-                <FormFieldTextInput<FormValues>
-                  id={`${formId}-subjectTitle`}
-                  name="subjectTitle"
-                  label="Subject title"
-                />
+                        Delete files
+                      </Link>
+                    }
+                  />
+                </SummaryList>
+              ))}
+            <FormFieldset
+              id={`${formId}-allFieldsFieldset`}
+              legend="Add new data to release"
+            >
+              <FormFieldTextInput<FormValues>
+                id={`${formId}-subjectTitle`}
+                name="subjectTitle"
+                label="Subject title"
+              />
 
-                <FormFieldFileSelector<FormValues>
-                  id={`${formId}-dataFile`}
-                  name="dataFile"
-                  label="Upload data file"
-                  formGroupClass="govuk-!-margin-top-6"
-                  form={form}
-                />
+              <FormFieldFileSelector<FormValues>
+                id={`${formId}-dataFile`}
+                name="dataFile"
+                label="Upload data file"
+                formGroupClass="govuk-!-margin-top-6"
+                form={form}
+              />
 
-                <FormFieldFileSelector<FormValues>
-                  id={`${formId}-metadataFile`}
-                  name="metadataFile"
-                  label="Upload metadata file"
-                  form={form}
-                />
-              </FormFieldset>
+              <FormFieldFileSelector<FormValues>
+                id={`${formId}-metadataFile`}
+                name="metadataFile"
+                label="Upload metadata file"
+                form={form}
+              />
+            </FormFieldset>
 
-              <Button type="submit" className="govuk-!-margin-top-6">
-                Upload data files
-              </Button>
+            <Button type="submit" className="govuk-!-margin-top-6">
+              Upload data files
+            </Button>
 
-              <div className="govuk-!-margin-top-6">
-                <Link to="#" onClick={() => resetPage(form)}>
-                  Cancel
-                </Link>
-              </div>
+            <div className="govuk-!-margin-top-6">
+              <Link to="#" onClick={() => resetPage(form)}>
+                Cancel
+              </Link>
+            </div>
 
-              <ModalConfirm
-                mounted={deleteFileName != null && deleteFileName.length > 0}
-                title="Confirm deletion of selected data files"
-                onExit={() => setDeleteFileName('')}
-                onCancel={() => setDeleteFileName('')}
-                onConfirm={async () => {
-                  await service.deleteDataFiles(releaseId, deleteFileName);
-                  setDeleteFileName('');
-                  resetPage(form);
-                }}
-              >
-                <p>
-                  This data will no longer be available for use in this release
-                </p>
-              </ModalConfirm>
-            </Form>
-          );
-        }}
-      />
-    </>
+            <ModalConfirm
+              mounted={deleteFileName != null && deleteFileName.length > 0}
+              title="Confirm deletion of selected data files"
+              onExit={() => setDeleteFileName('')}
+              onCancel={() => setDeleteFileName('')}
+              onConfirm={async () => {
+                await service.deleteDataFiles(releaseId, deleteFileName);
+                setDeleteFileName('');
+                resetPage(form);
+              }}
+            >
+              <p>
+                This data will no longer be available for use in this release
+              </p>
+            </ModalConfirm>
+          </Form>
+        );
+      }}
+    />
   );
 };
 
--- src/explore-education-statistics-admin/src/pages/release/edit-release/data/ReleaseFileUploadsSection.tsx
+++ src/explore-education-statistics-admin/src/pages/release/edit-release/data/ReleaseFileUploadsSection.tsx
@@ -47,107 +47,104 @@ const ReleaseFileUploadsSection = ({ publicationId, releaseId }: Props) => {
   };
 
   return (
-    <>
-      {files &&
-        files.map(file => (
-          <SummaryList key={file.file.fileName}>
-            <SummaryListItem term="Name">{file.title}</SummaryListItem>
-            <SummaryListItem term="File">
-              <a
-                href={service.createDownloadDataFileLink(
-                  releaseId,
-                  file.file.fileName,
-                )}
-              >
-                {file.file.fileName}
-              </a>
-            </SummaryListItem>
-            <SummaryListItem term="Filesize">
-              {file.fileSize.size.toLocaleString()} {file.fileSize.unit}
-            </SummaryListItem>
-            <SummaryListItem
-              term="Actions"
-              actions={
-                <Link
-                  to="#"
-                  onClick={() => setDeleteFileName(file.file.fileName)}
-                >
-                  Delete file
-                </Link>
-              }
-            />
-          </SummaryList>
-        ))}
-      <Formik<FormValues>
-        enableReinitialize
-        initialValues={{
-          name: '',
-          file: null,
-        }}
-        onSubmit={async (values: FormValues, actions) => {
-          await service.uploadAncillaryFile(releaseId, {
-            name: values.name,
-            file: values.file as File,
-          });
+    <Formik<FormValues>
+      enableReinitialize
+      initialValues={{
+        name: '',
+        file: null,
+      }}
+      onSubmit={async (values: FormValues, actions) => {
+        await service.uploadAncillaryFile(releaseId, {
+          name: values.name,
+          file: values.file as File,
+        });
 
-          resetPage(actions);
-        }}
-        validationSchema={Yup.object<FormValues>({
-          name: Yup.string().required('Enter a name'),
-          file: Yup.mixed().required('Choose a file'),
-        })}
-        render={(form: FormikProps<FormValues>) => {
-          return (
-            <Form id={formId}>
-              <FormFieldset
-                id={`${formId}-allFieldsFieldset`}
-                legend="Upload file"
-              >
-                <FormFieldTextInput<FormValues>
-                  id={`${formId}-name`}
-                  name="name"
-                  label="Name"
+        resetPage(actions);
+      }}
+      validationSchema={Yup.object<FormValues>({
+        name: Yup.string().required('Enter a name'),
+        file: Yup.mixed().required('Choose a file'),
+      })}
+      render={(form: FormikProps<FormValues>) => {
+        return (
+          <Form id={formId}>
+            {files && files.map(file => (
+              <SummaryList key={file.file.fileName}>
+                <SummaryListItem term="Name">{file.title}</SummaryListItem>
+                <SummaryListItem term="File">
+                  <a
+                    href={service.createDownloadDataFileLink(
+                      releaseId,
+                      file.file.fileName,
+                    )}
+                  >
+                    {file.file.fileName}
+                  </a>
+                </SummaryListItem>
+                <SummaryListItem term="Filesize">
+                  {file.fileSize.size.toLocaleString()} {file.fileSize.unit}
+                </SummaryListItem>
+                <SummaryListItem
+                  term="Actions"
+                  actions={
+                    <Link
+                      to="#"
+                      onClick={() => setDeleteFileName(file.file.fileName)}
+                    >
+                      Delete file
+                    </Link>
+                  }
                 />
+              </SummaryList>
+            ))}
+            <FormFieldset
+              id={`${formId}-allFieldsFieldset`}
+              legend="Upload file"
+            >
+              <FormFieldTextInput<FormValues>
+                id={`${formId}-name`}
+                name="name"
+                label="Name"
+              />
 
-                <FormFieldFileSelector<FormValues>
-                  id={`${formId}-file`}
-                  name="file"
-                  label="Upload file"
-                  formGroupClass="govuk-!-margin-top-6"
-                  form={form}
-                />
-              </FormFieldset>
+              <FormFieldFileSelector<FormValues>
+                id={`${formId}-file`}
+                name="file"
+                label="Upload file"
+                formGroupClass="govuk-!-margin-top-6"
+                form={form}
+              />
+            </FormFieldset>
 
-              <Button type="submit" className="govuk-!-margin-top-6">
-                Upload file
-              </Button>
+            <Button type="submit" className="govuk-!-margin-top-6">
+              Upload file
+            </Button>
 
-              <div className="govuk-!-margin-top-6">
-                <Link to={dataRoute.generateLink(publicationId, releaseId)}>
-                  Cancel
-                </Link>
-              </div>
+            <div className="govuk-!-margin-top-6">
+              <Link to={dataRoute.generateLink(publicationId, releaseId)}>
+                Cancel
+              </Link>
+            </div>
 
-              <ModalConfirm
-                mounted={deleteFileName != null && deleteFileName.length > 0}
-                title="Confirm deletion of file"
-                onExit={() => setDeleteFileName('')}
-                onCancel={() => setDeleteFileName('')}
-                onConfirm={async () => {
-                  await service.deleteAncillaryFile(releaseId, deleteFileName);
-                  setDeleteFileName('');
-                  resetPage(form);
-                }}
-              >
-                <p>
-                  This file will no longer be available for use in this release
-                </p>
-              </ModalConfirm>
-            </Form>
-          );
-        }}
-      />
-    </>
+            <ModalConfirm
+              mounted={deleteFileName != null && deleteFileName.length > 0}
+              title="Confirm deletion of file"
+              onExit={() => setDeleteFileName('')}
+              onCancel={() => setDeleteFileName('')}
+              onConfirm={async () => {
+                await service.deleteAncillaryFile(releaseId, deleteFileName);
+                setDeleteFileName('');
+                resetPage(form);
+              }}
+            >
+              <p>
+                This file will no longer be available for use in this release
+              </p>
+            </ModalConfirm>
+          </Form>
+        );
+      }}
+    />
   );
 };
 
