8911c4ecbf38c0003575b6654fcaf7fe4c081760
James Cutts <james@jamescutts.co.uk>
2019-08-15 10:37:48 +0100

    Adding fast track controller and services

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/ViewModels/FastTrackViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+using GovUk.Education.ExploreEducationStatistics.Data.Api.Models.Query;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Api.ViewModels
+{
+    public class FastTrackViewModel
+    {
+        public Guid Id { get; set; }
+
+        public TableBuilderConfiguration Configuration { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public TableBuilderResultViewModel FullTable { get; set; }
+    }
+}
