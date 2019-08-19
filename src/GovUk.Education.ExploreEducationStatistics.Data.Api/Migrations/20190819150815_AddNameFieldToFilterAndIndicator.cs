6b4384de0a9804069569db8a3c0389451d71142c
Ben Outram <ben@hiveit.co.uk>
2019-08-19 16:14:04 +0100

    DFE-1098 Temporary migrations fix to prevent removing data

--- src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190819150815_AddNameFieldToFilterAndIndicator.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190819150815_AddNameFieldToFilterAndIndicator.cs
@@ -4,14 +4,24 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Migrations
 {
     [ExcludeFromCodeCoverage]
-    public partial class AddNameFieldToFilter : Migration
+    public partial class AddNameFieldToFilterAndIndicator : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.AddColumn<string>(
                 name: "Name",
+                table: "Indicator",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
                 table: "Filter",
                 nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Indicator_Name",
+                table: "Indicator",
+                column: "Name");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Filter_Name",
@@ -22,8 +32,16 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropIndex(
+                name: "IX_Indicator_Name",
+                table: "Indicator");
+
+            migrationBuilder.DropIndex(
                 name: "IX_Filter_Name",
                 table: "Filter");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Indicator");
 
             migrationBuilder.DropColumn(
                 name: "Name",
