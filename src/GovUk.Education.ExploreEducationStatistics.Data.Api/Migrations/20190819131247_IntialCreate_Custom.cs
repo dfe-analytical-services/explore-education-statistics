c86a9ad27a4e0de5aba63e2a5315330926293d9f
Ben Outram <ben@hiveit.co.uk>
2019-08-19 15:19:09 +0100

    DFE-1336 Add _migrationId string.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190819131247_IntialCreate_Custom.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/Migrations/20190819131247_IntialCreate_Custom.cs
@@ -8,24 +8,25 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Migrations
     public partial class IntialCreate_Custom : Migration
     {
         private const string _migrationsPath = "Migrations";
+        private const string _migrationId = "20190819131247";
 
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             // Geometry
-            ExecuteFile(migrationBuilder, "20190819131247_Geometry.sql");
+            ExecuteFile(migrationBuilder, $"{_migrationId}_Geometry.sql");
             // Types
-            ExecuteFile(migrationBuilder, "20190819131247_TableTypes.sql");
+            ExecuteFile(migrationBuilder, $"{_migrationId}_TableTypes.sql");
             // Routines
-            ExecuteFile(migrationBuilder, "20190819131247_Routine_FilteredObservations.sql");
-            ExecuteFile(migrationBuilder, "20190819131247_Routine_FilteredFootnotes.sql");
-            ExecuteFile(migrationBuilder, "20190819131247_Routine_geometry2json.sql");
+            ExecuteFile(migrationBuilder, $"{_migrationId}_Routine_FilteredObservations.sql");
+            ExecuteFile(migrationBuilder, $"{_migrationId}_Routine_FilteredFootnotes.sql");
+            ExecuteFile(migrationBuilder, $"{_migrationId}_Routine_geometry2json.sql");
             // Views
-            ExecuteFile(migrationBuilder, "20190819131247_Views.sql");
+            ExecuteFile(migrationBuilder, $"{_migrationId}_Views.sql");
             // Indexes
-            ExecuteFile(migrationBuilder, "20190819131247_Indexes.sql");
+            ExecuteFile(migrationBuilder, $"{_migrationId}_Indexes.sql");
             // Data
-            ExecuteFile(migrationBuilder, "20190819131247_BoundaryLevelData.sql");
-            ExecuteLines(migrationBuilder, "20190819131247_GeometryData.sql");
+            ExecuteFile(migrationBuilder, $"{_migrationId}_BoundaryLevelData.sql");
+            ExecuteLines(migrationBuilder, $"{_migrationId}_GeometryData.sql");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
