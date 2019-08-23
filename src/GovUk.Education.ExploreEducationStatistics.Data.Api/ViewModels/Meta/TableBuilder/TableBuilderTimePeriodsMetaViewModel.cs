04a09272b4c780c0269e17ce1b93a400909170df
Ben Outram <ben@hiveit.co.uk>
2019-08-23 11:56:07 +0100

    DFE-980 Adding import

--- src/GovUk.Education.ExploreEducationStatistics.Data.Api/ViewModels/Meta/TableBuilder/TableBuilderTimePeriodsMetaViewModel.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/ViewModels/Meta/TableBuilder/TableBuilderTimePeriodsMetaViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using GovUk.Education.ExploreEducationStatistics.Data.Services.ViewModels.Meta;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Api.ViewModels.Meta.TableBuilder
 {
