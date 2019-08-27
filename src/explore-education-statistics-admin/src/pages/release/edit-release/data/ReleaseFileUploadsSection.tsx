3136e99ae265b085ebb273912f7ab9df9f981a6f
Duncan Watson <duncan@hiveit.co.uk>
2019-08-27 14:25:47 +0100

    DFE-1355 - adding file upload validation handling

--- src/explore-education-statistics-admin/src/pages/release/edit-release/data/ReleaseFileUploadsSection.tsx
+++ src/explore-education-statistics-admin/src/pages/release/edit-release/data/ReleaseFileUploadsSection.tsx
@@ -6,6 +6,7 @@ import Button from '@common/components/Button';
 import FormFieldFileSelector from '@common/components/form/FormFieldFileSelector';
 import FormFieldTextInput from '@common/components/form/FormFieldTextInput';
 import { Form, FormFieldset, Formik } from '@common/components/form';
+import handleServerSideValidation, {errorCodeToFieldError} from "@common/components/form/util/serverValidationHandler";
 import ModalConfirm from '@common/components/ModalConfirm';
 import SummaryList from '@common/components/SummaryList';
 import SummaryListItem from '@common/components/SummaryListItem';
@@ -46,6 +47,19 @@ const ReleaseFileUploadsSection = ({ publicationId, releaseId }: Props) => {
     setFiles(latestFiles);
   };
 
+  const handleServerValidation = handleServerSideValidation(
+    errorCodeToFieldError(
+      'CANNOT_OVERWRITE_FILE',
+      'file',
+      'Choose a unique file name',
+    ),
+    errorCodeToFieldError(
+      'FILE_CAN_NOT_BE_EMPTY',
+      'file',
+      'Choose a file that is not empty',
+    ),
+  );
+
   return (
     <Formik<FormValues>
       enableReinitialize
@@ -67,7 +81,10 @@ const ReleaseFileUploadsSection = ({ publicationId, releaseId }: Props) => {
       })}
       render={(form: FormikProps<FormValues>) => {
         return (
-          <Form id={formId}>
+          <Form
+            id={formId}
+            submitValidationHandler={handleServerValidation}
+          >
             {files &&
               files.map(file => (
                 <SummaryList key={file.filename}>
