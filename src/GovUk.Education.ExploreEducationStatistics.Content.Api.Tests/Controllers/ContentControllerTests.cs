e9e503bb3fb4fe02464ec1ff604714e51bc10bfb
James Cutts <james@hiveit.co.uk>
2019-08-27 11:23:41 +0100

    DFE-772: fix failing unit test

--- src/GovUk.Education.ExploreEducationStatistics.Content.Api.Tests/Controllers/ContentControllerTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api.Tests/Controllers/ContentControllerTests.cs
@@ -168,7 +168,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Tests.Controlle
 
             var result = controller.GetRelease("publication-a");
 
-            Assert.IsAssignableFrom<Release>(result.Value);
+            Assert.IsAssignableFrom<ReleaseViewModel>(result.Value);
             Assert.Equal("Academic Year", result.Value.Title);
             Assert.Equal("publication-a", result.Value.Slug);
         }
