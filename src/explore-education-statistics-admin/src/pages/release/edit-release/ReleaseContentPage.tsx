525e3cd3e274d3d09c27533d65414a55a876def0
Duncan Watson <duncan@hiveit.co.uk>
2019-09-02 09:26:13 +0100

    DFE-1398 - added basic header and Edit / Preview mode selection

--- src/explore-education-statistics-admin/src/pages/release/edit-release/ReleaseContentPage.tsx
+++ src/explore-education-statistics-admin/src/pages/release/edit-release/ReleaseContentPage.tsx
@@ -1,7 +1,79 @@
-import React from 'react';
+import {Comment} from "@admin/services/dashboard/types";
+import FormFieldset from "@common/components/form/FormFieldset";
+import FormRadio from "@common/components/form/FormRadio";
+import FormRadioGroup from "@common/components/form/FormRadioGroup";
+import React, {useEffect, useState} from 'react';
+
+interface Model {
+  unresolvedComments: Comment[];
+  pageMode: 'edit' | 'preview';
+}
 
 const ReleaseContentPage = () => {
-  return <h2 className="govuk-heading-m">Add / edit content</h2>;
+
+  const [model, setModel] = useState();
+
+  useEffect(() => {
+    const unresolvedComments: Comment[] = [{
+      message: 'Please resolve this.\nThank you.',
+      authorName: 'Amy Newton',
+      createdDate: new Date('2019-08-10 10:15').toISOString(),
+    }, {
+      message: 'And this too.\nThank you.',
+      authorName: 'Dave Matthews',
+      createdDate: new Date('2019-08-10 10:15').toISOString(),
+    }];
+
+    setModel({
+      unresolvedComments,
+      pageView: 'edit',
+    })
+  }, []);
+
+  return (
+    <>
+      {model && (
+        <div className="govuk-form-group">
+          {model.unresolvedComments.length > 0 && (
+            <div className="govuk-warning-text">
+              <span className="govuk-warning-text__icon" aria-hidden="true">
+                !
+              </span>
+              <strong className="govuk-warning-text__text">
+                <span className="govuk-warning-text__assistive">Warning</span>
+                There are {model.unresolvedComments.length} unresolved comments
+              </strong>
+            </div>
+          )}
+
+          <FormFieldset
+            id='pageModelFieldSet'
+            legend=''
+            className='dfe-toggle-edit'
+            legendHidden
+          >
+            <FormRadioGroup
+              id='pageMode'
+              name='pageMode'
+              value={model.pageMode}
+              legend='Set page view'
+              options={[
+                {
+                  label: 'Add / view comments and edit content',
+                  value: 'edit',
+                },
+                {
+                  label: 'Preview content',
+                  value: 'preview',
+                },
+              ]}
+              inline
+            />
+          </FormFieldset>
+        </div>
+      )}
+    </>
+  )
 };
 
 export default ReleaseContentPage;
