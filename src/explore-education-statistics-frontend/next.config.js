3cb2a18a169e5f2fadc9e8d93391c10ded2c2f3f
Duncan Watson <duncan@hiveit.co.uk>
2019-09-17 15:10:07 +0100

    Removed polyfill from config as its now explicitly in the _app file

--- src/explore-education-statistics-frontend/next.config.js
+++ src/explore-education-statistics-frontend/next.config.js
@@ -155,18 +155,6 @@ const nextConfig = {
       }
     }
 
-    const originalEntry = config.entry;
-
-    config.entry = async () => {
-      const entries = await originalEntry();
-
-      if (entries['main.js'] && !entries['main.js'].includes('./polyfill.js')) {
-        entries['main.js'].unshift('./polyfill.js');
-      }
-
-      return entries;
-    };
-
     config.plugins.push(
       new DotEnvPlugin({
         path: envFilePath,
