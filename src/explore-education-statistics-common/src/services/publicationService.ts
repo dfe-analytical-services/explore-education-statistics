53b6a218cea5767c9a550b44ce7520a50cab1944
James Cutts <james@hiveit.co.uk>
2019-09-30 10:49:09 +0100

    Fix react app content api call for adjusted endpoint

--- src/explore-education-statistics-common/src/services/publicationService.ts
+++ src/explore-education-statistics-common/src/services/publicationService.ts
@@ -228,7 +228,7 @@ export default {
   getLatestPublicationRelease(publicationSlug: string): Promise<Release> {
     return contentApi.get(`content/publication/${publicationSlug}/latest`);
   },
-  getPublicationRelease(releaseId: string): Promise<Release> {
-    return contentApi.get(`content/release/${releaseId}`);
+  getPublicationRelease(publicationSlug: string, releaseSlug: string): Promise<Release> {
+    return contentApi.get(`content/publication/${publicationSlug}/${releaseSlug}`);
   },
 };
