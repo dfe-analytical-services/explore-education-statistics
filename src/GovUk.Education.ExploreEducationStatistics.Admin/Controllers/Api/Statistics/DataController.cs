e210f62d5811eb2b0645cba02c1c1ef3e31f3918
Simon Shakespeare <si.shakes@gmail.com>
2019-09-30 17:18:13 +0100

    EES-388 Pass release id optionally to admin data request.

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/Statistics/DataController.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/Statistics/DataController.cs
@@ -1,9 +1,11 @@
+using System.Web.Http;
 using GovUk.Education.ExploreEducationStatistics.Data.Model.Query;
 using GovUk.Education.ExploreEducationStatistics.Data.Services.Interfaces;
 using GovUk.Education.ExploreEducationStatistics.Data.Services.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using ReleaseId = System.Guid;
+ 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Controllers.Api.Statistics
 {
     [Route("api/data/[controller]")]
@@ -12,16 +14,16 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Controllers.Api.Stati
     public class DataController : ControllerBase
     {
         private readonly IDataService<ResultWithMetaViewModel> _dataService;
-
+ 
         public DataController(IDataService<ResultWithMetaViewModel> dataService)
         {
             _dataService = dataService;
         }
-
+ 
         [HttpPost]
-        public ActionResult<ResultWithMetaViewModel> Query([FromBody] ObservationQueryContext query)
+        public ActionResult<ResultWithMetaViewModel> Query([FromUri] ReleaseId release, [FromBody] ObservationQueryContext query)
         {
-            return _dataService.Query(query);
+            return _dataService.Query(query, release);
         }
     }
 }
