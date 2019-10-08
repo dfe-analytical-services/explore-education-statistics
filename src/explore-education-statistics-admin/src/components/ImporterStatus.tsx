e505b70a76d05143bb89464e344e1fa56ee1a2f5
bennettstuart <bennettstuart92@gmail.com>
2019-10-08 16:25:40 +0100

    added Details Summary list component

--- src/explore-education-statistics-admin/src/components/ImporterStatus.tsx
+++ src/explore-education-statistics-admin/src/components/ImporterStatus.tsx
@@ -9,6 +9,7 @@ import styles from '@admin/pages/release/edit-release/data/ReleaseDataUploadsSec
 import SummaryListItem from '@common/components/SummaryListItem';
 import { DataFile } from '@admin/services/release/edit-release/data/types';
 import LoadingSpinner from '@common/components/LoadingSpinner';
+import Details from '@common/components/Details';
 
 interface State {
   isFetching: boolean;
@@ -140,25 +141,36 @@ class ImporterStatus extends Component<Props> {
 
     return (
       <SummaryListItem term="Status">
-        <div className={styles.currentStatusContainer}>
-          <strong
-            className={classNames(
-              'govuk-!-margin-right-1',
-              'govuk-tag',
-              currentStatus && this.getImportStatusClass(currentStatus.status),
+        <div>
+          <div className={styles.currentStatusContainer}>
+            <strong
+              className={classNames(
+                'govuk-!-margin-right-1',
+                'govuk-tag',
+                currentStatus &&
+                  this.getImportStatusClass(currentStatus.status),
+              )}
+            >
+              {currentStatus && getImportStatusLabel(currentStatus.status)}
+            </strong>
+            {running && (
+              <>
+                <LoadingSpinner inline size={22} />{' '}
+                <span className="govuk-visually-hidden">
+                  Currently processing data
+                </span>
+              </>
             )}
-          >
-            {currentStatus && getImportStatusLabel(currentStatus.status)}
-          </strong>
-          {running && (
-            <>
-              <LoadingSpinner inline size={22} />{' '}
-              <span className="govuk-visually-hidden">
-                Currently processing data
-              </span>
-            </>
-          )}
-          {currentStatus && currentStatus.errors}
+          </div>
+          {currentStatus &&
+            currentStatus.errors &&
+            currentStatus.errors.length > 0 && (
+              <Details summary="See Errors">
+                {currentStatus.errors.map((error, index) => (
+                  <li key={index.toString()}>{error}</li>
+                ))}
+              </Details>
+            )}
         </div>
       </SummaryListItem>
     );
