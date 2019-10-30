7b09e6b5b76387292cffccdfd8f442776b1b7f52
Mark Youngman <mark@hiveit.co.uk>
2019-10-30 13:03:28 +0000

    Updated drag and drop functionality description

--- src/explore-education-statistics-common/src/modules/table-tool/components/TableHeadersForm.tsx
+++ src/explore-education-statistics-common/src/modules/table-tool/components/TableHeadersForm.tsx
@@ -42,13 +42,13 @@ const TableHeadersForm = ({
     <Details summary="Re-order table headers">
       <p className="govuk-hint">
         Drag and drop the options below to re-order the table headers. For
-        keyboard users select the draggable item with space and use the arrow
-        keys to move the item accordingly.
+        keyboard users, select and deselect a draggable item with space and use
+        the arrow keys to move a selected item.
       </p>
       <div className="govuk-visually-hidden">
-        To move a draggable item select the item with space and use the arrow
-        keys to move the item accordingly. If you are using a screen reader
-        disable scan mode.
+        To move a draggable item, select and deselect the item with space and
+        use the arrow keys to move a selected item. If you are using a screen
+        reader disable scan mode.
       </div>
       <Formik<TableHeadersFormValues>
         enableReinitialize
