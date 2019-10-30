b8aac3fd449643799e9bc44d30befc905e30739b
Ben Outram <boutram@worth.systems>
2019-10-30 11:54:10 +0000

    EES-589 Fixing Footnotes Admin API after updgrade to .Net EF Core 3

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Models/Api/Statistics/FootnoteViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GovUk.Education.ExploreEducationStatistics.Admin.Models.Api.Statistics
+{
+    public class FootnoteViewModel
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+    }
+}
