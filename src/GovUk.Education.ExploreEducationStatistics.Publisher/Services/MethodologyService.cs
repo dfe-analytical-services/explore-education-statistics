c6be1c73282b6a57cb36fda8f57f0d5655d119c2
Ben Outram <boutram@worth.systems>
2019-10-31 14:20:15 +0000

    EES-591 Fixes to DownloadService and MethodologyService in the Publishing function for EF Core 3.

--- src/GovUk.Education.ExploreEducationStatistics.Publisher/Services/MethodologyService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher/Services/MethodologyService.cs
@@ -4,6 +4,7 @@ using GovUk.Education.ExploreEducationStatistics.Content.Model;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.ViewModels;
 using GovUk.Education.ExploreEducationStatistics.Publisher.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace GovUk.Education.ExploreEducationStatistics.Publisher.Services
 {
@@ -24,27 +25,56 @@ namespace GovUk.Education.ExploreEducationStatistics.Publisher.Services
 
         public List<ThemeTree> GetTree()
         {
-            var tree = _context.Themes.Select(t => new ThemeTree
-            {
-                Id = t.Id,
-                Title = t.Title,
-                Topics = t.Topics.Select(x => new TopicTree
-                {
-                    Id = x.Id,
-                    Title = x.Title,
-                    Summary = x.Summary,
-                    Publications = x.Publications.Where(p => p.Methodology != null && p.Releases.Any(r => r.Published != null))
-                        .Select(p => new PublicationTree
-                        {
-                            Id = p.Methodology.Id,
-                            Title = p.Methodology.Title,
-                            Summary = p.Methodology.Summary,
-                            Slug = p.Slug
-                        }).OrderBy(publication => publication.Title).ToList()
-                }).Where(x => x.Publications.Any()).OrderBy(topic => topic.Title).ToList()
-            }).Where(x => x.Topics.Any()).OrderBy(theme => theme.Title).ToList();
-
+            var tree = _context.Themes
+                .Include(theme => theme.Topics)
+                .ThenInclude(topic => topic.Publications)
+                .ThenInclude(publication => publication.Releases)
+                .Select(BuildThemeTree)
+                .Where(themeTree => themeTree.Topics.Any())
+                .OrderBy(theme => theme.Title)
+                .ToList();
+            
             return tree;
+        }
+
+        private static ThemeTree BuildThemeTree(Theme theme)
+        {
+            return new ThemeTree
+            {
+                Id = theme.Id,
+                Title = theme.Title,
+                Topics = theme.Topics
+                    .Select(BuildTopicTree)
+                    .Where(topicTree => topicTree.Publications.Any())
+                    .OrderBy(topic => topic.Title)
+                    .ToList()
+            };
+        }
+
+        private static TopicTree BuildTopicTree(Topic topic)
+        {
+            return new TopicTree
+            {
+                Id = topic.Id,
+                Title = topic.Title,
+                Summary = topic.Summary,
+                Publications = topic.Publications
+                    .Where(publication => publication.Methodology != null && publication.Releases.Any(r => r.Published != null))
+                    .Select(BuildPublicationTree)
+                    .OrderBy(publication => publication.Title)
+                    .ToList()
+            };
+        }
+
+        private static PublicationTree BuildPublicationTree(Publication publication)
+        {
+            return new PublicationTree
+            {
+                Id = publication.Methodology.Id,
+                Title = publication.Methodology.Title,
+                Summary = publication.Methodology.Summary,
+                Slug = publication.Slug
+            };
         }
     }
 }
