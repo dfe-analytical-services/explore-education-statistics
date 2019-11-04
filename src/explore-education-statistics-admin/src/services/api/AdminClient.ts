cf7f6b93644597acd78bee7fd31ca0485c8d6bc9
James Cutts <james@hiveit.co.uk>
2019-11-04 15:26:59 +0000

    formatting

--- src/explore-education-statistics-admin/src/services/api/AdminClient.ts
+++ src/explore-education-statistics-admin/src/services/api/AdminClient.ts
@@ -1,40 +1,63 @@
 import Client from '@common/services/api/Client';
-import {AxiosInstance, AxiosRequestConfig} from "axios";
-import authService from "@admin/components/api-authorization/AuthorizeService";
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
+import authService from '@admin/components/api-authorization/AuthorizeService';
 
 class AdminClient extends Client {
-
   public constructor(api: AxiosInstance) {
     super(api);
   }
 
-  public get<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T> {
-    return AdminClient.addSecurityHeaders(config)
-      .then(extendedConfig => super.get(url, extendedConfig));
+  public get<T = unknown>(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<T> {
+    return AdminClient.addSecurityHeaders(config).then(extendedConfig =>
+      super.get(url, extendedConfig),
+    );
   }
 
-  public post<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T> {
-    return AdminClient.addSecurityHeaders(config)
-      .then(extendedConfig => super.post(url, data, extendedConfig));
+  public post<T = unknown>(
+    url: string,
+    data?: unknown,
+    config?: AxiosRequestConfig,
+  ): Promise<T> {
+    return AdminClient.addSecurityHeaders(config).then(extendedConfig =>
+      super.post(url, data, extendedConfig),
+    );
   }
 
-  public put<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T> {
-    return AdminClient.addSecurityHeaders(config)
-      .then(extendedConfig => super.put(url, data, extendedConfig));
+  public put<T = unknown>(
+    url: string,
+    data?: unknown,
+    config?: AxiosRequestConfig,
+  ): Promise<T> {
+    return AdminClient.addSecurityHeaders(config).then(extendedConfig =>
+      super.put(url, data, extendedConfig),
+    );
   }
 
-  public patch<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T> {
-    return AdminClient.addSecurityHeaders(config)
-      .then(extendedConfig => super.patch(url, data, extendedConfig));
+  public patch<T = unknown>(
+    url: string,
+    data?: unknown,
+    config?: AxiosRequestConfig,
+  ): Promise<T> {
+    return AdminClient.addSecurityHeaders(config).then(extendedConfig =>
+      super.patch(url, data, extendedConfig),
+    );
   }
 
-  public delete<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T> {
-    return AdminClient.addSecurityHeaders(config)
-      .then(extendedConfig => super.delete(url, extendedConfig));
+  public delete<T = unknown>(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<T> {
+    return AdminClient.addSecurityHeaders(config).then(extendedConfig =>
+      super.delete(url, extendedConfig),
+    );
   }
 
-  private static async addSecurityHeaders(config?: AxiosRequestConfig): Promise<AxiosRequestConfig> {
-
+  private static async addSecurityHeaders(
+    config?: AxiosRequestConfig,
+  ): Promise<AxiosRequestConfig> {
     const token = await authService.getAccessToken();
 
     const c: AxiosRequestConfig = config || {};
@@ -47,7 +70,7 @@ class AdminClient extends Client {
         ...headers,
       },
       ...rest,
-    }
+    };
   }
 }
 
