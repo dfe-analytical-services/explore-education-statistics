ac9a07534121efeafd2764f75a97c14d1b5f693f
Duncan Watson <duncan@hiveit.co.uk>
2019-11-04 16:15:09 +0000

    EES-559 - refactored protected route components to prevent multiple calls for user details

--- src/explore-education-statistics-admin/src/App.tsx
+++ src/explore-education-statistics-admin/src/App.tsx
@@ -1,5 +1,6 @@
-import { ApplicationPaths } from '@admin/components/api-authorization/ApiAuthorizationConstants';
-import AuthorizeRoute from '@admin/components/api-authorization/AuthorizeRoute';
+import {ApplicationPaths} from '@admin/components/api-authorization/ApiAuthorizationConstants';
+import ProtectedRoute from "@admin/components/ProtectedRoute";
+import ProtectedRoutes from "@admin/components/ProtectedRoutes";
 import CreatePublicationPage from '@admin/pages/create-publication/CreatePublicationPage';
 import CreateReleasePage from '@admin/pages/release/create-release/CreateReleasePage';
 import ManageReleasePageContainer from '@admin/pages/release/ManageReleasePageContainer';
@@ -9,19 +10,19 @@ import publicationRoutes from '@admin/routes/edit-publication/routes';
 import releaseRoutes from '@admin/routes/edit-release/routes';
 import PrototypeLoginService from '@admin/services/PrototypeLoginService';
 import React from 'react';
-import { Route } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import {Route} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
 
 import './App.scss';
 
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 
-import { LoginContext } from './components/Login';
+import {LoginContext} from './components/Login';
 import AdminDashboardPage from './pages/admin-dashboard/AdminDashboardPage';
 import AdminDocumentationCreateNewRelease from './pages/documentation/DocumentationCreateNewRelease';
-import AdminDocumentationEditRelease from './pages/documentation/DocumentationEditRelease';
 
 import AdminDocumentationContentDesignStandards from './pages/documentation/DocumentationDesignStandards';
+import AdminDocumentationEditRelease from './pages/documentation/DocumentationEditRelease';
 import AdminDocumentationGlossary from './pages/documentation/DocumentationGlossary';
 import AdminDocumentationHome from './pages/documentation/DocumentationHome';
 import AdminDocumentationManageContent from './pages/documentation/DocumentationManageContent';
@@ -40,7 +41,8 @@ import PublicationConfirmNew from './pages/prototypes/PrototypePublicationPageCo
 import PublicationCreateNew from './pages/prototypes/PrototypePublicationPageCreateNew';
 
 import PublicationEditPage from './pages/prototypes/PrototypePublicationPageEditAbsence';
-import PublicationEditUnresolvedComments from './pages/prototypes/PrototypePublicationPageEditAbsenceUnresolvedComments';
+import PublicationEditUnresolvedComments
+  from './pages/prototypes/PrototypePublicationPageEditAbsenceUnresolvedComments';
 import PublicationEditNew from './pages/prototypes/PrototypePublicationPageEditNew';
 import PublicationCreateNewAbsenceConfig from './pages/prototypes/PrototypePublicationPageNewAbsenceConfig';
 import PublicationCreateNewAbsenceConfigEdit from './pages/prototypes/PrototypePublicationPageNewAbsenceConfigEdit';
@@ -62,94 +64,85 @@ function App() {
 
       <ApiAuthorizationRoutes />
 
-      <AuthorizeRoute
-        exact
-        path={dashboardRoutes.adminDashboard}
-        component={AdminDashboardPage}
-      />
-
-      <AuthorizeRoute
-        exact
-        path={ApplicationPaths.LoggedOut}
-        component={SignedOutPage}
-        renderIfNotAuthenticated
-      />
-
-      <AuthorizeRoute
-        exact
-        path={publicationRoutes.createPublication.route}
-        component={CreatePublicationPage}
-      />
-
-      <AuthorizeRoute
-        exact
-        path={releaseRoutes.createReleaseRoute.route}
-        component={CreateReleasePage}
-      />
-
-      <AuthorizeRoute
-        path="/publication/:publicationId/release/:releaseId"
-        component={ManageReleasePageContainer}
-      />
+      <ProtectedRoutes>
+        <ProtectedRoute
+          exact
+          path={dashboardRoutes.adminDashboard}
+          component={AdminDashboardPage}
+        />
+        <ProtectedRoute
+          exact
+          path={publicationRoutes.createPublication.route}
+          component={CreatePublicationPage}
+        />
+        <ProtectedRoute
+          exact
+          path={releaseRoutes.createReleaseRoute.route}
+          component={CreateReleasePage}
+        />
+        <ProtectedRoute
+          path="/publication/:publicationId/release/:releaseId"
+          component={ManageReleasePageContainer}
+        />
+        <ProtectedRoute
+          exact
+          path="/documentation/"
+          component={AdminDocumentationHome}
+        />
+        <ProtectedRoute
+          exact
+          path="/documentation/content-design-standards-guide"
+          component={AdminDocumentationContentDesignStandards}
+        />
+        <ProtectedRoute
+          exact
+          path="/documentation/glossary"
+          component={AdminDocumentationGlossary}
+        />
+        <ProtectedRoute
+          exact
+          path="/documentation/style-guide"
+          component={AdminDocumentationStyle}
+        />
+        <ProtectedRoute
+          exact
+          path="/documentation/using-dashboard"
+          component={AdminDocumentationUsingDashboard}
+        />
+        <ProtectedRoute
+          exact
+          path="/documentation/create-new-release"
+          component={AdminDocumentationCreateNewRelease}
+        />
+        <ProtectedRoute
+          exact
+          path="/documentation/edit-release"
+          component={AdminDocumentationEditRelease}
+        />
+        <ProtectedRoute
+          exact
+          path="/documentation/manage-content"
+          component={AdminDocumentationManageContent}
+        />
+        <ProtectedRoute
+          exact
+          path="/documentation/manage-data"
+          component={AdminDocumentationManageData}
+        />
+        <ProtectedRoute
+          exact
+          path="/documentation/manage-data-block"
+          component={AdminDocumentationManageDataBlocks}
+        />
+        <ProtectedRoute
+          exact
+          path="/prototypes/documentation/create-new-publication"
+          component={AdminDocumentationCreateNewPublication}
+        />
+      </ProtectedRoutes>
 
       {/* Prototype Routes */}
       <Route exact path="/index" component={IndexPage} />
-
-      <AuthorizeRoute
-        exact
-        path="/documentation/"
-        component={AdminDocumentationHome}
-      />
-      <AuthorizeRoute
-        exact
-        path="/documentation/content-design-standards-guide"
-        component={AdminDocumentationContentDesignStandards}
-      />
-      <AuthorizeRoute
-        exact
-        path="/documentation/glossary"
-        component={AdminDocumentationGlossary}
-      />
-      <AuthorizeRoute
-        exact
-        path="/documentation/style-guide"
-        component={AdminDocumentationStyle}
-      />
-      <AuthorizeRoute
-        exact
-        path="/documentation/using-dashboard"
-        component={AdminDocumentationUsingDashboard}
-      />
-      <AuthorizeRoute
-        exact
-        path="/documentation/create-new-release"
-        component={AdminDocumentationCreateNewRelease}
-      />
-      <AuthorizeRoute
-        exact
-        path="/documentation/edit-release"
-        component={AdminDocumentationEditRelease}
-      />
-      <AuthorizeRoute
-        exact
-        path="/documentation/manage-content"
-        component={AdminDocumentationManageContent}
-      />
-      <AuthorizeRoute
-        exact
-        path="/documentation/manage-data"
-        component={AdminDocumentationManageData}
-      />
-      <AuthorizeRoute
-        exact
-        path="/documentation/manage-data-block"
-        component={AdminDocumentationManageDataBlocks}
-      />
-      <AuthorizeRoute
-        exact
-        path="/prototypes/documentation/create-new-publication"
-        component={AdminDocumentationCreateNewPublication}
-      />
 
       <LoginContext.Provider value={PrototypeLoginService.login()}>
         <Route exact path="/prototypes/" component={PrototypesIndexPage} />
--- src/explore-education-statistics-admin/src/components/api-authorization/AuthorizeRoute.js
+++ src/explore-education-statistics-admin/src/components/api-authorization/AuthorizeRoute.js
@@ -1,99 +1,69 @@
 /* eslint-disable */
 /* Code originally from the api-authorization folder from running
   "dotnet new react -o <output_directory_name> -au Individual"
-
-  Amendments to the original file are marked within this file with the word "HIVE".
 */
 import React from 'react';
 import { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { LoginContext } from '../Login';
 import {
-  ApplicationPaths,
-  QueryParameterNames,
+    ApplicationPaths,
+    QueryParameterNames,
 } from './ApiAuthorizationConstants';
 import authService from './AuthorizeService';
-import loginService from '@admin/services/sign-in/service';
 
 export default class AuthorizeRoute extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      ready: false,
-      // HIVE
-      // swapped "authenticated" for "user" here to support adding user profile in a Context for components
-      // further down in the component hierarchy
-      user: undefined,
-    };
-  }
-
-  componentDidMount() {
-    this._subscription = authService.subscribe(() =>
-      this.authenticationChanged(),
-    );
-    this.populateAuthenticationState();
-  }
-
-  componentWillUnmount() {
-    authService.unsubscribe(this._subscription);
-  }
-
-  render() {
-    const { ready, user } = this.state;
-    const redirectUrl = `${ApplicationPaths.Login}?${
-      QueryParameterNames.ReturnUrl
-    }=${encodeURI(window.location.href)}`;
-    if (!ready) {
-      return <div></div>;
-    } else {
-      const {
-        component: Component,
-        renderIfNotAuthenticated,
-        ...rest
-      } = this.props;
-      return (
-        <Route
-          {...rest}
-          render={props => {
-            if (user || renderIfNotAuthenticated) {
-              return (
-                // HIVE
-                // added LoginContext tags here to surround Component to allow users to be available as a
-                // Context further down in the component hierarchy
-                <>
-                  <LoginContext.Provider value={{ user }}>
-                    <Component {...props} />
-                  </LoginContext.Provider>
-                </>
-              );
-            } else {
-              return <Redirect to={redirectUrl} />;
-            }
-          }}
-        />
-      );
+        this.state = {
+            ready: false,
+            authenticated: false,
+        };
     }
-  }
 
-  async populateAuthenticationState() {
-    const authenticated = await authService.isAuthenticated();
-
-    if (authenticated) {
-      // HIVE
-      // calling our own service to retrieve user details from our own User domain model
-      await loginService
-        .getUserDetails()
-        .then(user => this.setState({ ready: true, user }))
-        .catch(() => this.setState({ ready: false, user: undefined }));
-    } else {
-      this.setState({ ready: true, user: undefined });
+    componentDidMount() {
+        this._subscription = authService.subscribe(() =>
+            this.authenticationChanged(),
+        );
+        this.populateAuthenticationState();
     }
-  }
 
-  async authenticationChanged() {
-    this.setState({ ready: false, user: undefined });
-    await this.populateAuthenticationState();
-  }
+    componentWillUnmount() {
+        authService.unsubscribe(this._subscription);
+    }
+
+    render() {
+        const { ready, authenticated } = this.state;
+        const redirectUrl = `${ApplicationPaths.Login}?${
+            QueryParameterNames.ReturnUrl
+            }=${encodeURI(window.location.href)}`;
+        if (!ready) {
+            return <div></div>;
+        } else {
+            const { component: Component, ...rest } = this.props;
+            return (
+                <Route
+                    {...rest}
+                    render={props => {
+                        if (authenticated) {
+                            return <Component {...props} />;
+                        } else {
+                            return <Redirect to={redirectUrl} />;
+                        }
+                    }}
+                />
+            );
+        }
+    }
+
+    async populateAuthenticationState() {
+        const authenticated = await authService.isAuthenticated();
+        this.setState({ ready: true, authenticated });
+    }
+
+    async authenticationChanged() {
+        this.setState({ ready: false, authenticated: false });
+        await this.populateAuthenticationState();
+    }
 }
 /* eslint-enable */
