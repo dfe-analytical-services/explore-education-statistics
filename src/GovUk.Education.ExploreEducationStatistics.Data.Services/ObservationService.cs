5ad5160e01d6cc30fa8e750d653904f043cbeaa5
Duncan Watson <duncan@hiveit.co.uk>
2019-11-04 23:47:24 +0000

    IFS-599 - replaced usages of Azure AD cookie with Authorization Bearer header

--- src/GovUk.Education.ExploreEducationStatistics.Data.Services/ObservationService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Services/ObservationService.cs
@@ -57,8 +57,9 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services
                 CreateIdListType("wardList", query.Ward);
             var filterItemListParam = CreateIdListType("filterItemList", query.Filters);
 
-            var inner = _context.Query<IdWrapper>().AsNoTracking()
-                .FromSql("EXEC dbo.FilteredObservations " +
+            var inner = _context
+                .Set<IdWrapper>()
+                .FromSqlRaw("EXEC dbo.FilteredObservations " +
                          "@subjectId," +
                          "@geographicLevel," +
                          "@timePeriodList," +
@@ -117,18 +118,6 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services
             }
 
             _logger.LogTrace("Fetched Observations by id from {Count} batches in {Time} ms", batches.Count(),
-                stopwatch.Elapsed.TotalMilliseconds);
-            stopwatch.Restart();
-
-            // Nullify all of the Observational Unit fields which are empty
-            // Do this here on the set of distinct Locations rather than when building each ObservationViewModel
-            // (since a Location may be referenced by more than one Observation) 
-            result.Select(observation => observation.Location)
-                .Distinct()
-                .ToList()
-                .ForEach(location => location.ReplaceEmptyOwnedTypeValuesWithNull());
-
-            _logger.LogTrace("Nullified all of the empty Observational Unit fields in {Time} ms",
                 stopwatch.Elapsed.TotalMilliseconds);
             stopwatch.Stop();
 
