6b9a58ecfcef67b795869ef5a89c5309332b278f
Ben Outram <boutram@worth.systems>
2019-11-08 16:11:26 +0000

    EES-204 Adding StringEnumConvert to the GeographicLevel Level property so that the level can be seen in the JSON response of DataController queries.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Services/ViewModels/Meta/ObservationalUnitGeoJsonMeta.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Services/ViewModels/Meta/ObservationalUnitGeoJsonMeta.cs
@@ -1,9 +1,12 @@
 using GovUk.Education.ExploreEducationStatistics.Data.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Services.ViewModels.Meta
 {
     public class ObservationalUnitGeoJsonMeta : LabelValue
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public GeographicLevel Level { get; set; }
         public dynamic GeoJson { get; set; }
     }
