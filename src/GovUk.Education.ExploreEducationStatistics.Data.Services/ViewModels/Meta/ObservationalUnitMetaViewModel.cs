b812f8669bee4b9958baa1193cfe3dd1c2582b3f
Ben Outram <boutram@worth.systems>
2019-11-12 17:27:32 +0000

    EES-226 Reworking TransformDuplicateObservationalUnitsWithUniqueLabels to not append the Value to the Label if the observational unit is a duplicate with the same label-value pair but the level is different. These cases will be dealt with by EES-204.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Services/ViewModels/Meta/ObservationalUnitMetaViewModel.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Services/ViewModels/Meta/ObservationalUnitMetaViewModel.cs
@@ -5,5 +5,28 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services.ViewModels.Me
     public class ObservationalUnitMetaViewModel : LabelValue
     {
         public string Level { get; set; }
+
+        protected bool Equals(ObservationalUnitMetaViewModel other)
+        {
+            return base.Equals(other) && Level == other.Level;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ObservationalUnitMetaViewModel) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = base.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Level != null ? Level.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
     }
 }
