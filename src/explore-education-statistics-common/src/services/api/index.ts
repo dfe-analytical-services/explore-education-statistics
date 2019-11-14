1a8f3d081baed219e200ea810d15c85438659e60
Duncan Watson <duncan@hiveit.co.uk>
2019-11-14 23:39:51 +0000

    EES-235 - adding Bearer auth to common services

--- src/explore-education-statistics-common/src/services/api/index.ts
+++ src/explore-education-statistics-common/src/services/api/index.ts
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { commaSeparated } from '../util/paramSerializers';
 import Client from './Client';
+
+export type AxiosConfigurer = (axios: AxiosInstance) => AxiosInstance;
 
 export const baseUrl = {
   content: process.env.CONTENT_API_BASE_URL,
@@ -8,23 +10,36 @@ export const baseUrl = {
   function: process.env.FUNCTION_API_BASE_URL,
 };
 
+// @ts-ignore
+const axiosConfigurer: AxiosConfigurer =
+  typeof window !== 'undefined' && window.AxiosConfigurer
+    ? // @ts-ignore
+      window.AxiosConfigurer
+    : instance => instance;
+
 export const contentApi = new Client(
-  axios.create({
-    baseURL: `${baseUrl.content}/`,
-    paramsSerializer: commaSeparated,
-  }),
+  axiosConfigurer(
+    axios.create({
+      baseURL: `${baseUrl.content}/`,
+      paramsSerializer: commaSeparated,
+    }),
+  ),
 );
 
 export const dataApi = new Client(
-  axios.create({
-    baseURL: `${baseUrl.data}/`,
-    paramsSerializer: commaSeparated,
-  }),
+  axiosConfigurer(
+    axios.create({
+      baseURL: `${baseUrl.data}/`,
+      paramsSerializer: commaSeparated,
+    }),
+  ),
 );
 
 export const functionApi = new Client(
-  axios.create({
-    baseURL: `${baseUrl.function}/`,
-    paramsSerializer: commaSeparated,
-  }),
+  axiosConfigurer(
+    axios.create({
+      baseURL: `${baseUrl.function}/`,
+      paramsSerializer: commaSeparated,
+    }),
+  ),
 );
