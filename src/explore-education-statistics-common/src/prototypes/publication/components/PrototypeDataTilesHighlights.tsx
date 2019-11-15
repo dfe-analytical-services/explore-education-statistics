b71e94417341da7427be8c82b98c4b0dcec91d66
Martin Waite <martin@hiveit.co.uk>
2019-11-15 14:09:57 +0000

    EES-652 Key indicator prototype

--- src/explore-education-statistics-common/src/prototypes/publication/components/PrototypeDataTilesHighlights.tsx
+++ src/explore-education-statistics-common/src/prototypes/publication/components/PrototypeDataTilesHighlights.tsx
@@ -1,6 +1,6 @@
 import Details from '@common/components/Details';
 import styles from '@common/modules/find-statistics/components//SummaryRenderer.module.scss';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   editing?: boolean;
@@ -8,8 +8,6 @@ interface Props {
   indicator?: string;
   indicatorValue?: string;
   trend?: string;
-  guidanceTitle?: string;
-  guidanceText?: string;
 }
 
 const KeyIndicator = ({
@@ -18,104 +16,133 @@ const KeyIndicator = ({
   indicator,
   indicatorValue,
   trend,
-  guidanceTitle,
-  guidanceText,
 }: Props) => {
+  const [selectedKeyIndicator, setSelectedKeyIndicator] = useState(false);
+  const [previewIndicator, setPreviewIndicator] = useState(false);
+
   return (
     <div className={styles.keyStatTile}>
       {!editing && (
         <>
           <div className={styles.keyStat}>
-            <h3 className="govuk-heading-s">{indicator}</h3>{' '}
+            <h3 className="govuk-heading-s">{selectedKeyIndicator}</h3>{' '}
             <p className="govuk-heading-xl govuk-!-margin-bottom-2">
               {indicatorValue}
             </p>
             <p className="govuk-body-s">{trend}</p>
           </div>
-          <Details summary={guidanceTitle}>{guidanceText}</Details>
+          <Details summary={`What is ${selectedKeyIndicator}?`}>
+            {`${selectedKeyIndicator} is ...`}
+          </Details>
         </>
       )}
-      {editing && (
+      {editing && previewIndicator && (
+        <>
+          <button
+            type="button"
+            className="govuk-button govuk-!-margin-bottom-1"
+            onClick={() => setPreviewIndicator(false)}
+          >
+            Edit key indicator
+          </button>
+
+          <div className={styles.keyStat} style={{ flexGrow: 0 }}>
+            <h3 className="govuk-heading-s">{selectedKeyIndicator}</h3>{' '}
+            <p className="govuk-heading-xl govuk-!-margin-bottom-2">
+              {indicatorValue}
+            </p>
+            <p className="govuk-body-s">{trend}</p>
+          </div>
+          <Details summary={`What is ${selectedKeyIndicator}?`}>
+            {`${selectedKeyIndicator} ...`}
+          </Details>
+        </>
+      )}
+      {editing && !previewIndicator && (
         <>
           <form>
             <legend className="govuk-heading-s">
-              TEST Key indicator {indicatorOrder}
+              Key indicator {indicatorOrder}
             </legend>
             <div className={styles.keyStat}>
+              <label htmlFor={`data-keystat-${indicatorOrder}`}>
+                Select data
+              </label>
               <select
-                className="govuk-select govuk-!-width-full"
+                className="govuk-select govuk-!-margin-bottom-2 govuk-!-width-full"
                 name={`data-keystat-${indicatorOrder}`}
                 id={`data-keystat-${indicatorOrder}`}
+                onBlur={e => {
+                  setSelectedKeyIndicator(e.target.value);
+                }}
               >
                 <option value={indicator} selected>
-                  {indicator}
+                  {`key-${indicator}`}
                 </option>
-                <option value="Authorised absence">Authorised abence</option>
+                <option value="Authorised absence">
+                  Key - Authorised absence
+                </option>
                 <option value="Unauthorised absence">
-                  Unauthorised abence
+                  Key - Unauthorised abence
                 </option>
-                <option value="Overall absence">Overall abence</option>
+                <option value="Overall absence">Key - Overall absence</option>
               </select>
-              <label htmlFor={`key-indicator-${indicatorOrder}`}>
-                Indicator
-              </label>
-              <input
-                type="text"
-                className="govuk-!-margin-bottom-2 govuk-input govuk-!-width-full"
-                placeholder={indicator}
-                name={`key-indicator-${indicatorOrder}`}
-                id={`key-indicator-${indicatorOrder}`}
-              />
-              <label htmlFor={`value-1-${indicatorOrder}`}>Value</label>
-              <input
-                type="text"
-                className="govuk-!-margin-bottom-2 govuk-input"
-                placeholder={indicatorValue}
-                id={`value-1-${indicatorOrder}`}
-                name={`value-1-${indicatorOrder}`}
-              />
-              <label htmlFor={`trend-1-${indicatorOrder}`}>Trend</label>
-              <input
-                type="text"
-                className="govuk-!-margin-bottom-3 govuk-input govuk-!-width-full"
-                placeholder={trend}
-                id={`trend-1-${indicatorOrder}`}
-                name={`trend-1-${indicatorOrder}`}
-              />
+
+              {selectedKeyIndicator && (
+                <>
+                  <h3 className="govuk-heading-s">{selectedKeyIndicator}</h3>
+
+                  <p className="govuk-heading-xl">{indicatorValue}</p>
+
+                  <label htmlFor={`trend-1-${indicatorOrder}`}>Trend</label>
+                  <input
+                    type="text"
+                    className="govuk-!-margin-bottom-3 govuk-input govuk-!-width-full"
+                    placeholder={trend}
+                    id={`trend-1-${indicatorOrder}`}
+                    name={`trend-1-${indicatorOrder}`}
+                  />
+                </>
+              )}
             </div>
-            <Details summary="Guidance text" open>
-              <label htmlFor={`help-title-${indicatorOrder}`}>
-                Guidance title
-              </label>
-              <input
-                type="text"
-                className="govuk-!-margin-bottom-2 govuk-input govuk-!-width-full"
-                placeholder={guidanceTitle}
-                id={`help-title-${indicatorOrder}`}
-                name={`help-title-${indicatorOrder}`}
-              />
-              <label htmlFor={`help-text-1${indicatorOrder}`}>
-                Guidance text
-              </label>
-              <textarea
-                id={`help-text-1${indicatorOrder}`}
-                className="govuk-!-margin-bottom-2 govuk-body-s govuk-textarea govuk-!-width-full"
-                rows={5}
-                placeholder={`${guidanceText} is the adipisicing elit. Dolorum hic nobis voluptas quidem fugiat enim ipsa reprehenderit nulla.`}
-              />
-            </Details>
-            <button
-              type="button"
-              className="govuk-button govuk-!-margin-top-3 govuk-!-margin-right-3"
-            >
-              Save
-            </button>
-            <button
-              type="button"
-              className="govuk-button govuk-button--secondary govuk-!-margin-top-3"
-            >
-              Remove
-            </button>
+            {selectedKeyIndicator && (
+              <>
+                <Details summary="Guidance text" open>
+                  <label htmlFor={`help-title-${indicatorOrder}`}>
+                    Guidance title
+                  </label>
+                  <input
+                    type="text"
+                    className="govuk-!-margin-bottom-2 govuk-input govuk-!-width-full"
+                    placeholder={`What is ${selectedKeyIndicator}`}
+                    id={`help-title-${indicatorOrder}`}
+                    name={`help-title-${indicatorOrder}`}
+                  />
+                  <label htmlFor={`help-text-1${indicatorOrder}`}>
+                    Guidance text
+                  </label>
+                  <textarea
+                    id={`help-text-1${indicatorOrder}`}
+                    className="govuk-!-margin-bottom-2 govuk-body-s govuk-textarea govuk-!-width-full"
+                    rows={5}
+                    placeholder={`${selectedKeyIndicator} is the adipisicing elit. Dolorum hic nobis voluptas quidem fugiat enim ipsa reprehenderit nulla.`}
+                  />
+                </Details>
+                <button
+                  type="button"
+                  className="govuk-button govuk-!-margin-top-3 govuk-!-margin-right-3"
+                  onClick={() => setPreviewIndicator(true)}
+                >
+                  Save
+                </button>
+                <button
+                  type="button"
+                  className="govuk-button govuk-button--secondary govuk-!-margin-top-3"
+                >
+                  Remove
+                </button>
+              </>
+            )}
           </form>
         </>
       )}
@@ -123,36 +150,83 @@ const KeyIndicator = ({
   );
 };
 
-const PrototypeDataTileHighlights = ({ editing }: Props) => (
-  <div className={styles.keyStatsContainer}>
-    <KeyIndicator
-      editing={editing}
-      indicatorOrder="1"
-      indicator="Overall absence"
-      indicatorValue="4.7%"
-      trend="Up from 4.7% in 2015/16"
-      guidanceTitle="What is overall absence?"
-      guidanceText="Overall absence is"
-    />
-    <KeyIndicator
-      editing={editing}
-      indicatorOrder="2"
-      indicator="Authorised absence rate"
-      indicatorValue="3.4%"
-      trend="Similar to previous years"
-      guidanceTitle="What is authorised absence?"
-      guidanceText="Authorised absence is"
-    />
-    <KeyIndicator
-      editing={editing}
-      indicatorOrder="3"
-      indicator="Unauthorised absence rate"
-      indicatorValue="1.3%"
-      trend="Up from 1.1% in 2015/16"
-      guidanceTitle="What is unauthorised absence?"
-      guidanceText="Unauthorised absence is"
-    />
-  </div>
-);
+const PrototypeDataTileHighlights = ({ editing }: Props) => {
+  const [addKeyIndicator1, setAddKeyIndicator1] = useState(false);
+  const [addKeyIndicator2, setAddKeyIndicator2] = useState(false);
+  const [addKeyIndicator3, setAddKeyIndicator3] = useState(false);
+
+  return (
+    <>
+      <div className={styles.keyStatsContainer}>
+        {addKeyIndicator1 && (
+          <KeyIndicator
+            editing={editing}
+            indicatorOrder="1"
+            indicatorValue="4.7%"
+            trend="Up from 4.7% in 2015/16"
+          />
+        )}
+        {addKeyIndicator2 && (
+          <KeyIndicator
+            editing={editing}
+            indicatorOrder="2"
+            indicatorValue="3.4%"
+            trend="Similar to previous years"
+          />
+        )}
+        {addKeyIndicator3 && (
+          <KeyIndicator
+            editing={editing}
+            indicatorOrder="3"
+            indicatorValue="1.3%"
+            trend="Up from 1.1% in 2015/16"
+            remove="tester"
+          />
+        )}
+      </div>
+
+      {editing && (
+        <>
+          {!addKeyIndicator1 && !addKeyIndicator2 && !addKeyIndicator3 && (
+            <button
+              type="button"
+              className="govuk-button"
+              onClick={() => setAddKeyIndicator1(true)}
+            >
+              Add a key indicator
+            </button>
+          )}
+          {addKeyIndicator1 && !addKeyIndicator2 && !addKeyIndicator3 && (
+            <button
+              type="button"
+              className="govuk-button"
+              onClick={() => setAddKeyIndicator2(true)}
+            >
+              Add another key indicator
+            </button>
+          )}
+          {addKeyIndicator1 && addKeyIndicator2 && !addKeyIndicator3 && (
+            <button
+              type="button"
+              className="govuk-button"
+              onClick={() => setAddKeyIndicator3(true)}
+            >
+              Add another key indicator
+            </button>
+          )}
+          {addKeyIndicator1 && addKeyIndicator2 && addKeyIndicator3 && (
+            <button
+              type="button"
+              className="govuk-button"
+              onClick={() => setAddKeyIndicator3(true)}
+            >
+              Add another key indicator
+            </button>
+          )}
+        </>
+      )}
+    </>
+  );
+};
 
 export default PrototypeDataTileHighlights;
