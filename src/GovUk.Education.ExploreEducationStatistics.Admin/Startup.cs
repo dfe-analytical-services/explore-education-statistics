6a5eb563fbae950bcecec088c4d4c0a5c37739ac
James Cutts <james@jamescutts.co.uk>
2019-11-28 17:08:18 +0000

    Adding Bearer token to admin swagger documentation

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Startup.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Startup.cs
@@ -231,6 +231,23 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin
                     new OpenApiInfo {Title = "Explore education statistics - Admin API", Version = "v1"});
                 // c.SwaggerDoc("v1", new Info {Title = "Explore education statistics - Admin API", Version = "v1"});
                 c.CustomSchemaIds((type) => type.FullName);
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Description = "Please enter into field the word 'Bearer' following by space and JWT",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                        },
+                        new[] {string.Empty}
+                    }
+                });
             });
         }
 
