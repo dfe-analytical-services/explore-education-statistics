7343b68e3a95b3e7d61ac773ac83e0586e3583fe
Martin Waite <martin@hiveit.co.uk>
2019-11-28 14:36:17 +0000

    EES-119 methodology prot

--- src/explore-education-statistics-admin/src/pages/prototypes/components/PrototypeMethodologyPageSummary.tsx
+++ src/explore-education-statistics-admin/src/pages/prototypes/components/PrototypeMethodologyPageSummary.tsx
@@ -23,6 +23,28 @@ const PrototypeMethodologySummary = () => {
           <dt className="govuk-summary-list__key">Scheduled publish date</dt>
           <dd className="govuk-summary-list__value">20 September 2019</dd>
         </div>
+
+        <div className="govuk-summary-list__row">
+          <dt className="govuk-summary-list__key">Scheduled publish date</dt>
+          <dd className="govuk-summary-list__value">20 September 2019</dd>
+        </div>
+
+        <div className="govuk-summary-list__row">
+          <dt className="govuk-summary-list__key">Methodology author</dt>
+          <dd className="govuk-summary-list__value">Ann evans</dd>
+        </div>
+
+        <div className="govuk-summary-list__row">
+          <dt className="govuk-summary-list__key">
+            Methodology primary contact
+          </dt>
+          <dd className="govuk-summary-list__value">
+            Alex Millar <br />
+            <a href="mailto:#">example@email.co.uk</a>
+            <br />
+            07954 765423
+          </dd>
+        </div>
       </dl>
     </>
   );
