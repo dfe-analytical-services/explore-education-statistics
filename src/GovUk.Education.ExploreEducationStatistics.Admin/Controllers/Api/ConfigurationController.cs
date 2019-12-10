bc636702f9169ed2921e0b2c54b607847052ed48
Lewis Toseland <lewis@hiveit.co.uk>
2019-12-10 14:55:10 +0000

    EES-15 - API endpoint added

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/ConfigurationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Controllers.Api
+{
+    public class ConfigurationController
+    {
+        private readonly IConfiguration _configuration;
+        public ConfigurationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET api/configuration/application-insights/
+        [HttpGet("api/configuration/application-insights")]
+        public string GetInsightsKey()
+        {
+            var instrumentationKey = _configuration.GetSection("ApplicationInsights").GetValue("InstrumentationKey", "Instrumentation key not found");
+            return instrumentationKey;
+        }
+    }
+}
