c903d504e2759410180dc7b32ac44adf1b256f54
James Cutts <james@hiveit.co.uk>
2020-01-02 13:30:38 +0000

    formatting

--- src/explore-education-statistics-admin/src/pages/users/UserInvitePage.tsx
+++ src/explore-education-statistics-admin/src/pages/users/UserInvitePage.tsx
@@ -80,7 +80,9 @@ const UserInvitePage = ({
               userEmail: '',
             }}
             validationSchema={Yup.object({
-              userEmail: Yup.string().required('Provide the users email').email('Provide a valid email address'),
+              userEmail: Yup.string()
+                .required('Provide the users email')
+                .email('Provide a valid email address'),
             })}
             onSubmit={submitFormHandler}
             render={(form: FormikProps<FormValues>) => {
