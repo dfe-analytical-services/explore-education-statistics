2e53439df219a1cbe102b3df06be6cbe8d69badf
Lewis Toseland <lewis@hiveit.co.uk>
2020-01-24 16:18:44 +0000

    EES-1117 - Regex temp solution

--- src/explore-education-statistics-frontend/src/modules/find-statistics/PublicationReleasePage.tsx
+++ src/explore-education-statistics-frontend/src/modules/find-statistics/PublicationReleasePage.tsx
@@ -139,9 +139,10 @@ class PublicationReleasePage extends Component<Props> {
               className="govuk-body"
               source={
                 data.summarySection.content &&
-                data.summarySection.content[0].body
+                data.summarySection.content[0].body.replace(/ {15} +/g, '')
               }
             />
+
             {data.downloadFiles && (
               <Details
                 summary="Download data files"
