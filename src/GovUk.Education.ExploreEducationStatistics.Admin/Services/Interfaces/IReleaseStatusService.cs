12916fd980e4d064b51c38dfa7c51cb99cbe4021
Ben Outram <boutram@worth.systems>
2020-01-27 12:32:28 +0000

    EES-1140 Get the first release status from table storage matching the release id because there should only be one.

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/IReleaseStatusService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/IReleaseStatusService.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using GovUk.Education.ExploreEducationStatistics.Admin.Models.Api;
 using GovUk.Education.ExploreEducationStatistics.Common.Model;
@@ -9,6 +8,6 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces
 {
     public interface IReleaseStatusService
     {
-        Task<Either<ActionResult, IEnumerable<ReleaseStatusViewModel>>> GetReleaseStatusesAsync(Guid releaseId);
+        Task<Either<ActionResult, ReleaseStatusViewModel>> GetReleaseStatusesAsync(Guid releaseId);
     }
 }
