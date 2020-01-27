a6c7d42aa7fa79a193ca9c5a6f0eeba1ec214011
James Cutts <james@hiveit.co.uk>
2020-01-27 15:54:02 +0000

    EES-1132: update methodology dashboard

--- src/explore-education-statistics-admin/src/pages/methodology/ListMethodologyPages.tsx
+++ src/explore-education-statistics-admin/src/pages/methodology/ListMethodologyPages.tsx
@@ -3,6 +3,8 @@ import Page from '@admin/components/Page';
 import methodologyService from '@admin/services/methodology/service';
 import { MethodologyStatus } from '@admin/services/methodology/types';
 import RelatedInformation from '@common/components/RelatedInformation';
+import Tabs from '@common/components/Tabs';
+import TabsSection from '@common/components/TabsSection';
 import React, { useEffect, useState } from 'react';
 
 interface Model {
@@ -39,51 +41,58 @@ const ListMethodologyPages = () => {
         </div>
       </div>
 
-      {model && (
-        <table className="govuk-table">
-          <caption className="govuk-table__caption">
-            Current methodologies
-          </caption>
-          <thead className="govuk-table__head">
-            <tr className="govuk-table__row">
-              <th scope="col" className="govuk-table__header">
-                Methodology
-              </th>
-              <th scope="col" className="govuk-table__header">
-                Status
-              </th>
-              <th scope="col" className="govuk-table__header">
-                Publications
-              </th>
-            </tr>
-          </thead>
-          <tbody className="govuk-table__body">
-            {model.methodologies.map(methodology => (
-              <tr className="govuk-table__row" key={methodology.id}>
-                <td className="govuk-table__header">
-                  <Link to={`/methodology/${methodology.id}`}>
-                    {methodology.title}
-                  </Link>
-                </td>
-                <td className="govuk-table__cell">
-                  <strong className="govuk-tag">{methodology.status}</strong>
-                </td>
-                <td className="govuk-table__cell">
-                  <ul className="govuk-list">
-                    {methodology.publications.map(publication => (
-                      <li key={publication.id}>{publication.title}</li>
-                    ))}
-                  </ul>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-
-      <Link to="/methodology/create" className="govuk-button">
-        Create new methodology
-      </Link>
+      <Tabs id="methodologyTabs">
+        <TabsSection id="manage-methodology" title="Manage methodology">
+          {model && (
+            <table className="govuk-table">
+              <thead className="govuk-table__head">
+                <tr className="govuk-table__row">
+                  <th scope="col" className="govuk-table__header">
+                    Methodology
+                  </th>
+                  <th scope="col" className="govuk-table__header">
+                    Status
+                  </th>
+                  <th scope="col" className="govuk-table__header">
+                    Publications
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="govuk-table__body">
+                {model.methodologies.map(methodology => (
+                  <tr className="govuk-table__row" key={methodology.id}>
+                    <td className="govuk-table__header">
+                      <Link to={`/methodology/${methodology.id}`}>
+                        {methodology.title}
+                      </Link>
+                    </td>
+                    <td className="govuk-table__cell">
+                      <strong className="govuk-tag">
+                        {methodology.status}
+                      </strong>
+                    </td>
+                    <td className="govuk-table__cell">
+                      <ul className="govuk-list">
+                        {methodology.publications.map(publication => (
+                          <li key={publication.id}>{publication.title}</li>
+                        ))}
+                      </ul>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+          <Link to="/methodology/create" className="govuk-button">
+            Create new methodology
+          </Link>
+        </TabsSection>
+        <TabsSection id="draft-methodology" title="Draft methodology (0)">
+          <div className="govuk-inset-text">
+            There is currently no draft methodology
+          </div>
+        </TabsSection>
+      </Tabs>
     </Page>
   );
 };
