29938757f1cc11d71f12abdb3cc3960119e37c5c
Ben Outram <boutram@worth.systems>
2020-01-29 17:09:23 +0000

    EES-753 Removing unused method

--- src/GovUk.Education.ExploreEducationStatistics.Publisher/ViewModels/ContentSectionViewModel.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher/ViewModels/ContentSectionViewModel.cs
@@ -1,32 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using GovUk.Education.ExploreEducationStatistics.Content.Model;
 
 namespace GovUk.Education.ExploreEducationStatistics.Publisher.ViewModels
 {
     public class ContentSectionViewModel
     {
-        public ContentSectionViewModel()
-        {
-        }
-
-        public static ContentSectionViewModel ToViewModel(ContentSection section)
-        {
-            var model = new ContentSectionViewModel
-            {
-                Id = section.Id,
-                Caption = section.Caption,
-                Content = section.Content?.OrderBy(contentBlock => contentBlock.Order).ToList(),
-                Heading = section.Heading,
-                Order = section.Order
-            };
-            
-            return model;
-        }
-
         public Guid Id { get; set; }
-        
+
         public int Order { get; set; }
 
         public string Heading { get; set; }
