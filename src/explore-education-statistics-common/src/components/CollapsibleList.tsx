03b23ab9336a1cde846489aa45f1f8ce7805759b
Lewis Toseland <lewis@hiveit.co.uk>
2020-01-30 11:16:56 +0000

    PP-161 - Requested changes made

--- src/explore-education-statistics-common/src/components/CollapsibleList.tsx
+++ src/explore-education-statistics-common/src/components/CollapsibleList.tsx
@@ -13,33 +13,49 @@ interface Props {
   children: ReactNode;
   collapseAfter?: number;
   isCollapsed?: boolean;
+  ordered?: boolean;
+  listStyle?: 'none' | 'number' | 'bullet';
 }
 
 const CollapsibleList = ({
   children,
   collapseAfter = 5,
   isCollapsed = true,
+  ordered = false,
+  listStyle = 'none',
 }: Props) => {
   const [collapsed, setCollapsed] = useState<boolean>(isCollapsed);
 
   if (Children.count(children) > collapseAfter) {
+    const listItems = Children.map(children, (child, i) => {
+      if (i >= collapseAfter - 2) {
+        if (isValidElement(child)) {
+          return cloneElement(child, {
+            ...child.props,
+            'aria-hidden': false,
+            className: classNames(
+              child.props.className,
+              collapsed && 'govuk-visually-hidden',
+            ),
+          });
+        }
+      }
+      return child;
+    });
+
+    const listClasses = classNames(
+      'govuk-list',
+      styles.listContainer,
+      listStyle === 'number' && 'govuk-list--number',
+      listStyle === 'bullet' && 'govuk-list--bullet',
+    );
     return (
       <>
-        {Children.map(children, (child, i) => {
-          if (i >= collapseAfter - 2) {
-            if (isValidElement(child)) {
-              return cloneElement(child, {
-                ...child.props,
-                'aria-hidden': false,
-                className: classNames(
-                  child.props.className,
-                  collapsed && 'govuk-visually-hidden',
-                ),
-              });
-            }
-          }
-          return child;
-        })}
+        {ordered ? (
+          <ol className={listClasses}>{listItems}</ol>
+        ) : (
+          <ul className={listClasses}>{listItems}</ul>
+        )}
         <div className={styles.hidePrint} aria-hidden="true">
           {collapsed && Children.count(children) - (collapseAfter - 2) && (
             <strong>
--- src/explore-education-statistics-common/src/modules/table-tool/components/Footnote.tsx
+++ src/explore-education-statistics-common/src/modules/table-tool/components/Footnote.tsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import { FullTableMeta } from '@common/modules/full-table/types/fullTable';
 import CollapsibleList from '@common/components/CollapsibleList';
-import classNames from 'classnames';
-import styles from './Footnote.module.scss';
 
 export interface FootnoteProps {
   content: FullTableMeta['footnotes'];
 }
 
 const Footnote = ({ content }: FootnoteProps) => {
-  const footnoteList = content.map(function createFootnotes(footnotes, index) {
-    return (
-      <li key={footnotes.id}>
-        <span className="govuk-!-font-weight-bold">{index + 1}. </span>
-        {footnotes.label}
-      </li>
-    );
+  const footnoteList = content.map(function createFootnotes(footnotes) {
+    return <li key={footnotes.id}>{footnotes.label}</li>;
   });
 
   return (
-    <ol className={classNames('govuk-list', styles.footnotesContainer)}>
-      <CollapsibleList>{footnoteList}</CollapsibleList>
-    </ol>
+    <CollapsibleList ordered listStyle="number">
+      {footnoteList}
+    </CollapsibleList>
   );
 };
 
