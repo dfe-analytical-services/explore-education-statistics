13c80649353a8ac03f8ca2e95c29a9421b1cc38b
Lewis Toseland <lewis@hiveit.co.uk>
2020-01-31 16:01:25 +0000

    PP-244-Duplicate methodologies removed

--- src/explore-education-statistics-frontend/src/modules/methodologies/components/MethodologyList.tsx
+++ src/explore-education-statistics-frontend/src/modules/methodologies/components/MethodologyList.tsx
@@ -10,17 +10,25 @@ function MethodologyList({ publications }: Props) {
   return (
     <ul className="govuk-list govuk-list--bullet">
       {publications.length > 0 ? (
-        publications.map(({ title, slug }) => (
-          <>
-            <h3 className="govuk-heading-s govuk-!-margin-bottom-0">{title}</h3>
-            <div className="govuk-!-margin-bottom-1">
-              <Link to={`/methodology/${slug}`}>
-                View methodology
-                <span className="govuk-visually-hidden">for {title}</span>
-              </Link>
-            </div>
-          </>
-        ))
+        publications.map(({ title, slug, id }, index, array) => {
+          const previousItem = array[index - 1];
+          if (index === 0 || previousItem.slug !== slug) {
+            return (
+              <div key={id}>
+                <h3 className="govuk-heading-s govuk-!-margin-bottom-0">
+                  {title}
+                </h3>
+                <div className="govuk-!-margin-bottom-1">
+                  <Link to={`/methodology/${slug}`}>
+                    View methodology
+                    <span className="govuk-visually-hidden">for {title}</span>
+                  </Link>
+                </div>
+              </div>
+            );
+          }
+          return null;
+        })
       ) : (
         <></>
       )}
