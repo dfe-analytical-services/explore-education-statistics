4d24a9c9b0dc9bbe6cf73abc033ac985fda8a0ca
Lewis Toseland <lewis@hiveit.co.uk>
2020-02-04 11:01:38 +0000

    PP-159 - Requested changes made

--- src/explore-education-statistics-common/src/modules/table-tool/components/DataTableCaption.tsx
+++ src/explore-education-statistics-common/src/modules/table-tool/components/DataTableCaption.tsx
@@ -6,17 +6,20 @@ import {
 import React, { useState } from 'react';
 import { FullTableMeta } from '@common/modules/full-table/types/fullTable';
 import ButtonText from '@common/components/ButtonText';
+import classNames from 'classnames';
 
 export function generateTableTitle({
   timePeriodRange,
   locations,
   subjectName,
   publicationName,
+  expanded,
 }: {
   timePeriodRange: FullTableMeta['timePeriodRange'];
   locations: FullTableMeta['locations'];
   subjectName: FullTableMeta['subjectName'];
   publicationName: FullTableMeta['publicationName'];
+  expanded: boolean;
 }) {
   let timePeriodString = '';
 
@@ -31,10 +34,16 @@ export function generateTableTitle({
   }
 
   const locationsString = ` in ${commaList(
-    locations.map(location => location.label),
+    locations.length > 10 && !expanded
+      ? locations.slice(0, 10).map(location => location.label)
+      : locations.map(location => location.label),
   )}`;
 
-  return `Table showing '${subjectName}' from '${publicationName}'${locationsString}${timePeriodString}`;
+  return `Table showing '${subjectName}' from '${publicationName}'${
+    !expanded && locations.length > 10
+      ? `${locationsString} and ${locations.length} other locations...`
+      : locationsString
+  }${timePeriodString}`;
 }
 
 interface Props {
@@ -52,32 +61,28 @@ const DataTableCaption = ({
   subjectName,
   publicationName,
 }: Props) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
   const caption = generateTableTitle({
     timePeriodRange,
     locations,
     subjectName,
     publicationName,
+    expanded,
   });
 
-  const [expanded, setExpanded] = useState<boolean>(false);
-  const captionLimit = 300;
   return (
     <>
-      <strong id={id}>
-        {caption.length > captionLimit && !expanded
-          ? `${caption.substring(0, captionLimit)}...`
-          : caption}{' '}
-      </strong>
-      {caption.length > captionLimit && (
+      <strong id={id}>{caption}</strong>
+      {locations.length > 10 && (
         <ButtonText
+          className={classNames('govuk-!-display-block govuk-!-margin-top-2')}
           onClick={() => {
             return expanded ? setExpanded(false) : setExpanded(true);
           }}
         >
-          {expanded ? 'View less' : 'View more'}
+          {`${expanded ? 'Hide' : 'View'} full table title`}
         </ButtonText>
       )}
-
       <ul>
         <li>
           <strong>n/a</strong> - value matching this criteria could not be
