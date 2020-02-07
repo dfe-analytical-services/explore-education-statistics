d456565e7fc51ba06654b736b56c40af9374a7d7
Duncan Watson <duncan@hiveit.co.uk>
2020-02-07 16:28:51 +0000

    EES-1247 - reinstating MimeTypes utility

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/FileTypeService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/FileTypeService.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Common.Utils;
 using HeyRed.Mime;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.FileProviders;
@@ -21,7 +22,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Services
 
         public string GetMimeType(IFormFile file)
         {
-            return GuessMagicInfo(file.OpenReadStream(), MagicOpenFlags.MAGIC_MIME_TYPE);
+            return file.OpenReadStream().GetFileType().Mime;
         }
         
         public string GetMimeEncoding(IFormFile file)
@@ -31,20 +32,20 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Services
         
         public bool HasMatchingMimeType(IFormFile file, IEnumerable<Regex> mimeTypes)
         {
-            var mimeType = GuessMagicInfo(file.OpenReadStream(), MagicOpenFlags.MAGIC_MIME_TYPE);
+            var mimeType = GetMimeType(file);
             return mimeTypes.Any(pattern => pattern.Match(mimeType).Success);
         }
         
         public bool HasMatchingEncodingType(IFormFile file, IEnumerable<string> encodingTypes)
         {
-            var encodingType = GuessMagicInfo(file.OpenReadStream(), MagicOpenFlags.MAGIC_MIME_ENCODING);
+            var encodingType = GetMimeEncoding(file);
             return encodingTypes.Any(pattern => pattern.Equals(encodingType));
         }
         
         private string GuessMagicInfo(Stream fileStream, MagicOpenFlags flag)
         {
             using var reader = new StreamReader(fileStream);
-            var magic = new Magic(flag, _magicFilePath);
+            var magic = new Magic(flag);
             var bufferSize = reader.BaseStream.Length >= 1024 ? 1024 : (int) reader.BaseStream.Length;
             return magic.Read(reader.BaseStream, bufferSize);
         }
