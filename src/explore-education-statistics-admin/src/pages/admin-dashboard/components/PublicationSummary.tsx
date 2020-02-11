6bcef499c7a8db983c9bc81476b29faef2cab9ae
Stuart Bennett <bennettstuart92@gmail.com>
2020-02-11 13:46:46 +0000

    initial publication/assign-methodology page

--- src/explore-education-statistics-admin/src/pages/admin-dashboard/components/PublicationSummary.tsx
+++ src/explore-education-statistics-admin/src/pages/admin-dashboard/components/PublicationSummary.tsx
@@ -1,5 +1,6 @@
 import ButtonLink from '@admin/components/ButtonLink';
 import Link from '@admin/components/Link';
+import ThemeAndTopicContext from '@admin/components/ThemeAndTopicContext';
 import ReleaseSummary from '@admin/pages/admin-dashboard/components/ReleaseSummary';
 import { getReleaseSummaryLabel } from '@admin/pages/release/util/releaseSummaryUtil';
 import releaseRoutes, { summaryRoute } from '@admin/routes/edit-release/routes';
@@ -7,13 +8,14 @@ import { AdminDashboardPublication } from '@admin/services/dashboard/types';
 import SummaryList from '@common/components/SummaryList';
 import SummaryListItem from '@common/components/SummaryListItem';
 import { formatTestId } from '@common/util/test-utils';
-import React from 'react';
+import React, { useContext } from 'react';
 
 export interface Props {
   publication: AdminDashboardPublication;
 }
 
 const PublicationSummary = ({ publication }: Props) => {
+  const { selectedThemeAndTopic } = useContext(ThemeAndTopicContext);
   return (
     <>
       <SummaryList>
@@ -74,24 +76,12 @@ const PublicationSummary = ({ publication }: Props) => {
               Create new release
             </ButtonLink>
           )}
-
-          {publication.methodology && (
-            <ButtonLink
-              to={`/methodology/${publication.methodology.id}`}
-              className="govuk-button--secondary"
-            >
-              Edit methodology
-            </ButtonLink>
-          )}
-
-          {!publication.methodology && (
-            <ButtonLink
-              to={`/methodology/create?publicationId=${publication.id}`}
-              className="govuk-button--secondary"
-            >
-              Add methodology
-            </ButtonLink>
-          )}
+          <ButtonLink
+            to={`/theme/${selectedThemeAndTopic.theme.id}/topic/${selectedThemeAndTopic.topic.id}/publication/${publication.id}/assign-methodology`}
+            className="govuk-button--secondary"
+          >
+            {!publication.methodology ? 'Add' : 'Edit'} methodology
+          </ButtonLink>
         </SummaryListItem>
       </SummaryList>
     </>
