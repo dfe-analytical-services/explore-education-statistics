9445c06e77e38563b407ddab0a98d60d8d360051
Ben Outram <ben@hiveit.co.uk>
2020-02-14 17:44:45 +0000

    EES-1317 Adding TimeIdentifier and Year to Release for ordering.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200214173736_AddTimeIdentifierAndYear.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    public partial class AddTimeIdentifierAndYear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Release");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimeIdentifier",
+                table: "Release",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Release",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeIdentifier",
+                table: "Release");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Release");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Release",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
