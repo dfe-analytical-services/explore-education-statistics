7ff07e8e17a1217e2a178a03dcd03b51f2f2dd5c
Nicholas Tsim <nick@hiveit.co.uk>
2020-02-20 13:39:32 +0000

    Add `loading` and `children` props to LoadingSpinner

--- src/explore-education-statistics-common/src/components/LoadingSpinner.tsx
+++ src/explore-education-statistics-common/src/components/LoadingSpinner.tsx
@@ -1,51 +1,59 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import styles from './LoadingSpinner.module.scss';
 
 interface Props {
   alert?: boolean;
   className?: string;
+  children?: ReactNode;
   hideText?: boolean;
   text?: string;
   size?: number;
   inline?: boolean;
+  loading?: boolean;
   overlay?: boolean;
 }
 
 const LoadingSpinner = ({
   alert = false,
   className,
+  children,
   hideText = false,
   inline = false,
+  loading = true,
   overlay = false,
   size = 80,
   text,
 }: Props) => {
   return (
     <>
-      <div
-        className={classNames({
-          [styles.noInlineFlex]: size >= 80 || !inline,
-          [styles.container]: true,
-          [styles.overlay]: overlay,
-        })}
-      >
+      {loading ? (
         <div
-          className={classNames(styles.spinner, className)}
-          style={{
-            height: `${size}px`,
-            width: `${size}px`,
-            borderWidth: `${size * 0.15}px`,
-          }}
-        />
-
-        <span
-          role={alert ? 'alert' : undefined}
-          className={classNames({ 'govuk-visually-hidden': hideText })}
+          className={classNames({
+            [styles.noInlineFlex]: size >= 80 || !inline,
+            [styles.container]: true,
+            [styles.overlay]: overlay,
+          })}
         >
-          {text}
-        </span>
-      </div>
+          <div
+            className={classNames(styles.spinner, className)}
+            style={{
+              height: `${size}px`,
+              width: `${size}px`,
+              borderWidth: `${size * 0.15}px`,
+            }}
+          />
+
+          <span
+            role={alert ? 'alert' : undefined}
+            className={classNames({ 'govuk-visually-hidden': hideText })}
+          >
+            {text}
+          </span>
+        </div>
+      ) : (
+        children
+      )}
     </>
   );
 };
