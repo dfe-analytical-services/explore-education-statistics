376710699d861c6c3a081f30d7c0928a49641a9f
Ben Outram <ben@hiveit.co.uk>
2020-02-26 13:44:13 +0000

    EES-1126 Adding TODO comment to live check.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/Release.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Release.cs
@@ -16,6 +16,11 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Model
         public TimeIdentifier TimeIdentifier { get; set; }
         public int Year { get; set; }
 
+        // TODO EES-1417 ReleaseDate originates from the scheduled publish date of the Content Release set in the Admin.
+        // TODO (See the Release summary tab). It gets copied to the Statistics db as the ReleaseDate field.
+        // TODO This live check should use an actual Published date (or flag) that is set once content is published
+        // TODO by the PublishReleaseContentFunction. See method PublishingService.PublishReleaseContentAsync
+        // TODO where the Published date on a Release is set in the Content db for the same Live check there.
         public bool Live => Compare(UtcNow, ReleaseDate) > 0;
     }
 }
