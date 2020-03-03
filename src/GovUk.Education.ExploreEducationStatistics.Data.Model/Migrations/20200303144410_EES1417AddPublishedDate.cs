bc6d7b247faf49fa2c9042a80ce666ad79bae6eb
Ben Outram <ben@hiveit.co.uk>
2020-03-03 14:58:40 +0000

    EES-1417 Preparation for new Published field on Statistics Release data.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200303144410_EES1417AddPublishedDate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    public partial class EES1417AddPublishedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Release");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Published",
+                table: "Release",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Published",
+                table: "Release");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Release",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
