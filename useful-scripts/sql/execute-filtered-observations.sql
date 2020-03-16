50cfd46aafb39798d987f4d91b43c1f50f0327b1
Ben Outram <ben@hiveit.co.uk>
2020-03-16 13:35:13 +0000

    Formatting

--- useful-scripts/sql/execute-filtered-observations.sql
+++ useful-scripts/sql/execute-filtered-observations.sql
@@ -1,64 +1,64 @@
 begin
-	declare @subjectId uniqueidentifier = '568576e5-d386-450e-a8db-307b7061d0d8'
+    declare @subjectId uniqueidentifier = '568576e5-d386-450e-a8db-307b7061d0d8'
 
-	declare @geographicLevel nvarchar(6) = 'SCH'
+    declare @geographicLevel nvarchar(6) = 'SCH'
 
-	declare @timePeriodList TimePeriodListType
-	insert @timePeriodList values (2016, 'AY')
+    declare @timePeriodList TimePeriodListType
+    insert @timePeriodList values (2016, 'AY')
 
-	declare @countriesList IdListVarcharType
+    declare @countriesList IdListVarcharType
 
-	declare @institutionsList IdListVarcharType
+    declare @institutionsList IdListVarcharType
 
-	declare @localAuthorityList IdListVarcharType
+    declare @localAuthorityList IdListVarcharType
 
-	declare @localAuthorityDistrictList IdListVarcharType
+    declare @localAuthorityDistrictList IdListVarcharType
 
-	declare @localAuthorityOldCodeList IdListVarcharType
+    declare @localAuthorityOldCodeList IdListVarcharType
 
-	declare @localEnterprisePartnershipsList IdListVarcharType
+    declare @localEnterprisePartnershipsList IdListVarcharType
 
-	declare @mayoralCombinedAuthoritiesList IdListVarcharType
+    declare @mayoralCombinedAuthoritiesList IdListVarcharType
 
-	declare @multiAcademyTrustList IdListVarcharType
+    declare @multiAcademyTrustList IdListVarcharType
 
-	declare @opportunityAreasList IdListVarcharType
+    declare @opportunityAreasList IdListVarcharType
 
-	declare @parliamentaryConstituenciesList IdListVarcharType
+    declare @parliamentaryConstituenciesList IdListVarcharType
 
-	declare @regionsList IdListVarcharType
-	insert @regionsList values ('E13000001')
+    declare @regionsList IdListVarcharType
+    insert @regionsList values ('E13000001')
 
-	declare @rscRegionsList IdListVarcharType
+    declare @rscRegionsList IdListVarcharType
 
-	declare @sponsorList IdListVarcharType
+    declare @sponsorList IdListVarcharType
 
-	declare @wardsList IdListVarcharType
+    declare @wardsList IdListVarcharType
 
-	declare @planningAreasList IdListVarcharType
+    declare @planningAreasList IdListVarcharType
 
-	declare @filterItemList IdListGuidType
+    declare @filterItemList IdListGuidType
 
-	declare @result int
-	exec
-		@result = FilteredObservations
-			@subjectId,
-			@geographicLevel,
-			@timePeriodList,
-			@countriesList,
-			@institutionsList,
-			@localAuthorityList,
-			@localAuthorityDistrictList,
-		    @localAuthorityOldCodeList,
-			@localEnterprisePartnershipsList,
-			@mayoralCombinedAuthoritiesList,
-			@multiAcademyTrustList,
-			@opportunityAreasList,
-			@parliamentaryConstituenciesList,
-			@regionsList,
-			@rscRegionsList,
-			@sponsorList,
-			@wardsList,
-			@planningAreasList,
-			@filterItemList
+    declare @result int
+    exec
+        @result = FilteredObservations
+                  @subjectId,
+                  @geographicLevel,
+                  @timePeriodList,
+                  @countriesList,
+                  @institutionsList,
+                  @localAuthorityList,
+                  @localAuthorityDistrictList,
+                  @localAuthorityOldCodeList,
+                  @localEnterprisePartnershipsList,
+                  @mayoralCombinedAuthoritiesList,
+                  @multiAcademyTrustList,
+                  @opportunityAreasList,
+                  @parliamentaryConstituenciesList,
+                  @regionsList,
+                  @rscRegionsList,
+                  @sponsorList,
+                  @wardsList,
+                  @planningAreasList,
+                  @filterItemList
 end
