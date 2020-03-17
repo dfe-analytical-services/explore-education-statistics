d2510b31c52bbfe541498dcff3813f824131bbd4
Mark Youngman <mark@hiveit.co.uk>
2020-03-17 08:28:40 +0000

    EES-1515 Small tweak to Manage data blocks tab

--- src/explore-education-statistics-admin/src/pages/release/edit-release/manage-datablocks/ReleaseManageDataBlocksPage.tsx
+++ src/explore-education-statistics-admin/src/pages/release/edit-release/manage-datablocks/ReleaseManageDataBlocksPage.tsx
@@ -194,28 +194,29 @@ const ReleaseManageDataBlocksPage = () => {
   return canUpdateRelease ? (
     <>
       {dataBlockOptions.length > 0 && (
-        <FormSelect
-          id="selectDataBlock"
-          name="selectDataBlock"
-          label="Select an existing data block to edit or create a new one"
-          onChange={e => {
-            setSelectedDataBlock(e.target.value);
-          }}
-          order={[]}
-          optGroups={{
-            'Create data block': [
-              {
-                label: 'Create new data block',
-                value: '',
-              },
-            ],
-            'Edit existing': dataBlockOptions,
-          }}
-          value={selectedDataBlock}
-        />
+        <>
+          <FormSelect
+            id="selectDataBlock"
+            name="selectDataBlock"
+            label="Select an existing data block to edit or create a new one"
+            onChange={e => {
+              setSelectedDataBlock(e.target.value);
+            }}
+            order={[]}
+            optGroups={{
+              'Create data block': [
+                {
+                  label: 'Create new data block',
+                  value: '',
+                },
+              ],
+              'Edit existing': dataBlockOptions,
+            }}
+            value={selectedDataBlock}
+          />
+          <hr />
+        </>
       )}
-
-      <hr />
 
       <div style={{ position: 'relative' }}>
         {(isLoading || isSaving) && (
