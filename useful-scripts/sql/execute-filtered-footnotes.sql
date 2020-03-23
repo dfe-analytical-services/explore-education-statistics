49f3a7ec5be032c0d3ce76d35500daf25489808a
Ben Outram <ben@hiveit.co.uk>
2020-03-23 11:29:15 +0000

    Removing comment

--- useful-scripts/sql/execute-filtered-footnotes.sql
+++ useful-scripts/sql/execute-filtered-footnotes.sql
@@ -1,5 +1,5 @@
 begin
-    declare @subjectId uniqueidentifier = '803FBF56-600F-490F-8409-6413A891720D' -- ReleaseId 4FA4FE8E-9A15-46BB-823F-49BF8E0CDEC5
+    declare @subjectId uniqueidentifier = '803FBF56-600F-490F-8409-6413A891720D'
 
     declare @filterItemList IdListGuidType
     insert @filterItemList values ('0520D3CB-EE9F-4CC0-BAF7-08D78F6F2C4D'), ('A9FE9FA6-E91F-460B-A0B1-66877B97C581')
