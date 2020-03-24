f77d905205e5ebffabf69033763854976ab7c97c
Mark Youngman <mark@hiveit.co.uk>
2020-03-24 14:29:08 +0000

    BAU-424 Comment out DELETEs from FootnoteData.sql to avoid deleting manually added footnotes accidentally

--- useful-scripts/sql/FootnoteData.sql
+++ useful-scripts/sql/FootnoteData.sql
@@ -97,12 +97,12 @@ END CATCH
 GO;
 
 
-DELETE FROM SubjectFootnote WHERE 1=1;
-DELETE FROM IndicatorFootnote WHERE 1=1;
-DELETE FROM FilterItemFootnote WHERE 1=1;
-DELETE FROM FilterGroupFootnote WHERE 1=1;
-DELETE FROM FilterFootnote WHERE 1=1;
-DELETE FROM Footnote WHERE 1=1;
+-- DELETE FROM SubjectFootnote WHERE 1=1;
+-- DELETE FROM IndicatorFootnote WHERE 1=1;
+-- DELETE FROM FilterItemFootnote WHERE 1=1;
+-- DELETE FROM FilterGroupFootnote WHERE 1=1;
+-- DELETE FROM FilterFootnote WHERE 1=1;
+-- DELETE FROM Footnote WHERE 1=1;
 
 
 --
