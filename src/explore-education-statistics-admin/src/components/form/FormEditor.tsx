fc4ecc52db33a1d2075d56faa7250a31234258d9
Nicholas Tsim <nicholastsim@gmail.com>
2020-03-27 22:32:52 +0000

    refactor: cleanup and simplify FormEditor styling

--- src/explore-education-statistics-admin/src/components/form/FormEditor.tsx
+++ src/explore-education-statistics-admin/src/components/form/FormEditor.tsx
@@ -4,8 +4,10 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 // No types generated for ckeditor 5 for react
 // @ts-ignore
 import CKEditor from '@ckeditor/ckeditor5-react';
+import Button from '@common/components/Button';
 import ErrorMessage from '@common/components/ErrorMessage';
 import ModalConfirm from '@common/components/ModalConfirm';
+import useToggle from '@common/hooks/useToggle';
 import classNames from 'classnames';
 import marked from 'marked';
 import React, { ChangeEvent, useCallback, useState } from 'react';
@@ -16,7 +18,6 @@ const turndownService = new Turndown();
 export interface FormEditorProps {
   allowHeadings?: boolean;
   canDelete?: boolean;
-  value: string;
   editable?: boolean;
   error?: string;
   hideLabel?: boolean;
@@ -27,6 +28,7 @@ export interface FormEditorProps {
   resolveComments?: boolean;
   toolbarConfig?: string[];
   useMarkdown?: boolean;
+  value: string;
   onChange: (content: string) => void;
   onDelete?: () => void;
 }
@@ -66,169 +68,174 @@ const FormEditor = ({
   onDelete,
   useMarkdown = false,
 }: FormEditorProps) => {
-  const [editing, setEditing] = useState(false);
-  const [saved, setSaved] = useState(false);
+  const [editing, toggleEditing] = useToggle(false);
 
-  const [temporaryContent, setTemporaryContent] = useState(() => {
+  const [content, setContent] = useState(() => {
     if (useMarkdown) {
       return marked(value);
     }
 
     return value;
   });
-  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [showConfirmation, toggleConfirmation] = useToggle(false);
 
   const handleSave = useCallback(() => {
-    setEditing(false);
-    let contentChangeContent = temporaryContent;
+    toggleEditing.off();
+
+    let nextValue = content;
 
     if (useMarkdown) {
-      contentChangeContent = turndownService.turndown(contentChangeContent);
+      nextValue = turndownService.turndown(nextValue);
     }
 
-    onChange(contentChangeContent);
-    setSaved(true);
-  }, [onChange, temporaryContent, useMarkdown]);
+    onChange(nextValue);
+  }, [onChange, content, toggleEditing, useMarkdown]);
+
+  const handleCancel = useCallback(() => {
+    toggleEditing.off();
+
+    if (useMarkdown) {
+      return setContent(marked(value));
+    }
+
+    return setContent(value);
+  }, [toggleEditing, useMarkdown, value]);
 
   return (
     <div>
-      <span
-        className={classNames('govuk-label', {
-          'govuk-visually-hidden': hideLabel,
-        })}
-      >
-        {label}
-      </span>
-
-      {hint && (
-        <span id={`${id}-hint`} className="govuk-hint">
-          {hint}
-        </span>
-      )}
-
-      {error && <ErrorMessage id={`${id}-error`}>{error}</ErrorMessage>}
-
       <ModalConfirm
-        onConfirm={() => {
-          if (onDelete) onDelete();
-          setShowConfirmation(false);
-        }}
-        onExit={() => {
-          setShowConfirmation(false);
-        }}
-        onCancel={() => {
-          setShowConfirmation(false);
-        }}
         title="Delete section"
         mounted={showConfirmation}
+        onConfirm={() => {
+          if (onDelete) {
+            onDelete();
+          }
+
+          toggleConfirmation.off();
+        }}
+        onExit={toggleConfirmation.off}
+        onCancel={toggleConfirmation.off}
       >
         <p>Are you sure you want to delete this section?</p>
       </ModalConfirm>
 
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/interactive-supports-focus */}
-      <div
-        role="button"
-        className={classNames({
-          [styles.editableContentEditing]: editable && editing,
-          [styles.editableContent]: editable && !editing,
-          [styles.unsaved]: editable && !saved,
-        })}
-        onClick={() => {
-          if (!editing) {
-            setEditing(editable === true);
-          }
-        }}
-        tabIndex={undefined}
-      >
-        {editable && (
-          <div className={styles.editableButton}>
-            {editing ? (
-              <button
-                className="govuk-button"
-                onClick={handleSave}
-                type="button"
-              >
-                Save
-              </button>
-            ) : (
-              <div className={styles.editableButtonContent}>
-                <span className="govuk-button govuk-button--secondary govuk-body-s govuk-!-margin-bottom-0">
-                  Edit
-                </span>
-                {canDelete && (
-                  <>
-                    <button
-                      type="button"
-                      onClick={e => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        setShowConfirmation(true);
-                      }}
-                      className="govuk-button govuk-button--warning govuk-body-s govuk-!-margin-bottom-0"
-                    >
-                      Delete
-                    </button>
-                  </>
-                )}
-              </div>
+      {editing ? (
+        <>
+          <span
+            className={classNames('govuk-label', {
+              'govuk-visually-hidden': hideLabel,
+            })}
+          >
+            {label}
+          </span>
+
+          {hint && (
+            <span id={`${id}-hint`} className="govuk-hint">
+              {hint}
+            </span>
+          )}
+
+          {error && <ErrorMessage id={`${id}-error`}>{error}</ErrorMessage>}
+
+          <div className={styles.editor}>
+            <CKEditor
+              editor={ClassicEditor}
+              config={{
+                toolbar: toolbarConfig,
+                heading: allowHeadings && {
+                  options: [
+                    {
+                      model: 'paragraph',
+                      title: 'Paragraph',
+                      class: 'ck-heading_paragraph',
+                    },
+                    {
+                      model: 'heading3',
+                      view: 'h3',
+                      title: 'Heading 3',
+                      class: 'ck-heading_heading3',
+                    },
+                    {
+                      model: 'heading4',
+                      view: 'h4',
+                      title: 'Heading 4',
+                      class: 'ck-heading_heading4',
+                    },
+                    {
+                      model: 'heading5',
+                      view: 'h5',
+                      title: 'Heading 5',
+                      class: 'ck-heading_heading5',
+                    },
+                  ],
+                },
+              }}
+              data={content}
+              onChange={(event: ChangeEvent, editor: { getData(): string }) => {
+                setContent(editor.getData());
+              }}
+              onInit={(editor: { editing: { view: { focus(): void } } }) => {
+                editor.editing.view.focus();
+              }}
+            />
+          </div>
+
+          <div>
+            <Button onClick={handleSave}>Save</Button>
+            <Button variant="secondary" onClick={handleCancel}>
+              Cancel
+            </Button>
+          </div>
+        </>
+      ) : (
+        <div
+          role="button"
+          className={classNames({
+            [styles.readOnly]: editable && !editing,
+          })}
+          tabIndex={0}
+          onClick={toggleEditing.on}
+          onKeyPress={e => {
+            switch (e.key) {
+              case 'Enter':
+              case ' ':
+                toggleEditing.on();
+                break;
+              default:
+                break;
+            }
+          }}
+        >
+          <div className={styles.readOnlyButtons}>
+            <Button variant="secondary" className={styles.readOnlyButton}>
+              Edit
+            </Button>
+            {canDelete && (
+              <>
+                <Button
+                  variant="warning"
+                  className={styles.readOnlyButton}
+                  onClick={e => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    toggleConfirmation.on();
+                  }}
+                >
+                  Delete
+                </Button>
+              </>
             )}
           </div>
-        )}
 
-        {editable && editing ? (
-          <CKEditor
-            editor={ClassicEditor}
-            config={{
-              toolbar: toolbarConfig,
-              heading: allowHeadings && {
-                options: [
-                  {
-                    model: 'paragraph',
-                    title: 'Paragraph',
-                    class: 'ck-heading_paragraph',
-                  },
-                  {
-                    model: 'heading3',
-                    view: 'h3',
-                    title: 'Heading 3',
-                    class: 'ck-heading_heading3',
-                  },
-                  {
-                    model: 'heading4',
-                    view: 'h4',
-                    title: 'Heading 4',
-                    class: 'ck-heading_heading4',
-                  },
-                  {
-                    model: 'heading5',
-                    view: 'h5',
-                    title: 'Heading 5',
-                    class: 'ck-heading_heading5',
-                  },
-                ],
-              },
-            }}
-            data={temporaryContent}
-            onChange={(event: ChangeEvent, editor: { getData(): string }) => {
-              setTemporaryContent(editor.getData());
-            }}
-            onInit={(editor: { editing: { view: { focus(): void } } }) => {
-              editor.editing.view.focus();
-            }}
-          />
-        ) : (
           <div
+            className={styles.preview}
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
-              __html: temporaryContent || '<p>This section is empty</p>',
+              __html: content || '<p>This section is empty</p>',
             }}
-            className={`${
-              styles.preview
-            } govuk-!-padding-left-2 govuk-!-padding-right-2 ${!temporaryContent &&
-              styles.previewPlaceholder}`}
           />
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
