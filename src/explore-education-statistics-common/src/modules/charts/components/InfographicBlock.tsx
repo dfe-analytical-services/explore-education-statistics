442e1a4d6ff0ef3e55406a3e28b4fc621d55a89a
Nicholas Tsim <nicholastsim@gmail.com>
2020-03-27 11:51:04 +0000

    EES-1550 Refactor InfographicBlock to show loading/error states properly

--- src/explore-education-statistics-common/src/modules/charts/components/InfographicBlock.tsx
+++ src/explore-education-statistics-common/src/modules/charts/components/InfographicBlock.tsx
@@ -1,8 +1,11 @@
+import LoadingSpinner from '@common/components/LoadingSpinner';
+import useAsyncRetry from '@common/hooks/useAsyncRetry';
 import {
   ChartDefinition,
   ChartProps,
 } from '@common/modules/charts/types/chart';
-import React, { memo, useEffect, useState } from 'react';
+import toDataUrl from '@common/utils/file/toDataUrl';
+import React, { memo } from 'react';
 
 export type GetInfographic = (fileId: string) => Promise<Blob>;
 
@@ -17,30 +20,35 @@ const InfographicBlock = ({
   width = 0,
   height = 0,
 }: InfographicChartProps) => {
-  const [file, setFile] = useState<string>();
-
-  useEffect(() => {
+  const { value: file, error, isLoading } = useAsyncRetry(async () => {
     if (fileId && getInfographic) {
-      getInfographic(fileId).then(blob => {
-        const a = new FileReader();
-        // @ts-ignore
-        a.onload = (e: ProgressEvent) => setFile(e.target.result);
-        a.readAsDataURL(blob);
-      });
+      const infographic = await getInfographic(fileId);
+      const dataUrl = await toDataUrl(infographic);
+      return dataUrl.toString();
     }
+
+    return undefined;
   }, [getInfographic, fileId]);
 
-  if (!fileId || !file) {
+  if (error) {
+    return <p className="govuk-inset-text">Could not load infographic</p>;
+  }
+
+  if (!fileId) {
     return null;
   }
 
   return (
-    <img
-      alt="infographic"
-      src={file}
-      width={width > 0 ? width : undefined}
-      height={height > 0 ? height : undefined}
-    />
+    <LoadingSpinner loading={isLoading}>
+      {file && (
+        <img
+          alt="infographic"
+          src={file}
+          width={width > 0 ? width : undefined}
+          height={height > 0 ? height : undefined}
+        />
+      )}
+    </LoadingSpinner>
   );
 };
 
