3731b09d429ecd660e6caa3df1a7f6c6901289a2
Ben Outram <ben@hiveit.co.uk>
2020-04-08 17:18:58 +0100

    BAU-565 Adding comments to explain the difference between publishing a release and the release content.

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/BAU/BauReleaseController.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/BAU/BauReleaseController.cs
@@ -19,8 +19,30 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Controllers.Api.BAU
             _releaseService = releaseService;
         }
 
+        /**
+         * Publish a Release immediately.
+         * Runs all of the stages of the publishing workflow except that they are combined to act immediately.
+         * Intended to be used with a Release that's already approved but not necessarily published.
+         * Since the Data task deletes all existing Release statistics data before copying there will be downtime if this Release is already published.
+         * If the Release is already published consider creating a new version instead.
+         * Alternatively, republish only the content.
+         * Publishing will fail at the validation stage if this Release is already in the process of being published.
+         * An existing schedule for this Release that's not complete will be cancelled unless it's already started.
+         */
+        [HttpPut("bau/release/{releaseId}/publish")]
+        public async Task<ActionResult<bool>> PublishReleaseAsync(Guid releaseId)
+        {
+            // BAU-564 TODO
+            return false;
+        }
+
+        /**
+         * Regenerates and publishes the content for a Release immediately.
+         * Note that this is a combination of the Content and Publishing stages of the publishing workflow.
+         * It doesn't include the Files or Data stages so is intended to be used with a Release already published.
+         */
         [HttpPut("bau/release/{releaseId}/publish/content")]
-        public async Task<ActionResult<bool>> PublishContentAsync(Guid releaseId)
+        public async Task<ActionResult<bool>> PublishReleaseContentAsync(Guid releaseId)
         {
             return await _releaseService
                 .PublishContentAsync(releaseId)
