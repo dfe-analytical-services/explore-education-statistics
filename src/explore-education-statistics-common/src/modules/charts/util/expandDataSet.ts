28defd273559e7e8eb9340f1823a5cb6b54e2ed6
Nicholas Tsim <nicholastsim@gmail.com>
2020-04-21 02:19:53 +0100

    EES-616 Add data consistency checks to `expandDataSet`

--- src/explore-education-statistics-common/src/modules/charts/util/expandDataSet.ts
+++ src/explore-education-statistics-common/src/modules/charts/util/expandDataSet.ts
@@ -3,21 +3,30 @@ import { CategoryFilter } from '@common/modules/table-tool/types/filters';
 import { FullTableMeta } from '@common/modules/table-tool/types/fullTable';
 
 /**
- * Expand a {@param dataSet} into its
- * {@see ExpandedDataSet} for that contains all
- * of its filters in their class-based forms
- * using the given {@param meta}.
+ * Expand a {@param dataSet} into its {@see ExpandedDataSet}
+ * for that contains all of its filters in their class-based
+ * forms using the given {@param meta}.
  */
 export default function expandDataSet(
   dataSet: DataSet,
   meta: FullTableMeta,
 ): ExpandedDataSet {
-  const categoricalFilters = Object.values(meta.filters).flat();
+  const categoricalFilters = Object.values(meta.filters).flatMap(
+    filterGroup => filterGroup.options,
+  );
 
   const filters = dataSet.filters
-    .map(filterValue =>
-      categoricalFilters.find(filter => filter.value === filterValue),
-    )
+    .map(filterValue => {
+      const matchingFilter = categoricalFilters.find(
+        filter => filter.value === filterValue,
+      );
+
+      if (!matchingFilter) {
+        throw new Error(`Could not find category filter: '${filterValue}'`);
+      }
+
+      return matchingFilter;
+    })
     .filter(filter => !!filter) as CategoryFilter[];
 
   const indicator = meta.indicators.find(
@@ -25,7 +34,7 @@ export default function expandDataSet(
   );
 
   if (!indicator) {
-    throw new Error(`Could not find indicator: ${dataSet.indicator}`);
+    throw new Error(`Could not find indicator: '${dataSet.indicator}'`);
   }
 
   const location = meta.locations.find(
@@ -34,9 +43,19 @@ export default function expandDataSet(
       dataSet.location?.value === filter.value,
   );
 
+  if (dataSet.location && !location) {
+    throw new Error(
+      `Could not find location with value: '${dataSet.location.value}', level: '${dataSet.location.level}'`,
+    );
+  }
+
   const timePeriod = meta.timePeriodRange.find(
     filter => dataSet.timePeriod === filter.value,
   );
+
+  if (dataSet.timePeriod && !timePeriod) {
+    throw new Error(`Could not find time period: '${dataSet.timePeriod}'`);
+  }
 
   return {
     filters,
