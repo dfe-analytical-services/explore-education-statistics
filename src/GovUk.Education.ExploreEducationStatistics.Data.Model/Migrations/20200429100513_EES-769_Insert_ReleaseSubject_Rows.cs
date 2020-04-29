64b8f2dc222ede362a695a0bcbdf3c15552a7479
Simon Shakespeare <si.shakes@gmail.com>
2020-04-29 11:30:22 +0100

    EES-769 Amendments - Add migration for ReleaseSubjects & reinstate temporarily the deleted Footnote entity relationship with Release entity to avoid drop index in migration.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200429100513_EES-769_Insert_ReleaseSubject_Rows.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200429100513_EES-769_Insert_ReleaseSubject_Rows.cs
@@ -4,18 +4,18 @@ using static GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations.Mi
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
 {
-    public partial class UpdateFootnotes : Migration
+    public partial class EES769_Insert_ReleaseSubject_Rows : Migration
     {
-        private const string MigrationId = "20200325150211";
-        
+        private const string MigrationId = "20200429100513";
+
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_FootnoteData.sql");
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_ReleaseSubject_Up.sql");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_FootnoteData_Down.sql");
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_ReleaseSubject_Down.sql");
         }
     }
 }
