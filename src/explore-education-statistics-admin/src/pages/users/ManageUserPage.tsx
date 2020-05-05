3a4610da3ff462876fdcf9c402cbd6ac3452e9d6
James Cutts <james@jamescutts.co.uk>
2020-05-05 17:52:36 +0100

    cleanup manage user page and add mailto link

--- src/explore-education-statistics-admin/src/pages/users/ManageUserPage.tsx
+++ src/explore-education-statistics-admin/src/pages/users/ManageUserPage.tsx
@@ -128,170 +128,181 @@ const ManageUserPage = ({
       </h1>
 
       <LoadingSpinner loading={isLoading} text="Loading user details">
-        <Formik<UpdateRoleFormValues>
-          enableReinitialize
-          initialValues={{
-            selectedRoleId: model?.user.role ?? '',
-          }}
-          validationSchema={Yup.object({
-            selectedRoleId: Yup.string().required('Choose role for the user'),
-          })}
-          onSubmit={handleSubmit}
-          render={_ => {
-            return (
-              <Form id={formId}>
-                <FormFieldset
-                  id={`${formId}-details`}
-                  legend="Detals"
-                  legendSize="m"
-                >
-                  <SummaryList>
-                    <SummaryListItem term="Name">
-                      {model?.user.name}
-                    </SummaryListItem>
-                    <SummaryListItem term="Email">
-                      {model?.user.email}
-                    </SummaryListItem>
-                    <SummaryListItem term="Phone">-</SummaryListItem>
-                  </SummaryList>
-                </FormFieldset>
-                <FormFieldset
-                  id={`${formId}-role`}
-                  legend="Role"
-                  legendSize="m"
-                  hint="The users role within the service."
-                >
-                  <div className="govuk-grid-row">
-                    <div className="govuk-grid-column-one-quarter">
-                      <FormFieldSelect
-                        id={`${formId}-selectedRoleId`}
-                        label="Role"
-                        name="selectedRoleId"
-                        options={roles?.map(role => ({
-                          label: role.name,
-                          value: role.id,
-                        }))}
-                      />
-                    </div>
-                    <div className="govuk-grid-column-one-quarter">
-                      <Button type="submit" className="govuk-!-margin-top-6">
-                        Update role
-                      </Button>
-                    </div>
-                  </div>
-                </FormFieldset>
-              </Form>
-            );
-          }}
-        />
-        <Formik<AddReleaseRoleFormValues>
-          initialValues={{
-            selectedReleaseId:
-              orderBy(releases, release => release)?.[0]?.id ?? '',
-            selectedReleaseRoleId:
-              orderBy(releaseRoles, releaseRole => releaseRole)?.[0]?.value ??
-              '',
-          }}
-          enableReinitialize
-          onSubmit={addReleaseRole}
-          render={() => {
-            return (
-              <Form id={`${formId}-releaseRole`}>
-                <FormFieldset
-                  id={`${formId}-role`}
-                  legend="Release access"
-                  legendSize="m"
-                  hint="The releases a user can access within the service."
-                >
-                  <div className="govuk-grid-row">
-                    <div className="govuk-grid-column-one-half">
-                      <FormFieldSelect
-                        id={`${formId}-selectedReleaseId`}
-                        label="Release"
-                        name="selectedReleaseId"
-                        options={releases?.map(release => ({
-                          label: `${release.publicationTitle} - ${release.releaseName}`,
-                          value: release.id,
-                        }))}
-                      />
-                    </div>
+        {model && (
+          <>
+            <Formik<UpdateRoleFormValues>
+              enableReinitialize
+              initialValues={{
+                selectedRoleId: model.user.role ?? '',
+              }}
+              validationSchema={Yup.object({
+                selectedRoleId: Yup.string().required(
+                  'Choose role for the user',
+                ),
+              })}
+              onSubmit={handleSubmit}
+              render={_ => {
+                return (
+                  <Form id={formId}>
+                    <FormFieldset
+                      id={`${formId}-details`}
+                      legend="Detals"
+                      legendSize="m"
+                    >
+                      <SummaryList>
+                        <SummaryListItem term="Name">
+                          {model.user.name}
+                        </SummaryListItem>
+                        <SummaryListItem term="Email">
+                          <a href={`mailto:${model.user.email}`}>{model.user.email}</a>
+                        </SummaryListItem>
+                        <SummaryListItem term="Phone">-</SummaryListItem>
+                      </SummaryList>
+                    </FormFieldset>
+                    <FormFieldset
+                      id={`${formId}-role`}
+                      legend="Role"
+                      legendSize="m"
+                      hint="The users role within the service."
+                    >
+                      <div className="govuk-grid-row">
+                        <div className="govuk-grid-column-one-quarter">
+                          <FormFieldSelect
+                            id={`${formId}-selectedRoleId`}
+                            label="Role"
+                            name="selectedRoleId"
+                            options={roles?.map(role => ({
+                              label: role.name,
+                              value: role.id,
+                            }))}
+                          />
+                        </div>
+                        <div className="govuk-grid-column-one-quarter">
+                          <Button
+                            type="submit"
+                            className="govuk-!-margin-top-6"
+                          >
+                            Update role
+                          </Button>
+                        </div>
+                      </div>
+                    </FormFieldset>
+                  </Form>
+                );
+              }}
+            />
+            <Formik<AddReleaseRoleFormValues>
+              initialValues={{
+                selectedReleaseId:
+                  orderBy(releases, release => release)?.[0]?.id ?? '',
+                selectedReleaseRoleId:
+                  orderBy(releaseRoles, releaseRole => releaseRole)?.[0]
+                    ?.value ?? '',
+              }}
+              enableReinitialize
+              onSubmit={addReleaseRole}
+              render={() => {
+                return (
+                  <Form id={`${formId}-releaseRole`}>
+                    <FormFieldset
+                      id={`${formId}-role`}
+                      legend="Release access"
+                      legendSize="m"
+                      hint="The releases a user can access within the service."
+                    >
+                      <div className="govuk-grid-row">
+                        <div className="govuk-grid-column-one-half">
+                          <FormFieldSelect
+                            id={`${formId}-selectedReleaseId`}
+                            label="Release"
+                            name="selectedReleaseId"
+                            options={releases?.map(release => ({
+                              label: `${release.publicationTitle} - ${release.releaseName}`,
+                              value: release.id,
+                            }))}
+                          />
+                        </div>
 
-                    <div className="govuk-grid-column-one-quarter">
-                      <FormFieldSelect
-                        id={`${formId}-selectedReleaseRoleId`}
-                        label="Release role"
-                        name="selectedReleaseRoleId"
-                        options={releaseRoles?.map(releaseRole => ({
-                          label: releaseRole.name,
-                          value: releaseRole.value,
-                        }))}
-                      />
-                    </div>
-                    <div className="govuk-grid-column-one-quarter">
-                      {model?.user && (
-                        <Button type="submit" className="govuk-!-margin-top-6">
-                          Add release access
-                        </Button>
-                      )}
-                    </div>
-                  </div>
-                </FormFieldset>
-                <table className="govuk-table">
-                  <thead className="govuk-table__head">
-                    <tr className="govuk-table__row">
-                      <th scope="col" className="govuk-table__header">
-                        Publication
-                      </th>
-                      <th scope="col" className="govuk-table__header">
-                        Release
-                      </th>
-                      <th scope="col" className="govuk-table__header">
-                        Role
-                      </th>
-                      <th scope="col" className="govuk-table__header">
-                        Actions
-                      </th>
-                    </tr>
-                  </thead>
-                  {model?.user && (
-                    <tbody className="govuk-table__body">
-                      {model.user.userReleaseRoles.map(userReleaseRole => (
-                        <tr
-                          className="govuk-table__row"
-                          key={userReleaseRole.id}
-                        >
-                          <td className="govuk-table__cell">
-                            {userReleaseRole.publication.title}
-                          </td>
-                          <td className="govuk-table__cell">
-                            {userReleaseRole.release.title}
-                          </td>
-                          <td className="govuk-table__cell">
-                            {userReleaseRole.releaseRole.name}
-                          </td>
-                          <td className="govuk-table__cell">
-                            <ButtonText
-                              onClick={() => {
-                                userService
-                                  .removeUserReleaseRole(
-                                    userId,
-                                    userReleaseRole,
-                                  )
-                                  .then(() => getUser());
-                              }}
+                        <div className="govuk-grid-column-one-quarter">
+                          <FormFieldSelect
+                            id={`${formId}-selectedReleaseRoleId`}
+                            label="Release role"
+                            name="selectedReleaseRoleId"
+                            options={releaseRoles?.map(releaseRole => ({
+                              label: releaseRole.name,
+                              value: releaseRole.value,
+                            }))}
+                          />
+                        </div>
+                        <div className="govuk-grid-column-one-quarter">
+                          {model?.user && (
+                            <Button
+                              type="submit"
+                              className="govuk-!-margin-top-6"
                             >
-                              Remove
-                            </ButtonText>
-                          </td>
+                              Add release access
+                            </Button>
+                          )}
+                        </div>
+                      </div>
+                    </FormFieldset>
+                    <table className="govuk-table">
+                      <thead className="govuk-table__head">
+                        <tr className="govuk-table__row">
+                          <th scope="col" className="govuk-table__header">
+                            Publication
+                          </th>
+                          <th scope="col" className="govuk-table__header">
+                            Release
+                          </th>
+                          <th scope="col" className="govuk-table__header">
+                            Role
+                          </th>
+                          <th scope="col" className="govuk-table__header">
+                            Actions
+                          </th>
                         </tr>
-                      ))}
-                    </tbody>
-                  )}
-                </table>
-              </Form>
-            );
-          }}
-        />
+                      </thead>
+
+                      <tbody className="govuk-table__body">
+                        {model.user.userReleaseRoles.map(userReleaseRole => (
+                          <tr
+                            className="govuk-table__row"
+                            key={userReleaseRole.id}
+                          >
+                            <td className="govuk-table__cell">
+                              {userReleaseRole.publication.title}
+                            </td>
+                            <td className="govuk-table__cell">
+                              {userReleaseRole.release.title}
+                            </td>
+                            <td className="govuk-table__cell">
+                              {userReleaseRole.releaseRole.name}
+                            </td>
+                            <td className="govuk-table__cell">
+                              <ButtonText
+                                onClick={() => {
+                                  userService
+                                    .removeUserReleaseRole(
+                                      userId,
+                                      userReleaseRole,
+                                    )
+                                    .then(() => getUser());
+                                }}
+                              >
+                                Remove
+                              </ButtonText>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </Form>
+                );
+              }}
+            />
+          </>
+        )}
       </LoadingSpinner>
       <div className="govuk-!-margin-top-6">
         <Link to="/administration/users" className="govuk-back-link">
