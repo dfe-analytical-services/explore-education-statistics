c022449c45dd03e65979f19a6b2c3b11e641b0fc
Nicholas Tsim <nicholastsim@gmail.com>
2020-05-06 12:08:55 +0100

    EES-818 Always render PreReleasePage layout even if no content is shown

--- src/explore-education-statistics-admin/src/pages/release/prerelease/PreReleasePage.tsx
+++ src/explore-education-statistics-admin/src/pages/release/prerelease/PreReleasePage.tsx
@@ -54,81 +54,76 @@ const PreReleasePage = ({ match }: RouteComponentProps<MatchProps>) => {
   }, [handleManualErrors, publicationId, releaseId]);
 
   return (
-    <>
-      {model && (
-        <Page
-          wide
-          breadcrumbs={
-            user && user.permissions.canAccessAnalystPages
-              ? [{ name: 'Pre Release access' }]
-              : []
-          }
-          includeHomeBreadcrumb={user && user.permissions.canAccessAnalystPages}
-        >
-          {model.preReleaseWindowStatus.preReleaseAccess === 'Within' &&
-            model.content && (
-              <ReleaseProvider
-                value={{
-                  ...model?.content,
-                  canUpdateRelease: false,
-                  unresolvedComments: [],
-                }}
-              >
-                <PublicationReleaseContent />
-              </ReleaseProvider>
+    <Page
+      wide
+      breadcrumbs={
+        user && user.permissions.canAccessAnalystPages
+          ? [{ name: 'Pre Release access' }]
+          : []
+      }
+      includeHomeBreadcrumb={user && user.permissions.canAccessAnalystPages}
+    >
+      {model?.preReleaseWindowStatus.preReleaseAccess === 'Within' &&
+        model?.content && (
+          <ReleaseProvider
+            value={{
+              ...model?.content,
+              canUpdateRelease: false,
+              unresolvedComments: [],
+            }}
+          >
+            <PublicationReleaseContent />
+          </ReleaseProvider>
+        )}
+
+      {model?.preReleaseWindowStatus.preReleaseAccess === 'Before' && (
+        <>
+          <h1 className="govuk-heading-l">
+            Pre Release access is not yet available
+          </h1>
+          <p className="govuk-body">
+            Pre Release access for the{' '}
+            <strong>{model.content.release.title}</strong> release of{' '}
+            <strong>{model.publication.title}</strong> is not yet available.
+          </p>
+          <p className="govuk-body">
+            Pre Release access will be available from{' '}
+            {format(
+              model.preReleaseWindowStatus.preReleaseWindowStartTime,
+              'd MMMM yyyy',
             )}
-
-          {model.preReleaseWindowStatus.preReleaseAccess === 'Before' && (
-            <>
-              <h1 className="govuk-heading-l">
-                Pre Release access is not yet available
-              </h1>
-              <p className="govuk-body">
-                Pre Release access for the{' '}
-                <strong>{model.content.release.title}</strong> release of{' '}
-                <strong>{model.publication.title}</strong> is not yet available.
-              </p>
-              <p className="govuk-body">
-                Pre Release access will be available from{' '}
-                {format(
-                  model.preReleaseWindowStatus.preReleaseWindowStartTime,
-                  'd MMMM yyyy',
-                )}
-                {' at '}
-                {format(
-                  model.preReleaseWindowStatus.preReleaseWindowStartTime,
-                  'HH:mm',
-                )}{' '}
-                until{' '}
-                {format(
-                  model.preReleaseWindowStatus.preReleaseWindowEndTime,
-                  'd MMMM yyyy',
-                )}
-                {' at '}
-                {format(
-                  model.preReleaseWindowStatus.preReleaseWindowEndTime,
-                  'HH:mm',
-                )}
-                .
-              </p>
-              <p className="govuk-body">Please try again later.</p>
-            </>
-          )}
-
-          {model.preReleaseWindowStatus.preReleaseAccess === 'After' && (
-            <>
-              <h1 className="govuk-heading-l">Pre Release access has ended</h1>
-              <p className="govuk-body">
-                Pre Release access for the{' '}
-                <strong>{model.content.release.title}</strong> release of{' '}
-                <strong>{model.publication.title}</strong> is no longer
-                available.
-              </p>
-            </>
-          )}
-        </Page>
+            {' at '}
+            {format(
+              model.preReleaseWindowStatus.preReleaseWindowStartTime,
+              'HH:mm',
+            )}{' '}
+            until{' '}
+            {format(
+              model.preReleaseWindowStatus.preReleaseWindowEndTime,
+              'd MMMM yyyy',
+            )}
+            {' at '}
+            {format(
+              model.preReleaseWindowStatus.preReleaseWindowEndTime,
+              'HH:mm',
+            )}
+            .
+          </p>
+          <p className="govuk-body">Please try again later.</p>
+        </>
       )}
-    </>
+
+      {model?.preReleaseWindowStatus.preReleaseAccess === 'After' && (
+        <>
+          <h1 className="govuk-heading-l">Pre Release access has ended</h1>
+          <p className="govuk-body">
+            Pre Release access for the{' '}
+            <strong>{model.content.release.title}</strong> release of{' '}
+            <strong>{model.publication.title}</strong> is no longer available.
+          </p>
+        </>
+      )}
+    </Page>
   );
 };
 
