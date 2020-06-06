3fb0fd341faf49b67ccf42c29ece183c0fb5ef8d
Nicholas Tsim <nicholastsim@gmail.com>
2020-06-06 18:27:39 +0100

    EES-18 Add more spacing and bigger text to Comments styling

--- src/explore-education-statistics-admin/src/modules/find-statistics/components/Comments.tsx
+++ src/explore-education-statistics-admin/src/modules/find-statistics/components/Comments.tsx
@@ -13,9 +13,8 @@ import { Form } from '@common/components/form';
 import FormFieldTextArea from '@common/components/form/FormFieldTextArea';
 import FormattedDate from '@common/components/FormattedDate';
 import Yup from '@common/validation/yup';
-import classNames from 'classnames';
 import { Formik } from 'formik';
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './Comments.module.scss';
 
 interface FormValues {
@@ -42,8 +41,6 @@ const Comments = ({
   onChange,
   canComment = false,
 }: Props) => {
-  const ref = useRef<HTMLDivElement>(null);
-
   const [editingComment, setEditingComment] = useState<ExtendedComment>();
 
   const { user } = useAuthContext();
@@ -100,151 +97,123 @@ const Comments = ({
   };
 
   return (
-    <>
-      <div
-        role="presentation"
-        ref={ref}
-        className={classNames('dfe-comment-block', styles.addComment)}
+    <div className={styles.container}>
+      <Details
+        className="govuk-!-margin-bottom-1 govuk-body-s"
+        summary={`${canComment ? `Add / ` : ''}View comments (${
+          comments.length
+        })`}
       >
-        <Details
-          summary={`${canComment ? `Add / ` : ''}View comments (${
-            comments.length
-          })`}
-          className="govuk-!-margin-bottom-1 govuk-body-s"
-          onToggle={isOpen => {
-            if (ref.current) {
-              const section = document.getElementById(
-                `content-section-${blockId}`,
-              );
-              if (isOpen) {
-                ref.current.classList.add(styles.top);
-                if (section) {
-                  section.classList.add(styles.commentWhileCommenting);
-                }
-              } else {
-                ref.current.classList.remove(styles.top);
-                if (section) {
-                  section.classList.remove(styles.commentWhileCommenting);
-                }
-              }
-            }
-          }}
-        >
-          {canComment && (
-            <Formik<FormValues>
-              initialValues={{
-                content: '',
-              }}
-              validationSchema={Yup.object({
-                content: Yup.string().required('Enter a comment'),
-              })}
-              onSubmit={values => {
-                addComment(values.content);
-              }}
-            >
-              <Form id={`${blockId}-addCommentForm`} showErrorSummary={false}>
-                <FormFieldTextArea<FormValues>
-                  label="Comment"
-                  name="content"
-                  id={`${blockId}-addCommentForm-content`}
-                  rows={3}
-                />
-
-                <Button type="submit">Add comment</Button>
-              </Form>
-            </Formik>
-          )}
-
-          <div className={styles.commentsContainer}>
-            {comments.map((comment, index) => {
-              const { createdBy } = comment;
-
-              return (
-                <div key={comment.id}>
-                  <p className="govuk-body-s govuk-!-margin-0">
-                    <strong>
-                      {`${createdBy.firstName} ${createdBy.lastName}`}
-                    </strong>
-                  </p>
-                  <p className="govuk-body-xs">
-                    <FormattedDate format="d MMMM yyyy HH:mm">
-                      {comment.updated || comment.created}
-                    </FormattedDate>
-                  </p>
-
-                  {editingComment?.id === comment.id ? (
-                    <Formik<FormValues>
-                      initialValues={{
-                        content: editingComment.content,
-                      }}
-                      validationSchema={Yup.object({
-                        content: Yup.string().required('Enter a comment'),
-                      })}
-                      onSubmit={values => {
-                        updateComment(index, values.content);
-                      }}
-                    >
-                      <Form
-                        id={`${blockId}-editCommentForm`}
-                        showErrorSummary={false}
-                      >
-                        <FormFieldTextArea<FormValues>
-                          label="Comment"
-                          name="content"
-                          id={`${blockId}-editCommentForm-editComment`}
-                          rows={3}
-                        />
-
-                        <ButtonGroup>
-                          <Button type="submit">Update</Button>
-                          <ButtonText
-                            onClick={() => {
-                              setEditingComment(undefined);
-                            }}
-                          >
-                            Cancel
-                          </ButtonText>
-                        </ButtonGroup>
-                      </Form>
-                    </Formik>
-                  ) : (
-                    <>
-                      <p className="govuk-body-xs govuk-!-margin-bottom-1">
-                        {comment.content}
-                      </p>
-
-                      {canComment && user?.id === createdBy.id && (
-                        <ButtonGroup>
-                          <ButtonText
-                            className="govuk-body-xs"
-                            onClick={() => {
-                              setEditingComment(comment);
-                            }}
-                          >
-                            Edit
-                          </ButtonText>
-
-                          <ButtonText
-                            className="govuk-body-xs"
-                            onClick={() => {
-                              removeComment(index);
-                            }}
-                          >
-                            Remove
-                          </ButtonText>
-                        </ButtonGroup>
-                      )}
-                    </>
-                  )}
-
-                  <hr />
-                </div>
-              );
+        {canComment && (
+          <Formik<FormValues>
+            initialValues={{
+              content: '',
+            }}
+            validationSchema={Yup.object({
+              content: Yup.string().required('Enter a comment'),
             })}
-          </div>
-        </Details>
-      </div>
-    </>
+            onSubmit={values => {
+              addComment(values.content);
+            }}
+          >
+            <Form id={`${blockId}-addCommentForm`} showErrorSummary={false}>
+              <FormFieldTextArea<FormValues>
+                label="Comment"
+                name="content"
+                id={`${blockId}-addCommentForm-content`}
+                rows={3}
+              />
+
+              <Button type="submit">Add comment</Button>
+            </Form>
+          </Formik>
+        )}
+
+        <ul className={styles.commentsContainer}>
+          {comments.map((comment, index) => {
+            const { createdBy } = comment;
+
+            return (
+              <li key={comment.id} className="govuk-body-s">
+                <p className="govuk-!-margin-0">
+                  <strong>
+                    {`${createdBy.firstName} ${createdBy.lastName}`}
+                  </strong>
+                </p>
+                <p>
+                  <FormattedDate format="d MMMM yyyy HH:mm">
+                    {comment.updated || comment.created}
+                  </FormattedDate>
+                </p>
+
+                {editingComment?.id === comment.id ? (
+                  <Formik<FormValues>
+                    initialValues={{
+                      content: editingComment.content,
+                    }}
+                    validationSchema={Yup.object({
+                      content: Yup.string().required('Enter a comment'),
+                    })}
+                    onSubmit={values => {
+                      updateComment(index, values.content);
+                    }}
+                  >
+                    <Form
+                      id={`${blockId}-editCommentForm`}
+                      showErrorSummary={false}
+                    >
+                      <FormFieldTextArea<FormValues>
+                        label="Comment"
+                        name="content"
+                        id={`${blockId}-editCommentForm-editComment`}
+                        rows={3}
+                      />
+
+                      <ButtonGroup>
+                        <Button type="submit">Update</Button>
+                        <ButtonText
+                          onClick={() => {
+                            setEditingComment(undefined);
+                          }}
+                        >
+                          Cancel
+                        </ButtonText>
+                      </ButtonGroup>
+                    </Form>
+                  </Formik>
+                ) : (
+                  <>
+                    <p>{comment.content}</p>
+
+                    {canComment && user?.id === createdBy.id && (
+                      <ButtonGroup>
+                        <ButtonText
+                          onClick={() => {
+                            setEditingComment(comment);
+                          }}
+                        >
+                          Edit
+                        </ButtonText>
+
+                        <ButtonText
+                          onClick={() => {
+                            removeComment(index);
+                          }}
+                        >
+                          Remove
+                        </ButtonText>
+                      </ButtonGroup>
+                    )}
+                  </>
+                )}
+
+                <hr />
+              </li>
+            );
+          })}
+        </ul>
+      </Details>
+    </div>
   );
 };
 
