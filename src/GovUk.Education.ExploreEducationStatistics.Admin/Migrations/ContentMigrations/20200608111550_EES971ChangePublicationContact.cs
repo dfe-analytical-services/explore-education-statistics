5b23af7583b77282c92ef814fff5c38ad954db73
Ben Outram <ben@hiveit.co.uk>
2020-06-08 12:18:01 +0100

    EES-971 Change Publication Contact

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20200608111550_EES971ChangePublicationContact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    public partial class EES971ChangePublicationContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: new Guid("74f5aade-6d24-4a0b-be23-2ab4b4b2d191"),
+                column: "ContactTelNo",
+                value: "0370 000 2288");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: new Guid("74f5aade-6d24-4a0b-be23-2ab4b4b2d191"),
+                column: "ContactTelNo",
+                value: "020 7783 8553");
+        }
+    }
+}
