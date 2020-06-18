edbe60e300166724e01d2d94b41ee261b05577a8
Nicholas Tsim <nicholastsim@gmail.com>
2020-06-18 19:25:57 +0100

    refactor: remove unused FormFieldCheckboxSubGroups component

--- src/explore-education-statistics-common/src/components/form/index.ts
+++ src/explore-education-statistics-common/src/components/form/index.ts
@@ -7,7 +7,6 @@ export { default as FormCheckboxSearchGroup } from './FormCheckboxSearchGroup';
 export { default as FormFieldCheckboxGroup } from './FormFieldCheckboxGroup';
 export { default as FormFieldCheckboxSearchGroup } from './FormFieldCheckboxSearchGroup';
 export { default as FormFieldCheckboxSearchSubGroups } from './FormFieldCheckboxSearchSubGroups';
-export { default as FormFieldCheckboxSubGroups } from './FormFieldCheckboxSubGroups';
 export { default as FormFieldRadioGroup } from './FormFieldRadioGroup';
 export { default as FormFieldSelect } from './FormFieldSelect';
 export { default as FormFieldset } from './FormFieldset';
