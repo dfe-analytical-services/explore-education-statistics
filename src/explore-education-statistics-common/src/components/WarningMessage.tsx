a1456b62d5c64e6f108aee1336b2f640a771ac2b
Nicholas Tsim <nicholastsim@gmail.com>
2020-07-09 12:23:28 +0100

    fix: prevent WarningMessage icon squashing and center text correctly

--- src/explore-education-statistics-common/src/components/WarningMessage.tsx
+++ src/explore-education-statistics-common/src/components/WarningMessage.tsx
@@ -1,17 +1,25 @@
+import classNames from 'classnames';
 import React, { ReactNode } from 'react';
+import styles from './WarningMessage.module.scss';
 
 interface Props {
   children: ReactNode;
+  className?: string;
   icon?: string;
 }
 
-const WarningMessage = ({ icon = '!', children }: Props) => {
+const WarningMessage = ({ children, className, icon = '!' }: Props) => {
   return (
-    <div className="govuk-warning-text">
-      <span className="govuk-warning-text__icon" aria-hidden="true">
+    <div
+      className={classNames('govuk-warning-text', styles.warning, className)}
+    >
+      <span
+        className={classNames('govuk-warning-text__icon', styles.icon)}
+        aria-hidden="true"
+      >
         {icon}
       </span>
-      <strong className="govuk-warning-text__text">
+      <strong className={classNames('govuk-warning-text__text', styles.text)}>
         <span className="govuk-warning-text__assistive">Warning</span>
         {children}
       </strong>
