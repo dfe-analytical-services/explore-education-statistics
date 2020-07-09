5cf8ab7d19a7d4ecd748c92cc5e1b239d1d47a2f
Simon Shakespeare <si.shakes@gmail.com>
2020-07-09 18:11:23 +0100

    EES-1146 Remove softDelete from Release & publisher now removes previous release.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200709170734_EES-1146_Remove_Release_SoftDelete.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    public partial class EES1146_Remove_Release_SoftDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SoftDeleted",
+                table: "Release");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SoftDeleted",
+                table: "Release",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
