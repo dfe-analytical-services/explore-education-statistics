18878e140762ca04167c4783cf54a926418585a2
Simon Shakespeare <si.shakes@gmail.com>
2020-07-14 12:35:23 +0100

    EES-1169 Correct incorrect ReleaseType definition.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200713151302_Type_Previous_ReleaseType.sql
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200713151302_Type_Previous_ReleaseType.sql
@@ -2,7 +2,6 @@ CREATE TYPE ReleaseType AS TABLE
 (
     Id             UNIQUEIDENTIFIER NOT NULL,
     TimeIdentifier VARCHAR(6)       NOT NULL,
-    ReleaseDate    DATETIME2        NOT NULL,
     Slug           VARCHAR(MAX)     NOT NULL,
     Year           INT              NOT NULL,
     PublicationId  UNIQUEIDENTIFIER NOT NULL
