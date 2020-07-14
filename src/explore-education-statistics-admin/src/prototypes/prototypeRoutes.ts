3765ddc5d0d6d7a2ab95a1a3c278a3a1f1808fa2
Martin Waite <martin@hiveit.co.uk>
2020-07-14 17:22:40 +0100

    EES-1130 edit glossary page

--- src/explore-education-statistics-admin/src/prototypes/prototypeRoutes.ts
+++ src/explore-education-statistics-admin/src/prototypes/prototypeRoutes.ts
@@ -1,6 +1,7 @@
 import { RouteProps } from 'react-router';
 import PrototypeExamplePage from './PrototypeExamplePage';
 import PrototypePublicMetadata from './PrototypePublicMetadata';
+import PrototypeBauGlossary from './PrototypeBauGlossary';
 
 interface PrototypeRoute extends RouteProps {
   name: string;
@@ -17,6 +18,11 @@ const prototypeRoutes: PrototypeRoute[] = [
     name: 'Public metadata',
     path: '/prototypes/public-metadata',
     component: PrototypePublicMetadata,
+  },
+  {
+    name: 'BAU manage glossary',
+    path: '/prototypes/manage-glossary',
+    component: PrototypeBauGlossary,
   },
 ];
 
