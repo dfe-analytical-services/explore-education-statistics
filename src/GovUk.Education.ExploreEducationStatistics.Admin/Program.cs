ef74ef2e6664a12fae1ba4914d490340801f1df3
Ben Outram <ben@hiveit.co.uk>
2020-07-20 11:23:23 +0100

    EES-1009 Upgrade to .NET Core 3.1 (#1762)
    * EES-1009 Upgrade to .NET Core 3.1
    - Target .NET Core 3.1
    - Target Azure function runtime 3.0 rather than 3.0 Preview
    - Dependency upgrades
    - Update Azure Storage dependencies.
    - Use generic host for app services (recommended in 3.0+)
    - Disable sensitive data logging
    - Remove the HostingStartup dependency added by EES-1061
    - Remove Backend Build SDK installation tasks for older versions. Change to using vm image label.
    - Use 3.1.x SDK for Admin Build
    - Remove unsupported Logger injection in Content API Startup
    - Temporarily add Publisher dependency on Microsoft.Azure.Storage.File to fix https://github.com/Azure/azure-storage-net/issues/999

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Program.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Program.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.ApplicationInsights;
 
@@ -9,13 +9,19 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(options => options.AddServerHeader = false)
-                .UseStartup<Startup>()
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.AddServerHeader = false;
+                    });
+                })
                 .ConfigureLogging(
                     builder =>
                     {
