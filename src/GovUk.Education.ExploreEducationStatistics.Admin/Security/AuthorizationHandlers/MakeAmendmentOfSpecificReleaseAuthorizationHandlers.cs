c7400a9b3982ff935b7e20d0c1b106142554e05c
Ben Outram <ben@hiveit.co.uk>
2020-07-22 11:28:19 +0100

    EES-1189 Prevent creating Amendments of older versions

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Security/AuthorizationHandlers/MakeAmendmentOfSpecificReleaseAuthorizationHandlers.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Security/AuthorizationHandlers/MakeAmendmentOfSpecificReleaseAuthorizationHandlers.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using GovUk.Education.ExploreEducationStatistics.Common.Security.AuthorizationHandlers;
 using GovUk.Education.ExploreEducationStatistics.Content.Model;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
@@ -12,29 +15,42 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Security.Authorizatio
     public class MakeAmendmentOfSpecificReleaseAuthorizationHandler : CompoundAuthorizationHandler<MakeAmendmentOfSpecificReleaseRequirement, Release>
     {
         public MakeAmendmentOfSpecificReleaseAuthorizationHandler(ContentDbContext context) : base(
-            new CanMakeAmendmentOfAllReleasesAuthorizationHandler(),
+            new CanMakeAmendmentOfAllReleasesAuthorizationHandler(context),
             new HasEditorRoleOnReleaseAuthorizationHandler(context))
         {
             
         }
-        
-        public class CanMakeAmendmentOfAllReleasesAuthorizationHandler : 
+
+        public class CanMakeAmendmentOfAllReleasesAuthorizationHandler :
             EntityAuthorizationHandler<MakeAmendmentOfSpecificReleaseRequirement, Release>
         {
-            public CanMakeAmendmentOfAllReleasesAuthorizationHandler() 
-                : base(ctx => 
-                    ctx.Entity.Live 
+            public CanMakeAmendmentOfAllReleasesAuthorizationHandler(ContentDbContext context)
+                : base(ctx =>
+                    ctx.Entity.Live
+                    && IsLatestVersionOfRelease(context, ctx.Entity)
                     && SecurityUtils.HasClaim(ctx.User, SecurityClaimTypes.MakeAmendmentsOfAllReleases)) {}
         }
 
         public class HasEditorRoleOnReleaseAuthorizationHandler
             : HasRoleOnReleaseAuthorizationHandler<MakeAmendmentOfSpecificReleaseRequirement>
         {
-            public HasEditorRoleOnReleaseAuthorizationHandler(ContentDbContext context) 
-                : base(context, ctx => 
-                    ctx.Release.Live 
+            public HasEditorRoleOnReleaseAuthorizationHandler(ContentDbContext context)
+                : base(context, ctx =>
+                    ctx.Release.Live
+                    && IsLatestVersionOfRelease(context, ctx.Release)
                     && ContainsEditorRole(ctx.Roles))
             {}
+        }
+
+        private static bool IsLatestVersionOfRelease(ContentDbContext context, Release release)
+        {
+            var releases = context.Releases.Where(r => r.PublicationId == release.PublicationId);
+            return IsLatestVersionOfRelease(releases, release.Id);
+        }
+
+        private static bool IsLatestVersionOfRelease(IEnumerable<Release> releases, Guid releaseId)
+        {
+            return !releases.Any(r => r.PreviousVersionId == releaseId && r.Id != releaseId);
         }
     }
 }
