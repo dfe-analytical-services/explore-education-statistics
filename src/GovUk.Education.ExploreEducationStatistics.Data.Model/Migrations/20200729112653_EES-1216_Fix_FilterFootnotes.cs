3841933337dd17bc0aa2ef72fb805e7fc6ea31af
Simon Shakespeare <si.shakes@gmail.com>
2020-07-29 17:08:26 +0100

    EES-1216 Fix routine FilteredFootnotes

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200729112653_EES-1216_Fix_FilterFootnotes.cs
@@ -0,0 +1,22 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations.MigrationConstants;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    public partial class EES1216_Fix_FilterFootnotes : Migration
+    {
+        private const string MigrationId = "20200729112653";
+        private const string PreviousVersionMigrationId = "20200521104834";
+        
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_Routine_FilteredFootnotes.sql");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{PreviousVersionMigrationId}_Routine_FilteredFootnotes.sql");
+        }
+    }
+}
