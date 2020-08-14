95e83c2ccbd7a00f9cbfcca62b59fe64112439f9
Ben Outram <ben@hiveit.co.uk>
2020-08-14 02:09:01 +0100

    EES-1294 Set all existing Publications as published if they have published Releases Adds a database migration to make sure all the existing Publications which are already available to the public have a Published date set, otherwise changes made in the Admin will not be reflected in the public and title updates will cause slugs to be changed.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20200814010041_EES1294SetPublicationPublishedDates.cs
@@ -0,0 +1,22 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static GovUk.Education.ExploreEducationStatistics.Admin.Migrations.MigrationConstants;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    public partial class EES1294SetPublicationPublishedDates : Migration
+    {
+        private const string MigrationId = "20200814010041";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlFromFile(ContentMigrationsPath,
+                $"{MigrationId}_{nameof(EES1294SetPublicationPublishedDates)}.sql");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE dbo.Publications SET Published = NULL WHERE 1 = 1;");
+        }
+    }
+}
