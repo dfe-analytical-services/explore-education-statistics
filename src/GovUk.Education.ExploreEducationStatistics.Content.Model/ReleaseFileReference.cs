6487d2787ec6d7f97b3e4ce19d24e15fafa8c7cd
Nicholas Tsim <nicholastsim@gmail.com>
2020-08-26 16:52:13 +0100

    EES-1334 Fix chart files not being published

--- src/GovUk.Education.ExploreEducationStatistics.Content.Model/ReleaseFileReference.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/ReleaseFileReference.cs
@@ -7,16 +7,32 @@ namespace GovUk.Education.ExploreEducationStatistics.Content.Model
     public class ReleaseFileReference
     {
         public Guid Id { get; set; }
-        
+
         [JsonIgnore]
         public Release Release { get; set; }
-        
+
         public Guid ReleaseId { get; set; }
-        
+
         public Guid? SubjectId { get; set; }
-        
+
         public string Filename { get; set; }
-        
+
         public ReleaseFileTypes ReleaseFileType { get; set; }
+
+        public string BlobStorageName
+        {
+            get
+            {
+                // Ideally all blob storage files should be
+                // saved by ID instead of file name to
+                // prevent naming collisions.
+                if (ReleaseFileType == ReleaseFileTypes.Chart)
+                {
+                    return Id.ToString();
+                }
+
+                return Filename;
+            }
+        }
     }
 }
