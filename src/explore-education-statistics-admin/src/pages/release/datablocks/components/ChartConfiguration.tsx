0bffeeb49bed646f66845d6be19ac5ad31d971f1
Lewis Toseland <lewis@hiveit.co.uk>
2020-09-03 11:09:55 +0100

    EES-1324 - Removed limit

--- src/explore-education-statistics-admin/src/pages/release/datablocks/components/ChartConfiguration.tsx
+++ src/explore-education-statistics-admin/src/pages/release/datablocks/components/ChartConfiguration.tsx
@@ -89,7 +89,6 @@ const ChartConfiguration = ({
       title: Yup.string().required('Enter chart title'),
       alt: Yup.string()
         .required('Enter chart alt text')
-        .max(300, 'Alt text must be less than 300 characters')
         .test({
           name: 'noRepeatTitle',
           message: 'Alt text should not repeat the title',
@@ -233,8 +232,7 @@ const ChartConfiguration = ({
               className="govuk-!-width-three-quarters"
               name="alt"
               label="Alt text"
-              hint="Brief and accurate description of the chart in less than 125 characters. Should not repeat the title."
-              maxLength={300}
+              hint="Brief and accurate description of the chart. Should not repeat the title."
               rows={3}
               onChange={replaceNewLines}
             />
