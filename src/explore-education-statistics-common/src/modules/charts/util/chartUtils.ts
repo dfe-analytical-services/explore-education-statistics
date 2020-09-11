f29e0f1e93f639baf90614b87399e91f7ef0f2b5
Nicholas Tsim <nicholastsim@gmail.com>
2020-09-11 15:19:10 +0100

    EES-1325 Add more chart colour options

--- src/explore-education-statistics-common/src/modules/charts/util/chartUtils.ts
+++ src/explore-education-statistics-common/src/modules/charts/util/chartUtils.ts
@@ -1,11 +1,19 @@
 import { ChartSymbol } from '@common/modules/charts/types/chart';
 
 export const colours: string[] = [
-  '#4763a5',
-  '#f5a450',
   '#1d70b8',
-  '#800080',
-  '#C0C0C0',
+  '#912b88',
+  '#d53880',
+  '#f499be',
+  '#f47738',
+  '#b58840',
+  '#85994b',
+  '#28a197',
+  '#505a5f',
+  '#0b0c0c',
+  '#d4351c',
+  '#ffdd00',
+  '#00703c',
 ];
 
 export const symbols: ChartSymbol[] = [
