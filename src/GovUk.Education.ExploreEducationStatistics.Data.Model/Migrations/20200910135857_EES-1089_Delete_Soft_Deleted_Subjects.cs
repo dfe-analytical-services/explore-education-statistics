586eb42a7ecf8004831bfd434ae21393a883dad0
shakes <si@hiveit.co.uk>
2020-09-15 16:23:07 +0100

    EES-1089 Add DB housekeeping functionality to datafactory.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200910135857_EES-1089_Delete_Soft_Deleted_Subjects.cs
@@ -0,0 +1,21 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations.MigrationConstants;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    public partial class EES1089_Delete_Soft_Deleted_Subjects : Migration
+    {
+        private const string MigrationId = "20200910135857";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_Routine_RemoveSoftDeletedSubjects.sql");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE dbo.RemoveSoftDeletedSubjects");
+        }
+    }
+}
