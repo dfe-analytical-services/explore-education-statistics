cbc3aaa79ef73eb9e7c6b5a883ae35f8ff4cc283
shakes90 <si.shakes@gmail.com>
2020-09-18 11:36:46 +0100

    EES-1089 Add DB housekeeping functionality to datafactory. (#1992)
    EES-1089 Add DB housekeeping functionality to datafactory

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200910135829_EES-1089_Rebuild_Indexes.cs
@@ -0,0 +1,21 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations.MigrationConstants;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    public partial class EES1089_Rebuild_Indexes : Migration
+    {
+        private const string MigrationId = "20200910135829";
+        
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_Routine_RebuildIndexes.sql");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE dbo.RebuildIndexes");
+        }
+    }
+}
