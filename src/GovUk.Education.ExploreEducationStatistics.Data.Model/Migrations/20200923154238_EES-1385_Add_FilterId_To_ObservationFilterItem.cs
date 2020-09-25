245c26cdd1128c5ebd39d466476b30a41f3e766b
shakes <si@hiveit.co.uk>
2020-09-25 13:50:51 +0100

    EES-1385 Add FilterId to ObservationFilterItem type.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200923154238_EES-1385_Add_FilterId_To_ObservationFilterItem.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20200923154238_EES-1385_Add_FilterId_To_ObservationFilterItem.cs
@@ -8,6 +8,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
     public partial class EES1385_Add_FilterId_To_ObservationFilterItem : Migration
     {
         private const string MigrationId = "20200923154238";
+        private const string PreviousInsertObservationFilterItemsMigrationId = "20200304145743";
 
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -29,11 +30,20 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
                 principalColumn: "Id",
                 onDelete: ReferentialAction.Restrict);
             
+            migrationBuilder.Sql("DROP PROCEDURE dbo.InsertObservationFilterItems");
+            migrationBuilder.Sql("DROP TYPE dbo.ObservationFilterItemType");
+
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_Type_ObservationFilterItemType.sql");
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{PreviousInsertObservationFilterItemsMigrationId}_Routine_InsertObservationFilterItems.sql");
+            
             migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_Populate_Ofi_FilterId.sql");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("DROP PROCEDURE dbo.InsertObservations");
+            migrationBuilder.Sql("DROP TYPE dbo.ObservationFilterItemType");
+
             migrationBuilder.DropForeignKey(
                 name: "FK_ObservationFilterItem_Filter_FilterId",
                 table: "ObservationFilterItem");
@@ -45,6 +55,9 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
             migrationBuilder.DropColumn(
                 name: "FilterId",
                 table: "ObservationFilterItem");
+            
+            migrationBuilder.Sql("CREATE TYPE ObservationFilterItemType AS table( ObservationId uniqueidentifier NOT NULL, FilterItemId  uniqueidentifier NOT NULL)");
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{PreviousInsertObservationFilterItemsMigrationId}_Routine_InsertObservationFilterItems.sql");
         }
     }
 }
