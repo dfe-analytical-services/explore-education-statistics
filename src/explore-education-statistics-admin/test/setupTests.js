c3aa59f8dec0bdafd64a6e08da808af7e88ab7cd
Nicholas Tsim <nicholastsim@gmail.com>
2020-09-30 16:28:29 +0100

    test: increase test timeout to try prevent timing issue failures

--- src/explore-education-statistics-admin/test/setupTests.js
+++ src/explore-education-statistics-admin/test/setupTests.js
@@ -3,6 +3,8 @@ import 'core-js/features/array/flat-map';
 import '@common-test/setupGlobals';
 import '@common-test/extend-expect';
 
+jest.setTimeout(10000);
+
 if (typeof window !== 'undefined') {
   // fetch polyfill for making API calls.
   require('cross-fetch');
