1ce9f0be59a276af8bfb1b9a88a2c3921aae8550
Stuart Bennett <bennettstuart92@gmail.com>
2020-10-07 18:11:28 +0100

    added unit tests, check all inputs render, and check validations works as expected

--- src/explore-education-statistics-admin/src/pages/release/components/__tests__/ReleaseSummaryForm.test.tsx
+++ src/explore-education-statistics-admin/src/pages/release/components/__tests__/ReleaseSummaryForm.test.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import ReleaseSummaryForm, {
   ReleaseSummaryFormValues,
 } from '../ReleaseSummaryForm';
+import userEvent from '@testing-library/user-event';
 
 const metaService = _metaService as jest.Mocked<typeof _metaService>;
 
@@ -34,26 +35,6 @@ describe('ReleaseSummaryForm', () => {
         { identifier: { value: 'AYQ4', label: 'Academic Year Q4' } },
       ],
     },
-    // {
-    //   category: { value: 'CalendarYear', label: 'Calendar year' },
-    //   timeIdentifiers: [
-    //     { identifier: { value: 'CY', label: 'Calendar Year' } },
-    //     { identifier: { value: 'CYQ1', label: 'Calendar Year Q1' } },
-    //     { identifier: { value: 'CYQ2', label: 'Calendar Year Q2' } },
-    //     { identifier: { value: 'CYQ3', label: 'Calendar Year Q3' } },
-    //     { identifier: { value: 'CYQ4', label: 'Calendar Year Q4' } },
-    //   ],
-    // },
-    // {
-    //   category: { value: 'FinancialYear', label: 'Financial Year' },
-    //   timeIdentifiers: [
-    //     { identifier: { value: 'FY', label: 'Financial Year' } },
-    //     { identifier: { value: 'FYQ1', label: 'Financial Year Q1' } },
-    //     { identifier: { value: 'FYQ2', label: 'Financial Year Q2' } },
-    //     { identifier: { value: 'FYQ3', label: 'Financial Year Q3' } },
-    //     { identifier: { value: 'FYQ4', label: 'Financial Year Q4' } },
-    //   ],
-    // },
   ];
   test('renders correctly', async () => {
     metaService.getReleaseTypes.mockResolvedValue(testReleaseTypes);
@@ -82,12 +63,127 @@ describe('ReleaseSummaryForm', () => {
       ).toBeInTheDocument();
     });
 
-    const selectYearType = screen.getByLabelText('Type');
+    const selectYearType = screen.getByLabelText('Type', {
+      selector: 'select',
+    });
     expect(selectYearType).toBeInTheDocument();
 
     const inputYear = screen.getByLabelText(
       testTimeIdentifiers[0].category.label,
+      { selector: 'input' },
     );
     expect(inputYear).toBeInTheDocument();
+
+    testReleaseTypes.forEach(type => {
+      const radioReleaseType = screen.getByLabelText(type.title, {
+        selector: 'input',
+      });
+      expect(radioReleaseType).toBeInTheDocument();
+    });
+
+    const buttonCreate = screen.getByText('Create new release', {
+      selector: 'button',
+    });
+    expect(buttonCreate).toBeInTheDocument();
+    const buttonCancel = screen.getByText('Cancel', { selector: 'button' });
+    expect(buttonCancel).toBeInTheDocument();
+  });
+
+  test('validation error render where appropriate', async () => {
+    metaService.getReleaseTypes.mockResolvedValue(testReleaseTypes);
+    metaService.getTimePeriodCoverageGroups.mockResolvedValue(
+      testTimeIdentifiers,
+    );
+
+    render(
+      <ReleaseSummaryForm<ReleaseSummaryFormValues>
+        submitText="Create new release"
+        initialValues={() => {
+          return {
+            timePeriodCoverageCode: '',
+            timePeriodCoverageStartYear: '',
+            releaseTypeId: '',
+          };
+        }}
+        onSubmit={noop}
+        onCancel={noop}
+      />,
+    );
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Select time period coverage'),
+      ).toBeInTheDocument();
+    });
+
+    const inputYear = screen.getByLabelText(
+      testTimeIdentifiers[0].category.label,
+      { selector: 'input' },
+    );
+    const radioOptionsReleaseType = testReleaseTypes.map(type =>
+      screen.getByLabelText(type.title, {
+        selector: 'input',
+      }),
+    );
+    const buttonCreate = screen.getByText('Create new release', {
+      selector: 'button',
+    });
+
+    userEvent.click(buttonCreate);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Enter a year', { selector: 'a' }),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText('Choose a release type', { selector: 'a' }),
+      ).toBeInTheDocument();
+    });
+
+    userEvent.type(inputYear, '2');
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Year must be exactly 4 characters', {
+          selector: 'a',
+        }),
+      ).toBeInTheDocument();
+    });
+
+    userEvent.type(inputYear, '202021');
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Year must be exactly 4 characters', {
+          selector: 'a',
+        }),
+      ).toBeInTheDocument();
+    });
+
+    userEvent.clear(inputYear);
+    userEvent.type(inputYear, '1966');
+
+    await waitFor(() => {
+      expect(
+        screen.queryByText('Year must be exactly 4 characters', {
+          selector: 'a',
+        }),
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('Enter a year', {
+          selector: 'a',
+        }),
+      ).not.toBeInTheDocument();
+    });
+
+    userEvent.click(radioOptionsReleaseType[0]);
+
+    await waitFor(() => {
+      expect(
+        screen.queryByText('Choose a release type', {
+          selector: 'a',
+        }),
+      ).not.toBeInTheDocument();
+    });
   });
 });
