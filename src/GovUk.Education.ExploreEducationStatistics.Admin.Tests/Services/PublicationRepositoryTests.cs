5e339c531cbef9c035bc4ee9bbd9a27079ef9620
Mark Youngman <mark@hiveit.co.uk>
2020-11-02 12:02:08 +0000

    EES-1504 Unit test that dashboard releases are returned in the correct order

--- src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/PublicationRepositoryTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/PublicationRepositoryTests.cs
@@ -16,8 +16,9 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services
         public async void ReleasesCorrectlyOrdered()
         {
             var topicId = Guid.NewGuid();
+            var contextId = Guid.NewGuid().ToString();
 
-            using (var context = InMemoryApplicationDbContext("ReleasesCorrectlyOrdered"))
+            using (var context = InMemoryApplicationDbContext(contextId))
             {
                 context.Add(new Topic
                 {
@@ -78,69 +79,77 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services
                     }
                 });
                 context.SaveChanges();
-
-                using (var context = InMemoryApplicationDbContext("ReleasesCorrectlyOrdered"))
-                {
-                    var publicationService = new PublicationRepository(context, AdminMapper());
-                    var publications = await publicationService.GetAllPublicationsForTopicAsync(topicId);
-                    var releases = publications.Single().Releases;
-                }
             }
 
-            [Fact]
-            public async void LatestReleaseCorrectlyReportedInPublication()
+            using (var context = InMemoryApplicationDbContext(contextId))
             {
-                var latestReleaseId = Guid.NewGuid();
-                var notLatestReleaseId = Guid.NewGuid();
-                var topicId = Guid.NewGuid();
+                var publicationService = new PublicationRepository(context, AdminMapper());
+                var publications = await publicationService.GetAllPublicationsForTopicAsync(topicId);
+                var releases = publications.Single().Releases;
+                Assert.Equal("Week 1 2001", releases[0].Title);
+                Assert.Equal("Week 11 2000", releases[1].Title);
+                Assert.Equal("Week 3 2000", releases[2].Title);
+                Assert.Equal("Week 2 2000", releases[3].Title);
+                Assert.Equal("Week 1 2000", releases[4].Title);
+                Assert.Equal("Week 1 1999", releases[5].Title);
+            }
+        }
 
-                using (var context = InMemoryApplicationDbContext("LatestReleaseCorrectlyReportedInPublication"))
+        [Fact]
+        public async void LatestReleaseCorrectlyReportedInPublication()
+        {
+            var latestReleaseId = Guid.NewGuid();
+            var notLatestReleaseId = Guid.NewGuid();
+            var topicId = Guid.NewGuid();
+            var contextId = Guid.NewGuid().ToString();
+
+            using (var context = InMemoryApplicationDbContext(contextId))
+            {
+                context.Add(new Topic
                 {
-                    context.Add(new Topic
+                    Id = topicId,
+                    Publications = new List<Publication>
                     {
-                        Id = topicId,
-                        Publications = new List<Publication>
+                        new Publication
                         {
-                            new Publication
+                            Id = Guid.NewGuid(),
+                            Title = "Publication",
+                            TopicId = topicId,
+                            Releases = new List<Release>
                             {
-                                Id = Guid.NewGuid(),
-                                Title = "Publication",
-                                TopicId = topicId,
-                                Releases = new List<Release>
+                                new Release
                                 {
-                                    new Release
-                                    {
-                                        Id = notLatestReleaseId,
-                                        ReleaseName = "2019",
-                                        TimePeriodCoverage = TimeIdentifier.December,
-                                        Published = DateTime.UtcNow
-                                    },
-                                    new Release
-                                    {
-                                        Id = latestReleaseId,
-                                        ReleaseName = "2020",
-                                        TimePeriodCoverage = TimeIdentifier.June,
-                                        Published = DateTime.UtcNow
-                                    }
+                                    Id = notLatestReleaseId,
+                                    ReleaseName = "2019",
+                                    TimePeriodCoverage = TimeIdentifier.December,
+                                    Published = DateTime.UtcNow
+                                },
+                                new Release
+                                {
+                                    Id = latestReleaseId,
+                                    ReleaseName = "2020",
+                                    TimePeriodCoverage = TimeIdentifier.June,
+                                    Published = DateTime.UtcNow
                                 }
                             }
                         }
-                    });
-                    context.SaveChanges();
-                }
+                    }
+                });
+                context.SaveChanges();
+            }
 
-                using (var context = InMemoryApplicationDbContext("LatestReleaseCorrectlyReportedInPublication"))
-                {
-                    var publicationService = new PublicationRepository(context, AdminMapper());
+            using (var context = InMemoryApplicationDbContext(contextId))
+            {
+                var publicationService = new PublicationRepository(context, AdminMapper());
 
-                    // Method under test - this return a list of publication for a user. The releases in the publication
-                    // should correctly report whether they are the latest or not. Note that this is dependent on the mapper
-                    // that we are passing in.
-                    var publications = await publicationService.GetAllPublicationsForTopicAsync(topicId);
-                    var releases = publications.Single().Releases;
-                    Assert.True(releases.Exists(r => r.Id == latestReleaseId && r.LatestRelease));
-                    Assert.True(releases.Exists(r => r.Id == notLatestReleaseId && !r.LatestRelease));
-                }
+                // Method under test - this return a list of publication for a user. The releases in the publication
+                // should correctly report whether they are the latest or not. Note that this is dependent on the mapper
+                // that we are passing in.
+                var publications = await publicationService.GetAllPublicationsForTopicAsync(topicId);
+                var releases = publications.Single().Releases;
+                Assert.True(releases.Exists(r => r.Id == latestReleaseId && r.LatestRelease));
+                Assert.True(releases.Exists(r => r.Id == notLatestReleaseId && !r.LatestRelease));
             }
         }
     }
+}
