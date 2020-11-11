a0c364b91f93cbd5f73d6610f7c61102a1515429
Nicholas Tsim <nicholastsim@gmail.com>
2020-11-11 10:47:40 +0000

    EES-1598 Add migration to uppercase chart `Symbol` fields

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20201111104202_EES1598UppercaseDataBlockChartSymbols.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    public partial class EES1598UppercaseDataBlockChartSymbols : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+                UPDATE ContentBlock
+                SET DataBlock_Charts = REPLACE(DataBlock_Charts, '""symbol"":', '""Symbol"":');
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+                UPDATE ContentBlock
+                SET DataBlock_Charts = REPLACE(DataBlock_Charts, '""Symbol"":', '""symbol"":');
+            ");
+        }
+    }
+}
