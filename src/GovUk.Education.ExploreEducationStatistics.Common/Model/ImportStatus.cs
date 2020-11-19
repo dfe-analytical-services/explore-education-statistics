c74b16ac732486bcfba422a3e2842984ba8a2974
Duncan Watson <duncan@hiveit.co.uk>
2020-11-19 00:52:59 +0000

    EES-1606 - removed unused status-checking methods from ImportStatus after refacor of Processor

--- src/GovUk.Education.ExploreEducationStatistics.Common/Model/ImportStatus.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Model/ImportStatus.cs
@@ -19,36 +19,6 @@ namespace GovUk.Education.ExploreEducationStatistics.Common.Model
 
         public int NumberOfRows { get; set; }
 
-        public bool IsAfterArchiveProcessing()
-        {
-            return IsAfterStage(IStatus.PROCESSING_ARCHIVE_FILE);
-        }
-
-        public bool IsAfterStage1()
-        {
-            return IsAfterStage(IStatus.STAGE_1);
-        }
-        
-        public bool IsAfterStage2()
-        {
-            return IsAfterStage(IStatus.STAGE_2);
-        }
-        
-        public bool IsAfterStage3()
-        {
-            return IsAfterStage(IStatus.STAGE_3);
-        }
-        
-        public bool IsAfterStage4()
-        {
-            return IsAfterStage(IStatus.STAGE_4);
-        }
-        
-        public bool IsAfterStage(IStatus stage)
-        {
-            return Status.CompareTo(stage) > 0;
-        }
-
         public override string ToString()
         {
             return $"{Status} {PhasePercentageComplete}%, overall {PercentageComplete}%";
