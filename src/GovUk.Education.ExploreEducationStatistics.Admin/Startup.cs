3971d30f700128e0a12acbd16c19fc783404a20f
Mark Youngman <mark@hiveit.co.uk>
2020-11-23 13:09:33 +0000

    EES-1597 Add endpoint to get all incomplete imports

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Startup.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Startup.cs
@@ -208,6 +208,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin
             services.AddTransient<IReleaseDataFileService, ReleaseDataFileService>();
             services.AddTransient<IReleaseFileService, ReleaseFileService>();
             services.AddTransient<IImportService, ImportService>();
+            services.AddTransient<IImportStatusBauService, ImportStatusBauService>();
             services.AddTransient<IPublishingService, PublishingService>(provider =>
                 new PublishingService(
                     provider.GetService<IPersistenceHelper<ContentDbContext>>(),
