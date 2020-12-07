e35cc9387828a1b79ae57adf2f644ac8ab0d4c75
Mark Youngman <mark@hiveit.co.uk>
2020-12-07 08:28:09 +0000

    EES-169 Update imports/incomplete endpoint
    Updating the imports/incomplete endpoint in preparation for the
    associated frontend work.

--- src/GovUk.Education.ExploreEducationStatistics.Admin/ViewModels/ImportStatusBau.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/ViewModels/ImportStatusBau.cs
@@ -1,17 +1,20 @@
 using System;
 using GovUk.Education.ExploreEducationStatistics.Common.Services;
-using GovUk.Education.ExploreEducationStatistics.Data.Processor.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.ViewModels
 {
     public class ImportStatusBau
     {
+        public string SubjectTitle { get; set; }
         public Guid SubjectId { get; set; }
+        public Guid PublicationId { get; set; }
+        public Guid ReleaseId { get; set; }
         public string DataFileName { get; set; }
         public string MetaFileName { get; set; }
-        public Release Release { get; set; }
-        public string Errors { get; set; }
         public int NumberOfRows { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
         public IStatus Status { get; set; }
         public int StagePercentageComplete { get; set; }
     }
