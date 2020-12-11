d107f65cc440fe7e2f8f120061f3087105adc2fd
Ben Outram <ben@hiveit.co.uk>
2020-12-11 15:32:10 +0000

    EES-1703 Add Image to FileType.

--- src/GovUk.Education.ExploreEducationStatistics.Common/Model/FileType.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Model/FileType.cs
@@ -4,15 +4,17 @@ namespace GovUk.Education.ExploreEducationStatistics.Common.Model
 {
     public enum FileType
     {
+        [EnumLabelValue("ancillary")]
+        Ancillary,
+        [EnumLabelValue("chart")]
+        Chart,
         [EnumLabelValue("data")]
         Data,
         [EnumLabelValue("zip")]
         DataZip,
+        [EnumLabelValue("image")]
+        Image,
         [EnumLabelValue("metadata")]
-        Metadata,
-        [EnumLabelValue("ancillary")]
-        Ancillary,
-        [EnumLabelValue("chart")]
-        Chart
+        Metadata
     }
 }
