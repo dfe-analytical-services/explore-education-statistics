aec3a572636c04319b8f8a0474222fb367f9fe99
Duncan Watson <duncan@hiveit.co.uk>
2020-12-16 16:14:41 +0000

    EES-1553 - bring mocking of service dependencies more in line with more recent style in the codebase

--- src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/ImportServiceTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/ImportServiceTests.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Threading.Tasks;
+using AutoMapper;
 using GovUk.Education.ExploreEducationStatistics.Admin.Models;
 using GovUk.Education.ExploreEducationStatistics.Admin.Security;
 using GovUk.Education.ExploreEducationStatistics.Admin.Services;
 using GovUk.Education.ExploreEducationStatistics.Common.Services.Interfaces;
 using GovUk.Education.ExploreEducationStatistics.Common.Services.Interfaces.Security;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
 using GovUk.Education.ExploreEducationStatistics.Data.Processor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
 
@@ -36,15 +39,14 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services
                                                     && m.DataFileName == cancelRequest.DataFileName))).
                 Returns(Task.CompletedTask);
 
-            var service = new ImportService(
-                null, null, null, queueService.Object, null, null, userService.Object);
+            var service = BuildImportService(queueService: queueService.Object, userService: userService.Object);
             
             var result = await service.CancelImport(cancelRequest);
             Assert.True(result.IsRight);
             
             MockUtils.VerifyAllMocks(userService, queueService);
         }
-        
+
         [Fact]
         public async void CancelFileImportButNotAllowed()
         {
@@ -61,14 +63,32 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services
                 .Setup(s => s.MatchesPolicy(cancelRequest, SecurityPolicies.CanCancelOngoingImports))
                 .ReturnsAsync(false);
 
-            var service = new ImportService(
-                null, null, null, queueService.Object, null, null, userService.Object);
+            var service = BuildImportService(queueService: queueService.Object, userService: userService.Object);
             
             var result = await service.CancelImport(cancelRequest);
             Assert.True(result.IsLeft);
             Assert.IsType<ForbidResult>(result.Left);
 
             MockUtils.VerifyAllMocks(userService, queueService);
+        }
+
+        private static ImportService BuildImportService(
+            ContentDbContext context = null,
+            IMapper mapper = null,
+            ILogger<ImportService> logger = null,
+            ITableStorageService tableStorageService = null,
+            IGuidGenerator guidGenerator = null,
+            IStorageQueueService queueService = null,
+            IUserService userService = null)
+        {
+            return new ImportService(
+                context ?? DbUtils.InMemoryApplicationDbContext(), 
+                mapper ?? new Mock<IMapper>().Object, 
+                logger ?? new Mock<ILogger<ImportService>>().Object, 
+                queueService ?? new Mock<IStorageQueueService>().Object,
+                tableStorageService ?? new Mock<ITableStorageService>().Object, 
+                guidGenerator ?? new Mock<IGuidGenerator>().Object, 
+                userService ?? MockUtils.AlwaysTrueUserService().Object);
         }
     }
 }
