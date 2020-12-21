8bccf8abbf4db97d1143697712810d5a01875024
Duncan Watson <duncan@hiveit.co.uk>
2020-12-21 11:57:55 +0000

    EES-1744 - add extra checks to ensure that a CANCELLING import status is not accidentally misinterpreted as a failed import if the batch file messages are all in flight

--- src/GovUk.Education.ExploreEducationStatistics.Data.Processor/ValidationError.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Processor/ValidationError.cs
@@ -8,5 +8,23 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Processor
         {
             Message = message;
         }
+
+        private bool Equals(ValidationError other)
+        {
+            return Message == other.Message;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ValidationError) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Message != null ? Message.GetHashCode() : 0);
+        }
     }
 }
