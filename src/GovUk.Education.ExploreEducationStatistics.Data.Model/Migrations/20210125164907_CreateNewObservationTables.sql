e0ebbc432a73da5f62fcdd924b278e0c6a81175c
Duncan Watson <duncan@hiveit.co.uk>
2021-01-26 09:09:23 +0000

    EES-1830 - create new tables for Observations and ObservationFilterItems using an Identity Id column rather than a Guid.  Will be populated and used for future optimisation work.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20210125164907_CreateNewObservationTables.sql
@@ -0,0 +1,67 @@
+create table ObservationRow
+(
+    Id       bigint identity(1,1) PRIMARY KEY NOT NULL,
+    ObservationId              uniqueidentifier not null,
+    SubjectId       uniqueidentifier not null
+        constraint FK_ObservationRow_Subject_SubjectId
+            references Subject
+            on delete cascade,
+    GeographicLevel nvarchar(6)      not null,
+    LocationId      uniqueidentifier not null
+        constraint FK_ObservationRow_Location_LocationId
+            references Location,
+    Year            int              not null,
+    TimeIdentifier  nvarchar(6)      not null,
+    Measures        nvarchar(max),
+    CsvRow          bigint           not null
+)
+go
+
+create index IX_ObservationRow_GeographicLevel
+    on ObservationRow (GeographicLevel)
+go
+
+create index IX_ObservationRow_LocationId
+    on ObservationRow (LocationId)
+go
+
+create index IX_ObservationRow_SubjectId
+    on ObservationRow (SubjectId)
+go
+
+create index IX_ObservationRow_TimeIdentifier
+    on ObservationRow (TimeIdentifier)
+go
+
+create index IX_ObservationRow_Year
+    on ObservationRow (Year)
+go
+
+create index NCI_WI_ObservationRow_SubjectId
+    on ObservationRow (SubjectId) include (GeographicLevel, LocationId, TimeIdentifier, Year)
+go
+
+create table ObservationRowFilterItem
+(
+    ObservationId bigint not null
+        constraint FK_ObservationRowFilterItem_ObservationRow_Id
+            references ObservationRow (Id)
+            on delete cascade,
+    OldObservationId uniqueidentifier not null,
+    FilterItemId  uniqueidentifier not null
+        constraint FK_ObservationRowFilterItem_FilterItem_FilterItemId
+            references FilterItem,
+    FilterId      uniqueidentifier
+        constraint FK_ObservationRowFilterItem_Filter_FilterId
+            references Filter,
+    constraint PK_ObservationRowFilterItem
+        primary key (ObservationId, FilterItemId)
+)
+go
+
+create index IX_ObservationRowFilterItem_FilterItemId
+    on ObservationRowFilterItem (FilterItemId)
+
+create index IX_ObservationRowFilterItem_FilterId
+    on ObservationRowFilterItem (FilterId)
+go
