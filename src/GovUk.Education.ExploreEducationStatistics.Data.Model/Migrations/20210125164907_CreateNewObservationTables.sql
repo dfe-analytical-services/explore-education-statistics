6267f5a926ef30591b7f7f287a952e8edadacb4f
Duncan Watson <duncan@hiveit.co.uk>
2021-01-26 17:05:08 +0000

    EES-1830 - remove constraints from new tables during the migration process to greatly increase the speed of migration into the new tale structure

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20210125164907_CreateNewObservationTables.sql
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20210125164907_CreateNewObservationTables.sql
@@ -1,15 +1,22 @@
+--
+-- Leaving in foreign keys and indexes as commented-out lines to show the constraints that 
+-- would have come as a part of standard CREATE scripts for these tables.  We can achieve
+-- migration from Observation and ObservationFilterItem tables in half the time without 
+-- these constraints in place, then we will reinstate these constraints at a later time when 
+-- we have migrated the bulk of records over.
+--
 create table ObservationRow
 (
     Id              bigint identity(1,1) NOT NULL,
     ObservationId   uniqueidentifier not null,
-    SubjectId       uniqueidentifier not null
-        CONSTRAINT FK_ObservationRow_Subject_SubjectId
-            references Subject
-            on delete cascade,
+    SubjectId       uniqueidentifier not null,
+--         CONSTRAINT FK_ObservationRow_Subject_SubjectId
+--             references Subject
+--             on delete cascade,
     GeographicLevel nvarchar(6)      not null,
-    LocationId      uniqueidentifier not null
-        constraint FK_ObservationRow_Location_LocationId
-            references Location,
+    LocationId      uniqueidentifier not null,
+--         constraint FK_ObservationRow_Location_LocationId
+--             references Location,
     Year            int              not null,
     TimeIdentifier  nvarchar(6)      not null,
     Measures        nvarchar(max),
@@ -18,55 +25,55 @@ create table ObservationRow
 )
 go
 
-create index IX_ObservationRow_GeographicLevel
-    on ObservationRow (GeographicLevel)
-go
-
-create index IX_ObservationRow_LocationId
-    on ObservationRow (LocationId)
-go
-
-create index IX_ObservationRow_SubjectId
-    on ObservationRow (SubjectId)
-go
-
-create index IX_ObservationRow_TimeIdentifier
-    on ObservationRow (TimeIdentifier)
-go
-
-create index IX_ObservationRow_Year
-    on ObservationRow (Year)
-go
-
-create index NCI_WI_ObservationRow_SubjectId
-    on ObservationRow (SubjectId) include (GeographicLevel, LocationId, TimeIdentifier, Year)
-go
+-- create index IX_ObservationRow_GeographicLevel
+--     on ObservationRow (GeographicLevel)
+-- go
+-- 
+-- create index IX_ObservationRow_LocationId
+--     on ObservationRow (LocationId)
+-- go
+-- 
+-- create index IX_ObservationRow_SubjectId
+--     on ObservationRow (SubjectId)
+-- go
+-- 
+-- create index IX_ObservationRow_TimeIdentifier
+--     on ObservationRow (TimeIdentifier)
+-- go
+-- 
+-- create index IX_ObservationRow_Year
+--     on ObservationRow (Year)
+-- go
+-- 
+-- create index NCI_WI_ObservationRow_SubjectId
+--     on ObservationRow (SubjectId) include (GeographicLevel, LocationId, TimeIdentifier, Year)
+-- go
 
 create table ObservationRowFilterItem
 (
     Id            bigint identity(1,1) NOT NULL,
-    ObservationId bigint not null
-        constraint FK_ObservationRowFilterItem_ObservationRow_Id
-            references ObservationRow (Id)
-            on delete cascade,
+    ObservationId bigint not null,
+--         constraint FK_ObservationRowFilterItem_ObservationRow_Id
+--             references ObservationRow (Id)
+--             on delete cascade,
     OldObservationId uniqueidentifier not null,
-    FilterItemId  uniqueidentifier not null
-        constraint FK_ObservationRowFilterItem_FilterItem_FilterItemId
-            references FilterItem,
-    FilterId      uniqueidentifier
-        constraint FK_ObservationRowFilterItem_Filter_FilterId
-            references Filter,
+    FilterItemId  uniqueidentifier not null,
+--         constraint FK_ObservationRowFilterItem_FilterItem_FilterItemId
+--             references FilterItem,
+    FilterId      uniqueidentifier,
+--         constraint FK_ObservationRowFilterItem_Filter_FilterId
+--             references Filter,
     constraint PK_ObservationRowFilterItem
         primary key (Id)
 )
 go
 
-create index IX_ObservationRowFilterItem_FilterItemId_ObservationId
-    on ObservationRowFilterItem (FilterItemId, ObservationId)
-
-create index IX_ObservationRowFilterItem_FilterItemId
-    on ObservationRowFilterItem (FilterItemId)
-
-create index IX_ObservationRowFilterItem_FilterId
-    on ObservationRowFilterItem (FilterId)
-go
+-- create index IX_ObservationRowFilterItem_FilterItemId_ObservationId
+--     on ObservationRowFilterItem (FilterItemId, ObservationId)
+-- 
+-- create index IX_ObservationRowFilterItem_FilterItemId
+--     on ObservationRowFilterItem (FilterItemId)
+-- 
+-- create index IX_ObservationRowFilterItem_FilterId
+--     on ObservationRowFilterItem (FilterId)
+-- go
