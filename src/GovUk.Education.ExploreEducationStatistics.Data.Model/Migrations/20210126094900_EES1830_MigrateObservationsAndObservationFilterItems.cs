1d894fff562363dd1aa88e52f57b009cf10c1740
Duncan Watson <duncan@hiveit.co.uk>
2021-01-26 14:38:30 +0000

    EES-1830 - add stored procedure to migrate rows from Observation and ObservationFilterItem into new tables.  Call stored procedure from Data Factory trigger

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20210126094900_EES1830_MigrateObservationsAndObservationFilterItems.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    public partial class EES1830_MigrateObservationsAndObservationFilterItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
