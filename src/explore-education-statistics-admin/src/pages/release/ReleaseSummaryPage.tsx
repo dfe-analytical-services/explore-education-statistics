36d2f53960f4275ea2f402122c049329995e1fca
Lewis Toseland <lewis@hiveit.co.uk>
2021-01-27 14:59:26 +0000

    EES-1508-Split-release-summary-into-two-tables

--- src/explore-education-statistics-admin/src/pages/release/ReleaseSummaryPage.tsx
+++ src/explore-education-statistics-admin/src/pages/release/ReleaseSummaryPage.tsx
@@ -39,34 +39,23 @@ const ReleaseSummaryPage = () => {
             These details will be shown to users to help identify this release.
           </p>
 
+          <h3>Publication details</h3>
           <SummaryList>
             <SummaryListItem term="Publication title">
               {publication.title}
             </SummaryListItem>
+            <SummaryListItem term="Lead statistician">
+              {publication.contact && publication.contact.contactName}
+            </SummaryListItem>
+          </SummaryList>
+
+          <h3>Release summary</h3>
+          <SummaryList>
             <SummaryListItem term="Time period">
               {release.timePeriodCoverage.label}
             </SummaryListItem>
             <SummaryListItem term="Release period">
               <time>{release.title}</time>
-            </SummaryListItem>
-            <SummaryListItem term="Lead statistician">
-              {publication.contact && publication.contact.contactName}
-            </SummaryListItem>
-            <SummaryListItem term="Scheduled release">
-              {release.publishScheduled ? (
-                <FormattedDate>
-                  {parseISO(release.publishScheduled)}
-                </FormattedDate>
-              ) : (
-                'Not scheduled'
-              )}
-            </SummaryListItem>
-            <SummaryListItem term="Next release expected">
-              {isValidPartialDate(release.nextReleaseDate) ? (
-                <time>{formatPartialDate(release.nextReleaseDate)}</time>
-              ) : (
-                'Not set'
-              )}
             </SummaryListItem>
             <SummaryListItem term="Release type">
               {release.type.title}
