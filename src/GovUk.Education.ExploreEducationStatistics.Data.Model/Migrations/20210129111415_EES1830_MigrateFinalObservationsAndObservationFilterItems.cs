81aadf714d0388f86a1e661d4140f5922237db9d
Duncan Watson <duncan@hiveit.co.uk>
2021-01-29 11:53:33 +0000

    EES-1836 - added migration to perform the migration of the final Observation and ObservationFilterItem rows into the new tables before using

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20210129111415_EES1830_MigrateFinalObservationsAndObservationFilterItems.cs
@@ -0,0 +1,23 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations.MigrationConstants;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    public partial class EES1830_MigrateFinalObservationsAndObservationFilterItems : Migration
+    {
+        private const string MigrationId = "20210129111415";
+        private const string PreviousMigrationId = "20210126094900";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_MigrateFinalObservationsAndObservationFilterItems.sql");
+            migrationBuilder.Sql("DROP PROCEDURE MigrateObservationsAndObservationFilterItems");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{PreviousMigrationId}_Routine_MigrateObservationsAndObservationFilterItems.sql");
+        }
+    }
+}
