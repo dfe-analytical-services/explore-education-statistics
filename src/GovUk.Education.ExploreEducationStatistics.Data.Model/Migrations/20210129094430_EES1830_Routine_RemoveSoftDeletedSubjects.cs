b9b185efe781ee0b2dbbc90914d46003399234af
Duncan Watson <duncan@hiveit.co.uk>
2021-01-29 10:52:20 +0000

    EES-1830 - add missing stored procedure migration file.  Add current versions of sotred procedure "Routine" files so that future diffs are easier to review.  Add weekday and weekend triggers to Subject migration triggers

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20210129094430_EES1830_Routine_RemoveSoftDeletedSubjects.cs
@@ -0,0 +1,22 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations.MigrationConstants;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    public partial class EES1830_Routine_RemoveSoftDeletedSubjects : Migration
+    {
+        private const string MigrationId = "20210129094430";
+        private const string PreviousMigrationId = "20201120140950";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_Routine_RemoveSoftDeletedSubjects.sql");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{PreviousMigrationId}_Routine_RemoveSoftDeletedSubjects.sql");
+        }
+    }
+}
