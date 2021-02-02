ffe5631e090f5719e411ca01090f2f36e3a39f13
Mark Youngman <mark@hiveit.co.uk>
2021-02-02 11:28:39 +0000

    EES-1692 Create MethodologyRepository unit tests

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/Methodologies/MethodologyRepositoryTests.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Admin.Services.Methodologies;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using Xunit;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services.Methodologies
+{
+    public class MethodologyRepositoryTests
+    {
+        [Fact]
+        public async Task UserHasReleaseRoleAssociatedWithMethodology_ReleaseRoleLead()
+        {
+            var userId = Guid.NewGuid();
+            var publication = new Publication
+            {
+                Id = Guid.NewGuid()
+            };
+            var release = new Release
+            {
+                Id = Guid.NewGuid(),
+                PublicationId = publication.Id
+            };
+            var userReleaseRole = new UserReleaseRole
+            {
+                UserId = userId,
+                Release = release,
+                Role = ReleaseRole.Lead,
+            };
+            var methodology = new Methodology
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test methodology",
+                Publications = new List<Publication> {publication}
+            };
+
+            var contentDbContextId = Guid.NewGuid().ToString();
+            await using (var contentDbContext = DbUtils.InMemoryApplicationDbContext(contentDbContextId))
+            {
+                await contentDbContext.AddAsync(publication);
+                await contentDbContext.AddAsync(release);
+                await contentDbContext.AddAsync(userReleaseRole);
+                await contentDbContext.AddAsync(methodology);
+                await contentDbContext.SaveChangesAsync();
+            }
+
+            await using (var contentDbContext = DbUtils.InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var methodologyRepository = new MethodologyRepository(contentDbContext);
+                Assert.True(
+                    await methodologyRepository.UserHasReleaseRoleAssociatedWithMethodology(userId, methodology));
+            }
+        }
+
+        [Fact]
+        public async Task GetMethodologiesForUser_ReleaseRoleLead()
+        {
+            var userId = Guid.NewGuid();
+            var publication = new Publication
+            {
+                Id = Guid.NewGuid()
+            };
+            var release = new Release
+            {
+                Id = Guid.NewGuid(),
+                PublicationId = publication.Id
+            };
+            var userReleaseRole = new UserReleaseRole
+            {
+                UserId = userId,
+                Release = release,
+                Role = ReleaseRole.Lead,
+            };
+            var methodology = new Methodology
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test methodology",
+                Publications = new List<Publication> {publication}
+            };
+
+            var contentDbContextId = Guid.NewGuid().ToString();
+            await using (var contentDbContext = DbUtils.InMemoryApplicationDbContext(contentDbContextId))
+            {
+                await contentDbContext.AddAsync(publication);
+                await contentDbContext.AddAsync(release);
+                await contentDbContext.AddAsync(userReleaseRole);
+                await contentDbContext.AddAsync(methodology);
+                await contentDbContext.SaveChangesAsync();
+            }
+
+            await using (var contentDbContext = DbUtils.InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var methodologyRepository = new MethodologyRepository(contentDbContext);
+                var methodologies = await methodologyRepository.GetMethodologiesForUser(userId);
+
+                Assert.Single(methodologies);
+                Assert.Equal(methodology.Id, methodologies[0].Id);
+                Assert.Equal(methodology.Title, methodologies[0].Title);
+            }
+        }
+
+        [Fact]
+        public async Task UserHasReleaseRoleAssociatedWithMethodology_ReleaseRolePrerelease()
+        {
+            var userId = Guid.NewGuid();
+            var publication = new Publication
+            {
+                Id = Guid.NewGuid()
+            };
+            var release = new Release
+            {
+                Id = Guid.NewGuid(),
+                PublicationId = publication.Id
+            };
+            var userReleaseRole = new UserReleaseRole
+            {
+                UserId = userId,
+                Release = release,
+                Role = ReleaseRole.PrereleaseViewer,
+            };
+            var methodology = new Methodology
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test methodology",
+                Publications = new List<Publication> {publication}
+            };
+
+            var contentDbContextId = Guid.NewGuid().ToString();
+            await using (var contentDbContext = DbUtils.InMemoryApplicationDbContext(contentDbContextId))
+            {
+                await contentDbContext.AddAsync(publication);
+                await contentDbContext.AddAsync(release);
+                await contentDbContext.AddAsync(userReleaseRole);
+                await contentDbContext.AddAsync(methodology);
+                await contentDbContext.SaveChangesAsync();
+            }
+
+            await using (var contentDbContext = DbUtils.InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var methodologyRepository = new MethodologyRepository(contentDbContext);
+                Assert.False(
+                    await methodologyRepository.UserHasReleaseRoleAssociatedWithMethodology(userId, methodology));
+            }
+        }
+
+        [Fact]
+        public async Task GetMethodologiesForUser_ReleaseRolePrerelease()
+        {
+            var userId = Guid.NewGuid();
+            var publication = new Publication
+            {
+                Id = Guid.NewGuid()
+            };
+            var release = new Release
+            {
+                Id = Guid.NewGuid(),
+                PublicationId = publication.Id
+            };
+            var userReleaseRole = new UserReleaseRole
+            {
+                UserId = userId,
+                Release = release,
+                Role = ReleaseRole.PrereleaseViewer,
+            };
+            var methodology = new Methodology
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test methodology",
+                Publications = new List<Publication> {publication}
+            };
+
+            var contentDbContextId = Guid.NewGuid().ToString();
+            await using (var contentDbContext = DbUtils.InMemoryApplicationDbContext(contentDbContextId))
+            {
+                await contentDbContext.AddAsync(publication);
+                await contentDbContext.AddAsync(release);
+                await contentDbContext.AddAsync(userReleaseRole);
+                await contentDbContext.AddAsync(methodology);
+                await contentDbContext.SaveChangesAsync();
+            }
+
+            await using (var contentDbContext = DbUtils.InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var methodologyRepository = new MethodologyRepository(contentDbContext);
+                var methodologies = await methodologyRepository.GetMethodologiesForUser(userId);
+
+                Assert.Empty(methodologies);
+            }
+        }
+        
+        [Fact]
+        public async Task GetMethodologiesForUser_Multiple()
+        {
+            var userId = Guid.NewGuid();
+            var publication1 = new Publication
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test pub 1"
+            };
+            var release1 = new Release
+            {
+                Id = Guid.NewGuid(),
+                PublicationId = publication1.Id
+            };
+            var userReleaseRole1 = new UserReleaseRole
+            {
+                UserId = userId,
+                Release = release1,
+                Role = ReleaseRole.Lead,
+            };
+            var methodology1 = new Methodology
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test methodology 1",
+                Publications = new List<Publication> {publication1}
+            };
+
+            var publication2 = new Publication
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test pub 2"
+            };
+            var release2 = new Release
+            {
+                Id = Guid.NewGuid(),
+                PublicationId = publication2.Id
+            };
+            var userReleaseRole2 = new UserReleaseRole
+            {
+                UserId = userId,
+                Release = release2,
+                Role = ReleaseRole.Contributor,
+            };
+            var methodology2 = new Methodology
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test methodology 2",
+                Publications = new List<Publication> {publication2}
+            };
+            
+            var publication3 = new Publication
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test pub 3"
+            };
+            var release3 = new Release
+            {
+                Id = Guid.NewGuid(),
+                PublicationId = publication3.Id
+            };
+            var userReleaseRole3 = new UserReleaseRole
+            {
+                UserId = Guid.NewGuid(),
+                Release = release3,
+                Role = ReleaseRole.Lead,
+            };
+            var methodology3 = new Methodology
+            {
+                Id = Guid.NewGuid(),
+                Title = "Ignored methodology 3",
+                Publications = new List<Publication> {publication3}
+            };
+            
+            var methodology4 = new Methodology
+            {
+                Id = Guid.NewGuid(),
+                Title = "Ignored methodology 3",
+                Publications = new List<Publication>()
+            };
+            
+            var contentDbContextId = Guid.NewGuid().ToString();
+            await using (var contentDbContext = DbUtils.InMemoryApplicationDbContext(contentDbContextId))
+            {
+                await contentDbContext.AddAsync(publication1);
+                await contentDbContext.AddAsync(release1);
+                await contentDbContext.AddAsync(userReleaseRole1);
+                await contentDbContext.AddAsync(methodology1);
+                
+                await contentDbContext.AddAsync(publication2);
+                await contentDbContext.AddAsync(release2);
+                await contentDbContext.AddAsync(userReleaseRole2);
+                await contentDbContext.AddAsync(methodology2);
+                
+                await contentDbContext.AddAsync(publication3);
+                await contentDbContext.AddAsync(release3);
+                await contentDbContext.AddAsync(userReleaseRole3);
+                await contentDbContext.AddAsync(methodology3);
+                
+                await contentDbContext.AddAsync(methodology4);
+                
+                await contentDbContext.SaveChangesAsync();
+            }
+
+            await using (var contentDbContext = DbUtils.InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var methodologyRepository = new MethodologyRepository(contentDbContext);
+                var methodologies = await methodologyRepository.GetMethodologiesForUser(userId);
+
+                Assert.Equal(2, methodologies.Count);
+                Assert.Contains(methodologies, m => m.Id == methodology1.Id);
+                Assert.Contains(methodologies, m => m.Id == methodology2.Id);
+                Assert.DoesNotContain(methodologies, m => m.Id == methodology3.Id);
+                Assert.DoesNotContain(methodologies, m => m.Id == methodology4.Id);
+            }
+        }
+        
+        
+    }
+}
