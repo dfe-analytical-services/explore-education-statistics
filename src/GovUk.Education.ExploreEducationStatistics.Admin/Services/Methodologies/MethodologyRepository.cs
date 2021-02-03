a2a2e8b2fdefb11215f3a3a1b8e0495b2c43e886
Mark Youngman <mark@hiveit.co.uk>
2021-02-03 16:04:00 +0000

    EES-1692 Access methodologies through publications

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Methodologies/MethodologyRepository.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Methodologies/MethodologyRepository.cs
@@ -23,45 +23,26 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Methodologie
             Guid userId,
             Guid methodologyId)
         {
-            var methodology = await _contentDbContext.Methodologies.FindAsync(methodologyId);
-            await _contentDbContext.Entry(methodology)
-                .Collection(m => m.Publications)
-                .LoadAsync();
-
-            var viewablePublications = await GetViewablePublications(userId);
-
-            return viewablePublications
-                .Any(p1 => methodology.Publications
-                    .Select(p2 => p2.Id)
-                    .Contains(p1.Id));
-        }
-
-        public async Task<List<Methodology>> GetMethodologiesForUser(Guid userId)
-        {
-            var viewablePublications = await GetViewablePublications(userId);
-
-            var allMethodologies = await _contentDbContext.Methodologies
-                .Include(m => m.Publications)
-                .ToListAsync();
-
-            return allMethodologies
-                .Where(m => viewablePublications
-                    .Any(p1 => m.Publications
-                        .Select(p2 => p2.Id)
-                        .Contains(p1.Id)))
-                .OrderBy(m => m.Title)
-                .ToList();
-        }
-
-        private async Task<List<Publication>> GetViewablePublications(Guid userId)
-        {
             return await _contentDbContext.UserReleaseRoles
                 .Include(urr => urr.Release)
                 .ThenInclude(r => r.Publication)
                 .Where(urr =>
                     urr.UserId == userId
+                    && urr.Role != ReleaseRole.PrereleaseViewer
+                    && urr.Release.Publication.MethodologyId == methodologyId)
+                .AnyAsync();
+        }
+
+        public async Task<List<Methodology>> GetMethodologiesForUser(Guid userId)
+        {
+            return await _contentDbContext.UserReleaseRoles
+                .Include(urr => urr.Release)
+                .ThenInclude(r => r.Publication)
+                .ThenInclude(p => p.Methodology)
+                .Where(urr =>
+                    urr.UserId == userId
                     && urr.Role != ReleaseRole.PrereleaseViewer)
-                .Select(urr => urr.Release.Publication)
+                .Select(urr => urr.Release.Publication.Methodology)
                 .Distinct()
                 .ToListAsync();
         }
