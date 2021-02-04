fd623e470337f3f92ed65d000088743145bd3a23
Nicholas Tsim <nicholastsim@gmail.com>
2021-02-04 12:41:32 +0000

    EES-1848 Replace directory test data with DataBlockPageTabs test data

--- src/explore-education-statistics-admin/src/pages/release/datablocks/components/__tests__/DataBlockPageTabs.test.tsx
+++ src/explore-education-statistics-admin/src/pages/release/datablocks/components/__tests__/DataBlockPageTabs.test.tsx
@@ -1,11 +1,12 @@
+import {
+  testSubjectMeta,
+  testTableData,
+} from '@admin/pages/release/datablocks/__data__/tableToolServiceData';
 import DataBlockPageTabs from '@admin/pages/release/datablocks/components/DataBlockPageTabs';
 import _dataBlockService, {
   ReleaseDataBlock,
 } from '@admin/services/dataBlockService';
-import _tableBuilderService, {
-  SubjectMeta,
-  TableDataResponse,
-} from '@common/services/tableBuilderService';
+import _tableBuilderService from '@common/services/tableBuilderService';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import noop from 'lodash/noop';
@@ -22,114 +23,6 @@ const tableBuilderService = _tableBuilderService as jest.Mocked<
 >;
 
 describe('DataBlockPageTabs', () => {
-  const testSubjectMeta: SubjectMeta = {
-    filters: {
-      Characteristic: {
-        totalValue: '',
-        hint: 'Filter by pupil characteristic',
-        legend: 'Characteristic',
-        name: 'characteristic',
-        options: {
-          Gender: {
-            label: 'Gender',
-            options: [
-              {
-                label: 'Gender female',
-                value: 'gender-female',
-              },
-            ],
-          },
-        },
-      },
-    },
-    indicators: {
-      AbsenceFields: {
-        label: 'Absence fields',
-        options: [
-          {
-            value: 'authorised-absence-sessions',
-            label: 'Number of authorised absence sessions',
-            unit: '',
-            name: 'sess_authorised',
-            decimalPlaces: 2,
-          },
-        ],
-      },
-    },
-    locations: {
-      localAuthority: {
-        legend: 'Local authority',
-        options: [{ value: 'barnet', label: 'Barnet' }],
-      },
-    },
-    timePeriod: {
-      legend: 'Time period',
-      options: [{ label: '2020/21', code: 'AY', year: 2020 }],
-    },
-  };
-
-  const testTableData: TableDataResponse = {
-    subjectMeta: {
-      publicationName: '',
-      boundaryLevels: [],
-      footnotes: [],
-      subjectName: 'Subject 1',
-      geoJsonAvailable: false,
-      locations: [
-        {
-          level: 'localAuthority',
-          label: 'Barnet',
-          value: 'barnet',
-        },
-      ],
-      timePeriodRange: [{ code: 'AY', year: 2020, label: '2020/21' }],
-      indicators: [
-        {
-          value: 'authorised-absence-sessions',
-          label: 'Number of authorised absence sessions',
-          unit: '',
-          name: 'sess_authorised',
-          decimalPlaces: 2,
-        },
-      ],
-      filters: {
-        Characteristic: {
-          totalValue: '',
-          hint: 'Filter by pupil characteristic',
-          legend: 'Characteristic',
-          name: 'characteristic',
-          options: {
-            Gender: {
-              label: 'Gender',
-              options: [
-                {
-                  label: 'Gender female',
-                  value: 'gender-female',
-                },
-              ],
-            },
-          },
-        },
-      },
-    },
-    results: [
-      {
-        timePeriod: '2020_AY',
-        measures: {
-          'authorised-absence-sessions': '123',
-        },
-        location: {
-          localAuthority: {
-            name: 'Barnet',
-            code: 'barnet',
-          },
-        },
-        geographicLevel: 'localAuthority',
-        filters: ['gender-female'],
-      },
-    ],
-  };
-
   const testDataBlock: ReleaseDataBlock = {
     name: 'Test data block',
     heading: 'Test title',
