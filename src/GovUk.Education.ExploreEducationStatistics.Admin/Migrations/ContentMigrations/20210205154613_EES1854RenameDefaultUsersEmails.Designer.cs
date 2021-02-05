b40a9fed3b6dd0357a1eaad184950af86375c772
Mark Youngman <mark@hiveit.co.uk>
2021-02-05 15:46:47 +0000

    EES-1854 Create ContentDb migration to rename default users' emails

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20210205154613_EES1854RenameDefaultUsersEmails.Designer.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20210205154613_EES1854RenameDefaultUsersEmails.Designer.cs
@@ -4,14 +4,16 @@ using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
 {
     [DbContext(typeof(ContentDbContext))]
-    partial class ContentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210205154613_EES1854RenameDefaultUsersEmails")]
+    partial class EES1854RenameDefaultUsersEmails
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
