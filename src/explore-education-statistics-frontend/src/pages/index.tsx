f004716595a8cbb614ae80fe0ece9a7e88015118
Nicholas Tsim <nicholastsim@gmail.com>
2021-02-11 15:36:22 +0000

    EES-1895 Cleanup markup for public home page

--- src/explore-education-statistics-frontend/src/pages/index.tsx
+++ src/explore-education-statistics-frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import ButtonLink from '@frontend/components/ButtonLink';
 import React from 'react';
 import Link from '../components/Link';
 import Page from '../components/Page';
@@ -13,18 +14,19 @@ function HomePage() {
       <div className="govuk-grid-row dfe-card__container">
         <div className="govuk-grid-column-one-half dfe-card__item">
           <div className="dfe-card">
-            <h2 className="govuk-heading-l govuk-!-margin-bottom-0">
+            <h2 className="govuk-!-margin-bottom-0">
               Find statistics and data
             </h2>
+
             <p className="govuk-!-margin-top-2">
               Browse to find statistical summaries and explanations to help you
               understand and analyse our range of national and regional
               statistics and data.
             </p>
-            <Link
+            <ButtonLink
               to="/find-statistics"
               data-testid="home--find-statistics-link"
-              className="govuk-button govuk-button--start"
+              className="govuk-button--start"
             >
               Explore
               <svg
@@ -38,22 +40,21 @@ function HomePage() {
               >
                 <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
               </svg>
-            </Link>
+            </ButtonLink>
           </div>
         </div>
         <div className="govuk-grid-column-one-half dfe-card__item">
           <div className="dfe-card">
-            <h2 className="govuk-heading-l govuk-!-margin-bottom-0">
-              Create your own tables
-            </h2>
+            <h2 className="govuk-!-margin-bottom-0">Create your own tables</h2>
+
             <p className="govuk-!-margin-top-2">
               Use our tool to build tables using our range of national and
               regional data.
             </p>
-            <Link
+            <ButtonLink
               to="/data-tables"
               data-testid="home--table-tool-link"
-              className="govuk-button govuk-button--start"
+              className="govuk-button--start"
             >
               Create
               <svg
@@ -67,23 +68,23 @@ function HomePage() {
               >
                 <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
               </svg>
-            </Link>
+            </ButtonLink>
           </div>
         </div>
       </div>
-      <h2 className="govuk-heading-l govuk-!-margin-top-6">
-        Supporting information
-      </h2>
+
+      <h2 className="govuk-!-margin-top-6">Supporting information</h2>
+
       <div className="govuk-grid-row">
         <div className="govuk-grid-column-two-thirds">
-          <h3 className="govuk-heading-m govuk-!-margin-bottom-1">
+          <h3 className="govuk-!-margin-bottom-1">
             <Link to="/methodology">Education statistics: methodology</Link>
           </h3>
           <p className="govuk-caption-m">
             Browse to find out more about the methodology behind education
             statistics and data and how and why they're collected and published.
           </p>
-          <h3 className="govuk-heading-m govuk-!-margin-bottom-1">
+          <h3 className="govuk-!-margin-bottom-1">
             <Link to="/glossary">Education statistics: glossary</Link>
           </h3>
           <p className="govuk-caption-m">
@@ -94,18 +95,18 @@ function HomePage() {
       </div>
 
       <hr />
-      <h3 className="govuk-heading-l govuk-!-margin-top-9">Related services</h3>
-      <p className="govuk-body">
+
+      <h2 className="govuk-!-margin-top-9">Related services</h2>
+
+      <p>
         Use these services to find specific performance and other information
         about schools and colleges in England:
       </p>
+
       <div className="govuk-grid-row govuk-!-margin-bottom-3">
         <div className="govuk-grid-column-one-half">
-          <h4 className="govuk-heading-s govuk-!-margin-bottom-0">
-            <a
-              className="govuk-link"
-              href="https://www.gov.uk/school-performance-tables"
-            >
+          <h4 className="govuk-!-margin-bottom-0">
+            <a href="https://www.gov.uk/school-performance-tables">
               Find and compare schools in England
             </a>
           </h4>
@@ -115,11 +116,8 @@ function HomePage() {
           </p>
         </div>
         <div className="govuk-grid-column-one-half">
-          <h4 className="govuk-heading-s govuk-!-margin-bottom-0">
-            <a
-              className="govuk-link"
-              href="https://www.get-information-schools.service.gov.uk/"
-            >
+          <h4 className="govuk-!-margin-bottom-0">
+            <a href="https://www.get-information-schools.service.gov.uk/">
               Get information about schools
             </a>
           </h4>
@@ -129,13 +127,11 @@ function HomePage() {
           </p>
         </div>
       </div>
+
       <div className="govuk-grid-row govuk-!-margin-bottom-9">
         <div className="govuk-grid-column-one-half">
-          <h4 className="govuk-heading-s govuk-!-margin-bottom-0">
-            <a
-              className="govuk-link"
-              href="https://schools-financial-benchmarking.service.gov.uk/"
-            >
+          <h4 className="govuk-!-margin-bottom-0">
+            <a href="https://schools-financial-benchmarking.service.gov.uk/">
               Schools financial benchmarking
             </a>
           </h4>
@@ -145,11 +141,8 @@ function HomePage() {
           </p>
         </div>
         <div className="govuk-grid-column-one-half">
-          <h4 className="govuk-heading-s govuk-!-margin-bottom-0">
-            <a
-              className="govuk-link"
-              href="https://www.gov.uk/government/organisations/department-for-education/about/statistics"
-            >
+          <h4 className="govuk-!-margin-bottom-0">
+            <a href="https://www.gov.uk/government/organisations/department-for-education/about/statistics">
               Statistics at DfE
             </a>
           </h4>
@@ -160,14 +153,17 @@ function HomePage() {
           </p>
         </div>
       </div>
+
       <hr />
-      <h3 className="govuk-heading-l govuk-!-margin-top-9">Contact Us</h3>
-      <p className="govuk-body govuk-!-margin-top-1">
+
+      <h2 className="govuk-!-margin-top-9">Contact Us</h2>
+
+      <p className="govuk-!-margin-top-1">
         If you need help and support or have a question about education
         statistics and data contact:
       </p>
 
-      <p className="govuk-body govuk-!-margin-top-1">
+      <p className="govuk-!-margin-top-1">
         <strong>Explore education statistics team</strong>
       </p>
 
