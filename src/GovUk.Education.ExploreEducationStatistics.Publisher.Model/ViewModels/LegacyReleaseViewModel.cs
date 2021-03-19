ba6145e3b6870b060e2872b1ba2c66ed3db43964
Ben Outram <ben@hiveit.co.uk>
2021-03-19 09:43:49 +0000

    Formatting

--- src/GovUk.Education.ExploreEducationStatistics.Publisher.Model/ViewModels/LegacyReleaseViewModel.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher.Model/ViewModels/LegacyReleaseViewModel.cs
@@ -5,7 +5,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Publisher.Model.ViewModels
     public class LegacyReleaseViewModel
     {
         public Guid Id { get; set; }
-        
+
         public string Description { get; set; }
 
         public string Url { get; set; }
