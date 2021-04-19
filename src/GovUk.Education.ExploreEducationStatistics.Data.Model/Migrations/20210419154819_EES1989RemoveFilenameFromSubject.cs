eb7f2f442a6ba78a94023ad7bda12e10aa668f49
Mark Youngman <mark@hiveit.co.uk>
2021-04-19 16:49:15 +0100

    EES-1989 Create migration to remove Filename from Subject

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20210419154819_EES1989RemoveFilenameFromSubject.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    public partial class EES1989RemoveFilenameFromSubject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Filename",
+                table: "Subject");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Filename",
+                table: "Subject",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
