a629d0ce0ab449e6bd8502c44abc3337056b5be4
Duncan Watson <duncan@hiveit.co.uk>
2021-04-26 09:08:11 +0100

    Revert "Revert "EES-2046 - backing out infrastructure and code changes for EES-1836 whilst attempting to provide better performance analysis""
    This reverts commit 475ec9866d69835313c0a06a85cca6630aed2fef.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/ObservationFilterItem.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/ObservationFilterItem.cs
@@ -1,15 +1,14 @@
 using System;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Model
 {
-    [Table("ObservationRowFilterItem")]
     public class ObservationFilterItem
     {
         public Observation Observation { get; set; }
-        public long ObservationId { get; set; }
-        public Guid OldObservationId { get; set; }
+        public Guid ObservationId { get; set; }
         public FilterItem FilterItem { get; set; }
         public Guid FilterItemId { get; set; }
+        public Filter Filter { get; set; }
+        public Guid? FilterId { get; set; }
     }
 }
