30fa007e3ac0f2e02c8170dfb3844844f6493fe6
Ben Outram <ben@hiveit.co.uk>
2021-05-19 09:47:14 +0100

    EES-2131 Add UserPublicationRoleRepositoryTests and UserReleaseRoleRepositoryTests

--- src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/UserPublicationRoleRepositoryTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/UserPublicationRoleRepositoryTests.cs
@@ -1,17 +1,86 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Admin.Services;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
+using Xunit;
+using static GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services.DbUtils;
+using static GovUk.Education.ExploreEducationStatistics.Content.Model.PublicationRole;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services
 {
     public class UserPublicationRoleRepositoryTests
     {
+        [Fact]
         public async Task Create()
         {
-            // TODO EES-2131
+            var userId = Guid.NewGuid();
+            var publicationId = Guid.NewGuid();
+            
+            var contentDbContextId = Guid.NewGuid().ToString();
+
+            await using (var contentDbContext = InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var service = SetupUserPublicationRoleRepository(contentDbContext);
+
+                var result = await service.Create(userId, publicationId, Owner);
+
+                Assert.NotEqual(Guid.Empty, result.Id);
+                Assert.Equal(userId, result.UserId);
+                Assert.Equal(publicationId, result.PublicationId);
+                Assert.Equal(Owner, result.Role);
+            }
+
+            await using (var contentDbContext = InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var userPublicationRoles = contentDbContext.UserPublicationRoles.ToList();
+                Assert.Single(userPublicationRoles);
+
+                Assert.NotEqual(Guid.Empty, userPublicationRoles[0].Id);
+                Assert.Equal(userId, userPublicationRoles[0].UserId);
+                Assert.Equal(publicationId, userPublicationRoles[0].PublicationId);
+                Assert.Equal(Owner, userPublicationRoles[0].Role);
+            }
         }
 
+        [Fact]
         public async Task GetByRole()
         {
-            // TODO EES-2131
+            var userPublicationRole = new UserPublicationRole
+            {
+                User = new User(),
+                Publication = new Publication(),
+                Role = Owner
+            };
+
+            var contentDbContextId = Guid.NewGuid().ToString();
+
+            await using (var contentDbContext = InMemoryApplicationDbContext(contentDbContextId))
+            {
+                await contentDbContext.AddAsync(userPublicationRole);
+                await contentDbContext.SaveChangesAsync();
+            }
+
+            await using (var contentDbContext = InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var service = SetupUserPublicationRoleRepository(contentDbContext);
+
+                var result = await service.GetByRole(userPublicationRole.UserId, userPublicationRole.PublicationId, Owner);
+                
+                Assert.Equal(userPublicationRole.Id, result.Id);
+                Assert.Equal(userPublicationRole.UserId, result.UserId);
+                Assert.Equal(userPublicationRole.PublicationId, result.PublicationId);
+                Assert.Equal(Owner, result.Role);
+            }
+        }
+
+        private static UserPublicationRoleRepository SetupUserPublicationRoleRepository(
+            ContentDbContext contentDbContext)
+        {
+            return new UserPublicationRoleRepository(
+                contentDbContext
+            );
         }
     }
 }
--- src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/UserReleaseRoleRepositoryTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/UserReleaseRoleRepositoryTests.cs
@@ -1,17 +1,86 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Admin.Services;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
+using Xunit;
+using static GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services.DbUtils;
+using static GovUk.Education.ExploreEducationStatistics.Content.Model.ReleaseRole;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services
 {
     public class UserReleaseRoleRepositoryTests
     {
+        [Fact]
         public async Task Create()
         {
-            // TODO EES-2131
+            var userId = Guid.NewGuid();
+            var releaseId = Guid.NewGuid();
+
+            var contentDbContextId = Guid.NewGuid().ToString();
+
+            await using (var contentDbContext = InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var service = SetupUserReleaseRoleRepository(contentDbContext);
+
+                var result = await service.Create(userId, releaseId, Contributor);
+
+                Assert.NotEqual(Guid.Empty, result.Id);
+                Assert.Equal(userId, result.UserId);
+                Assert.Equal(releaseId, result.ReleaseId);
+                Assert.Equal(Contributor, result.Role);
+            }
+
+            await using (var contentDbContext = InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var userReleaseRoles = contentDbContext.UserReleaseRoles.ToList();
+                Assert.Single(userReleaseRoles);
+
+                Assert.NotEqual(Guid.Empty, userReleaseRoles[0].Id);
+                Assert.Equal(userId, userReleaseRoles[0].UserId);
+                Assert.Equal(releaseId, userReleaseRoles[0].ReleaseId);
+                Assert.Equal(Contributor, userReleaseRoles[0].Role);
+            }
         }
 
+        [Fact]
         public async Task GetByRole()
         {
-            // TODO EES-2131
+            var userReleaseRole = new UserReleaseRole
+            {
+                User = new User(),
+                Release = new Release(),
+                Role = Contributor
+            };
+
+            var contentDbContextId = Guid.NewGuid().ToString();
+
+            await using (var contentDbContext = InMemoryApplicationDbContext(contentDbContextId))
+            {
+                await contentDbContext.AddAsync(userReleaseRole);
+                await contentDbContext.SaveChangesAsync();
+            }
+
+            await using (var contentDbContext = InMemoryApplicationDbContext(contentDbContextId))
+            {
+                var service = SetupUserReleaseRoleRepository(contentDbContext);
+
+                var result = await service.GetByRole(userReleaseRole.UserId, userReleaseRole.ReleaseId, Contributor);
+
+                Assert.Equal(userReleaseRole.Id, result.Id);
+                Assert.Equal(userReleaseRole.UserId, result.UserId);
+                Assert.Equal(userReleaseRole.ReleaseId, result.ReleaseId);
+                Assert.Equal(Contributor, result.Role);
+            }
+        }
+
+        private static UserReleaseRoleRepository SetupUserReleaseRoleRepository(
+            ContentDbContext contentDbContext)
+        {
+            return new UserReleaseRoleRepository(
+                contentDbContext
+            );
         }
     }
 }
