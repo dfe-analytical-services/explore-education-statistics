65033836512e05b1d7ae8e4f1a4fbb23899da704
Duncan Watson <duncan@hiveit.co.uk>
2021-05-25 09:56:05 +0100

    EES-2106 - class comments added

--- src/GovUk.Education.ExploreEducationStatistics.Common/Services/Security/UserClaimTypes.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Services/Security/UserClaimTypes.cs
@@ -1,5 +1,6 @@
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Security
 {
+    // A set of Claims specific to the service that can be placed on Users' JWTs
     public enum UserClaimTypes
     {
         // the User Id for the user in the "internal" Users table as opposed to the AspNetUsers table 
