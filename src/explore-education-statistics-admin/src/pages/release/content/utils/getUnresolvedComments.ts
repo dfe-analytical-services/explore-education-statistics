98b100c06bc8243d0d25cfa073c03d9094e75f80
amyb-hiveit <81572860+amyb-hiveit@users.noreply.github.com>
2021-06-16 16:44:49 +0100

    EES-2338 fix editing, deleting and num of unresolved comments

---
+++ src/explore-education-statistics-admin/src/pages/release/content/utils/getUnresolvedComments.ts
@@ -0,0 +1,38 @@
+import { EditableRelease } from '@admin/services/releaseContentService';
+import { Comment, EditableBlock } from '@admin/services/types/content';
+import { ContentSection } from '@common/services/publicationService';
+
+const getContentSectionComments = (
+  contentSection: ContentSection<EditableBlock>,
+): Comment[] => {
+  if (contentSection.content?.length) {
+    return contentSection.content.reduce<Comment[]>(
+      (allCommentsForSection, content) => {
+        content.comments.forEach(comment =>
+          allCommentsForSection.push(comment),
+        );
+        return allCommentsForSection;
+      },
+      [],
+    );
+  }
+
+  return [];
+};
+
+const getUnresolvedComments = (release: EditableRelease) =>
+  [
+    ...getContentSectionComments(release.summarySection),
+    ...getContentSectionComments(release.keyStatisticsSection),
+    ...release.content
+      .filter(_ => _.content !== undefined)
+      .reduce<Comment[]>(
+        (allComments, contentSection) => [
+          ...allComments,
+          ...getContentSectionComments(contentSection),
+        ],
+        [],
+      ),
+  ].filter(comment => comment !== undefined);
+
+export default getUnresolvedComments;
