6dde93347a6fd6220b829224185ced2688cba806
Duncan Watson <duncan@hiveit.co.uk>
2021-06-18 11:53:16 +0100

    EES-2222 - adding printing keyword support to running tests

--- tests/robot-tests/listeners/KeywordListener.py
+++ tests/robot-tests/listeners/KeywordListener.py
@@ -1,15 +1,5 @@
-import os.path
-import tempfile
-from logging import warn
-
 class KeywordListener:
     ROBOT_LISTENER_API_VERSION = 2
     
-#     def start_suite(self, suite, result):
-#         suite.tests.create(name='New test')
-#     
-#     def start_test(self, test, result):
-#         warn('Test started')
-#         
     def start_keyword(self, name, attributes):
         print(f'\t{attributes["kwname"]}   ${attributes["args"]}')
