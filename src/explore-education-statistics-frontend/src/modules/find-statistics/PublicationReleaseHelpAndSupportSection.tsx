2cf283da4562e106321ed7306080c0acb7b9b166
Ben Outram <ben@hiveit.co.uk>
2021-06-18 10:43:10 +0100

    EES-2182 EES-2183 Fix condition for including the Methodology section

--- src/explore-education-statistics-frontend/src/modules/find-statistics/PublicationReleaseHelpAndSupportSection.tsx
+++ src/explore-education-statistics-frontend/src/modules/find-statistics/PublicationReleaseHelpAndSupportSection.tsx
@@ -46,35 +46,34 @@ const PublicationReleaseHelpAndSupportSection = ({
         includeAnalytics={includeAnalytics}
         publicationTitle={publicationTitle}
       >
-        {methodologies.length > 0 ||
-          (externalMethodology && (
-            <AccordionSection
-              heading="Methodology"
-              caption="Find out how and why we collect, process and publish these statistics"
-              headingTag="h3"
-            >
-              {methodologies.map(methodology => (
-                <p key={methodology.id} className="govuk-!-margin-bottom-9">
-                  <Link
-                    to="/methodology/[methodology]"
-                    as={`/methodology/${methodology.slug}`}
-                  >
-                    {methodology.title}
-                  </Link>
-                </p>
-              ))}
-              {externalMethodology && (
-                <p className="govuk-!-margin-bottom-9">
-                  <Link
-                    to="/methodology/[methodology]"
-                    as={externalMethodology.url}
-                  >
-                    {externalMethodology.title}
-                  </Link>
-                </p>
-              )}
-            </AccordionSection>
-          ))}
+        {(methodologies.length > 0 || externalMethodology) && (
+          <AccordionSection
+            heading="Methodology"
+            caption="Find out how and why we collect, process and publish these statistics"
+            headingTag="h3"
+          >
+            {methodologies.map(methodology => (
+              <p key={methodology.id} className="govuk-!-margin-bottom-9">
+                <Link
+                  to="/methodology/[methodology]"
+                  as={`/methodology/${methodology.slug}`}
+                >
+                  {methodology.title}
+                </Link>
+              </p>
+            ))}
+            {externalMethodology && (
+              <p className="govuk-!-margin-bottom-9">
+                <Link
+                  to="/methodology/[methodology]"
+                  as={externalMethodology.url}
+                >
+                  {externalMethodology.title}
+                </Link>
+              </p>
+            )}
+          </AccordionSection>
+        )}
         {releaseType === ReleaseType.NationalStatistics && (
           <AccordionSection heading="National Statistics" headingTag="h3">
             <NationalStatisticsSection />
