019951c73d223e42e730b035c95f42139a5cb3de
amyb-hiveit <81572860+amyb-hiveit@users.noreply.github.com>
2021-06-22 15:48:14 +0100

    EES-2413 toggle set page view box

--- src/explore-education-statistics-admin/src/components/editable/EditablePageModeToggle.tsx
+++ src/explore-education-statistics-admin/src/components/editable/EditablePageModeToggle.tsx
@@ -1,34 +1,53 @@
 import { useEditingContext } from '@admin/contexts/EditingContext';
 import FormRadioGroup from '@common/components/form/FormRadioGroup';
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import styles from './EditablePageModeToggle.module.scss';
 
 const EditablePageModeToggle = () => {
   const { isEditing, setEditing } = useEditingContext();
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <div className={styles.toggle}>
-      <FormRadioGroup
-        id="pageMode"
-        name="pageMode"
-        className={styles.fieldset}
-        value={isEditing ? 'edit' : 'preview'}
-        legend="Set page view"
-        small
-        options={[
-          {
-            label: 'Add / view comments and edit content',
-            value: 'edit',
-          },
-          {
-            label: 'Preview content',
-            value: 'preview',
-          },
-        ]}
-        onChange={event => {
-          setEditing(event.target.value === 'edit');
-        }}
-      />
+    <div
+      className={classNames(styles.toggle, {
+        [styles.open]: isOpen,
+      })}
+    >
+      <button
+        type="button"
+        id="pageViewToggleButton"
+        onClick={() => setIsOpen(!isOpen)}
+        className={styles.button}
+        aria-expanded={isOpen}
+      >
+        Set page view
+        <span className="govuk-accordion__icon" aria-hidden />
+      </button>
+      <div aria-labelledby="pageViewToggleButton" className={styles.content}>
+        <FormRadioGroup
+          id="pageMode"
+          name="pageMode"
+          className={styles.fieldset}
+          value={isEditing ? 'edit' : 'preview'}
+          legend="Set page view"
+          legendHidden
+          small
+          options={[
+            {
+              label: 'Add / view comments and edit content',
+              value: 'edit',
+            },
+            {
+              label: 'Preview content',
+              value: 'preview',
+            },
+          ]}
+          onChange={event => {
+            setEditing(event.target.value === 'edit');
+          }}
+        />
+      </div>
     </div>
   );
 };
