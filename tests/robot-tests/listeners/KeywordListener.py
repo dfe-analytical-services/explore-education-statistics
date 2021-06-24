6020b0397d0952ff063bcad1b947903789c09118
Duncan Watson <duncan@hiveit.co.uk>
2021-06-24 09:46:54 +0100

    EES-2222 - added clearer differentiation between test suite keywords and common keywords in KeywordListener.  Updated utilities.py to check type of parent_locator arg to avoid looking up parent element if the parent_locator is already a WebElement

--- tests/robot-tests/listeners/KeywordListener.py
+++ tests/robot-tests/listeners/KeywordListener.py
@@ -4,13 +4,23 @@ from robot.running.context import EXECUTION_CONTEXTS
 class KeywordListener:
     ROBOT_LISTENER_API_VERSION = 2
     
+    TEST_SUITE_KEYWORD_COLOUR = '\n\033[93m'
+    
+    COMMON_KEYWORD_COLOUR = '\033[94m\t'
+    
     def start_keyword(self, name, attributes):
-        if attributes["type"].lower() == "keyword" and not attributes["libname"] in ["BuiltIn", "SeleniumLibrary"] and not attributes["source"] is None:
+        
+        source = attributes["source"]
+    
+        if attributes["type"].lower() == "keyword" and not attributes["libname"] in ["BuiltIn", "SeleniumLibrary"] and source is not None:
             args = attributes["args"]
             values = EXECUTION_CONTEXTS.current.namespace.variables.replace_list(args)
             args_and_values = list(zip(args, values))
             args_and_value_string = "\t".join(map(lambda kvp: f'{kvp[0]}={kvp[1]}', args_and_values))
-            print(f'\033[94m\tfile://{attributes["source"]} line {attributes["lineno"]}\t{attributes["kwname"]}\t\t{args_and_value_string}')
+            
+            logging_colour = KeywordListener.COMMON_KEYWORD_COLOUR if source.endswith('common.robot') else KeywordListener.TEST_SUITE_KEYWORD_COLOUR
+            
+            print(f'{logging_colour}\tfile://{attributes["source"]} line {attributes["lineno"]}\t{attributes["kwname"]}\t\t{args_and_value_string}')
 
     def end_keyword(self, name, attributes):
         if attributes["status"] == "FAIL":
