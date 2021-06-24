02a3db701b7d342caec7de6d41635ee4a0fa35e7
Duncan Watson <duncan@hiveit.co.uk>
2021-06-25 00:50:34 +0100

    EES-2222 - amending test result merging behaviour to count the test as passed if it passed in either the original run or the rerun

--- tests/robot-tests/report-modifiers/CheckForAtLeastOnePassingRunPrerebotModifier.py
+++ tests/robot-tests/report-modifiers/CheckForAtLeastOnePassingRunPrerebotModifier.py
@@ -7,7 +7,7 @@ class CheckForAtLeastOnePassingRunPrerebotModifier(SuiteVisitor):
         pass
 
     def visit_test(self, test):
-        if test.status == 'PASS' and 'Test has been re-executed and results merged.' in test.message:
+        if 'PASS' in test.message and 'Test has been re-executed and results merged.' in test.message:
             info(f"CheckForAtLeastOnePassingRunPrerebotModifier - marking test \"{test}\" as PASS because it passed in at least one of the test runs.")
             test.status = 'PASS'
             test.message = f'Marking test \"{test}\" as PASS because it passed in at least one of the test runs.  Previous message is {test.message}'
