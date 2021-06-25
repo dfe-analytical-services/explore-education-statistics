2c5766e0af38be56086f5f3cb4a376f22111e99a
Ben Outram <ben@hiveit.co.uk>
2021-06-25 11:15:33 +0100

    EES-2201 Show methodology status on dashboard

--- src/GovUk.Education.ExploreEducationStatistics.Admin/ViewModels/MyMethodologyViewModel.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/ViewModels/MyMethodologyViewModel.cs
@@ -1,9 +1,15 @@
 using GovUk.Education.ExploreEducationStatistics.Admin.ViewModels.Methodology;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.ViewModels
 {
     public class MyMethodologyViewModel : MethodologyTitleViewModel
     {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public MethodologyStatus Status { get; set; }
+
         public PermissionsSet Permissions { get; set; }
 
         public class PermissionsSet
