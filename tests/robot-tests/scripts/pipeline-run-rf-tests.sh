3f26d173df20ed18090bf52b5339d88f59ac8a17
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2021-06-30 13:28:01 +0100

    EES-2418

--- tests/robot-tests/scripts/pipeline-run-rf-tests.sh
+++ tests/robot-tests/scripts/pipeline-run-rf-tests.sh
@@ -11,4 +11,4 @@ google-chrome-stable --version
 python -m pip install --upgrade pip
 pip install pipenv
 pipenv install
-pipenv run python run_tests.py --admin-pass $1 --analyst-pass $2 -e $3 --ci --file $4 --rerun-failed-suites
+pipenv run python run_tests.py --admin-pass $1 --analyst-pass $2 -e $3 --ci --file $4 --rerun-failed-tests
