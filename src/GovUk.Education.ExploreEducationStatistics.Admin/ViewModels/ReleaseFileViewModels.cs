d7f01b1121fc585b367d916561a003bd0c1e34f3
Nicholas Tsim <nicholastsim@gmail.com>
2021-07-08 15:55:36 +0100

    EES-2363 Re-work release file rename endpoint to typical update endpoint
    This changes the existing endpoint for renaming files to a more
    standard endpoint to update the file. To make this endpoint a little
    more flexible, we're going with a PATCH based implementation so that
    we only need to modify specific fields when making requests.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/ViewModels/ReleaseFileViewModels.cs
@@ -0,0 +1,8 @@
+#nullable enable
+namespace GovUk.Education.ExploreEducationStatistics.Admin.ViewModels
+{
+    public class ReleaseFileUpdateViewModel
+    {
+        public string? Name { get; set; }
+    }
+}
