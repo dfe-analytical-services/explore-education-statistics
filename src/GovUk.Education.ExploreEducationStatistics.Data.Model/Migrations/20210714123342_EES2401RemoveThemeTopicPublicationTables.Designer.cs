135c9638810a694c9d6a38dd528aae3f10415c97
Mark Youngman <mark@hiveit.co.uk>
2021-07-14 13:45:51 +0100

    EES-2401 Drop Theme/Topic/Publication tables from Statistics DB

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20210714123342_EES2401RemoveThemeTopicPublicationTables.Designer.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20210714123342_EES2401RemoveThemeTopicPublicationTables.Designer.cs
@@ -4,14 +4,16 @@ using GovUk.Education.ExploreEducationStatistics.Data.Model.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
 {
     [DbContext(typeof(StatisticsDbContext))]
-    partial class StatisticsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210714123342_EES2401RemoveThemeTopicPublicationTables")]
+    partial class EES2401RemoveThemeTopicPublicationTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
