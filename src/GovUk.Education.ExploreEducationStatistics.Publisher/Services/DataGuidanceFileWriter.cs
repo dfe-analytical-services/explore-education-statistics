85c68d88172277ad687f8161f2c4afac7dcaea71
Nicholas Tsim <nicholastsim@gmail.com>
2021-07-21 20:56:33 +0100

    EES-1768 Add footnotes to data guidance text file

--- src/GovUk.Education.ExploreEducationStatistics.Publisher/Services/DataGuidanceFileWriter.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher/Services/DataGuidanceFileWriter.cs
@@ -121,12 +121,12 @@ namespace GovUk.Education.ExploreEducationStatistics.Publisher.Services
                         await file.WriteLineAsync($"Content summary: {content}");
                     }
 
-                    // TODO EES-1768 - Add footnotes
-
                     var variables = subject.Variables
-                        .Where(variable =>
-                            !variable.Label.IsNullOrWhitespace()
-                            || !variable.Value.IsNullOrWhitespace())
+                        .Where(
+                            variable =>
+                                !variable.Label.IsNullOrWhitespace()
+                                || !variable.Value.IsNullOrWhitespace()
+                        )
                         .ToList();
 
                     if (variables.Any())
@@ -173,6 +173,43 @@ namespace GovUk.Education.ExploreEducationStatistics.Publisher.Services
                                 );
                             }
                         );
+                    }
+
+                    var footnotes = subject.Footnotes
+                        .Where(footnote => !footnote.Label.IsNullOrWhitespace())
+                        .ToList();
+
+                    if (footnotes.Any())
+                    {
+                        await file.WriteLineAsync();
+                        await file.WriteLineAsync("Footnotes:");
+                        await file.WriteLineAsync();
+
+                        await footnotes
+                            .ForEachAsync(
+                                async (footnote, footnoteIndex) =>
+                                {
+                                    await file.WriteAsync($"{footnoteIndex + 1}. ");
+
+                                    var indexWidth = (footnoteIndex + 1).ToString().Length;
+                                    var indent = string.Empty.PadRight(indexWidth + 2);
+
+                                    await footnote.Label
+                                        .ToLines()
+                                        .ForEachAsync(
+                                            async (line, lineIndex) =>
+                                            {
+                                                if (lineIndex == 0)
+                                                {
+                                                    await file.WriteLineAsync(line);
+                                                    return;
+                                                }
+
+                                                await file.WriteLineAsync(indent + line);
+                                            }
+                                        );
+                                }
+                            );
                     }
 
                     // Add some extra lines between data files
