5c378c3cb10155ba45f5aa245169f297f97bc7ef
Ben Outram <ben@hiveit.co.uk>
2021-08-11 09:20:09 +0100

    EES-2261 Remove ApproveAllMethodologies claim from Analyst role

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/UsersAndRolesMigrations/20210811081814_EES2162_RemoveApproveAllMethodologiesClaimFromAnalystRole.Designer.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/UsersAndRolesMigrations/20210811081814_EES2162_RemoveApproveAllMethodologiesClaimFromAnalystRole.Designer.cs
@@ -4,14 +4,16 @@ using GovUk.Education.ExploreEducationStatistics.Admin.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.UsersAndRolesMigrations
 {
     [DbContext(typeof(UsersAndRolesDbContext))]
-    partial class UsersAndRolesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210811081814_EES2162_RemoveApproveAllMethodologiesClaimFromAnalystRole")]
+    partial class EES2162_RemoveApproveAllMethodologiesClaimFromAnalystRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
