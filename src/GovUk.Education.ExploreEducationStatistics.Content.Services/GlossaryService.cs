7eb5ff6ba9bd1c30d1149dff495dc7e4d4c64973
Mark Youngman <mark@hiveit.co.uk>
2021-09-13 14:53:18 +0100

    EES-2676 Update GetAllGlossaryEntries to make single DB call

--- src/GovUk.Education.ExploreEducationStatistics.Content.Services/GlossaryService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Services/GlossaryService.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GovUk.Education.ExploreEducationStatistics.Common.Cache;
+using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
 using GovUk.Education.ExploreEducationStatistics.Content.Services.Cache;
 using GovUk.Education.ExploreEducationStatistics.Content.Services.Interfaces;
 using GovUk.Education.ExploreEducationStatistics.Content.Services.ViewModels;
-using Microsoft.EntityFrameworkCore;
 
 namespace GovUk.Education.ExploreEducationStatistics.Content.Services
 {
@@ -24,29 +24,34 @@ namespace GovUk.Education.ExploreEducationStatistics.Content.Services
         public async Task<List<GlossaryCategoryViewModel>> GetAllGlossaryEntries()
         {
             var categories = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            var categoryDictionary = categories.ToDictionary(
+                c => c.ToString(),
+                c => new GlossaryCategoryViewModel
+                {
+                    Heading = c.ToString(),
+                    Entries = new List<GlossaryEntryViewModel>(),
+                });
 
-            return await categories
-                    .ToAsyncEnumerable()
-                    .SelectAwait(async category =>
+            var allGlossaryEntries = _contentDbContext.GlossaryEntries
+                .ToList();
+
+            allGlossaryEntries
+                .OrderBy(e => e.Title)
+                .ForEach(e =>
+                {
+                    categoryDictionary[e.Title[..1].ToUpper()].Entries
+                        .Add(new GlossaryEntryViewModel
                     {
-                        var entries = await _contentDbContext.GlossaryEntries.AsQueryable()
-                            .Where(e => e.Title.Substring(0, 1).ToUpper() == category.ToString())
-                            .Select(e => new GlossaryEntryViewModel
-                            {
-                                Title = e.Title,
-                                Slug = e.Slug,
-                                Body = e.Body,
-                            })
-                            .OrderBy(e => e.Title)
-                            .ToListAsync();
-                        return new GlossaryCategoryViewModel
-                        {
-                            Heading = category.ToString(),
-                            Entries = entries,
-                        };
-                    })
-                    .OrderBy(c => c.Heading)
-                    .ToListAsync();
+                        Title = e.Title,
+                        Slug = e.Slug,
+                        Body = e.Body,
+                    });
+                });
+
+            return categories
+                .Select(category => categoryDictionary[category.ToString()])
+                .OrderBy(c => c.Heading)
+                .ToList();
         }
     }
 }
