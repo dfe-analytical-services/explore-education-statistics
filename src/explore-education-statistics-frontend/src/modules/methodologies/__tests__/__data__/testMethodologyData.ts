76ed703416f281c98e83bcc118b46026098ec795
Amy Benson <amybenson@hiveit.co.uk>
2021-09-21 12:06:55 +0100

    EES-2717 more methodology page unit tests

--- src/explore-education-statistics-frontend/src/modules/methodologies/__tests__/__data__/testMethodologyData.ts
+++ src/explore-education-statistics-frontend/src/modules/methodologies/__tests__/__data__/testMethodologyData.ts
@@ -1,25 +1,92 @@
 import { Methodology } from '@common/services/methodologyService';
 
 export const testMethodology: Methodology = {
-  id: '0d31edaa-e750-4f39-90c4-9e21774f1ef3',
+  id: 'methodology-1',
   title: 'Pupil absence statistics: methodology',
   published: '2021-02-16T15:32:01.2216362',
   slug: 'pupil-absence-in-schools-in-england',
-  content: [],
-  annexes: [],
+  content: [
+    {
+      order: 0,
+      heading: 'Section 1',
+      caption: 'Section 1 caption',
+      content: [
+        {
+          body: '<p>section 1 content</p>',
+          id: 'section-1-content',
+          order: 0,
+          type: 'HtmlBlock',
+        },
+      ],
+    },
+    {
+      order: 1,
+      heading: 'Section 2',
+      caption: 'Section 2 caption',
+      content: [
+        {
+          body: '<p>section 2 content</p>',
+          id: 'section-2-content',
+          order: 0,
+          type: 'HtmlBlock',
+        },
+      ],
+    },
+  ],
+  annexes: [
+    {
+      order: 0,
+      heading: 'Annex 1',
+      caption: 'Annex 1 caption',
+      content: [
+        {
+          body: '<p>annex 1 content</p>',
+          id: 'annex-1-content',
+          order: 0,
+          type: 'HtmlBlock',
+        },
+      ],
+    },
+    {
+      order: 1,
+      heading: 'Annex 2',
+      caption: 'Annex 2 caption',
+      content: [
+        {
+          body: '<p>annex 2 content</p>',
+          id: 'annex-2-content',
+          order: 0,
+          type: 'HtmlBlock',
+        },
+      ],
+    },
+    {
+      order: 2,
+      heading: 'Annex 3',
+      caption: 'Annex 3 caption',
+      content: [
+        {
+          body: '<p>annex 3 content</p>',
+          id: 'annex-3-content',
+          order: 0,
+          type: 'HtmlBlock',
+        },
+      ],
+    },
+  ],
   notes: [
     {
-      id: '274d624c-2a60-4239-b9a3-a948df66e990',
+      id: 'note-1',
       displayDate: new Date('2021-09-15T00:00:00'),
       content: 'Latest note',
     },
     {
-      id: '30a2457e-910c-438e-806e-781adde29769',
+      id: 'note-2',
       displayDate: new Date('2021-04-19T00:00:00'),
       content: 'Other note',
     },
     {
-      id: 'aa571c68-00d7-48ed-8f53-b783f5b10638',
+      id: 'note-3',
       displayDate: new Date('2021-03-01T00:00:00'),
       content: 'Earliest note',
     },
