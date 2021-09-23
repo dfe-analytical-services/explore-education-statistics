da3ef327449f9c517edfb4a9abc1fc04ca8559f7
Mark Youngman <mark@hiveit.co.uk>
2021-09-23 14:39:49 +0100

    EES-2683 Add ability to show glossary entries in modals to ContentHtml

--- src/explore-education-statistics-common/src/components/ContentHtml.tsx
+++ src/explore-education-statistics-common/src/components/ContentHtml.tsx
@@ -1,13 +1,17 @@
 import sanitizeHtml, { SanitizeHtmlOptions } from '@common/utils/sanitizeHtml';
-import htmlParser from 'html-react-parser';
+import parseHtmlString, { DOMNode, domToReact } from 'html-react-parser';
+import { Element } from 'domhandler/lib/node';
 import classNames from 'classnames';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
+import { GlossaryEntry } from '@common/services/types/glossary';
+import Modal from '@common/components/Modal';
 
 export interface ContentHtmlProps {
   className?: string;
   html: string;
   sanitizeOptions?: SanitizeHtmlOptions;
   testId?: string;
+  getGlossaryEntry?: (slug: string) => Promise<GlossaryEntry>;
 }
 
 const ContentHtml = ({
@@ -15,17 +19,86 @@ const ContentHtml = ({
   html,
   sanitizeOptions,
   testId,
+  getGlossaryEntry,
 }: ContentHtmlProps) => {
+  const [displayGlossaryEntry, setDisplayGlossaryEntry] = useState(false);
+  const [glossaryEntry, setGlossaryEntry] = useState({
+    title: 'Glossary entry not retrieved',
+    slug: '',
+    body: '',
+    link: '',
+  });
+
   const cleanHtml = useMemo(() => {
     return sanitizeHtml(html, sanitizeOptions);
   }, [html, sanitizeOptions]);
 
-  const parsedContent = htmlParser(cleanHtml, {});
+  const parsedContent = parseHtmlString(cleanHtml, {
+    replace: (node: DOMNode) => {
+      if (
+        getGlossaryEntry &&
+        node instanceof Element &&
+        node.name === 'a' &&
+        node.attribs &&
+        Object.keys(node.attribs).includes('data-glossary')
+      ) {
+        return (
+          <>
+            <a href={node.attribs.href}>{domToReact(node.children)}</a>{' '}
+            <a
+              href="#"
+              onClick={async event => {
+                event.preventDefault();
+                const glossaryEntrySlug = node.attribs.href.split('#')[1];
+                const newGlossaryEntry = await getGlossaryEntry(
+                  glossaryEntrySlug,
+                );
+                setGlossaryEntry({
+                  title: newGlossaryEntry.title,
+                  slug: newGlossaryEntry.slug,
+                  body: newGlossaryEntry.body,
+                  link: node.attribs.href,
+                });
+                setDisplayGlossaryEntry(true);
+              }}
+            >
+              ?
+            </a>
+          </>
+        );
+      }
+      return undefined;
+    },
+  });
 
   return (
-    <div className={classNames('dfe-content', className)} data-testid={testId}>
-      {parsedContent}
-    </div>
+    <>
+      <div
+        className={classNames('dfe-content', className)}
+        data-testid={testId}
+      >
+        {parsedContent}
+      </div>
+      {displayGlossaryEntry && (
+        <Modal
+          title={glossaryEntry.title}
+          onExit={() => setDisplayGlossaryEntry(false)}
+        >
+          {parseHtmlString(sanitizeHtml(glossaryEntry.body))}
+          <p>
+            <a
+              href="#"
+              onClick={event => {
+                event.preventDefault();
+                setDisplayGlossaryEntry(false);
+              }}
+            >
+              Close
+            </a>
+          </p>
+        </Modal>
+      )}
+    </>
   );
 };
 
