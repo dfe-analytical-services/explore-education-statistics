514766add68b20d97509ec030394ac8575004de0
Nicholas Tsim <nicholastsim@gmail.com>
2021-09-28 23:20:23 +0100

    EES-2739 Downgrade AutoMapper across project due to conflicts with IdentityServer

--- src/GovUk.Education.ExploreEducationStatistics.Content.Api/Startup.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using AutoMapper;
 using Azure.Storage.Blobs;
 using GovUk.Education.ExploreEducationStatistics.Common.Cache;
 using GovUk.Education.ExploreEducationStatistics.Common.ModelBinding;
