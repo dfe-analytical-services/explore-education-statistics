6120bb2733a8822f86c2bb4dfd326cd1b6142ecb
Mark Youngman <mark@hiveit.co.uk>
2021-10-04 13:18:46 +0100

    EES-2750 Enable retries again for UI tests on CI pipeline

--- tests/robot-tests/scripts/pipeline-run-rf-tests.sh
+++ tests/robot-tests/scripts/pipeline-run-rf-tests.sh
@@ -12,3 +12,9 @@ python -m pip install --upgrade pip
 pip install pipenv
 pipenv install
 pipenv run python run_tests.py --admin-pass $1 --analyst-pass $2 -e $3 --ci --file $4 --processes 4
+
+if [ $? -ne 0 ]
+then
+  echo "Rerunning failed test suites..."
+  pipenv run python run_tests.py --admin-pass $1 --analyst-pass $2 -e $3 --ci --file $4 --processes 4 --rerun-failed-suites
+fi
