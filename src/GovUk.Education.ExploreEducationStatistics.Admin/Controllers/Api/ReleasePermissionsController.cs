8818a2a01c04bb57e91f48e201b46f1af8f764f4
Mark Youngman <mark@hiveit.co.uk>
2021-11-03 11:48:23 +0000

    EES-2688 Create endpoint to get release's contributor details

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/ReleasePermissionsController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Admin.ViewModels;
+using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Controllers.Api
+{
+    [Route("api")]
+    [ApiController]
+    [Authorize]
+    public class ReleasePermissionsController : ControllerBase
+    {
+        private readonly IReleasePermissionService _releasePermissionService;
+
+        public ReleasePermissionsController(
+            IReleasePermissionService releasePermissionService)
+        {
+            _releasePermissionService = releasePermissionService;
+        }
+
+        [HttpGet("releases/{releaseId}/contributors")]
+        public async Task<ActionResult<List<ReleaseContributorViewModel>>> GetReleaseContributors(Guid releaseId)
+        {
+            return await _releasePermissionService
+                .GetReleaseContributors(releaseId)
+                .HandleFailuresOrOk();
+        }
+    }
+}
