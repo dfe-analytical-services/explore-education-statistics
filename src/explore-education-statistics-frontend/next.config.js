a23b2a3ba0b71d5248cdaa253fcbd74b341c742d
Nicholas Tsim <nicholastsim@gmail.com>
2021-11-15 15:07:30 +0000

    EES-2893 Remove source maps from build

--- src/explore-education-statistics-frontend/next.config.js
+++ src/explore-education-statistics-frontend/next.config.js
@@ -14,7 +14,6 @@ const nextConfig = {
     HOTJAR_ID: process.env.HOTJAR_ID,
     PUBLIC_URL: process.env.PUBLIC_URL,
   },
-  productionBrowserSourceMaps: true,
   async redirects() {
     return [
       {
