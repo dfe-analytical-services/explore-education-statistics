7ff98c54445fd46ed049b2d0b9c695e401eac758
Nicholas Tsim <nicholastsim@gmail.com>
2021-12-02 16:15:35 +0000

    EES-2935 Update `useLoggedReducer` with Immer v9 compatible type

--- src/explore-education-statistics-common/src/hooks/useLoggedReducer.ts
+++ src/explore-education-statistics-common/src/hooks/useLoggedReducer.ts
@@ -1,5 +1,5 @@
 import logger from '@common/services/logger';
-import produce, { Draft, Immutable } from 'immer';
+import produce, { Immutable } from 'immer';
 import {
   Dispatch,
   Reducer,
@@ -7,16 +7,16 @@ import {
   useCallback,
   useReducer,
 } from 'react';
-import { ImmerHook, Reducer as ImmerReducer } from 'use-immer';
+import { Reducer as ImmerReducer } from 'use-immer';
 
 export default function useLoggedReducer<S, A>(
   name: string,
-  reducer: ImmerReducer<S, A>,
-  initialState: S | (() => S),
+  reducer: Reducer<S, A>,
+  initialState: S,
   initializer?: ReducerWithoutAction<S>,
-): ImmerHook<S> {
+): [S, Dispatch<A>] {
   const cachedReducer = useCallback(
-    (state: Draft<S>, action: A) => {
+    (state: S, action: A) => {
       logger.debugGroup(`${name} reducer:`);
       logger.debug(
         '%cPrevious State:',
@@ -47,27 +47,20 @@ export default function useLoggedReducer<S, A>(
     initializer as ReducerWithoutAction<S>,
   );
 }
-export function useLoggedImmerReducer2<S, A>(
-  name: string,
-  initialState: S | (() => S),
-  reducer: ImmerReducer<S, A>,
-  initializer?: ReducerWithoutAction<S>,
-): ImmerHook<S> {
-  return useLoggedReducer();
-}
 
 export function useLoggedImmerReducer<S, A>(
   name: string,
   reducer: ImmerReducer<S, A>,
   initialState: S,
   initializer?: ReducerWithoutAction<S>,
-) {
+): [S & Immutable<S>, Dispatch<A>] {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const cachedReducer = useCallback(produce(reducer), [reducer]);
+
   return useLoggedReducer(
     name,
-    cachedReducer as any,
+    cachedReducer as never,
     initialState,
     initializer,
-  );
+  ) as [S & Immutable<S>, Dispatch<A>];
 }
