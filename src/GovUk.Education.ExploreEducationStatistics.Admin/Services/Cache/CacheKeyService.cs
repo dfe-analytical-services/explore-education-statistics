20a6388996e4630c149969822483cef59bc953eb
Duncan Watson <duncan@hiveit.co.uk>
2021-12-06 13:46:07 +0000

    EES-2920 - removed commented out code

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Cache/CacheKeyService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Cache/CacheKeyService.cs
@@ -14,17 +14,10 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Cache
 {
     public class CacheKeyService : ICacheKeyService
     {
-        // private readonly ContentDbContext _contentDbContext;
-        // private readonly StatisticsDbContext _statisticsDbContext;
         private readonly IPersistenceHelper<ContentDbContext> _contentPersistenceHelper;
 
-        public CacheKeyService(
-            // ContentDbContext contentDbContext, 
-            // StatisticsDbContext statisticsDbContext,
-            IPersistenceHelper<ContentDbContext> contentPersistenceHelper)
+        public CacheKeyService(IPersistenceHelper<ContentDbContext> contentPersistenceHelper)
         {
-            // _contentDbContext = contentDbContext;
-            // _statisticsDbContext = statisticsDbContext;
             _contentPersistenceHelper = contentPersistenceHelper;
         }
         
@@ -38,40 +31,5 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Cache
                     .Where(rcb => rcb.ContentBlockId == dataBlockId && rcb.ReleaseId == releaseId))
                 .OnSuccess(releaseContentBlock => new DataBlockTableResultCacheKey(releaseContentBlock));
         }
-
-        // public async Task<Either<ActionResult, SubjectMetaCacheKey>> CreateCacheKeyForSubjectMeta(Guid subjectId)
-        // {
-        //     var publishedReleaseSubjects = await Queryable.Where(_statisticsDbContext
-        //             .ReleaseSubject
-        //             .Include(releaseSubject => releaseSubject.Release), releaseSubject => releaseSubject.SubjectId == subjectId && releaseSubject.Release.Published.HasValue)
-        //         .ToListAsync();
-        //
-        //     var liveReleaseSubjects = Enumerable.ToList(Enumerable.Where(publishedReleaseSubjects, releaseSubject => releaseSubject.Release.Live));
-        //
-        //     var previousReleaseVersionIds = Enumerable.Select(liveReleaseSubjects, releaseSubject => releaseSubject.Release.PreviousVersionId);
-        //
-        //     var latestReleaseSubject = Enumerable.SingleOrDefault(liveReleaseSubjects, releaseSubject => !Enumerable.Contains(previousReleaseVersionIds, releaseSubject.Release.Id));
-        //
-        //     if (latestReleaseSubject == null)
-        //     {
-        //         return new NotFoundResult();
-        //     }
-        //
-        //     var publication = await _contentDbContext
-        //         .Publications
-        //         .SingleAsync(publication => publication.Id == latestReleaseSubject.Release.PublicationId);
-        //
-        //     return new SubjectMetaCacheKey(publication.Slug, latestReleaseSubject.Release.Slug, subjectId);
-        // }
-        //
-        // public async Task<Either<ActionResult, ReleaseSubjectsCacheKey>> CreateCacheKeyForReleaseSubjects(Guid releaseId)
-        // {
-        //     var release = await _contentDbContext
-        //         .Releases
-        //         .Include(release => release.Publication)
-        //         .SingleAsync(release => release.Id == releaseId);
-        //
-        //     return new ReleaseSubjectsCacheKey(release.Publication.Slug, release.Slug, release.Id);
-        // }
     }
 }
