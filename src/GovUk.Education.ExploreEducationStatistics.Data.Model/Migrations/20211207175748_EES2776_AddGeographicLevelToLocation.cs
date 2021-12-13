64d2b5b050ce0e5240662c41107cc1113b0177a5
Ben Outram <ben@hiveit.co.uk>
2021-12-13 16:25:12 +0000

    EES-2933 Add new temporary script which sets Location.GeographicLevel based on Observation.GeographicLevel. Add a script for conveniently deleting orphaned locations.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20211207175748_EES2776_AddGeographicLevelToLocation.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20211207175748_EES2776_AddGeographicLevelToLocation.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations.MigrationConstants;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
 {
     public partial class EES2776_AddGeographicLevelToLocation : Migration
     {
+        private const string MigrationId = "20211207175748";
+        
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.AddColumn<string>(
@@ -16,10 +20,19 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
                 name: "IX_Location_GeographicLevel",
                 table: "Location",
                 column: "GeographicLevel");
+            
+            // Add a new procedure for conveniently deleting orphaned locations
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_Routine_DeleteOrphanedLocations.sql");
+
+            // Add new temporary procedure for safely copying Geographic Level from Observations to Locations
+            migrationBuilder.SqlFromFile(MigrationsPath, $"{MigrationId}_Routine_UpdateLocationGeographicLevel.sql");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("DROP PROCEDURE dbo.DeleteOrphanedLocations");
+            migrationBuilder.Sql("DROP PROCEDURE dbo.UpdateLocationGeographicLevel");
+
             migrationBuilder.DropIndex(
                 name: "IX_Location_GeographicLevel",
                 table: "Location");
