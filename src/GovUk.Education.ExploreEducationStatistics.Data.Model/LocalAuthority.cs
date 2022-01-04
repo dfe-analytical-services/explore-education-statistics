6ef5c5ead382b48787bec9b9c0d81b12f6e0af96
Ben Outram <ben@hiveit.co.uk>
2022-01-04 13:27:41 +0000

    EES-3011 Add comments about cache key string needing to identify a LocalAuthority uniquely by all attributes including OldCode.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/LocalAuthority.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/LocalAuthority.cs
@@ -14,6 +14,8 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Model
 
         public string GetCacheKey()
         {
+            // Don't use GetCodeOrFallback here as the string needs to represent the local authority uniquely by all
+            // attributes. Two local authorities with the same name and code but different old code are not identical.
             return $"{GetType().Name}:{Code ?? string.Empty}:{OldCode ?? string.Empty}:{Name ?? string.Empty}";
         }
 
