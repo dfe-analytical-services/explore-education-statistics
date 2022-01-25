a660a0fb891817fd8c2eb627f64c68cec4f02bb9
Duncan Watson <duncan@hiveit.co.uk>
2022-01-25 23:33:16 +0000

    EES-2999 - added in final ObservationService tests to test remaining clauses

--- src/GovUk.Education.ExploreEducationStatistics.Data.Services.Tests/ObservationServiceTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Services.Tests/ObservationServiceTests.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Common.Model;
 using GovUk.Education.ExploreEducationStatistics.Common.Model.Data.Query;
 using GovUk.Education.ExploreEducationStatistics.Common.Tests.Extensions;
@@ -21,6 +20,7 @@ using static GovUk.Education.ExploreEducationStatistics.Common.Tests.Utils.MockU
 using static GovUk.Education.ExploreEducationStatistics.Data.Model.Database.StatisticsDbUtils;
 using static GovUk.Education.ExploreEducationStatistics.Data.Services.ObservationService;
 using static Moq.MockBehavior;
+// ReSharper disable AccessToDisposedClosure
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests
 {
@@ -127,6 +127,59 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests
         }
         
         [Fact]
+        public async Task QueryGenerator_TimePeriodQuery()
+        {
+            var subjectId = Guid.NewGuid();
+            await using var context = InMemoryStatisticsDbContext();
+            
+            var cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = cancellationTokenSource.Token;
+
+            var tempTableCreator = new Mock<MatchingObservationsQueryGenerator.ITemporaryTableCreator>();
+            tempTableCreator
+                .Setup(s => s.CreateTemporaryTable<MatchedObservation>(context, cancellationToken))
+                .Returns(Task.CompletedTask);
+            
+            var queryGenerator = new MatchingObservationsQueryGenerator
+            {
+                TempTableCreator = tempTableCreator.Object
+            };
+
+            var (sql, sqlParameters, tempTableReferences) = 
+                await queryGenerator
+                    .GetMatchingObservationsQuery(
+                        context, 
+                        subjectId,
+                        null,
+                        null,
+                        new TimePeriodQuery
+                        {
+                            StartCode = TimeIdentifier.AcademicYear,
+                            StartYear = 2015,
+                            EndCode = TimeIdentifier.AcademicYear,
+                            EndYear = 2017
+                        },
+                        cancellationToken);
+
+            VerifyAllMocks(tempTableCreator);
+            
+            const string expectedSql = @"
+                  INSERT INTO #MatchedObservation 
+                  SELECT o.id FROM Observation o
+                  WHERE o.SubjectId = @subjectId
+                  AND (
+                    (o.TimeIdentifier = 'AY' AND o.Year = 2015) OR 
+                    (o.TimeIdentifier = 'AY' AND o.Year = 2016) OR 
+                    (o.TimeIdentifier = 'AY' AND o.Year = 2017)
+                  ) 
+                  ORDER BY o.Id;";
+            
+            Assert.Equal(FormatSql(expectedSql), FormatSql(sql));
+            sqlParameters.AssertDeepEqualTo(ListOf(new SqlParameter("subjectId", subjectId)));
+            Assert.Empty(tempTableReferences);
+        }
+        
+        [Fact]
         public async Task QueryGenerator_LocationIds()
         {
             var locationIds = ListOf(Guid.NewGuid(), Guid.NewGuid());
@@ -187,7 +240,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests
         }
         
         [Fact]
-        public async Task QueryGenerator_FullQuery()
+        public async Task QueryGenerator_FilterItems()
         {
             var subjectId = Guid.NewGuid();
 
@@ -251,6 +304,15 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests
                 var cancellationTokenSource = new CancellationTokenSource();
                 var cancellationToken = cancellationTokenSource.Token;
 
+                // Set up some selected Filter Item Ids from 3 of the 4 Filters on this Subject.
+                //
+                // Filter1 has the least Filter Items selected and will therefore appear first 
+                // in the list of EXISTS clauses, in order for it to filter as many table rows down
+                // as possible before the next EXISTS clause is evaluated.
+                //
+                // Filter3 then has the second lowest number of Filter Items selected so it will
+                // appear as the second EXISTS clause, and finally Filter3 will be the final EXISTS
+                // clause.
                 var selectedFilter1FilterItemIds = ListOf(
                     filter1.FilterGroups.ToList()[0].FilterItems.ToList()[0].Id,
                     filter1.FilterGroups.ToList()[0].FilterItems.ToList()[2].Id,
@@ -273,6 +335,8 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests
                     filter3.FilterGroups.ToList()[3].FilterItems.ToList()[0].Id,
                     filter3.FilterGroups.ToList()[4].FilterItems.ToList()[1].Id);
 
+                // An accidental inclusion of a Filter Item Id that doesn't belong to a
+                // Filter on this Subject will be ignored.
                 var invalidUnrelatedFilterFilterItemIds = ListOf(
                     unrelatedFilter.FilterGroups.ToList()[0].FilterItems.ToList()[0].Id);
 
@@ -286,25 +350,6 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests
                     .Setup(s => 
                         s.CreateTemporaryTable<MatchedObservation>(context, cancellationToken))
                     .Returns(Task.CompletedTask);
-
-                var selectedLocationIds = ListOf(Guid.NewGuid(), Guid.NewGuid());
-                
-                var selectedLocationIdTempTableEntries = selectedLocationIds
-                        .Select(id => new IdTempTable(id))
-                        .ToList();
-
-                var locationIdsTempTableReference = new Mock<ITempTableQuery<IdTempTable>>();
-                locationIdsTempTableReference
-                    .SetupGet(t => t.Name)
-                    .Returns("#LocationTempTable");
-                
-                tempTableCreator
-                    .Setup(s =>
-                        s.CreateTemporaryTableAndPopulate(
-                            context,
-                            ItIs.EnumerableSequenceEqualTo(selectedLocationIdTempTableEntries),
-                            cancellationToken))
-                    .ReturnsAsync(locationIdsTempTableReference.Object);
 
                 var selectedFilterItemsByFilter = ListOf(
                     selectedFilter1FilterItemIds,
@@ -342,19 +387,142 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests
 
                             return tempTableReference;
                         })
-                        .ToList();                
-                
-                var tempTableMocks = 
-                    ListOf(locationIdsTempTableReference)
-                        .Concat(filterItemIdTempTableReferences)
                         .ToList();
-
+                
                 var (sql, sqlParameters, tempTableReferences) = 
                     await queryGenerator
                         .GetMatchingObservationsQuery(
                             context, 
                             subjectId,
                             selectFilterItemIds,
+                            null,
+                            null,
+                            cancellationToken);
+                
+                VerifyAllMocks(tempTableCreator);
+                VerifyAllMocks(filterItemIdTempTableReferences.Cast<Mock>().ToArray());
+
+                // Ensure that the EXISTS clauses appear in the expected order in the generated query,
+                // with the Filter with the least number of Filter Items chosen being the first. 
+                const string expectedSql = @"
+                      INSERT INTO #MatchedObservation 
+                      SELECT o.id FROM Observation o
+                      WHERE o.SubjectId = @subjectId 
+                      AND (
+                          EXISTS (SELECT 1 FROM ObservationFilterItem ofi 
+                                  WHERE ofi.ObservationId = o.id 
+                                  AND ofi.FilterItemId IN (SELECT Id FROM #Filter1TempTable)) AND
+                          EXISTS (SELECT 1 FROM ObservationFilterItem ofi 
+                                  WHERE ofi.ObservationId = o.id 
+                                  AND ofi.FilterItemId IN (SELECT Id FROM #Filter3TempTable)) AND
+                          EXISTS (SELECT 1 FROM ObservationFilterItem ofi 
+                                  WHERE ofi.ObservationId = o.id 
+                                  AND ofi.FilterItemId IN (SELECT Id FROM #Filter2TempTable))
+                      )
+                      ORDER BY o.Id;";
+                
+                Assert.Equal(FormatSql(expectedSql), FormatSql(sql));
+                
+                sqlParameters.AssertDeepEqualTo(ListOf(new SqlParameter("subjectId", subjectId)));
+
+                var expectedTempTableReferences = 
+                    filterItemIdTempTableReferences.Select(mock => mock.Object);
+
+                // Assert that the temporary table for the LocationIds, and the 3 temporary tables for the
+                // 3 Filters' sets of selected FilterItemIds are returned so that the calling code can
+                // dispose of them as it needs to (the order they are returned in is not important).
+                Assert.Equal(
+                    expectedTempTableReferences.OrderBy(t => t.GetHashCode()), 
+                    tempTableReferences.OrderBy(t => t.GetHashCode()));
+            }
+        }
+        
+        [Fact]
+        public async Task QueryGenerator_FullQuery()
+        {
+            var subjectId = Guid.NewGuid();
+
+            var filter = new Filter
+            {
+                Id = Guid.NewGuid(),
+                SubjectId = subjectId,
+                FilterGroups = CreateFilterGroups(5, 5)
+            };
+            
+            var contextId = Guid.NewGuid().ToString();
+            await using (var context = InMemoryStatisticsDbContext(contextId))
+            {
+                await context.AddAsync(filter);
+                await context.SaveChangesAsync();
+            }
+            
+            var tempTableCreator = new Mock<MatchingObservationsQueryGenerator.ITemporaryTableCreator>(Strict);
+            
+            var queryGenerator = new MatchingObservationsQueryGenerator
+            {
+                TempTableCreator = tempTableCreator.Object
+            };
+
+            await using (var context = InMemoryStatisticsDbContext(contextId))
+            {
+                var cancellationTokenSource = new CancellationTokenSource();
+                var cancellationToken = cancellationTokenSource.Token;
+
+                var selectedFilterItemIds = ListOf(
+                    filter.FilterGroups.ToList()[0].FilterItems.ToList()[0].Id,
+                    filter.FilterGroups.ToList()[0].FilterItems.ToList()[2].Id,
+                    filter.FilterGroups.ToList()[1].FilterItems.ToList()[1].Id);
+                
+                tempTableCreator
+                    .Setup(s => 
+                        s.CreateTemporaryTable<MatchedObservation>(context, cancellationToken))
+                    .Returns(Task.CompletedTask);
+
+                var selectedLocationIds = ListOf(Guid.NewGuid(), Guid.NewGuid());
+                
+                var selectedLocationIdTempTableEntries = selectedLocationIds
+                        .Select(id => new IdTempTable(id))
+                        .ToList();
+
+                var locationIdsTempTableReference = new Mock<ITempTableQuery<IdTempTable>>();
+                locationIdsTempTableReference
+                    .SetupGet(t => t.Name)
+                    .Returns("#LocationTempTable");
+                
+                tempTableCreator
+                    .Setup(s =>
+                        s.CreateTemporaryTableAndPopulate(
+                            context,
+                            ItIs.EnumerableSequenceEqualTo(selectedLocationIdTempTableEntries),
+                            cancellationToken))
+                    .ReturnsAsync(locationIdsTempTableReference.Object);
+
+                var filterItemIdsTempTableReference = new Mock<ITempTableQuery<IdTempTable>>();
+                filterItemIdsTempTableReference
+                    .SetupGet(t => t.Name)
+                    .Returns("#FilterTempTable");
+
+                tempTableCreator
+                    .Setup(s =>
+                        s.CreateTemporaryTableAndPopulate(
+                            context,
+                            ItIs.ListSequenceEqualTo(
+                                selectedFilterItemIds
+                                    .OrderBy(id => id)
+                                    .Select(id => new IdTempTable(id))
+                                    .ToList()),
+                            cancellationToken))
+                    .ReturnsAsync(filterItemIdsTempTableReference.Object);     
+                
+                var tempTableMocks = ListOf(locationIdsTempTableReference);
+                tempTableMocks.Add(filterItemIdsTempTableReference);            
+
+                var (sql, sqlParameters, tempTableReferences) = 
+                    await queryGenerator
+                        .GetMatchingObservationsQuery(
+                            context, 
+                            subjectId,
+                            selectedFilterItemIds,
                             selectedLocationIds,
                             new TimePeriodQuery
                             {
@@ -381,13 +549,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests
                       AND (
                           EXISTS (SELECT 1 FROM ObservationFilterItem ofi 
                                   WHERE ofi.ObservationId = o.id 
-                                  AND ofi.FilterItemId IN (SELECT Id FROM #Filter1TempTable)) AND
-                          EXISTS (SELECT 1 FROM ObservationFilterItem ofi 
-                                  WHERE ofi.ObservationId = o.id 
-                                  AND ofi.FilterItemId IN (SELECT Id FROM #Filter3TempTable)) AND
-                          EXISTS (SELECT 1 FROM ObservationFilterItem ofi 
-                                  WHERE ofi.ObservationId = o.id 
-                                  AND ofi.FilterItemId IN (SELECT Id FROM #Filter2TempTable))
+                                  AND ofi.FilterItemId IN (SELECT Id FROM #FilterTempTable))
                       )
                       ORDER BY o.Id;";
                 
@@ -407,7 +569,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests
             }
         }
 
-        private string FormatSql(string sql)
+        private static string FormatSql(string sql)
         {
             var removeNewLines = new Regex("\n", RegexOptions.Compiled);
             var removeWhitespaceAfterOpeningBracket = new Regex("\\( *", RegexOptions.Compiled);
