6a49ed57d151c710f7fa144e5794f1e0f4d89498
Ben Outram <ben@hiveit.co.uk>
2022-03-09 19:04:36 +0000

    EES-3237 Reset the migration status of all data blocks that are already migrated.

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20220309154956_EES3237_AddDataBlockColumnForLocationsMigration.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20220309154956_EES3237_AddDataBlockColumnForLocationsMigration.cs
@@ -14,6 +14,13 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMig
                 type: "bit",
                 nullable: true,
                 defaultValue: false);
+
+            // We will need to re-run the migration of data blocks to calculate a value for 'TableHeaderCountChanged'.
+            // Reset the migration status of all data blocks that are already migrated.
+            // Migrating them again will be safe since the chart/query/table pre-migration remain the same post-migration.
+            // The new values are written to temporary columns which are safe to overwrite until EES-2955.
+            migrationBuilder.Sql(
+                "UPDATE dbo.ContentBlock SET DataBlock_LocationsMigrated = 0 WHERE Type = 'DataBlock' AND DataBlock_LocationsMigrated = 1");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
