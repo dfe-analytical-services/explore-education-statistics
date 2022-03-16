1dcd7dbbedb15fc67a329b6c119a641c1d67d9d1
Ben Outram <ben@hiveit.co.uk>
2022-03-16 18:20:20 +0000

    EES-2955 Add extra tests to groupResultsMeasuresByCombination.test.ts for Permalinks where results can still contain location codes rather than id's.

--- src/explore-education-statistics-common/src/modules/table-tool/components/utils/__tests__/groupResultsMeasuresByCombination.test.ts
+++ src/explore-education-statistics-common/src/modules/table-tool/components/utils/__tests__/groupResultsMeasuresByCombination.test.ts
@@ -56,6 +56,74 @@ describe('groupResultMeasuresByCombination', () => {
     });
   });
 
+  // Test for Permalinks created prior to the switchover to using location id's.
+  test('creates different groups based on location codes', () => {
+    const resultMeasures = groupResultMeasuresByCombination(
+      // Results contain a 'location' object with codes rather than a 'locationId'
+      [
+        {
+          geographicLevel: 'localAuthority',
+          location: {
+            localAuthority: {
+              code: 'location-1',
+              name: 'LA 1',
+            },
+          },
+          timePeriod: '2018_AY',
+          measures: {
+            'indicator-1': '1000',
+            'indicator-2': '2000',
+          },
+          filters: ['filter-1', 'filter-2'],
+        },
+        {
+          geographicLevel: 'localAuthority',
+          location: {
+            localAuthority: {
+              code: 'location-2',
+              name: 'LA 2',
+            },
+          },
+          timePeriod: '2018_AY',
+          measures: {
+            'indicator-1': '3000',
+            'indicator-2': '4000',
+          },
+          filters: ['filter-1', 'filter-2'],
+        },
+      ],
+    );
+
+    expect(resultMeasures).toEqual({
+      [LocationFilter.createId({
+        level: 'localAuthority',
+        value: 'location-1',
+      })]: {
+        '2018_AY': {
+          'filter-1': {
+            'filter-2': {
+              'indicator-1': '1000',
+              'indicator-2': '2000',
+            },
+          },
+        },
+      },
+      [LocationFilter.createId({
+        level: 'localAuthority',
+        value: 'location-2',
+      })]: {
+        '2018_AY': {
+          'filter-1': {
+            'filter-2': {
+              'indicator-1': '3000',
+              'indicator-2': '4000',
+            },
+          },
+        },
+      },
+    });
+  });
+
   test('creates different groups based on time period', () => {
     const resultMeasures = groupResultMeasuresByCombination([
       {
@@ -216,6 +284,66 @@ describe('groupResultMeasuresByCombination', () => {
         {
           geographicLevel: 'localAuthority',
           locationId: 'location-1',
+          timePeriod: '2018_AY',
+          measures: {
+            'indicator-3': '3000',
+            'indicator-4': '4000',
+          },
+          filters: ['filter-1', 'filter-2'],
+        },
+      ],
+      excludedFilterIds,
+    );
+
+    expect(resultMeasures).toEqual({
+      '2018_AY': {
+        'filter-1': {
+          'filter-2': {
+            'indicator-1': '1000',
+            'indicator-2': '2000',
+            'indicator-3': '3000',
+            'indicator-4': '4000',
+          },
+        },
+      },
+    });
+  });
+
+  // Test for Permalinks created prior to the switchover to using location id's.
+  test('can exclude grouping by location code', () => {
+    const excludedFilterIds = [
+      LocationFilter.createId({
+        level: 'localAuthority',
+        value: 'location-1',
+      }),
+    ];
+
+    const resultMeasures = groupResultMeasuresByCombination(
+      // Results contain a 'location' object with codes rather than a 'locationId'
+      [
+        {
+          geographicLevel: 'localAuthority',
+          location: {
+            localAuthority: {
+              code: 'location-1',
+              name: 'LA 1',
+            },
+          },
+          timePeriod: '2018_AY',
+          measures: {
+            'indicator-1': '1000',
+            'indicator-2': '2000',
+          },
+          filters: ['filter-1', 'filter-2'],
+        },
+        {
+          geographicLevel: 'localAuthority',
+          location: {
+            localAuthority: {
+              code: 'location-1',
+              name: 'LA 1',
+            },
+          },
           timePeriod: '2018_AY',
           measures: {
             'indicator-3': '3000',
