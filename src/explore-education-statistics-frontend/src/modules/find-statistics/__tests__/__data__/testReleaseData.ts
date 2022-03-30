5b1351b1a68b72ce8d1effae46abbda21bd7ff78
Mark Youngman <mark@hiveit.co.uk>
2022-03-30 14:37:35 +0100

    EES-3149 Update permissions and endpoint for setting supersededById on Manage publication page

--- src/explore-education-statistics-frontend/src/modules/find-statistics/__tests__/__data__/testReleaseData.ts
+++ src/explore-education-statistics-frontend/src/modules/find-statistics/__tests__/__data__/testReleaseData.ts
@@ -4,7 +4,7 @@ export const testPublication: Publication = {
   id: 'publication-1',
   title: 'Pupil absence in schools in England',
   slug: 'pupil-absence-in-schools-in-england',
-  releases: [],
+  releases: [{ id: 'release-id', slug: 'release-slug', title: 'Test release' }],
   legacyReleases: [
     {
       id: 'legacy-release-3',
