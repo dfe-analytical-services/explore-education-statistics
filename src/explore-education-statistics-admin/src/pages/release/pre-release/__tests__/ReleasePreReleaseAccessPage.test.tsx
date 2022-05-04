cfddc200cc4971de54f9681fc8c91b80f3b98071
Amy Benson <amybenson@hiveit.co.uk>
2022-05-04 16:18:12 +0100

    EES-2825 hide pre-release users tab for amendments

--- src/explore-education-statistics-admin/src/pages/release/pre-release/__tests__/ReleasePreReleaseAccessPage.test.tsx
+++ src/explore-education-statistics-admin/src/pages/release/pre-release/__tests__/ReleasePreReleaseAccessPage.test.tsx
@@ -2,13 +2,24 @@ import { testRelease } from '@admin/pages/release/__data__/testRelease';
 import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import _releaseService, { Release } from '@admin/services/releaseService';
+import _permissionService from '@admin/services/permissionService';
+import _preReleaseUserService from '@admin/services/preReleaseUserService';
 import { ReleaseContextProvider } from '@admin/pages/release/contexts/ReleaseContext';
 import { MemoryRouter } from 'react-router';
 import { TestConfigContextProvider } from '@admin/contexts/ConfigContext';
 import ReleasePreReleaseAccessPage from '@admin/pages/release/pre-release/ReleasePreReleaseAccessPage';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('@admin/services/releaseService');
 const releaseService = _releaseService as jest.Mocked<typeof _releaseService>;
+jest.mock('@admin/services/permissionService');
+const permissionService = _permissionService as jest.Mocked<
+  typeof _permissionService
+>;
+jest.mock('@admin/services/preReleaseUserService');
+const preReleaseUserService = _preReleaseUserService as jest.Mocked<
+  typeof _preReleaseUserService
+>;
 
 const releaseData: Release = {
   id: 'release-1',
@@ -41,18 +52,39 @@ const releaseData: Release = {
 };
 
 describe('ReleasePreReleaseAccessPage', () => {
-  test('renders prerelease page link correctly', async () => {
+  test('renders the pre-release users tab', async () => {
     releaseService.getRelease.mockResolvedValue(releaseData);
+    permissionService.canUpdateRelease.mockResolvedValue(true);
+    preReleaseUserService.getUsers.mockResolvedValue([]);
 
-    render(
-      <MemoryRouter>
-        <TestConfigContextProvider>
-          <ReleaseContextProvider release={testRelease}>
-            <ReleasePreReleaseAccessPage />
-          </ReleaseContextProvider>
-        </TestConfigContextProvider>
-      </MemoryRouter>,
-    );
+    renderPage();
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Manage pre-release user access'),
+      ).toBeInTheDocument();
+    });
+
+    expect(
+      screen.getByRole('tab', { name: 'Pre-release users' }),
+    ).toBeInTheDocument();
+
+    expect(screen.getByLabelText('Url')).toBeInTheDocument();
+
+    expect(
+      screen.getByLabelText('Invite new users by email'),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('button', { name: 'Invite new users' }),
+    ).toBeInTheDocument();
+  });
+
+  test('renders pre-release page link correctly', async () => {
+    releaseService.getRelease.mockResolvedValue(releaseData);
+    permissionService.canUpdateRelease.mockResolvedValue(true);
+
+    renderPage();
 
     await waitFor(() => {
       expect(screen.queryByTestId('prerelease-url')).toBeInTheDocument();
@@ -62,4 +94,65 @@ describe('ReleasePreReleaseAccessPage', () => {
       'http://localhost/publication/publication-1/release/release-1/prerelease/content',
     );
   });
+
+  test('does not render the pre-release users tab for amendments', async () => {
+    const amendmentRelease = { ...releaseData, amendment: true };
+    releaseService.getRelease.mockResolvedValue(amendmentRelease);
+    permissionService.canUpdateRelease.mockResolvedValue(true);
+
+    renderPage();
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Public pre-release access list'),
+      ).toBeInTheDocument();
+    });
+
+    expect(
+      screen.queryByRole('tab', { name: 'Pre-release users' }),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.getByRole('tab', { name: 'Public access list' }),
+    ).toBeInTheDocument();
+  });
+
+  test('renders the public access list tab', async () => {
+    releaseService.getRelease.mockResolvedValue(releaseData);
+    permissionService.canUpdateRelease.mockResolvedValue(true);
+    preReleaseUserService.getUsers.mockResolvedValue([]);
+
+    renderPage();
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Manage pre-release user access'),
+      ).toBeInTheDocument();
+    });
+
+    userEvent.click(screen.getByRole('tab', { name: 'Public access list' }));
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Public pre-release access list'),
+      ).toBeInTheDocument();
+    });
+
+    expect(
+      screen.getByRole('button', {
+        name: 'Create public pre-release access list',
+      }),
+    ).toBeInTheDocument();
+  });
 });
+
+function renderPage() {
+  render(
+    <MemoryRouter>
+      <TestConfigContextProvider>
+        <ReleaseContextProvider release={testRelease}>
+          <ReleasePreReleaseAccessPage />
+        </ReleaseContextProvider>
+      </TestConfigContextProvider>
+    </MemoryRouter>,
+  );
+}
