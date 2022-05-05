129e82f38594bacaf870d21c74964943328c12ee
Nicholas Tsim <nicholastsim@gmail.com>
2022-05-05 14:26:27 +0100

    EES-3320 Update chart colour options to match ONS recommendations

--- src/explore-education-statistics-common/src/modules/charts/util/chartUtils.ts
+++ src/explore-education-statistics-common/src/modules/charts/util/chartUtils.ts
@@ -2,19 +2,13 @@ import { ChartSymbol, LineStyle } from '@common/modules/charts/types/chart';
 import { LegendPosition } from '@common/modules/charts/types/legend';
 
 export const colours: string[] = [
-  '#1d70b8',
-  '#912b88',
-  '#d53880',
-  '#f499be',
-  '#f47738',
-  '#b58840',
-  '#85994b',
-  '#28a197',
-  '#505a5f',
-  '#0b0c0c',
-  '#d4351c',
-  '#ffdd00',
-  '#00703c',
+  '#12436D',
+  '#F46A25',
+  '#801650',
+  '#28A197',
+  '#2073BC',
+  '#6BACE6',
+  '#BFBFBF',
 ];
 
 export const legendPositions: LegendPosition[] = ['bottom', 'top', 'none'];
