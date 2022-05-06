c886536c1f59fbb4105c5ed3f945d1dfbc34c443
Ben Outram <ben@hiveit.co.uk>
2022-05-06 16:27:38 +0100

    EES-1238 Adding extra permission and auth handler tests

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Services.Tests/Security/AuthorizationHandlers/ViewSubjectDataForPublishedReleasesAuthorizationHandlerTests.cs
@@ -0,0 +1,126 @@
+﻿#nullable enable
+using System;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Tests.Utils;
+using GovUk.Education.ExploreEducationStatistics.Data.Model;
+using GovUk.Education.ExploreEducationStatistics.Data.Services.Security;
+using GovUk.Education.ExploreEducationStatistics.Data.Services.Security.AuthorizationHandlers;
+using Microsoft.AspNetCore.Authorization;
+using Xunit;
+using Release = GovUk.Education.ExploreEducationStatistics.Content.Model.Release;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests.Security.AuthorizationHandlers;
+
+public class ViewSubjectDataForPublishedReleasesAuthorizationHandlerTests
+{
+    [Fact]
+    public async Task ReleaseSubjectIsNotPublished()
+    {
+        var publication = new Publication();
+
+        var previousVersion = new Release
+        {
+            Id = Guid.NewGuid(),
+            Publication = publication,
+            Published = DateTime.UtcNow,
+            PreviousVersionId = null
+        };
+
+        var latestVersion = new Release
+        {
+            Id = Guid.NewGuid(),
+            Publication = publication,
+            Published = null,
+            PreviousVersionId = previousVersion.Id
+        };
+
+        var releaseSubject = new ReleaseSubject
+        {
+            ReleaseId = latestVersion.Id,
+            SubjectId = Guid.NewGuid()
+        };
+
+        var contextId = Guid.NewGuid().ToString();
+
+        await using (var contentDbContext = ContentDbUtils.InMemoryContentDbContext(contextId))
+        {
+            await contentDbContext.Publications.AddAsync(publication);
+            await contentDbContext.Releases.AddRangeAsync(previousVersion, latestVersion);
+            await contentDbContext.SaveChangesAsync();
+        }
+
+        await using (var contentDbContext = ContentDbUtils.InMemoryContentDbContext(contextId))
+        {
+            var handler = new ViewSubjectDataForPublishedReleasesAuthorizationHandler(contentDbContext);
+
+            var authContext = new AuthorizationHandlerContext(
+                new IAuthorizationRequirement[] {Activator.CreateInstance<ViewSubjectDataRequirement>()},
+                null,
+                releaseSubject);
+
+            await handler.HandleAsync(authContext);
+
+            Assert.False(authContext.HasSucceeded);
+        }
+    }
+
+    [Fact]
+    public async Task ReleaseSubjectIsLatestPublishedVersion()
+    {
+        var publication = new Publication();
+
+        var previousVersion = new Release
+        {
+            Id = Guid.NewGuid(),
+            Publication = publication,
+            Published = DateTime.UtcNow,
+            PreviousVersionId = null
+        };
+
+        var latestPublishedVersion = new Release
+        {
+            Id = Guid.NewGuid(),
+            Publication = publication,
+            Published = DateTime.UtcNow,
+            PreviousVersionId = previousVersion.Id
+        };
+
+        var latestDraftVersion = new Release
+        {
+            Id = Guid.NewGuid(),
+            Publication = publication,
+            Published = null,
+            PreviousVersionId = latestPublishedVersion.Id
+        };
+
+        var releaseSubject = new ReleaseSubject
+        {
+            ReleaseId = latestPublishedVersion.Id,
+            SubjectId = Guid.NewGuid()
+        };
+
+        var contextId = Guid.NewGuid().ToString();
+
+        await using (var contentDbContext = ContentDbUtils.InMemoryContentDbContext(contextId))
+        {
+            await contentDbContext.Publications.AddAsync(publication);
+            await contentDbContext.Releases.AddRangeAsync(previousVersion, latestPublishedVersion, latestDraftVersion);
+            await contentDbContext.SaveChangesAsync();
+        }
+
+        await using (var contentDbContext = ContentDbUtils.InMemoryContentDbContext(contextId))
+        {
+            var handler = new ViewSubjectDataForPublishedReleasesAuthorizationHandler(contentDbContext);
+
+            var authContext = new AuthorizationHandlerContext(
+                new IAuthorizationRequirement[] {Activator.CreateInstance<ViewSubjectDataRequirement>()},
+                null,
+                releaseSubject);
+
+            await handler.HandleAsync(authContext);
+
+            Assert.True(authContext.HasSucceeded);
+        }
+    }
+}
---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Services.Tests/SubjectMetaServicePermissionTests.cs
@@ -0,0 +1,108 @@
+﻿#nullable enable
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Common.Model.Data.Query;
+using GovUk.Education.ExploreEducationStatistics.Common.Services.Interfaces.Security;
+using GovUk.Education.ExploreEducationStatistics.Common.Tests.Utils;
+using GovUk.Education.ExploreEducationStatistics.Common.Utils;
+using GovUk.Education.ExploreEducationStatistics.Data.Model;
+using GovUk.Education.ExploreEducationStatistics.Data.Model.Database;
+using GovUk.Education.ExploreEducationStatistics.Data.Model.Repository.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Data.Services.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Data.Services.Security;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+using static GovUk.Education.ExploreEducationStatistics.Common.Tests.Utils.PermissionTestUtils;
+using static Moq.MockBehavior;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests;
+
+public class SubjectMetaServicePermissionTests
+{
+    private readonly ReleaseSubject _releaseSubject = new()
+    {
+        ReleaseId = Guid.NewGuid(),
+        SubjectId = Guid.NewGuid()
+    };
+
+    [Fact]
+    public async Task GetSubjectMeta()
+    {
+        await PolicyCheckBuilder<DataSecurityPolicies>()
+            .SetupResourceCheckToFail(_releaseSubject, DataSecurityPolicies.CanViewSubjectData)
+            .AssertForbidden(
+                async userService =>
+                {
+                    var service = SetupSubjectMetaService(
+                        userService: userService.Object
+                    );
+
+                    return await service.GetSubjectMeta(_releaseSubject);
+                }
+            );
+    }
+
+    [Fact]
+    public async Task FilterSubjectMeta()
+    {
+        await PolicyCheckBuilder<DataSecurityPolicies>()
+            .SetupResourceCheckToFail(_releaseSubject, DataSecurityPolicies.CanViewSubjectData)
+            .AssertForbidden(
+                async userService =>
+                {
+                    var query = new ObservationQueryContext
+                    {
+                        SubjectId = _releaseSubject.SubjectId
+                    };
+
+                    var cancellationToken = new CancellationTokenSource().Token;
+
+                    var service = SetupSubjectMetaService(
+                        userService: userService.Object
+                    );
+
+                    return await service.FilterSubjectMeta(_releaseSubject, query, cancellationToken);
+                }
+            );
+    }
+
+    private SubjectMetaService SetupSubjectMetaService(
+        StatisticsDbContext? statisticsDbContext = null,
+        IPersistenceHelper<StatisticsDbContext>? statisticsPersistenceHelper = null,
+        IFilterRepository? filterRepository = null,
+        IFilterItemRepository? filterItemRepository = null,
+        IIndicatorGroupRepository? indicatorGroupRepository = null,
+        ILocationRepository? locationRepository = null,
+        IObservationService? observationService = null,
+        ITimePeriodService? timePeriodService = null,
+        IUserService? userService = null,
+        IOptions<LocationsOptions>? options = null)
+    {
+        return new(
+            statisticsPersistenceHelper ?? DefaultPersistenceHelperMock().Object,
+            statisticsDbContext ?? Mock.Of<StatisticsDbContext>(Strict),
+            filterRepository ?? Mock.Of<IFilterRepository>(Strict),
+            filterItemRepository ?? Mock.Of<IFilterItemRepository>(Strict),
+            indicatorGroupRepository ?? Mock.Of<IIndicatorGroupRepository>(Strict),
+            locationRepository ?? Mock.Of<ILocationRepository>(Strict),
+            Mock.Of<ILogger<SubjectMetaService>>(),
+            observationService ?? Mock.Of<IObservationService>(Strict),
+            timePeriodService ?? Mock.Of<ITimePeriodService>(Strict),
+            userService ?? Mock.Of<IUserService>(),
+            options ?? DefaultLocationOptions()
+        );
+    }
+
+    private static IOptions<LocationsOptions> DefaultLocationOptions()
+    {
+        return Options.Create(new LocationsOptions());
+    }
+
+    private Mock<IPersistenceHelper<StatisticsDbContext>> DefaultPersistenceHelperMock()
+    {
+        return MockUtils.MockPersistenceHelper<StatisticsDbContext, ReleaseSubject>(_releaseSubject);
+    }
+}
