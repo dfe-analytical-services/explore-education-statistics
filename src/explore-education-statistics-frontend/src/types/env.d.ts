a8f45124ede9c78bbdc18ad68619cd5dc05878b2
Nicholas Tsim <nicholastsim@gmail.com>
2022-05-13 13:54:19 +0100

    EES-3380 Add build numbers to page footers

--- src/explore-education-statistics-frontend/src/types/env.d.ts
+++ src/explore-education-statistics-frontend/src/types/env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     APP_ENV: 'Production' | 'Pre-Production' | 'Test' | 'Development';
     APPINSIGHTS_INSTRUMENTATIONKEY: string;
+    BUILD_NUMBER: string;
     HOTJAR_ID: string;
     GA_TRACKING_ID: string;
     NOTIFICATION_API_BASE_URL: string;
