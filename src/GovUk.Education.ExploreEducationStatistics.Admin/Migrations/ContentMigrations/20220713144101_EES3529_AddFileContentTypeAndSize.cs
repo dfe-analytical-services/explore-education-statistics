0429e5e594ee80195274b1dfc4425132f6dea471
Ben Outram <ben@hiveit.co.uk>
2022-07-11 13:40:00 +0100

    EES-3529 Add ContentType and Size to File and make Filename not nullable.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20220713144101_EES3529_AddFileContentTypeAndSize.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    public partial class EES3529_AddFileContentTypeAndSize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Filename",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Size",
+                table: "Files",
+                type: "bigint",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                table: "Files");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Files");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Filename",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
