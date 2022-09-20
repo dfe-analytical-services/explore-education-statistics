db55f08676eb3b0e79c38b377404b1963ea2f088
Ben Outram <ben@hiveit.co.uk>
2022-09-20 15:43:27 +0100

    EES-3719 Use file-scoped namespace

--- src/GovUk.Education.ExploreEducationStatistics.Content.Services/ViewModels/PublicationCacheViewModel.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Services/ViewModels/PublicationCacheViewModel.cs
@@ -2,28 +2,27 @@
 using System;
 using System.Collections.Generic;
 
-namespace GovUk.Education.ExploreEducationStatistics.Content.Services.ViewModels
+namespace GovUk.Education.ExploreEducationStatistics.Content.Services.ViewModels;
+
+public record PublicationCacheViewModel
 {
-    public record PublicationCacheViewModel
-    {
-        public Guid Id { get; init; }
+    public Guid Id { get; init; }
 
-        public string Title { get; init; } = string.Empty;
+    public string Title { get; init; } = string.Empty;
 
-        public string Slug { get; init; } = string.Empty;
+    public string Slug { get; init; } = string.Empty;
 
-        public Guid LatestReleaseId { get; init; }
+    public Guid LatestReleaseId { get; init; }
 
-        public bool IsSuperseded { get; init; }
+    public bool IsSuperseded { get; init; }
 
-        public List<ReleaseTitleViewModel> Releases { get; init; } = new();
+    public List<ReleaseTitleViewModel> Releases { get; init; } = new();
 
-        public List<LegacyReleaseViewModel> LegacyReleases { get; init; } = new();
+    public List<LegacyReleaseViewModel> LegacyReleases { get; init; } = new();
 
-        public TopicViewModel Topic { get; init; } = null!;
+    public TopicViewModel Topic { get; init; } = null!;
 
-        public ContactViewModel Contact { get; init; } = null!;
+    public ContactViewModel Contact { get; init; } = null!;
 
-        public ExternalMethodologyViewModel? ExternalMethodology { get; init; }
-    }
+    public ExternalMethodologyViewModel? ExternalMethodology { get; init; }
 }
