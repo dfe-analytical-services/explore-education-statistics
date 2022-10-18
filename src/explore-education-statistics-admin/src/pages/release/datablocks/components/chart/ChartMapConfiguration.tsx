04c92909166f17e55d88fa8ed2b7bbc9afeb2505
Amy Benson <amybenson@hiveit.co.uk>
2022-10-18 13:57:31 +0100

    EES-3652 fix changing existing map to custom groups

--- src/explore-education-statistics-admin/src/pages/release/datablocks/components/chart/ChartMapConfiguration.tsx
+++ src/explore-education-statistics-admin/src/pages/release/datablocks/components/chart/ChartMapConfiguration.tsx
@@ -224,7 +224,7 @@ export default function ChartMapConfiguration({
 
           {form.values.dataClassification === 'Custom' && (
             <ChartMapCustomGroupsConfiguration
-              groups={form.values.customDataGroups}
+              groups={form.values.customDataGroups ?? []}
               id={`${formId}-customDataGroups`}
               showError={
                 hasSubmitted &&
