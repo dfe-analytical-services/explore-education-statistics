5ba0d9d2117566b67babbbb6ce421eef0ed69fe3
Ben Outram <ben@hiveit.co.uk>
2022-10-20 16:57:39 +0100

    EES-3661 Add a full-text catalog with an index on the Publications Summary and Title columns

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20221108151034_PublicationsFullTextCatalogAndIndex.sql
@@ -0,0 +1,10 @@
+﻿CREATE FULLTEXT CATALOG PublicationsFullTextCatalog;
+CREATE FULLTEXT INDEX ON dbo.Publications
+(
+  Summary            -- Column 1
+      Language 2057, -- UK English LCID
+  Title              -- Column 2
+      Language 2057  -- UK English LCID
+)
+KEY INDEX PK_Publications ON PublicationsFullTextCatalog --Unique index
+WITH CHANGE_TRACKING AUTO; --Population type
