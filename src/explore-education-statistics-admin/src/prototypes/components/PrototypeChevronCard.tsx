eae072b80b8244dc4f0682360ed38ee9664e9158
Martin Waite <martin@hiveit.co.uk>
2022-11-04 09:45:10 +0000

    Initial feedbac updates

--- src/explore-education-statistics-admin/src/prototypes/components/PrototypeChevronCard.tsx
+++ src/explore-education-statistics-admin/src/prototypes/components/PrototypeChevronCard.tsx
@@ -12,10 +12,10 @@ const ChevronCard = ({ className, title, description }: Props) => {
   return (
     <li className={styles.card}>
       <div className={styles.wrapper}>
-        <h4 className="govuk-heading-s">
+        <h4 className="govuk-heading-m govuk-!-margin-bottom-0">
           <a
             href="#"
-            className={classNames(styles.cardLink, 'govuk-!-margin-bottom-3')}
+            className={classNames(styles.cardLink, 'govuk-!-margin-bottom-2')}
           >
             {title}
           </a>
