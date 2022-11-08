1e566180c4e693b8fa95eaf8233e28b1da95e773
Ben Outram <ben@hiveit.co.uk>
2022-11-08 15:27:03 +0000

    EES-3661 Add support to EF core for free-text searching publications by mapping a queryable function to a database table-valued function called PublicationsFreeTextTable which in turn uses FreeTextTable.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20221108151343_Routine_PublicationsFreeTextTable.sql
@@ -0,0 +1,15 @@
+﻿/**
+  Table-valued function which can be used to perform a full-text search on the Publications free-text indexed columns.
+  This can be mapped to a queryable function in EF Core.
+ */
+CREATE OR ALTER FUNCTION dbo.PublicationsFreeTextTable(@search nvarchar(4000))
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT [Key] AS Id, Rank
+    FROM FREETEXTTABLE(
+    dbo.Publications,
+    (Title, Summary),
+    @search)
+);
---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/Database/ModelBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
+
+public static class ModelBuilderExtensions
+{
+    private static readonly MethodInfo? PublicationsFreeTextTableMethodInfo = typeof(ContentDbContext)
+        .GetMethod(nameof(ContentDbContext.PublicationsFreeTextTable),
+            new[] { typeof(string) });
+
+    public static ModelBuilder AddFreeTextTableSupport(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FreeTextRank>().HasNoKey().ToTable((string?) null);
+
+        // Map the PublicationsFreeTextTable queryable function to database table-valued function
+        modelBuilder.HasDbFunction(PublicationsFreeTextTableMethodInfo!);
+
+        return modelBuilder;
+    }
+}
