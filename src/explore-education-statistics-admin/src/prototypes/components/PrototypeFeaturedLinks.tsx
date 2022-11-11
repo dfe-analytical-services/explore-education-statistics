fb1775626f01d89ca007b45acc617afdf29381eb
Martin Waite <martin@hiveit.co.uk>
2022-11-11 10:31:41 +0000

    featured tables links

--- src/explore-education-statistics-admin/src/prototypes/components/PrototypeFeaturedLinks.tsx
+++ src/explore-education-statistics-admin/src/prototypes/components/PrototypeFeaturedLinks.tsx
@@ -82,13 +82,18 @@ const FeaturedLink = ({ dataset, publication }: Props) => {
               <>
                 <PrototypeChevronCard
                   title="Characteristics summary"
-                  url="/data-tables/fast-track/87c4e983-9163-4a6d-a59c-58331454ece8"
+                  url="https://explore-education-statistics.service.gov.uk/data-tables/fast-track/5ff230ae-e6fc-4eba-772e-08dab100bfc2"
                   description="A summary of major entry and attainment data for pupils of different characteristics in all state-funded schools, 2018/19-2021/22"
                 />
                 <PrototypeChevronCard
                   title="Headline attainment measures by ethnic minor pupil characteristic, 2018/19 to 2021/22, state-funded schools, England"
                   url="https://explore-education-statistics.service.gov.uk/data-tables/fast-track/33a093be-9810-4890-a5a4-d0913cd967a0"
-                  description="AAttainment for the following measures broken down by ethnic minor: Average Attainment 8 score of all pupils Average EBacc APS score per pupil Percentage of pupils achieving grades 5 or above in English and mathematics GCSEs Percentage of pupils entering the English Baccalaureate"
+                  description="Attainment for the following measures broken down by ethnic minor: Average Attainment 8 score of all pupils Average EBacc APS score per pupil Percentage of pupils achieving grades 5 or above in English and mathematics GCSEs Percentage of pupils entering the English Baccalaureate"
+                />
+                <PrototypeChevronCard
+                  title="Attainment data by ethnicity and free school meal eligibility"
+                  url="https://explore-education-statistics.service.gov.uk/data-tables/fast-track/cc12dbe2-036d-45f2-b261-57d5376b6c80"
+                  description="KS4 attainment data broken down by ethnicity and free school meal eligiblity"
                 />
               </>
             )}
@@ -96,19 +101,19 @@ const FeaturedLink = ({ dataset, publication }: Props) => {
             {(dataset === 'subject-all' || dataset === 'subject-3') && (
               <>
                 <PrototypeChevronCard
-                  title="Achievements by level"
-                  url="https://explore-education-statistics.service.gov.uk/data-tables/fast-track/dad1704c-2e46-4a2a-8768-61fe94f94c1b"
-                  description="Apprenticeship achievements by level, 2015/16 to 2020/21"
+                  title="Headline measures for regions and local authorities"
+                  url="https://explore-education-statistics.service.gov.uk/data-tables/fast-track/97a699fd-1d72-473e-d48c-08dabceba8ca"
+                  description="Attainment 8, EBacc APS and other headline measures for regions and local authorities, state-funded schools (LA), 2021/22"
                 />
+              </>
+            )}
+
+            {(dataset === 'subject-all' || dataset === 'subject-4') && (
+              <>
                 <PrototypeChevronCard
-                  title="Rates demographic summary"
-                  url="https://explore-education-statistics.service.gov.uk/data-tables/fast-track/74fca436-5e3b-4d7e-8ccc-83a78086caee"
-                  description="All age apprenticeships overall achievement rates demographic summary"
-                />
-                <PrototypeChevronCard
-                  title="Starts and achievements by level, age, and funding type"
-                  url="https://explore-education-statistics.service.gov.uk/data-tables/fast-track/9b554ec1-8639-4b8c-50b9-08daa6e07a35"
-                  description="Apprenticeship participation and achievements by age and level, reported to date for 2021/22 with equivalent figures for 2018/19 to 2020/21"
+                  title="Regional and LA breakdown of headline measures for SEN description"
+                  url="https://explore-education-statistics.service.gov.uk/data-tables/fast-track/97a699fd-1d72-473e-d48c-08dabceba8ca"
+                  description="Regional and LA breakdown of headline measures for SEN description categories, state-funded schools (LA), 2021/22"
                 />
               </>
             )}
