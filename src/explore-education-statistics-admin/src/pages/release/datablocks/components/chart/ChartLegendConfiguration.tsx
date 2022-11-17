13c294ded676de19bdfcdae7557750fe1a744559
Amy Benson <amybenson@hiveit.co.uk>
2022-11-17 15:35:25 +0000

    EES-3785 fix legend label on small screens

--- src/explore-education-statistics-admin/src/pages/release/datablocks/components/chart/ChartLegendConfiguration.tsx
+++ src/explore-education-statistics-admin/src/pages/release/datablocks/components/chart/ChartLegendConfiguration.tsx
@@ -331,9 +331,8 @@ const ChartLegendConfiguration = ({
             </>
           )}
 
-          <div className="govuk-!-margin-bottom-6">
-            <h4>Legend items</h4>
-
+          <h4>Legend items</h4>
+          <div className={`${styles.container} govuk-!-margin-bottom-6`}>
             {form.values.items?.length > 0 ? (
               <>
                 {form.values.items?.map((dataSet, index) => {
