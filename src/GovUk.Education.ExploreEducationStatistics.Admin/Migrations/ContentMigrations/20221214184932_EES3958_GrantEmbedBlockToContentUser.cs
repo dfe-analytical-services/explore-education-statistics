577f67a1517d2b89e5104c409d22580d846b3bad
Ben Outram <ben@hiveit.co.uk>
2022-12-14 18:53:59 +0000

    EES-3958 Add missing grant on EmbedBlocks table related to EES-3682

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20221214184932_EES3958_GrantEmbedBlockToContentUser.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations;
+
+[ExcludeFromCodeCoverage]
+public partial class EES3958_GrantEmbedBlockToContentUser : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        // Add missing grant related to EES-3682
+        migrationBuilder.Sql("GRANT SELECT ON dbo.EmbedBlocks TO [content]");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql("REVOKE SELECT ON dbo.EmbedBlocks TO [content]");
+    }
+}
