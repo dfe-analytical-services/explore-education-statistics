2da13c793eb5288bf9f733447be7e29f5fe44e3c
Ben Outram <ben@hiveit.co.uk>
2023-01-03 12:27:51 +0000

    EES-3974 PermissionsController tidy up.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/ViewModels/GlobalPermissionsViewModel.cs
@@ -0,0 +1,9 @@
+﻿#nullable enable
+namespace GovUk.Education.ExploreEducationStatistics.Admin.ViewModels;
+
+public record GlobalPermissionsViewModel(bool CanAccessSystem,
+    bool CanAccessAnalystPages,
+    bool CanAccessAllImports,
+    bool CanAccessPrereleasePages,
+    bool CanAccessUserAdministrationPages,
+    bool CanManageAllTaxonomy);
