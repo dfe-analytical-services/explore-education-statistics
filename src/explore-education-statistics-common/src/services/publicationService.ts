6d5559bc27ae18ebd97c6d5f02ac6074ba8b75c1
Ben Outram <ben@hiveit.co.uk>
2023-01-09 11:58:54 +0000

    EES-3989 Remove PublicationType after switching over to new Find Stats page

--- src/explore-education-statistics-common/src/services/publicationService.ts
+++ src/explore-education-statistics-common/src/services/publicationService.ts
@@ -9,7 +9,6 @@ import {
   MethodologySummary,
   ExternalMethodology,
 } from '@common/services/types/methodology';
-import { PublicationType } from '@common/services/types/publicationType';
 import { PaginatedList } from '@common/services/types/pagination';
 import { PartialDate } from '@common/utils/date/partialDate';
 import { contentApi } from './api';
@@ -175,7 +174,6 @@ export interface PublicationTreeSummary {
   id: string;
   title: string;
   slug: string;
-  type: PublicationType;
   legacyPublicationUrl?: string;
   isSuperseded: boolean;
 }
