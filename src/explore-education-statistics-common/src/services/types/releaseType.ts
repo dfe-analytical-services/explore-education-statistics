15b382a359dae5f7bd311134b14addd72f950655
Mark Youngman <mark@hiveit.co.uk>
2023-01-13 15:54:03 +0000

    EES-4000 Reorder display of releaseTypes

--- src/explore-education-statistics-common/src/services/types/releaseType.ts
+++ src/explore-education-statistics-common/src/services/types/releaseType.ts
@@ -1,9 +1,10 @@
 export const releaseTypes = {
-  AdHocStatistics: 'Ad hoc statistics',
-  ExperimentalStatistics: 'Experimental statistics',
-  ManagementInformation: 'Management information',
+  // NOTE: Set in the desired display order
   NationalStatistics: 'National statistics',
   OfficialStatistics: 'Official statistics',
+  ExperimentalStatistics: 'Experimental statistics',
+  AdHocStatistics: 'Ad hoc statistics',
+  ManagementInformation: 'Management information',
 } as const;
 
 export type ReleaseType = keyof typeof releaseTypes;
