630ad7d015e7278b216114b204294dab63befb51
Ben Outram <ben@hiveit.co.uk>
2023-05-26 11:36:13 +0100

    EES-4282 Add unit test coverage to PermalinkService for writing csv's of permalinks without location id's.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api.Tests/ObservationViewModelBuilderTestUtils.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GovUk.Education.ExploreEducationStatistics.Data.Api.Tests.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Data.Model;
+using GovUk.Education.ExploreEducationStatistics.Data.Services.Utils;
+using GovUk.Education.ExploreEducationStatistics.Data.Services.ViewModels;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Tests;
+
+// TODO EES-3755 Remove after Permalink snapshot migration work is complete
+internal static class ObservationViewModelBuilderTestUtils
+{
+    public static ObservationViewModel BuildObservationViewModelWithoutLocationId(
+        Observation observation,
+        IEnumerable<Indicator> indicators)
+    {
+        var viewModel = ObservationViewModelBuilder.BuildObservation(observation, indicators.Select(i => i.Id));
+
+        // Deliberately swap out the location id for a location view model to represent what a permalink would
+        // looks like with observations that had a location object prior to location id being added.
+
+        // Ideally location id should have been made nullable to match the possibility that it can be null in permalink data
+        // but we'll leave it for now given that this possibility  is about to disappear with the snapshot migration.
+        viewModel.LocationId = Guid.Empty;
+
+        viewModel.Location = observation.Location.AsLocationViewModel();
+        return viewModel;
+    }
+}
