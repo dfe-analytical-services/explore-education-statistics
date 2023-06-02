e9d3152c312434b5a834153a47b96bdb854fea91
Mark Youngman <mark@hiveit.co.uk>
2023-06-02 16:21:44 +0100

    EES-3311 Set correct Azure Storage instance to use with BlobCacheService

--- src/GovUk.Education.ExploreEducationStatistics.Common.Tests/TestStartup.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common.Tests/TestStartup.cs
@@ -1,36 +1,10 @@
 #nullable enable
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
-using Azure.Storage.Blobs;
-using GovUk.Education.ExploreEducationStatistics.Common.Cache;
-using GovUk.Education.ExploreEducationStatistics.Common.Cancellation;
-using GovUk.Education.ExploreEducationStatistics.Common.Config;
-using GovUk.Education.ExploreEducationStatistics.Common.Database;
-using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
-using GovUk.Education.ExploreEducationStatistics.Common.Model.Data;
 using GovUk.Education.ExploreEducationStatistics.Common.ModelBinding;
 using GovUk.Education.ExploreEducationStatistics.Common.Services;
 using GovUk.Education.ExploreEducationStatistics.Common.Services.Interfaces;
-using GovUk.Education.ExploreEducationStatistics.Common.Services.Interfaces.Security;
-using GovUk.Education.ExploreEducationStatistics.Common.Services.Security;
 using GovUk.Education.ExploreEducationStatistics.Common.Tests.Fixtures;
-using GovUk.Education.ExploreEducationStatistics.Common.Utils;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Server.Features;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Rewrite;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace GovUk.Education.ExploreEducationStatistics.Common.Tests;
@@ -58,7 +32,7 @@ public class TestStartup
             }
         );
         
-        services.AddTransient<IBlobCacheService, BlobCacheService>();
+        services.AddTransient<IBlobCacheService, BlobCacheService>(); // @MarkFix
     }
 
     public void Configure(IApplicationBuilder app)
