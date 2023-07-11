91235c795c14875ea4e6a6c0f81842d7ca76fdeb
Mark Youngman <mark@hiveit.co.uk>
2023-07-11 11:05:21 +0100

    EES-4385 Remove InternalReleaseNote change from IsStatusUpdateRequired

--- src/GovUk.Education.ExploreEducationStatistics.Admin/ViewModels/Methodology/MethodologyApprovalUpdateRequest.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/ViewModels/Methodology/MethodologyApprovalUpdateRequest.cs
@@ -22,8 +22,8 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.ViewModels.Methodolog
         {
             return methodologyVersion.Status != Status
                    || methodologyVersion.PublishingStrategy != PublishingStrategy
-                   || methodologyVersion.ScheduledWithReleaseId != WithReleaseId
-                   || methodologyVersion.InternalReleaseNote != LatestInternalReleaseNote;
+                   || methodologyVersion.ScheduledWithReleaseId != WithReleaseId;
+                   //|| methodologyVersion.InternalReleaseNote != LatestInternalReleaseNote; // @MarkFix why internal release note check?
         }
     }
 }
