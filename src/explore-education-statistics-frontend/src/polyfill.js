dc3f854f3c82d0e1498386ae82007e863c148752
luke-h1 <luke.howsam@yahoo.com>
2023-07-11 21:50:20 +0100

    EES-3409 - remove IE11 shadydom polyfill for public-frontend

--- src/explore-education-statistics-frontend/src/polyfill.js
+++ src/explore-education-statistics-frontend/src/polyfill.js
@@ -2,9 +2,3 @@ import 'cross-fetch/polyfill';
 import 'core-js/features/array/flat';
 import 'core-js/features/array/flat-map';
 import '@common/polyfill';
-
-// Polyfill for dev server breaking in IE11
-// See: https://github.com/vercel/next.js/issues/13231
-if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
-  require('@webcomponents/shadydom');
-}
