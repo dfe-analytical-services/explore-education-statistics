5454ea1ccbc0a4a1b7379aa75a196293898086e8
jack-hive <148866614+jack-hive@users.noreply.github.com>
2023-11-13 14:49:08 +0000

    EES-4409 - Removing unused methods

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/Repository/Interfaces/ISubjectRepository.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Repository/Interfaces/ISubjectRepository.cs
@@ -1,17 +1,11 @@
 #nullable enable
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Repository.Interfaces
 {
     public interface ISubjectRepository
     {
-        Task<IList<Subject>> FindAll(
-            IEnumerable<Guid> subjectIds,
-            Func<IQueryable<Subject>, IQueryable<Subject>>? hydrateSubjectFunction = null);
-
         Task<Subject?> Find(Guid subjectId);
 
         Task<Guid?> FindPublicationIdForSubject(Guid subjectId);
