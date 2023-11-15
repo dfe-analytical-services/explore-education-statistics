43bcbf11de50e924555ef680e4e8e819d92479fd
jack-hive <148866614+jack-hive@users.noreply.github.com>
2023-11-15 16:44:07 +0000

    Adding validators for creating and updating releases

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Validators/ValidationErrorMessages.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Validators/ValidationErrorMessages.cs
@@ -79,6 +79,9 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Validators
         ReplacementFileTypesMustBeData,
         ReplacementMustBeValid,
 
+        // Release
+        ReleaseTypeInvalid,
+
         // Release approval
         ReleaseNotApproved,
         PublishedReleaseCannotBeUnapproved,
