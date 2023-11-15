59f4c053e5719140de8b103b51f6710fde4320c3
Duncan Watson <duncan@hiveit.co.uk>
2023-11-15 20:37:48 +0000

    EES-4639 - refactoring "Create Release from Template" code out of the domain model classes and into ReleaseService itself.

--- src/GovUk.Education.ExploreEducationStatistics.Content.Model/ContentSection.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/ContentSection.cs
@@ -30,15 +30,5 @@ namespace GovUk.Education.ExploreEducationStatistics.Content.Model
         public Guid ReleaseId { get; set; }
 
         [JsonIgnore] public ContentSectionType Type { get; set; }
-        
-        // TODO EES-4639 - adopt straight MemberwiseClone() usage during the generating of Release Amendments,
-        // Methodology Amendments and Releases from templates, then have each call site tailor the resulting
-        // cloned Content tree to their individual requirements, rather than support various Clone() methods in
-        // Release Content entities themselves.
-        public new ContentSection MemberwiseClone()
-        {
-            return base.MemberwiseClone() as ContentSection;
-        }
-
     }
 }
