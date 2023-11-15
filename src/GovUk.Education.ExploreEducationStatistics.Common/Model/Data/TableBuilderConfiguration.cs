7d9400cbc3161c9747a4031d8ee1e9ef2299dc8f
Duncan Watson <duncan@hiveit.co.uk>
2023-11-15 20:38:24 +0000

    EES-4639 - removing unused "Clone" methods from the rest of the codebase.

--- src/GovUk.Education.ExploreEducationStatistics.Common/Model/Data/TableBuilderConfiguration.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Model/Data/TableBuilderConfiguration.cs
@@ -4,13 +4,5 @@ namespace GovUk.Education.ExploreEducationStatistics.Common.Model.Data
     public class TableBuilderConfiguration
     {
         public TableHeaders TableHeaders { get; set; } = new();
-
-        public TableBuilderConfiguration Clone()
-        {
-            return new TableBuilderConfiguration
-            {
-                TableHeaders = TableHeaders.Clone()
-            };
-        }
     }
 }
--- src/GovUk.Education.ExploreEducationStatistics.Common/Model/Data/TableHeaders.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Model/Data/TableHeaders.cs
@@ -1,35 +1,12 @@
 #nullable enable
 using System.Collections.Generic;
-using System.Linq;
 
-namespace GovUk.Education.ExploreEducationStatistics.Common.Model.Data
+namespace GovUk.Education.ExploreEducationStatistics.Common.Model.Data;
+
+public class TableHeaders
 {
-    public class TableHeaders
-    {
-        public List<List<TableHeader>> ColumnGroups { get; set; } = new();
-        public List<TableHeader> Columns { get; set; } = new();
-        public List<List<TableHeader>> RowGroups { get; set; } = new();
-        public List<TableHeader> Rows { get; set; } = new();
-
-        public TableHeaders Clone()
-        {
-            return new TableHeaders
-            {
-                ColumnGroups = ColumnGroups.Select(CloneGroup).ToList(),
-                Columns = Columns.Select(CloneTableHeader).ToList(),
-                RowGroups = RowGroups.Select(CloneGroup).ToList(),
-                Rows = Rows.Select(CloneTableHeader).ToList()
-            };
-        }
-
-        private static List<TableHeader> CloneGroup(List<TableHeader> group)
-        {
-            return group.Select(CloneTableHeader).ToList();
-        }
-
-        private static TableHeader CloneTableHeader(TableHeader tableHeader)
-        {
-            return tableHeader.Clone();
-        }
-    }
+    public List<List<TableHeader>> ColumnGroups { get; set; } = new();
+    public List<TableHeader> Columns { get; set; } = new();
+    public List<List<TableHeader>> RowGroups { get; set; } = new();
+    public List<TableHeader> Rows { get; set; } = new();
 }
