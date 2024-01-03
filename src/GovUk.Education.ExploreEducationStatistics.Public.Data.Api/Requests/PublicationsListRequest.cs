ace234a650c1bc3a781ddf5ec6dfbf89fc66ad04
jack-hive <148866614+jack-hive@users.noreply.github.com>
2024-01-03 15:19:13 +0000

    EES-4705 Increasing max page size to 40 and default to 20. Also removing min and max for the search term

--- src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api/Requests/PublicationsListRequest.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api/Requests/PublicationsListRequest.cs
@@ -5,20 +5,17 @@ namespace GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Requests;
 public record PublicationsListRequest(
     string? Search,
     int Page = 1,
-    int PageSize = 10)
+    int PageSize = 20)
 {
     public class Validator : AbstractValidator<PublicationsListRequest>
     {
         public Validator()
         {
-            RuleFor(request => request.Search)
-                .MinimumLength(3)
-                .MaximumLength(100);
             RuleFor(request => request.Page)
                 .GreaterThanOrEqualTo(1);
             RuleFor(request => request.PageSize)
                 .GreaterThanOrEqualTo(1)
-                .LessThanOrEqualTo(10);
+                .LessThanOrEqualTo(40);
         }
     }
 }
