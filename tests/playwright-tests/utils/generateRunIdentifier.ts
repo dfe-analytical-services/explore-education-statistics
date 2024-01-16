1b3f24fa94fbbb05218f2dbc073e3d1ea14a5218
Saicharan Muthyapwar <smuthyapwar@hiveit.co.uk>
2024-01-16 10:47:51 +0000

    EES-4772 Removed unwanted datatype in generaterunIdentifier function

--- tests/playwright-tests/utils/generateRunIdentifier.ts
+++ tests/playwright-tests/utils/generateRunIdentifier.ts
@@ -1,4 +1,4 @@
-export default function generateRunIdentifier(): string {
+export default function generateRunIdentifier() {
   const date = new Date();
   const utcFormat = date.toISOString().replace('T', ' ').substring(0, 19);
   return `UI Test${utcFormat}`;
