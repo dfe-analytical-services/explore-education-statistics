bd84a202d7ce04ed01d7d31d9a6b0c36e52f255f
Amy Benson <amybenson@hiveit.co.uk>
2024-01-30 13:47:36 +0000

    EES-4606 specify redirect paths

--- src/explore-education-statistics-frontend/src/middleware.ts
+++ src/explore-education-statistics-frontend/src/middleware.ts
@@ -4,3 +4,19 @@ import type { NextRequest } from 'next/server';
 export default async function middleware(request: NextRequest) {
   return redirectPages(request);
 }
+
+// Only run the middleware on the specified paths below.
+// Ideally we'd just exclude build files and run it on all routes,
+// e.g. '/((?!api|_next/static|_next/image|favicon.ico|assets).*)'
+// But a bug in NextJS v12 causes problems for the back button
+// in the table tool (see https://github.com/vercel/next.js/pull/43919).
+export const config = {
+  matcher: [
+    '/cookies/:path*',
+    '/find-statistics/:path*/:path*',
+    '/data-tables',
+    '/data-catalogue',
+    '/methodology/:path*',
+    '/subscriptions',
+  ],
+};
