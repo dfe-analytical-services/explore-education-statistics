7cb2ff110b52810edc89ef9fdf5ae5191a4825fc
Mark Youngman <mark@hiveit.co.uk>
2024-01-30 16:25:32 +0000

    EES-4820 Included superseded publications in DataTables publication tree

--- src/GovUk.Education.ExploreEducationStatistics.Content.Services.Tests/Cache/PublicationCacheServiceTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Services.Tests/Cache/PublicationCacheServiceTests.cs
@@ -292,7 +292,7 @@ public class PublicationCacheServiceTests : CacheServiceTestFixture
     }
 
     [Fact]
-    public async Task GetPublicationTree_DataTables_SupersededPublicationWithDataOnLatestRelease_Excluded()
+    public async Task GetPublicationTree_DataTables_SupersededPublicationWithDataOnLatestRelease_Included()
     {
         var publicationTree = new PublicationTreeThemeViewModel
         {
@@ -312,7 +312,7 @@ public class PublicationCacheServiceTests : CacheServiceTestFixture
             }
         };
 
-        await AssertPublicationTreeEmpty(publicationTree, PublicationTreeFilter.DataTables);
+        await AssertPublicationTreeUnfiltered(publicationTree, PublicationTreeFilter.DataTables);
     }
 
     [Fact]
