5abf8c1ba750d8910e54e5de2eb0dc9c811b70f8
jack-hive <148866614+jack-hive@users.noreply.github.com>
2024-02-08 17:33:01 +0000

    EES-4707 Simplifying DataSetStatusSchemaFilterTests

--- src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Tests/Swagger/DataSetStatusSchemaFilterTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Tests/Swagger/DataSetStatusSchemaFilterTests.cs
@@ -1,5 +1,4 @@
 using System.Text.Json;
-using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Swagger;
 using GovUk.Education.ExploreEducationStatistics.Public.Data.Model;
 using Microsoft.OpenApi.Any;
@@ -29,43 +28,23 @@ public class DataSetStatusSchemaFilterTests
     {
         var schema = GenerateSchema();
 
-        var dataSetStatusSchema = schema[nameof(DataSetStatus)];
-        var testClassSchema = schema[nameof(TestClass)];
+        Assert.Equal("string", schema.Type);
+        Assert.Equal(3, schema.Enum.Count);
 
-        var dataSetStatusSchemaPropertyName = nameof(TestClass.DataSetStatus).CamelCase();
-
-        var dataSetStatusPropertySchema = testClassSchema.Properties[dataSetStatusSchemaPropertyName];
-
-        Assert.Equal("string", dataSetStatusSchema.Type);
-        Assert.Equal(3, dataSetStatusSchema.Enum.Count);
-
-        var enumString1 = Assert.IsType<OpenApiString>(dataSetStatusSchema.Enum[0]);
+        var enumString1 = Assert.IsType<OpenApiString>(schema.Enum[0]);
         Assert.Equal(DataSetStatus.Published.ToString(), enumString1.Value);
 
-        var enumString2 = Assert.IsType<OpenApiString>(dataSetStatusSchema.Enum[1]);
+        var enumString2 = Assert.IsType<OpenApiString>(schema.Enum[1]);
         Assert.Equal(DataSetStatus.Deprecated.ToString(), enumString2.Value);
 
-        var enumString3 = Assert.IsType<OpenApiString>(dataSetStatusSchema.Enum[2]);
+        var enumString3 = Assert.IsType<OpenApiString>(schema.Enum[2]);
         Assert.Equal(DataSetStatus.Unpublished.ToString(), enumString3.Value);
-
-        Assert.Equal("object", testClassSchema.Type);
-
-        var allOf = Assert.Single(dataSetStatusPropertySchema.AllOf);
-        Assert.Equal(nameof(DataSetStatus), allOf.Reference.Id);
-        Assert.Equal("#/definitions/DataSetStatus", allOf.Reference.ReferenceV2);
-        Assert.Equal("#/components/schemas/DataSetStatus", allOf.Reference.ReferenceV3);
     }
 
-    private IReadOnlyDictionary<string, OpenApiSchema> GenerateSchema()
+    private OpenApiSchema GenerateSchema()
     {
         _schemaGenerator.GenerateSchema(typeof(DataSetStatus), _schemaRepository);
-        _schemaGenerator.GenerateSchema(typeof(TestClass), _schemaRepository);
 
-        return _schemaRepository.Schemas;
-    }
-
-    private class TestClass
-    {
-        public DataSetStatus DataSetStatus { get; init; }
+        return _schemaRepository.Schemas[nameof(DataSetStatus)];
     }
 }
