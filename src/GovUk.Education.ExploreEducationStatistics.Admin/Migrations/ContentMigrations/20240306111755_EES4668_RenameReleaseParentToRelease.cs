5e73d3c084cd6ac2c6dfbd16e7b0af1af8512b09
Ben Outram <ben@hiveit.co.uk>
2024-03-06 14:35:04 +0000

    EES-4668 Rename indexes and foreign key constraints not covered by table renaming.

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20240306111755_EES4668_RenameReleaseParentToRelease.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20240306111755_EES4668_RenameReleaseParentToRelease.cs
@@ -10,6 +10,45 @@ public partial class EES4668_RenameReleaseParentToRelease : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
     {
+        // Rename ReleaseVersions indexes which were not renamed by the Releases -> ReleaseVersions table rename
+        // in migration 20240229162115_EES4668_RenameReleaseToReleaseVersion
+        migrationBuilder.RenameIndex(
+            name: "PK_Releases",
+            table: "ReleaseVersions",
+            newName: "PK_ReleaseVersions");
+
+        migrationBuilder.RenameIndex(
+            name: "IX_Releases_PublicationId",
+            table: "ReleaseVersions",
+            newName: "IX_ReleaseVersions_PublicationId");
+
+        migrationBuilder.RenameIndex(
+            name: "IX_Releases_CreatedById",
+            table: "ReleaseVersions",
+            newName: "IX_ReleaseVersions_CreatedById");
+
+        migrationBuilder.RenameIndex(
+            name: "IX_Releases_PreviousVersionId_Version",
+            table: "ReleaseVersions",
+            newName: "IX_ReleaseVersions_PreviousVersionId_Version");
+
+        migrationBuilder.RenameIndex(
+            name: "IX_Releases_Type",
+            table: "ReleaseVersions",
+            newName: "IX_ReleaseVersions_Type");
+
+        migrationBuilder.RenameIndex(
+            name: "IX_Releases_ReleaseParentId",
+            table: "ReleaseVersions",
+            newName: "IX_ReleaseVersions_ReleaseParentId");
+
+        // Rename foreign key constraints which were not renamed by the Releases -> ReleaseVersions table rename
+        // in migration 20240229162115_EES4668_RenameReleaseToReleaseVersion
+        migrationBuilder.Sql("EXEC sp_rename 'dbo.FK_Releases_Publications_PublicationId', 'FK_ReleaseVersions_Publications_PublicationId', 'OBJECT'");
+        migrationBuilder.Sql("EXEC sp_rename 'dbo.FK_Releases_ReleaseParents_ReleaseParentId', 'FK_ReleaseVersions_ReleaseParents_ReleaseParentId', 'OBJECT'");
+        migrationBuilder.Sql("EXEC sp_rename 'dbo.FK_Releases_Releases_PreviousVersionId', 'FK_ReleaseVersions_Releases_PreviousVersionId', 'OBJECT'");
+        migrationBuilder.Sql("EXEC sp_rename 'dbo.FK_Releases_Users_CreatedById', 'FK_ReleaseVersions_Users_CreatedById', 'OBJECT'");
+
         // Drop the ReleaseVersions foreign key constraint
         migrationBuilder.DropForeignKey(
             name: "FK_ReleaseVersions_ReleaseParents_ReleaseParentId",
@@ -19,6 +58,12 @@ public partial class EES4668_RenameReleaseParentToRelease : Migration
         migrationBuilder.RenameTable(
             name: "ReleaseParents",
             newName: "Releases");
+
+        // Rename indexes which are not renamed by the ReleaseParents -> Releases table rename
+        migrationBuilder.RenameIndex(
+            name: "PK_ReleaseParents",
+            table: "Releases",
+            newName: "PK_Releases");
 
         // Rename the ReleaseVersions.ReleaseParentId column
         migrationBuilder.RenameColumn(
