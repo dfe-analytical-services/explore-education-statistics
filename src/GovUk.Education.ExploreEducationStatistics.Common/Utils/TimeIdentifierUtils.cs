5b049528276afe83139c48a1f1176867e1265264
Nicholas Tsim <nicholastsim@gmail.com>
2024-03-06 22:40:40 +0000

    EES-4716 Add utils for getting data-only TimeIdentifiers

---
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Utils/TimeIdentifierUtils.cs
@@ -0,0 +1,37 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Common.Model;
+
+namespace GovUk.Education.ExploreEducationStatistics.Common.Utils;
+
+public static class TimeIdentifierUtils
+{
+    private static readonly HashSet<TimeIdentifier> ReleaseOnlyEnums = new()
+    {
+        TimeIdentifier.AcademicYearNationalTutoringProgramme
+    };
+
+    private static readonly Lazy<IReadOnlyList<TimeIdentifier>> DataEnumsLazy = new(() =>
+        EnumUtil.GetEnums<TimeIdentifier>()
+            .Where(ti => !ReleaseOnlyEnums.Contains(ti))
+            .ToList());
+
+    private static readonly Lazy<IReadOnlyList<string>> DataCodesLazy = new(() =>
+        DataEnumsLazy.Value
+            .Select(ti => ti.GetEnumValue())
+            .ToList());
+
+    private static readonly Lazy<IReadOnlyList<string>> DataLabelsLazy = new(() =>
+        DataEnumsLazy.Value
+            .Select(ti => ti.GetEnumLabel())
+            .ToList());
+
+    public static IReadOnlyList<TimeIdentifier> DataEnums => DataEnumsLazy.Value;
+
+    public static IReadOnlyList<string> DataCodes => DataCodesLazy.Value;
+
+    public static IReadOnlyList<string> DataLabels => DataLabelsLazy.Value;
+}
