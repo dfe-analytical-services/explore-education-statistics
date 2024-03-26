e41f354315c9904b36f4c848bfa1d639eb2c7899
Nicholas Tsim <nicholastsim@gmail.com>
2024-03-26 20:19:07 +0000

    EES-5017 Add DuckDB querying infrastructure for public API
    This includes upgrading DuckDB.NET to 0.10.1.2 and introduces:
    
    - Dapper - for executing queries and mapping results
    - InterpolatedSql - for writing SQL queries and mitigating SQL injection
    - MiniProfiler - for performance profiling

---
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Extensions/SqlBuilderExtensions.cs
@@ -0,0 +1,106 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using InterpolatedSql;
+using InterpolatedSql.SqlBuilders;
+
+namespace GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+
+public static class SqlBuilderExtensions
+{
+    /// <summary>
+    /// Append multiple literal fragments to the SQL builder.
+    /// </summary>
+    /// <param name="builder">The SQL builder</param>
+    /// <param name="literals">The literal fragments to append</param>
+    /// <param name="joinString">A string to join the fragments with</param>
+    /// <returns>The builder</returns>
+    public static TBuilder AppendRange<TBuilder>(
+        this TBuilder builder,
+        IEnumerable<string> literals,
+        string? joinString = null) where TBuilder : InterpolatedSqlBuilderBase
+    {
+        return builder.AppendRange(
+            items: literals,
+            append: builder.AppendLiteral,
+            joinString: joinString
+        );
+    }
+
+    /// <summary>
+    /// Append multiple <see cref="FormattableString"/> SQL fragments to the SQL builder.
+    /// </summary>
+    /// <param name="builder">The SQL builder</param>
+    /// <param name="fragments">The SQL fragments to append</param>
+    /// <param name="joinString">A string to join the fragments with</param>
+    /// <returns></returns>
+    public static TBuilder AppendRange<TBuilder>(
+        this TBuilder builder,
+        IEnumerable<FormattableString> fragments,
+        string? joinString = null) where TBuilder : InterpolatedSqlBuilderBase
+    {
+        return builder.AppendRange(
+            items: fragments,
+            append: builder.AppendFormattableString,
+            joinString: joinString
+        );
+    }
+
+    /// <summary>
+    /// Append multiple SQL builders to the SQL builder.
+    /// </summary>
+    /// <param name="builder">The SQL builder</param>
+    /// <param name="builders">The SQL builders to append</param>
+    /// <param name="joinString">A string to join the fragments with</param>
+    public static TBuilder AppendRange<TBuilder>(
+        this TBuilder builder,
+        IEnumerable<IBuildable<IInterpolatedSql>> builders,
+        string? joinString = null) where TBuilder : InterpolatedSqlBuilderBase
+    {
+        return builder.AppendRange(
+            items: builders,
+            append: b => builder.Append(b.Build()),
+            joinString: joinString
+        );
+    }
+
+    /// <summary>
+    /// Append multiple <see cref="IInterpolatedSql"/> fragments to the SQL builder.
+    /// </summary>
+    /// <param name="builder">The SQL builder</param>
+    /// <param name="fragments">The SQL fragments to append</param>
+    /// <param name="joinString">A string to join the fragments with</param>
+    public static TBuilder AppendRange<TBuilder>(
+        this TBuilder builder,
+        IEnumerable<IInterpolatedSql> fragments,
+        string? joinString = null) where TBuilder : InterpolatedSqlBuilderBase
+    {
+        return builder.AppendRange(
+            items: fragments,
+            append: builder.Append,
+            joinString: joinString
+        );
+    }
+
+    private static TBuilder AppendRange<TBuilder, TItem>(
+        this TBuilder builder,
+        IEnumerable<TItem> items,
+        Action<TItem> append,
+        string? joinString) where TBuilder : InterpolatedSqlBuilderBase
+    {
+        var itemsList = items.ToList();
+
+        foreach (var (item, index) in itemsList.WithIndex())
+        {
+            append(item);
+
+            if (!itemsList.IsLastIndex(index) && joinString is not null)
+            {
+                builder.AppendLiteral(joinString);
+            }
+        }
+
+        return builder;
+    }
+}
