d5042bd3159c238c54cc0214f42f4e18fa818326
jack-hive <148866614+jack-hive@users.noreply.github.com>
2024-04-08 17:17:09 +0100

    EES-5056 Adding relationship from the Content DB's Files table to the Public API DB's DataSetVersion table

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20240408155731_EES5056_AddPublicDataSetVersionIdToFilesTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    /// <inheritdoc />
+    public partial class EES5056_AddPublicDataSetVersionIdToFilesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PublicDataSetVersionId",
+                table: "Files",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicDataSetVersionId",
+                table: "Files");
+        }
+    }
+}
