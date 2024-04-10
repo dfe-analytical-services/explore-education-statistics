c693dc69f3995d66854468ca5abff5bb629c3e18
SaicharanMuthyapwar <smuthyapwar@gmail.com>
2024-04-10 11:33:20 +0100

    EES-5051 - Amending config file

--- tests/playwright-tests/playwright.config.ts
+++ tests/playwright-tests/playwright.config.ts
@@ -20,9 +20,16 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [
+        ['html'],
+        ['github'],
+        ['list'],
+        ['junit', { outputFile: 'test-results/playwright-results.xml' }],
+      ]
+    : [['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
