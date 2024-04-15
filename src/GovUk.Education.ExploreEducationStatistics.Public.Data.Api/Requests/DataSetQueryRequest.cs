d5ff6a228e4071039848d2bc836751403b0692c8
Nicholas Tsim <nicholastsim@gmail.com>
2024-04-16 00:11:12 +0100

    EES-4722 Refactor page, pageSize and debug into `DataSetQueryRequest`

--- src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api/Requests/DataSetQueryRequest.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api/Requests/DataSetQueryRequest.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Requests;
 
 /// <summary>
@@ -22,4 +24,24 @@ public record DataSetQueryRequest
     /// By default, results are sorted by time period in descending order.
     /// </summary>
     public IReadOnlyList<DataSetQuerySort>? Sorts { get; init; }
+
+    /// <summary>
+    /// Enable debug mode. Results will be formatted with human-readable
+    /// labels to assist in identification.
+    ///
+    /// This **should not** be enabled in a production environment.
+    /// </summary>
+    public bool Debug { get; init; }
+
+    /// <summary>
+    /// The page of results to fetch.
+    /// </summary>
+    [DefaultValue(1)]
+    public int Page { get; init; } = 1;
+
+    /// <summary>
+    /// The maximum number of results per page.
+    /// </summary>
+    [DefaultValue(1000)]
+    public int PageSize { get; init; } = 1000;
 }
