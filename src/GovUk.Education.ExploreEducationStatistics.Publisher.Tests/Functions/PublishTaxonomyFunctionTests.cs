ca4cff793684e2a4c4aae3a0f80b3212ed55cdf1
Duncan Watson <duncan@hiveit.co.uk>
2024-04-23 17:03:23 +0100

    EES-4909 - added in a simple example test class for integration-testing a Function directly using the new FunctionsIntegrationTest setup.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher.Tests/Functions/PublishTaxonomyFunctionTests.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Common.Tests.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Common.Tests.Functions;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Tests.Fixtures;
+using GovUk.Education.ExploreEducationStatistics.Content.Services.Cache;
+using GovUk.Education.ExploreEducationStatistics.Content.ViewModels;
+using GovUk.Education.ExploreEducationStatistics.Publisher.Functions;
+using GovUk.Education.ExploreEducationStatistics.Publisher.Model;
+using Xunit;
+using static GovUk.Education.ExploreEducationStatistics.Common.Tests.Utils.MockUtils;
+
+namespace GovUk.Education.ExploreEducationStatistics.Publisher.Tests.Functions;
+
+// This test has been added for EES-4909 as a simple example of using the Functions integration testing setup to test
+// a Function directly.
+public class PublishTaxonomyFunctionTests(PublisherFunctionsIntegrationTestFixture fixture)
+    : PublisherFunctionsIntegrationTest(fixture)
+{
+    public class PublishTaxonomy(PublisherFunctionsIntegrationTestFixture fixture)
+        : PublishTaxonomyFunctionTests(fixture)
+    {
+        [Fact]
+        public async Task MethodologyTree()
+        {
+            Topic topic = DataFixture
+                .DefaultTopic()
+                .WithTheme(DataFixture
+                    .DefaultTheme());
+
+            Publication publication = DataFixture
+                .DefaultPublication()
+                .WithTopic(topic);
+            
+            Methodology methodology = DataFixture
+                .DefaultMethodology()
+                .WithMethodologyVersions(DataFixture
+                    .DefaultMethodologyVersion()
+                    .Generate(1))
+                .FinishWith(methodology => methodology.LatestPublishedVersion = methodology.Versions[0])
+                .WithOwningPublication(publication);
+            
+            await AddTestData<ContentDbContext>(context =>
+            {
+                context.Methodologies.Add(methodology);
+            });
+
+            List<AllMethodologiesThemeViewModel> expectedMethodologyTree = 
+            [
+                new AllMethodologiesThemeViewModel()
+                {
+                    Id = topic.Theme.Id,
+                    Title = topic.Theme.Title,
+                    Topics =
+                    [
+                        new AllMethodologiesTopicViewModel
+                        {
+                            Id = topic.Id,
+                            Title = topic.Title,
+                            Publications =
+                            [
+                                new AllMethodologiesPublicationViewModel
+                                {
+                                    Id = publication.Id,
+                                    Title = publication.Title,
+                                    Methodologies =
+                                    [
+                                        new MethodologyVersionSummaryViewModel
+                                        {
+                                            Id = methodology.Versions[0].Id,
+                                            Title = methodology.Versions[0].Title,
+                                            Slug = methodology.Versions[0].Slug
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ];
+            
+            PublicBlobCacheService
+                .Setup(s => s.SetItemAsync<object>(new AllMethodologiesCacheKey(), ItIs.DeepEqualTo(expectedMethodologyTree)))
+                .Returns(Task.CompletedTask);
+            
+            PublicBlobCacheService
+                .Setup(s => s.SetItemAsync<object>(new PublicationTreeCacheKey(), new List<PublicationTreeThemeViewModel>()))
+                .Returns(Task.CompletedTask);
+            
+            var function = GetRequiredService<PublishTaxonomyFunction>();
+            await function.PublishTaxonomy(new PublishTaxonomyMessage(), new TestFunctionContext());
+            
+            VerifyAllMocks(PublicBlobCacheService);
+        }
+    }
+}
