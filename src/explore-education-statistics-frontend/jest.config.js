48ad652d885a77a41679252ccc2732040632df4b
Sam Biram <samuel.biram@airelogic.com>
2024-04-24 08:21:39 +0100

    Remove comment

--- src/explore-education-statistics-frontend/jest.config.js
+++ src/explore-education-statistics-frontend/jest.config.js
@@ -1,7 +1,6 @@
 const nextJest = require('next/jest.js');
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 });
 
