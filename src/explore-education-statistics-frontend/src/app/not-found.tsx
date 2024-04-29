ed3ad12a3e85dbda31bedb9d7c071d05950b9279
Sam Biram <samuel.biram@airelogic.com>
2024-04-29 09:26:50 +0100

    Implement NotFound route in app-router

--- src/explore-education-statistics-frontend/src/app/not-found.tsx
+++ src/explore-education-statistics-frontend/src/app/not-found.tsx
@@ -1,5 +1,23 @@
-import NotFoundPage from '@frontend/modules/NotFoundPage';
+import Link from '@frontend/components/Link';
+import Page from '@frontend/components/Page';
+import React from 'react';
 
-export default async function NotFound() {
-  return NotFoundPage;
+export default function NotFoundPage() {
+  return (
+    <Page title="Page not found">
+      <p>If you typed the web address, check it's correct.</p>
+      <p>
+        If you cut and pasted the web address, check you copied the entire
+        address.
+      </p>
+      <p>
+        If the web address is correct or you clicked a link or button and ended
+        up on this page,{' '}
+        <Link to="/contact-us">
+          contact our Explore education statistics team
+        </Link>{' '}
+        if you need any help or support.
+      </p>
+    </Page>
+  );
 }
