53ef76cb7fad93027beb4746bdedbdfeaf899ad1
SaicharanMuthyapwar <smuthyapwar@gmail.com>
2024-05-07 11:54:11 +0100

    EES-5051 Tidying up the code

--- tests/playwright-tests/utils/env.ts
+++ tests/playwright-tests/utils/env.ts
@@ -5,7 +5,6 @@ dotenv.config();
 
 const environment = {
   PUBLIC_URL: process.env.PUBLIC_URL ?? '',
-  PUBLIC_URL_WITHOUT_USERNAME_PASSWORD: process.env.PUBLIC_URL ?? '',
   PUBLIC_USERNAME: process.env.PUBLIC_USERNAME ?? '',
   PUBLIC_PASSWORD: process.env.PUBLIC_PASSWORD ?? '',
   ADMIN_URL: process.env.ADMIN_URL ?? '',
