977ee214ced4595288e9f4a8d47a1b0e466f682c
SaicharanMuthyapwar <smuthyapwar@gmail.com>
2024-05-09 13:45:51 +0100

    EES-5051 Actioned as per PR comments

--- tests/playwright-tests/playwright.config.ts
+++ tests/playwright-tests/playwright.config.ts
@@ -3,7 +3,6 @@ import environment from '@util/env';
 
 const { PUBLIC_USERNAME, PUBLIC_PASSWORD } = environment;
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 function appendBasicAuthCredentialsToPublic() {
   const username = PUBLIC_USERNAME;
   const password = PUBLIC_PASSWORD;
