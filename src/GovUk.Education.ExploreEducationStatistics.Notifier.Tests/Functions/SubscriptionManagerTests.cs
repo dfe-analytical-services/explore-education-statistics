c1efc40d82256e1e4de6fa2ca55c2674840f7e22
Sam Biram <samuel.biram@airelogic.com>
2024-05-21 13:39:42 +0100

    Add more tests

--- src/GovUk.Education.ExploreEducationStatistics.Notifier.Tests/Functions/SubscriptionManagerTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Notifier.Tests/Functions/SubscriptionManagerTests.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Common.Tests.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Common.Tests.Functions;
 using GovUk.Education.ExploreEducationStatistics.Notifier.Configuration;
 using GovUk.Education.ExploreEducationStatistics.Notifier.Functions;
@@ -14,6 +16,7 @@ using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
 using static GovUk.Education.ExploreEducationStatistics.Notifier.Tests.Utils.NotifierTestUtils;
+using static GovUk.Education.ExploreEducationStatistics.Common.TableStorageTableNames;
 
 namespace GovUk.Education.ExploreEducationStatistics.Notifier.Tests.Functions;
 
@@ -71,7 +74,7 @@ public class SubscriptionManagerFunctionTests(NotifierFunctionsIntegrationTestFi
         };
         
         // Act
-        var result = await notifierFunction.RequestPendingSubscriptionFunc(request, new TestFunctionContext());
+        var result = await notifierFunction.RequestPendingSubscriptionFunc(request, new TestFunctionContext(), new CancellationToken());
 
         // Assert
         var okResult = Assert.IsType<OkObjectResult>(result);
@@ -82,19 +85,336 @@ public class SubscriptionManagerFunctionTests(NotifierFunctionsIntegrationTestFi
             mock.SendEmail(notificationClient, "test@test.com", "subscription-verification-id",
                 It.Is<Dictionary<string, dynamic>>(d =>
                     AssertEmailTemplateValues(d, "Test Publication Title",
-                        "https://localhost:3000/subscriptions/test-publication-slug/confirm-subscription/activation-code-1")
+                        "https://localhost:3000/subscriptions/test-publication-slug/confirm-subscription/activation-code-1", null)
                 )), Times.Once);
     }
+    
+    [Fact]
+    public async Task DoesNotSendEmailAgainIfSubIsPending()
+    {
+        // Arrange (data)
+        await fixture.AddTestSubscription(NotifierPendingSubscriptionsTableName,
+            new("123abc", "test@test.com", "Test Publication Title", "test-publication-slug", DateTime.UtcNow));
+        
+        // Arrange (mocks)
+        var storageTableService = new StorageTableService(Options.Create(new AppSettingOptions()
+        {
+            TableStorageConnectionString = fixture.TableStorageConnectionString()
+        }));
+        
+        var notificationClient = MockNotificationClient(HttpStatusCode.OK, "response content");
+        var notificationClientProvider = new Mock<INotificationClientProvider>(MockBehavior.Strict);
+        notificationClientProvider.Setup(mock =>
+                mock.Get())
+            .Returns(notificationClient);
+        
+        
+        var tokenService = new Mock<ITokenService>(MockBehavior.Strict);
+        tokenService.Setup(mock => 
+                mock.GenerateToken("test@test.com", It.IsAny<DateTime>()))
+            .Returns("activation-code-1");
+        
+        var emailService = new Mock<IEmailService>(MockBehavior.Strict);
+        emailService.Setup(mock =>
+            mock.SendEmail(notificationClient, "test@test.com", "subscription-verification-id",
+                It.IsAny<Dictionary<string, dynamic>>()));
+        
+        var notifierFunction = BuildFunction(
+            storageTableService: storageTableService,
+            notificationClientProvider: notificationClientProvider.Object,
+            tokenService: tokenService.Object,
+            emailService: emailService.Object);
+        
+        var request = new NewPendingSubscriptionRequest()
+        {
+            Id = "123abc", 
+            Slug = "test-publication-slug", 
+            Email= "test@test.com", 
+            Title = "Test Publication Title"
+        };
+        
+        // Act
+        var result = await notifierFunction.RequestPendingSubscriptionFunc(request, new TestFunctionContext(), new CancellationToken());
 
+        // Assert
+        var okResult = Assert.IsType<OkResult>(result);
+        Assert.NotNull(okResult);
+        Assert.Equal(200, okResult.StatusCode);
+        
+        emailService.Verify(mock =>
+            mock.SendEmail(notificationClient, "test@test.com", "subscription-verification-id",
+                It.Is<Dictionary<string, dynamic>>(d =>
+                    AssertEmailTemplateValues(d, "Test Publication Title",
+                        "https://localhost:3000/subscriptions/test-publication-slug/confirm-subscription/activation-code-1", null)
+                )), Times.Never);
+    }
+    
+    [Fact]
+    public async Task SendsConfirmationEmailIfUserAlreadySubscribed()
+    {
+        // Arrange (data)
+        await fixture.AddTestSubscription(NotifierSubscriptionsTableName,
+            new("123abc-2", "test2@test.com", "Test Publication Title 2", "test-publication-slug-2", DateTime.UtcNow.AddDays(-4)));
+        
+        // Arrange (mocks)
+        var storageTableService = new StorageTableService(Options.Create(new AppSettingOptions()
+        {
+            TableStorageConnectionString = fixture.TableStorageConnectionString()
+        }));
+        
+        var notificationClient = MockNotificationClient(HttpStatusCode.OK, "response content");
+        var notificationClientProvider = new Mock<INotificationClientProvider>(MockBehavior.Strict);
+        notificationClientProvider.Setup(mock =>
+                mock.Get())
+            .Returns(notificationClient);
+        
+        
+        var tokenService = new Mock<ITokenService>(MockBehavior.Strict);
+        tokenService.Setup(mock => 
+                mock.GenerateToken("test2@test.com", It.IsAny<DateTime>()))
+            .Returns("activation-code-1");
+        
+        var emailService = new Mock<IEmailService>(MockBehavior.Strict);
+        emailService.Setup(mock =>
+            mock.SendEmail(notificationClient, "test2@test.com", "subscription-confirmation-id",
+                It.IsAny<Dictionary<string, dynamic>>()));
+        
+        var notifierFunction = BuildFunction(
+            storageTableService: storageTableService,
+            notificationClientProvider: notificationClientProvider.Object,
+            tokenService: tokenService.Object,
+            emailService: emailService.Object);
+        
+        var request = new NewPendingSubscriptionRequest()
+        {
+            Id = "123abc-2", 
+            Slug = "test-publication-slug-2", 
+            Email= "test2@test.com", 
+            Title = "Test Publication Title 2"
+        };
+        
+        // Act
+        var result = await notifierFunction.RequestPendingSubscriptionFunc(request, new TestFunctionContext(), new CancellationToken());
+
+        // Assert
+        var okResult = Assert.IsType<OkResult>(result);
+        Assert.NotNull(okResult);
+        Assert.Equal(200, okResult.StatusCode);
+        
+        emailService.Verify(mock =>
+            mock.SendEmail(notificationClient, "test2@test.com", "subscription-confirmation-id",
+                It.Is<Dictionary<string, dynamic>>(d =>
+                    AssertEmailTemplateValues(d, "Test Publication Title 2", null, "https://localhost:3000/subscriptions/test-publication-slug-2/confirm-unsubscription/activation-code-1")
+                )), Times.Once);
+    }
+    
+    [Fact]
+    public async Task RequestPendingSubscription_ReturnsValidationProblem_When_Id_Is_Blank()
+    {
+        // Arrange (mocks)
+        var storageTableService = new StorageTableService(Options.Create(new AppSettingOptions()
+        {
+            TableStorageConnectionString = fixture.TableStorageConnectionString()
+        }));
+        
+        var notificationClient = MockNotificationClient(HttpStatusCode.OK, "response content");
+        var notificationClientProvider = new Mock<INotificationClientProvider>(MockBehavior.Strict);
+        notificationClientProvider.Setup(mock =>
+                mock.Get())
+            .Returns(notificationClient);
+        
+        
+        var tokenService = new Mock<ITokenService>(MockBehavior.Strict);
+        tokenService.Setup(mock => 
+                mock.GenerateToken("test@test.com", It.IsAny<DateTime>()))
+            .Returns("activation-code-1");
+        
+        var emailService = new Mock<IEmailService>(MockBehavior.Strict);
+        emailService.Setup(mock =>
+            mock.SendEmail(notificationClient, "test@test.com", "subscription-verification-id",
+                It.IsAny<Dictionary<string, dynamic>>()));
+        
+        var notifierFunction = BuildFunction(
+            storageTableService: storageTableService,
+            notificationClientProvider: notificationClientProvider.Object,
+            tokenService: tokenService.Object,
+            emailService: emailService.Object);
+        
+        var request = new NewPendingSubscriptionRequest()
+        {
+            Id = "", 
+            Slug = "test-publication-slug", 
+            Email= "test@test.com", 
+            Title = "Test Publication Title"
+        };
+        
+        // Act
+        var result = await notifierFunction.RequestPendingSubscriptionFunc(request, new TestFunctionContext(), new CancellationToken());
+
+        // Assert
+        var validationProblem = result.AssertBadRequestWithValidationProblem();
+        validationProblem.AssertHasNotEmptyError(nameof(NewPendingSubscriptionRequest.Id));
+    }
+    
+    [Fact]
+    public async Task RequestPendingSubscription_ReturnsValidationProblem_When_Title_Is_Blank()
+    {
+        // Arrange (mocks)
+        var storageTableService = new StorageTableService(Options.Create(new AppSettingOptions()
+        {
+            TableStorageConnectionString = fixture.TableStorageConnectionString()
+        }));
+        
+        var notificationClient = MockNotificationClient(HttpStatusCode.OK, "response content");
+        var notificationClientProvider = new Mock<INotificationClientProvider>(MockBehavior.Strict);
+        notificationClientProvider.Setup(mock =>
+                mock.Get())
+            .Returns(notificationClient);
+        
+        
+        var tokenService = new Mock<ITokenService>(MockBehavior.Strict);
+        tokenService.Setup(mock => 
+                mock.GenerateToken("test@test.com", It.IsAny<DateTime>()))
+            .Returns("activation-code-1");
+        
+        var emailService = new Mock<IEmailService>(MockBehavior.Strict);
+        emailService.Setup(mock =>
+            mock.SendEmail(notificationClient, "test@test.com", "subscription-verification-id",
+                It.IsAny<Dictionary<string, dynamic>>()));
+        
+        var notifierFunction = BuildFunction(
+            storageTableService: storageTableService,
+            notificationClientProvider: notificationClientProvider.Object,
+            tokenService: tokenService.Object,
+            emailService: emailService.Object);
+        
+        var request = new NewPendingSubscriptionRequest()
+        {
+            Id = "123abc", 
+            Slug = "test-publication-slug", 
+            Email= "test@test.com", 
+            Title = ""
+        };
+        
+        // Act
+        var result = await notifierFunction.RequestPendingSubscriptionFunc(request, new TestFunctionContext(), new CancellationToken());
+
+        // Assert
+        var validationProblem = result.AssertBadRequestWithValidationProblem();
+        validationProblem.AssertHasNotEmptyError(nameof(NewPendingSubscriptionRequest.Title));
+    }
+    
+    [Fact]
+    public async Task RequestPendingSubscription_ReturnsValidationProblem_When_Email_Is_Blank()
+    {
+        // Arrange (mocks)
+        var storageTableService = new StorageTableService(Options.Create(new AppSettingOptions()
+        {
+            TableStorageConnectionString = fixture.TableStorageConnectionString()
+        }));
+        
+        var notificationClient = MockNotificationClient(HttpStatusCode.OK, "response content");
+        var notificationClientProvider = new Mock<INotificationClientProvider>(MockBehavior.Strict);
+        notificationClientProvider.Setup(mock =>
+                mock.Get())
+            .Returns(notificationClient);
+        
+        
+        var tokenService = new Mock<ITokenService>(MockBehavior.Strict);
+        tokenService.Setup(mock => 
+                mock.GenerateToken("test@test.com", It.IsAny<DateTime>()))
+            .Returns("activation-code-1");
+        
+        var emailService = new Mock<IEmailService>(MockBehavior.Strict);
+        emailService.Setup(mock =>
+            mock.SendEmail(notificationClient, "test@test.com", "subscription-verification-id",
+                It.IsAny<Dictionary<string, dynamic>>()));
+        
+        var notifierFunction = BuildFunction(
+            storageTableService: storageTableService,
+            notificationClientProvider: notificationClientProvider.Object,
+            tokenService: tokenService.Object,
+            emailService: emailService.Object);
+        
+        var request = new NewPendingSubscriptionRequest()
+        {
+            Id = "123abc", 
+            Slug = "test-publication-slug", 
+            Email= "", 
+            Title = "Test Publication Title"
+        };
+        
+        // Act
+        var result = await notifierFunction.RequestPendingSubscriptionFunc(request, new TestFunctionContext(), new CancellationToken());
+
+        // Assert
+        var validationProblem = result.AssertBadRequestWithValidationProblem();
+        validationProblem.AssertHasNotEmptyError(nameof(NewPendingSubscriptionRequest.Email));
+    }
+    
+    [Fact]
+    public async Task RequestPendingSubscription_ReturnsValidationProblem_When_Slug_Is_Blank()
+    {
+        // Arrange (mocks)
+        var storageTableService = new StorageTableService(Options.Create(new AppSettingOptions()
+        {
+            TableStorageConnectionString = fixture.TableStorageConnectionString()
+        }));
+        
+        var notificationClient = MockNotificationClient(HttpStatusCode.OK, "response content");
+        var notificationClientProvider = new Mock<INotificationClientProvider>(MockBehavior.Strict);
+        notificationClientProvider.Setup(mock =>
+                mock.Get())
+            .Returns(notificationClient);
+        
+        
+        var tokenService = new Mock<ITokenService>(MockBehavior.Strict);
+        tokenService.Setup(mock => 
+                mock.GenerateToken("test@test.com", It.IsAny<DateTime>()))
+            .Returns("activation-code-1");
+        
+        var emailService = new Mock<IEmailService>(MockBehavior.Strict);
+        emailService.Setup(mock =>
+            mock.SendEmail(notificationClient, "test@test.com", "subscription-verification-id",
+                It.IsAny<Dictionary<string, dynamic>>()));
+        
+        var notifierFunction = BuildFunction(
+            storageTableService: storageTableService,
+            notificationClientProvider: notificationClientProvider.Object,
+            tokenService: tokenService.Object,
+            emailService: emailService.Object);
+        
+        var request = new NewPendingSubscriptionRequest()
+        {
+            Id = "123abc", 
+            Slug = "", 
+            Email= "test@test.com", 
+            Title = "Test Publication Title"
+        };
+        
+        // Act
+        var result = await notifierFunction.RequestPendingSubscriptionFunc(request, new TestFunctionContext(), new CancellationToken());
+
+        // Assert
+        var validationProblem = result.AssertBadRequestWithValidationProblem();
+        validationProblem.AssertHasNotEmptyError(nameof(NewPendingSubscriptionRequest.Slug));
+    }
+        
     private static bool AssertEmailTemplateValues(Dictionary<string, dynamic> values,
         string publicationName,
-        string? verificationLink)
+        string? verificationLink,
+        string? unsubscribeLink)
     {
         Assert.Equal(publicationName, values["publication_name"]);
 
         if (verificationLink != null)
         {
             Assert.Equal(verificationLink, values["verification_link"]);
+        }
+        
+        if (unsubscribeLink != null)
+        {
+            Assert.Equal(unsubscribeLink, values["unsubscribe_link"]);
         }
 
         return true;
