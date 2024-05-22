ecb494b8739d0e01fb662db4ce5af3fd70b89d16
Ben Outram <ben@hiveit.co.uk>
2024-05-22 12:53:37 +0100

    EES-5097 Convert DuckDb queries in SeedDataCommand.Seeder to use SqlBuilder API

--- src/GovUk.Education.ExploreEducationStatistics.Public.Data.Model/DuckDb/ProfiledDuckDbConnection.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Model/DuckDb/ProfiledDuckDbConnection.cs
@@ -1,3 +1,4 @@
+using DuckDB.NET.Data;
 using StackExchange.Profiling;
 using StackExchange.Profiling.Data;
 
@@ -7,4 +8,12 @@ namespace GovUk.Education.ExploreEducationStatistics.Public.Data.Model.DuckDb;
 /// DuckDB connection profiled using MiniProfiler.
 /// </summary>
 public class ProfiledDuckDbConnection(string connectionString = "DataSource=:memory:", IDbProfiler? profiler = null)
-    : ProfiledDbConnection(new DuckDbConnection(connectionString), profiler ?? MiniProfiler.Current), IDuckDbConnection;
+    : ProfiledDbConnection(new DuckDbConnection(connectionString), profiler ?? MiniProfiler.Current), IDuckDbConnection
+{
+    private DuckDbConnection WrappedDuckDbConnection => (DuckDbConnection)WrappedConnection;
+
+    public DuckDBAppender CreateAppender(string table)
+    {
+        return WrappedDuckDbConnection.CreateAppender(table);
+    }
+}
