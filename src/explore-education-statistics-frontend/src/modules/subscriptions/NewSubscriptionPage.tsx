e9a116e9bfe545eac2cd0a7faef07eb203832e14
Sam Biram <samuel.biram@airelogic.com>
2024-05-22 12:26:47 +0100

    Revert new subscription form to RHF components because formik-removal work is not on master yet

--- src/explore-education-statistics-frontend/src/modules/subscriptions/NewSubscriptionPage.tsx
+++ src/explore-education-statistics-frontend/src/modules/subscriptions/NewSubscriptionPage.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { GetServerSideProps, NextPage } from 'next';
 import LoadingSpinner from '@common/components/LoadingSpinner';
 import Button from '@common/components/Button';
-import FormFieldTextInput from '@common/components/form/FormFieldTextInput';
-import FormProvider from '@common/components/form/FormProvider';
-import Form from '@common/components/form/Form';
+import RHFFormFieldTextInput from '@common/components/form/rhf/RHFFormFieldTextInput';
+import RHFForm from '@common/components/form/rhf/RHFForm';
+import FormProvider from '@common/components/form/rhf/FormProvider';
 import useMounted from '@common/hooks/useMounted';
 import publicationService from '@common/services/publicationService';
 import Yup from '@common/validation/yup';
@@ -86,8 +86,8 @@ const SubscriptionPage: NextPage<Props> = ({
                   <li>existing statistics and data are changed or corrected</li>
                 </ul>
 
-                <Form id="subscriptionForm" onSubmit={handleFormSubmit}>
-                  <FormFieldTextInput<FormValues>
+                <RHFForm id="subscriptionForm" onSubmit={handleFormSubmit}>
+                  <RHFFormFieldTextInput<FormValues>
                     label="Enter your email address"
                     hint="This will only be used to subscribe you to updates. You can unsubscribe at any time"
                     name="email"
@@ -99,7 +99,7 @@ const SubscriptionPage: NextPage<Props> = ({
                       ? 'Submitting'
                       : 'Subscribe'}
                   </Button>
-                </Form>
+                </RHFForm>
               </>
             )}
             {formState.isSubmitSuccessful && (
