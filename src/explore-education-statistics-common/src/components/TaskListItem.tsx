532fc3d2d13bc4977733dc2e8a33c06c442479e1
Nicholas Tsim <nicholastsim@gmail.com>
2024-05-22 19:05:36 +0100

    EES-4366 Add common `TaskList` component

---
+++ src/explore-education-statistics-common/src/components/TaskListItem.tsx
@@ -0,0 +1,52 @@
+import classNames from 'classnames';
+import React, { ReactNode } from 'react';
+import styles from './TaskListItem.module.scss';
+
+export interface TaskListItemRenderProps {
+  'aria-describedby': string;
+}
+
+export interface TaskListItemProps {
+  children: (props: TaskListItemRenderProps) => ReactNode;
+  className?: string;
+  hint?: string;
+  id: string;
+  status: ReactNode;
+}
+
+export default function TaskListItem({
+  children,
+  className,
+  hint,
+  id,
+  status,
+}: TaskListItemProps) {
+  const statusId = `${id}-status`;
+  const hintId = `${id}-hint`;
+
+  return (
+    <li className={classNames('govuk-task-list__item', styles.item, className)}>
+      <div
+        className={classNames(
+          'govuk-task-list__name-and-hint',
+          styles.nameAndHint,
+        )}
+      >
+        {children({
+          'aria-describedby': classNames(statusId, {
+            [hintId]: !!hint,
+          }),
+        })}
+
+        {hint && (
+          <div className="govuk-task-list__hint" id={hintId}>
+            {hint}
+          </div>
+        )}
+      </div>
+      <div className="govuk-task-list__status" id={statusId}>
+        {status}
+      </div>
+    </li>
+  );
+}
