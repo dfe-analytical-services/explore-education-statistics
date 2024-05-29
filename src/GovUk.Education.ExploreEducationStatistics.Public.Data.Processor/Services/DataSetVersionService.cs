587b42db89559db280695f9d15612104c9c0d23e
jack-hive <148866614+jack-hive@users.noreply.github.com>
2024-05-29 12:07:02 +0100

    EES-5172 Processor - Guarding against deleting a `Draft` DataSetVersion

--- src/GovUk.Education.ExploreEducationStatistics.Public.Data.Processor/Services/DataSetVersionService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Processor/Services/DataSetVersionService.cs
@@ -1,5 +1,8 @@
 using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Common.Model;
+using GovUk.Education.ExploreEducationStatistics.Common.Validators;
+using GovUk.Education.ExploreEducationStatistics.Common.Validators.ErrorDetails;
+using GovUk.Education.ExploreEducationStatistics.Common.ViewModels;
 using GovUk.Education.ExploreEducationStatistics.Content.Model;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
 using GovUk.Education.ExploreEducationStatistics.Public.Data.Model;
@@ -9,6 +12,7 @@ using GovUk.Education.ExploreEducationStatistics.Public.Data.Services.Interfaces
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Transactions;
+using ValidationMessages = GovUk.Education.ExploreEducationStatistics.Public.Data.Processor.Requests.Validators.ValidationMessages;
 
 namespace GovUk.Education.ExploreEducationStatistics.Public.Data.Processor.Services;
 
@@ -20,28 +24,29 @@ internal class DataSetVersionService(
 {
     public async Task<Either<ActionResult, Unit>> DeleteVersion(Guid dataSetVersionId, CancellationToken cancellationToken = default)
     {
-        var transactionOptions = new TransactionOptions
-        {
-            IsolationLevel = IsolationLevel.ReadCommitted
-        };
-
         var strategy = contentDbContext.Database.CreateExecutionStrategy();
 
         return await strategy.ExecuteAsync(
             async () =>
             {
-                using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                using var transactionScope = new TransactionScope(
+                    TransactionScopeOption.Required,
+                    new TransactionOptions
+                    {
+                        IsolationLevel = IsolationLevel.ReadCommitted
+                    },
+                    TransactionScopeAsyncFlowOption.Enabled);
 
                 return await GetDataSetVersion(dataSetVersionId, cancellationToken)
+                    .OnSuccessDo(CheckCanDeleteDataSetVersion)
                     .OnSuccessDo(async dataSetVersion => await GetReleaseFile(dataSetVersion, cancellationToken)
                         .OnSuccessVoid(async releaseFile => await UpdateFilePublicApiDataSetId(releaseFile, cancellationToken))
-                        .OnFailureDo(_ => throw new KeyNotFoundException($"Expected 'ReleaseFile' with ID '{dataSetVersion.ReleaseFileId}' not found.")))
+                        .OnFailureDo(_ => throw new KeyNotFoundException($"The expected 'ReleaseFile', with ID '{dataSetVersion.ReleaseFileId}', was not found.")))
                     .OnSuccessDo(async dataSetVersion => await DeleteDataSetVersion(dataSetVersion, cancellationToken))
                     .OnSuccessVoid(DeleteParquetFiles)
                     .OnSuccessVoid(transactionScope.Complete);
             });
     }
-
     private async Task<Either<ActionResult, DataSetVersion>> GetDataSetVersion(Guid dataSetVersionId, CancellationToken cancellationToken)
     {
         return await publicDataDbContext.DataSetVersions
@@ -49,6 +54,21 @@ internal class DataSetVersionService(
            .Include(dsv => dsv.DataSet)
            .Where(dsv => dsv.Id == dataSetVersionId)
            .SingleOrNotFoundAsync(cancellationToken);
+    }
+
+    private Either<ActionResult, Unit> CheckCanDeleteDataSetVersion(DataSetVersion dataSetVersion)
+    {
+        if (dataSetVersion.Status == DataSetVersionStatus.Draft)
+        {
+            return Unit.Instance;
+        }
+
+        return ValidationUtils.ValidationResult(new ErrorViewModel
+        {
+            Code = ValidationMessages.DataSetVersionCanNotBeDeleted.Code,
+            Message = ValidationMessages.DataSetVersionCanNotBeDeleted.Message,
+            Detail = new InvalidErrorDetail<Guid>(dataSetVersion.Id)
+        });
     }
 
     private async Task DeleteDataSetVersion(DataSetVersion dataSetVersion, CancellationToken cancellationToken)
