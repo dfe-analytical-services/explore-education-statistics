65ec3c217995698798cf9c15f0ca48ac6ffa2756
Mark Youngman <mark@hiveit.co.uk>
2024-06-24 17:30:09 +0100

    EES-4932 More updates to backend unit tests

--- src/GovUk.Education.ExploreEducationStatistics.Common.Tests/Utils/ResponseErrorAssertUtils.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common.Tests/Utils/ResponseErrorAssertUtils.cs
@@ -11,12 +11,16 @@ public class ResponseErrorAssertUtils
 {
         public static void AssertHasErrors(IEnumerable<ErrorViewModel> errors, List<ErrorViewModel> expectedErrors)
         {
+            List<ErrorViewModel> notFoundErrors = [];
+
             foreach (var error in errors)
             {
+
                 var foundError = expectedErrors.Find(expected => expected.Message == error.Message);
                 if (foundError == null)
                 {
-                    Assert.Fail($"Error message not found in expectedErrors:\n{error.Message}");
+                    notFoundErrors.Add(error);
+                    continue;
                 }
 
                 Assert.Equal(foundError.Code, error.Code);
@@ -25,13 +29,22 @@ public class ResponseErrorAssertUtils
                 expectedErrors.Remove(foundError);
             }
 
+            if (notFoundErrors.Count != 0)
+            {
+                var notFoundErrorMessages = notFoundErrors
+                    .Select(e => e.Message)
+                    .ToList()
+                    .JoinToString('\n');
+                Assert.Fail($"Error message(s) not found in expectedErrors:\n{notFoundErrorMessages}");
+            }
+
             if (expectedErrors.Count != 0)
             {
                 var expectedErrorMessages = expectedErrors
                     .Select(e => e.Message)
                     .ToList()
                     .JoinToString('\n');
-                Assert.Fail($"expectedErrors messages were not in the response:\n{expectedErrorMessages}");
+                Assert.Fail($"expectedErrors message(s) were not in the response:\n{expectedErrorMessages}");
             }
         }
 
