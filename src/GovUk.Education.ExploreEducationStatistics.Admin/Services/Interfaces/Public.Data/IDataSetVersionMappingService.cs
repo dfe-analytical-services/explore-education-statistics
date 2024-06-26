fc93e8e417bae89e26ace51f37db3952ecc7483e
Duncan Watson <duncan@hiveit.co.uk>
2024-06-26 16:56:15 +0100

    EES-5112 - added generic PSQL repository method for performing JSONB updates and service, controller and requests / responses for Admin endpoint.
    Added integration test to assert that updates are being performed successfully on the correct JSON fragments.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/Public.Data/IDataSetVersionMappingService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Admin.ViewModels.Public.Data;
+using GovUk.Education.ExploreEducationStatistics.Common.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces.Public.Data;
+
+public interface IDataSetVersionMappingService
+{
+    Task<Either<ActionResult, BatchMappingUpdatesResponseViewModel>> ApplyBatchMappingUpdates(
+        Guid nextDataSetVersionId,
+        BatchLocationMappingUpdatesRequest request,
+        CancellationToken cancellationToken = default);
+}
