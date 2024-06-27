007cb2f09cee50dc25655e19790ff47371b0b2c3
Duncan Watson <duncan@hiveit.co.uk>
2024-06-27 22:00:45 +0100

    EES-5112 - added retrieval of the updated JSON fragments after autosave completion, to be used in response.

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/IPostgreSqlRepository.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/IPostgreSqlRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,17 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
 
 public interface IPostgreSqlRepository
 {
+    Task<TResponse> GetJsonbFromPath<TDbContext, TRowId, TResponse>(
+        TDbContext context,
+        string tableName,
+        string idColumnName,
+        string jsonColumnName,
+        TRowId rowId,
+        string[] jsonPathSegments,
+        CancellationToken cancellationToken = default)
+        where TDbContext : DbContext
+        where TResponse : class;
+    
     Task UpdateJsonbByPath<TDbContext, TValue, TRowId>(
         TDbContext context,
         string tableName,
