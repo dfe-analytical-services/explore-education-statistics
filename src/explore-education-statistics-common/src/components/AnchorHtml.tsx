b4631b682456d9be65389e59bb145ba077c98ebf
Sam Biram <samuel.biram@airelogic.com>
2024-06-28 09:40:18 +0100

    Add concerns comments to AnchorHtml

--- src/explore-education-statistics-common/src/components/AnchorHtml.tsx
+++ src/explore-education-statistics-common/src/components/AnchorHtml.tsx
@@ -15,6 +15,11 @@ export default function AnchorHtml({
 }: AnchorHtmlProps) {
   const [sanitisedHref, setSanitisedHref] = useState<string | null>(null);
 
+  // TODO: Discuss how we want to achieve this in PR review
+  // One option is to make this a hook
+  // Another is to make this call during frontend's GetServerSideProps and expose to common through a context or something
+  // Another is to leave it like this - but there's still a question of what to render until the API has responded
+  // Also, can we handle caching a bit more smartly?
   useEffect(() => {
     const sanitiseHref = async (rawHref: string) => {
       setSanitisedHref(await applyRedirectRules(rawHref));
