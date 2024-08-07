b7f7cb18cfd5b5f89cf8a2ba71da799fc4becd4d
Nicholas Tsim <nicholastsim@gmail.com>
2024-08-07 17:33:39 +0100

    EES-4997 Refactor out common `getLocationCodeEntries` util

---
+++ src/explore-education-statistics-common/src/utils/getLocationCodeEntries.ts
@@ -0,0 +1,50 @@
+import typedKeys from '@common/utils/object/typedKeys';
+import pickBy from 'lodash/pickBy';
+import sortBy from 'lodash/sortBy';
+
+const codeKeys = ['id', 'code', 'oldCode', 'urn', 'laEstab', 'ukprn'] as const;
+
+export type LocationCodeKey = (typeof codeKeys)[number];
+
+export type LocationCodeLabels = Record<LocationCodeKey, string>;
+
+const defaultLabels = {
+  id: 'ID',
+  code: 'Code',
+  oldCode: 'Old code',
+  urn: 'URN',
+  laEstab: 'LAESTAB',
+  ukprn: 'UKPRN',
+} as const satisfies LocationCodeLabels;
+
+export interface LocationCodeEntry {
+  key: LocationCodeKey;
+  label: string;
+  value: string;
+}
+
+export default function getLocationCodeEntries(
+  location: Partial<LocationCodeLabels>,
+  overrideLabels: Partial<LocationCodeLabels> = {},
+): LocationCodeEntry[] {
+  const labels = {
+    ...defaultLabels,
+    ...overrideLabels,
+  };
+
+  const keys = typedKeys<Partial<LocationCodeLabels>>(
+    pickBy(
+      location,
+      (_, key) =>
+        labels[key as LocationCodeKey] && location[key as LocationCodeKey],
+    ),
+  );
+
+  return sortBy(keys, key => codeKeys.indexOf(key)).map(key => {
+    return {
+      key,
+      label: labels[key],
+      value: location[key] ?? '',
+    };
+  });
+}
