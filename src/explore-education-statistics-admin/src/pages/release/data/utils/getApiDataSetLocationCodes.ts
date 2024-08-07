8ebf29982876f03577b5d6e462f53939fe2dd573
Nicholas Tsim <nicholastsim@gmail.com>
2024-08-07 17:33:39 +0100

    EES-4997 Refactor out common `getLocationCodeEntries` util

--- src/explore-education-statistics-admin/src/pages/release/data/utils/getApiDataSetLocationCodes.ts
+++ src/explore-education-statistics-admin/src/pages/release/data/utils/getApiDataSetLocationCodes.ts
@@ -1,47 +1,18 @@
 import { LocationCandidate } from '@admin/services/apiDataSetVersionService';
-import pickBy from 'lodash/pickBy';
-import sortBy from 'lodash/sortBy';
-
-const fields = [
-  'code',
-  'oldCode',
-  'urn',
-  'laEstab',
-  'ukprn',
-] as const satisfies readonly (keyof LocationCandidate)[];
-
-type Field = (typeof fields)[number];
+import getLocationCodeEntries from '@common/utils/getLocationCodeEntries';
 
 export default function getApiDataSetLocationCodes(
   candidate: LocationCandidate,
 ): string {
-  const entries = Object.entries(
-    pickBy(
-      candidate,
-      (_, key) => fields.includes(key as Field) && candidate[key as Field],
-    ),
-  ) as [Field, string][];
+  const entries = getLocationCodeEntries(candidate);
 
-  if (entries.length === 1 && entries[0][0] === 'code') {
-    return entries[0][1];
+  if (entries.length === 1) {
+    const entry = entries[0];
+
+    if (entry.key === 'code' || entry.key === 'id') {
+      return entries[0].value;
+    }
   }
 
-  return sortBy(entries, ([key]) => fields.indexOf(key))
-    .map(([key, value]) => {
-      switch (key) {
-        case 'code':
-          return `Code: ${value}`;
-        case 'oldCode':
-          return `Old code: ${value}`;
-        case 'urn':
-          return `URN: ${value}`;
-        case 'laEstab':
-          return `LAESTAB: ${value}`;
-        case 'ukprn':
-          return `UKPRN: ${value}`;
-        default:
-          return '';
-      }
-    })
-    .join(', ');
+  return entries.map(entry => `${entry.label}: ${entry.value}`).join(', ');
 }
