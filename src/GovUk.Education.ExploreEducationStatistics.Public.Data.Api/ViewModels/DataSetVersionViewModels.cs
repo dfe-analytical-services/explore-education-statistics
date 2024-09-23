321b384973aebc531a0f730daaaed32675e1395d
Nicholas Tsim <nicholastsim@gmail.com>
2024-09-23 01:54:08 +0100

    EES-5494 Fix example for `DataSetVersionViewModel.GeographicLevels`

--- src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api/ViewModels/DataSetVersionViewModels.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api/ViewModels/DataSetVersionViewModels.cs
@@ -82,7 +82,7 @@ public class DataSetVersionViewModel
     /// <summary>
     /// The geographic levels available in the data set.
     /// </summary>
-    /// <example>["NAT", "REG", "LA"]</example>
+    /// <example>["National", "Regional", "Local authority"]</example>
     [JsonConverter(typeof(ReadOnlyListJsonConverter<GeographicLevel, EnumToEnumLabelJsonConverter<GeographicLevel>>))]
     public required IReadOnlyList<GeographicLevel> GeographicLevels { get; init; }
 
