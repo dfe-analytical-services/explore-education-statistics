0127c23bcbbd6c181b7c4d99ff1e3affb60fdcc1
Nicholas Tsim <nicholastsim@gmail.com>
2024-09-30 23:39:55 +0100

    EES-5497 Update preview token usage guidance

--- src/explore-education-statistics-common/src/components/CodeBlock.tsx
+++ src/explore-education-statistics-common/src/components/CodeBlock.tsx
@@ -1,17 +1,19 @@
 import styles from '@common/components/CodeBlock.module.scss';
 import useToggle from '@common/hooks/useToggle';
 import React, { useEffect } from 'react';
+import bashLang from 'react-syntax-highlighter/dist/cjs/languages/hljs/bash';
 import pythonLang from 'react-syntax-highlighter/dist/cjs/languages/hljs/python';
 import rLang from 'react-syntax-highlighter/dist/cjs/languages/hljs/r';
 import SyntaxHighlighter from 'react-syntax-highlighter/dist/cjs/light';
 import Button from './Button';
 
+SyntaxHighlighter.registerLanguage('bash', bashLang);
 SyntaxHighlighter.registerLanguage('r', rLang);
 SyntaxHighlighter.registerLanguage('python', pythonLang);
 
 export interface CodeBlockProps {
   children: string;
-  language: 'python' | 'r';
+  language: 'bash' | 'python' | 'r';
 }
 
 export default function CodeBlock({ children, language }: CodeBlockProps) {
