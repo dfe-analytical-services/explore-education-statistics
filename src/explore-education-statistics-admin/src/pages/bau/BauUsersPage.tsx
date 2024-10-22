faca7e145dcd3563029ad4fbe3174fc087ef9886
jack-hive <148866614+jack-hive@users.noreply.github.com>
2024-10-22 14:17:13 +0100

    EES-5568 Removing the custom `DeleteUserModal.tsx` component, and using the `ModalConfirm.tsx` component directly inside the `BauUsersPage`

--- src/explore-education-statistics-admin/src/pages/bau/BauUsersPage.tsx
+++ src/explore-education-statistics-admin/src/pages/bau/BauUsersPage.tsx
@@ -9,7 +9,8 @@ import styles from './BauUsersPage.module.scss';
 const BauUsersPage = () => {
   const { value, isLoading } = useAsyncRetry(() => userService.getUsers());
 
-  // const handleDeleteUser = async (userEmail: string) => { // EES-5573
+  // const handleDeleteUser = async (userEmail: string) => {
+  //   // EES-5573
   //   await userService
   //     .deleteUser(userEmail)
   //     .then(() => {
@@ -56,16 +57,23 @@ const BauUsersPage = () => {
                       Manage
                     </Link>
                     {/* EES-5573 */}
-                    {/* <DeleteUserModal
+                    {/* <ModalConfirm
+                      title="Confirm you want to delete this user"
                       triggerButton={
-                        <ButtonText
-                          className={styles.deleteUserButton}
-                        >
+                        <ButtonText className={styles.deleteUserButton}>
                           Delete
                         </ButtonText>
                       }
                       onConfirm={async () => await handleDeleteUser(user.email)}
-                    /> */}
+                    >
+                      <p>
+                        By deleting this User you will remove all access and
+                        permissions they have on the service. This change cannot
+                        be reversed. Users who are removed and then need access
+                        at a later point will need to be re-invited to the
+                        service as a new user.
+                      </p>
+                    </ModalConfirm> */}
                   </td>
                 </tr>
               ))}
