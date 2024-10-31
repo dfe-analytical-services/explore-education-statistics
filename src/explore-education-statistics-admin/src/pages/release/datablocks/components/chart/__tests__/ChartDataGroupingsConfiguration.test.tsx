0c0f1cdba2476702b3eeec1b4ca92e4b6286b0c6
Stuart Bennett <bennettstuart92@gmail.com>
2024-10-31 17:28:29 +0000

    EES-5541 mostly fixed tests: ChartDataGroupingsConfiguration

--- src/explore-education-statistics-admin/src/pages/release/datablocks/components/chart/__tests__/ChartDataGroupingsConfiguration.test.tsx
+++ src/explore-education-statistics-admin/src/pages/release/datablocks/components/chart/__tests__/ChartDataGroupingsConfiguration.test.tsx
@@ -4,8 +4,8 @@ import {
   ChartBuilderForms,
   ChartBuilderFormsContextProvider,
 } from '@admin/pages/release/datablocks/components/chart/contexts/ChartBuilderFormsContext';
-import { ChartOptions } from '@admin/pages/release/datablocks/components/chart/reducers/chartBuilderReducer';
 import baseRender from '@common-test/render';
+import { defaultDataGrouping } from '@common/modules/charts/util/getMapDataSetCategoryConfigs';
 import { screen, within } from '@testing-library/react';
 import noop from 'lodash/noop';
 import React, { ReactElement } from 'react';
@@ -34,12 +34,6 @@ describe('ChartDataGroupingsConfiguration', () => {
     },
   };
 
-  const testDefaultChartOptions: ChartOptions = {
-    alt: '',
-    height: 600,
-    titleType: 'default',
-  };
-
   function render(
     element: ReactElement,
     initialForms: ChartBuilderForms = testFormState,
@@ -54,22 +48,9 @@ describe('ChartDataGroupingsConfiguration', () => {
   test('renders correctly with no data groupings', () => {
     render(
       <ChartDataGroupingsConfiguration
-        meta={testTable.subjectMeta}
-        data={testTable.results}
-        axisMajor={{
-          dataSets: [],
-          groupBy: 'locations',
-          referenceLines: [],
-          type: 'major',
-          visible: true,
-        }}
-        legend={{
-          position: 'top',
-          items: [],
-        }}
-        options={testDefaultChartOptions}
-        onSubmit={noop}
+        meta={testFullTable.subjectMeta}
         onChange={noop}
+        onSubmit={noop}
       />,
     );
 
@@ -80,25 +61,18 @@ describe('ChartDataGroupingsConfiguration', () => {
     render(
       <ChartDataGroupingsConfiguration
         meta={testTable.subjectMeta}
-        data={testTable.results}
-        axisMajor={{
-          dataSets: [
+        map={{
+          dataSetConfigs: [
             {
-              filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2014_AY',
+              dataGrouping: defaultDataGrouping,
+              dataSet: {
+                filters: ['ethnicity-major-chinese', 'state-funded-primary'],
+                indicator: 'authorised-absence-sessions',
+                timePeriod: '2014_AY',
+              },
             },
           ],
-          groupBy: 'locations',
-          referenceLines: [],
-          type: 'major',
-          visible: true,
         }}
-        legend={{
-          position: 'top',
-          items: [],
-        }}
-        options={testDefaultChartOptions}
         onSubmit={noop}
         onChange={noop}
       />,
@@ -121,30 +95,26 @@ describe('ChartDataGroupingsConfiguration', () => {
     render(
       <ChartDataGroupingsConfiguration
         meta={testTable.subjectMeta}
-        data={testTable.results}
-        axisMajor={{
-          dataSets: [
+        map={{
+          dataSetConfigs: [
             {
-              filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2014_AY',
+              dataGrouping: defaultDataGrouping,
+              dataSet: {
+                filters: ['ethnicity-major-chinese', 'state-funded-primary'],
+                indicator: 'authorised-absence-sessions',
+                timePeriod: '2014_AY',
+              },
             },
             {
-              filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2015_AY',
+              dataGrouping: defaultDataGrouping,
+              dataSet: {
+                filters: ['ethnicity-major-chinese', 'state-funded-primary'],
+                indicator: 'authorised-absence-sessions',
+                timePeriod: '2015_AY',
+              },
             },
           ],
-          groupBy: 'locations',
-          referenceLines: [],
-          type: 'major',
-          visible: true,
         }}
-        legend={{
-          position: 'top',
-          items: [],
-        }}
-        options={testDefaultChartOptions}
         onSubmit={noop}
         onChange={noop}
       />,
@@ -176,20 +146,6 @@ describe('ChartDataGroupingsConfiguration', () => {
     render(
       <ChartDataGroupingsConfiguration
         meta={testTable.subjectMeta}
-        data={testTable.results}
-        axisMajor={{
-          dataSets: [
-            {
-              filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2014_AY',
-            },
-          ],
-          groupBy: 'locations',
-          referenceLines: [],
-          type: 'major',
-          visible: true,
-        }}
         map={{
           dataSetConfigs: [
             {
@@ -206,11 +162,6 @@ describe('ChartDataGroupingsConfiguration', () => {
             },
           ],
         }}
-        legend={{
-          position: 'top',
-          items: [],
-        }}
-        options={testDefaultChartOptions}
         onSubmit={noop}
         onChange={noop}
       />,
@@ -233,30 +184,6 @@ describe('ChartDataGroupingsConfiguration', () => {
     render(
       <ChartDataGroupingsConfiguration
         meta={testTable.subjectMeta}
-        data={testTable.results}
-        axisMajor={{
-          dataSets: [
-            {
-              filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2014_AY',
-            },
-            {
-              filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2015_AY',
-            },
-            {
-              filters: ['ethnicity-major-chinese', 'state-funded-secondary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2015_AY',
-            },
-          ],
-          groupBy: 'locations',
-          referenceLines: [],
-          type: 'major',
-          visible: true,
-        }}
         map={{
           dataSetConfigs: [
             {
@@ -296,201 +223,6 @@ describe('ChartDataGroupingsConfiguration', () => {
             },
           ],
         }}
-        legend={{
-          position: 'top',
-          items: [],
-        }}
-        options={testDefaultChartOptions}
-        onSubmit={noop}
-        onChange={noop}
-      />,
-    );
-
-    const rows = screen.getAllByRole('row');
-    expect(rows).toHaveLength(4);
-
-    const row1Cells = within(rows[1]).getAllByRole('cell');
-    expect(row1Cells[0]).toHaveTextContent(
-      'Number of authorised absence sessions (Ethnicity Major Chinese, State-funded primary, All locations, 2014/15)',
-    );
-    expect(row1Cells[1]).toHaveTextContent('5 equal intervals');
-    expect(
-      within(row1Cells[2]).getByRole('button', { name: 'Edit' }),
-    ).toBeInTheDocument();
-
-    const row2Cells = within(rows[2]).getAllByRole('cell');
-    expect(row2Cells[0]).toHaveTextContent(
-      'Number of authorised absence sessions (Ethnicity Major Chinese, State-funded primary, All locations, 2015/16)',
-    );
-    expect(row2Cells[1]).toHaveTextContent('2 quantiles');
-    expect(
-      within(row2Cells[2]).getByRole('button', { name: 'Edit' }),
-    ).toBeInTheDocument();
-
-    const row3Cells = within(rows[3]).getAllByRole('cell');
-    expect(row3Cells[0]).toHaveTextContent(
-      'Number of authorised absence sessions (Ethnicity Major Chinese, State-funded secondary, All locations, 2015/16)',
-    );
-    expect(row3Cells[1]).toHaveTextContent('Custom');
-    expect(
-      within(row3Cells[2]).getByRole('button', { name: 'Edit' }),
-    ).toBeInTheDocument();
-  });
-
-  test('renders correctly when data sets are removed', () => {
-    render(
-      <ChartDataGroupingsConfiguration
-        meta={testTable.subjectMeta}
-        data={testTable.results}
-        axisMajor={{
-          dataSets: [
-            {
-              filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2015_AY',
-            },
-            {
-              filters: ['ethnicity-major-chinese', 'state-funded-secondary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2015_AY',
-            },
-          ],
-          groupBy: 'locations',
-          referenceLines: [],
-          type: 'major',
-          visible: true,
-        }}
-        map={{
-          dataSetConfigs: [
-            {
-              dataSet: {
-                filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-                indicator: 'authorised-absence-sessions',
-                timePeriod: '2014_AY',
-              },
-              dataGrouping: {
-                customGroups: [],
-                numberOfGroups: 6,
-                type: 'EqualIntervals',
-              },
-            },
-            {
-              dataSet: {
-                filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-                indicator: 'authorised-absence-sessions',
-                timePeriod: '2015_AY',
-              },
-              dataGrouping: {
-                customGroups: [],
-                numberOfGroups: 2,
-                type: 'Quantiles',
-              },
-            },
-            {
-              dataSet: {
-                filters: ['ethnicity-major-chinese', 'state-funded-secondary'],
-                indicator: 'authorised-absence-sessions',
-                timePeriod: '2015_AY',
-              },
-              dataGrouping: {
-                customGroups: [{ min: 0, max: 50 }],
-                type: 'Custom',
-              },
-            },
-          ],
-        }}
-        legend={{
-          position: 'top',
-          items: [],
-        }}
-        options={testDefaultChartOptions}
-        onSubmit={noop}
-        onChange={noop}
-      />,
-    );
-
-    const rows = screen.getAllByRole('row');
-    expect(rows).toHaveLength(3);
-
-    const row1Cells = within(rows[1]).getAllByRole('cell');
-    expect(row1Cells[0]).toHaveTextContent(
-      'Number of authorised absence sessions (Ethnicity Major Chinese, State-funded primary, All locations, 2015/16)',
-    );
-    expect(row1Cells[1]).toHaveTextContent('2 quantiles');
-    expect(
-      within(row1Cells[2]).getByRole('button', { name: 'Edit' }),
-    ).toBeInTheDocument();
-
-    const row2Cells = within(rows[2]).getAllByRole('cell');
-    expect(row2Cells[0]).toHaveTextContent(
-      'Number of authorised absence sessions (Ethnicity Major Chinese, State-funded secondary, All locations, 2015/16)',
-    );
-    expect(row2Cells[1]).toHaveTextContent('Custom');
-    expect(
-      within(row2Cells[2]).getByRole('button', { name: 'Edit' }),
-    ).toBeInTheDocument();
-  });
-
-  test('renders correctly when data sets are added', () => {
-    render(
-      <ChartDataGroupingsConfiguration
-        meta={testTable.subjectMeta}
-        data={testTable.results}
-        axisMajor={{
-          dataSets: [
-            {
-              filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2014_AY',
-            },
-            {
-              filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2015_AY',
-            },
-            {
-              filters: ['ethnicity-major-chinese', 'state-funded-secondary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2015_AY',
-            },
-          ],
-          groupBy: 'locations',
-          referenceLines: [],
-          type: 'major',
-          visible: true,
-        }}
-        map={{
-          dataSetConfigs: [
-            {
-              dataSet: {
-                filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-                indicator: 'authorised-absence-sessions',
-                timePeriod: '2015_AY',
-              },
-              dataGrouping: {
-                customGroups: [],
-                numberOfGroups: 2,
-                type: 'Quantiles',
-              },
-            },
-            {
-              dataSet: {
-                filters: ['ethnicity-major-chinese', 'state-funded-secondary'],
-                indicator: 'authorised-absence-sessions',
-                timePeriod: '2015_AY',
-              },
-              dataGrouping: {
-                customGroups: [{ min: 0, max: 50 }],
-                type: 'Custom',
-              },
-            },
-          ],
-        }}
-        legend={{
-          position: 'top',
-          items: [],
-        }}
-        options={testDefaultChartOptions}
         onSubmit={noop}
         onChange={noop}
       />,
@@ -531,25 +263,6 @@ describe('ChartDataGroupingsConfiguration', () => {
     const { user } = render(
       <ChartDataGroupingsConfiguration
         meta={testTable.subjectMeta}
-        data={testTable.results}
-        axisMajor={{
-          dataSets: [
-            {
-              filters: ['ethnicity-major-chinese', 'state-funded-primary'],
-              indicator: 'authorised-absence-sessions',
-              timePeriod: '2014_AY',
-            },
-          ],
-          groupBy: 'locations',
-          referenceLines: [],
-          type: 'major',
-          visible: true,
-        }}
-        legend={{
-          position: 'top',
-          items: [],
-        }}
-        options={testDefaultChartOptions}
         onSubmit={noop}
         onChange={noop}
       />,
