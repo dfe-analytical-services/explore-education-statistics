f91b13e0b762174e9bbf4402a66a342280da0185
Mark Youngman <mark@hiveit.co.uk>
2024-11-26 11:25:08 +0000

    EES-5666 Create new Files.DataSetFileMeta column

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20241126112007_EES5666_CreateNewFilesColumnToDataSetFileMeta.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    /// <inheritdoc />
+    public partial class EES5666_CreateNewFilesColumnToDataSetFileMeta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DataSetFileMeta",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataSetFileMeta",
+                table: "Files");
+        }
+    }
+}
