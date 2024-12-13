9d44812e503d4d3dfe235f6e8ac192811e589fce
Mark Youngman <mark@hiveit.co.uk>
2024-12-13 08:59:43 +0000

    EES-5736 Saving work

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/DataSetFileVersion.cs
@@ -0,0 +1,48 @@
+﻿#nullable enable
+using System;
+using GovUk.Education.ExploreEducationStatistics.Common.Model;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Model;
+
+public class DataSetFileVersion : ICreatedTimestamp<DateTime?>
+{
+    public Guid Id { get; set; }
+
+    public Guid DataSetFileId { get; set; }
+
+    public int Version { get; set; }
+
+    public Guid RootPath { get; set; }
+
+    public Guid SubjectId { get; set; }
+
+    public string DataFilename { get; set; } = string.Empty;
+
+    public string MetaFilename { get; set; } = string.Empty;
+
+    public long DataContentLength { get; set; }
+
+    public long MetaContentLength { get; set; }
+
+    // public string DataContentType // @MarkFix probably don't need this?
+    // public string MetaContentType // @MarkFix probably don't need this?
+
+    public DataSetFileMeta? DataSetFileMeta { get; set; }
+
+    public Guid? ReplacedById { get; set; }
+
+    public DataSetFileVersion? ReplacedBy { get; set; }
+
+    public DataSetFileVersion? Replacing { get; set; }
+
+    public Guid? SourceZipFileId { get; set; }
+
+    public File? SourceZipFile { get; set; }
+
+
+    public DateTime? Created { get; set; }
+
+    public User? CreatedBy { get; set; }
+
+    public Guid? CreatedById { get; set; }
+}
---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/ReleaseDataSetFileVersion.cs
@@ -0,0 +1,50 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using Semver;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Model;
+
+public class ReleaseDataSetFileVersion
+{
+    public Guid Id { get; set; }
+
+    public ReleaseVersion ReleaseVersion { get; set; } = null!;
+
+    public Guid ReleaseVersionId { get; set; }
+
+    public DataSetFileVersion DataSetFileVersion { get; set; } = null!;
+
+    public Guid DataSetFileVersionId { get; set; }
+
+    public string Title { get; set; }
+
+    public string? Summary { get; set; }
+
+    public int Order { get; set; }
+
+    public Guid? PublicApiDataSetId { get; set; }
+
+    public SemVersion? PublicApiDataSetVersion { get; set; }
+
+    public string? PublicApiDataSetVersionString => PublicApiDataSetVersion is not null
+        ? $"{PublicApiDataSetVersion.Major}.{PublicApiDataSetVersion.Minor}"
+        : null;
+
+    public List<FilterSequenceEntry>? FilterSequence { get; set; }
+
+    public List<IndicatorGroupSequenceEntry>? IndicatorSequence { get; set; }
+
+    public DateTime? Published { get; set; }
+}
+
+public abstract record SequenceEntry<TEntry, TChild>(TEntry Id, List<TChild> ChildSequence);
+
+public record FilterSequenceEntry(Guid Id, List<FilterGroupSequenceEntry> ChildSequence) :
+    SequenceEntry<Guid, FilterGroupSequenceEntry>(Id, ChildSequence);
+
+public record FilterGroupSequenceEntry(Guid Id, List<Guid> ChildSequence) :
+    SequenceEntry<Guid, Guid>(Id, ChildSequence);
+
+public record IndicatorGroupSequenceEntry(Guid Id, List<Guid> ChildSequence) :
+    SequenceEntry<Guid, Guid>(Id, ChildSequence);
