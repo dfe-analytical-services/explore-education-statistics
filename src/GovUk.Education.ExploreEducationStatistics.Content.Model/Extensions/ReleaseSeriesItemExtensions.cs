f3012b1a60743c5929bb03d5169a7e1a7baf7786
Ben Outram <ben@hiveit.co.uk>
2024-12-17 16:05:04 +0000

    EES-5656 Refactor logic to extract release id's from a  release series to be reusable.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/Extensions/ReleaseSeriesItemExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Model.Extensions;
+
+public static class ReleaseSeriesItemExtensions
+{
+    /// <summary>
+    /// Retrieves the release id's by the order they appear in a <see cref="List{T}"/> of type <see cref="ReleaseSeriesItem"/>,
+    /// ignoring any legacy links.
+    /// </summary>
+    /// <param name="releaseSeriesItems"></param>
+    /// <returns>A <see cref="IReadOnlyList{T}"/> of type <see cref="Guid"/> containing the release id's in the order
+    /// they appear in the release series.</returns>
+    public static IReadOnlyList<Guid> ReleaseIds(this List<ReleaseSeriesItem> releaseSeriesItems)
+    {
+        return releaseSeriesItems
+            .Where(rsi => !rsi.IsLegacyLink)
+            .Select(rs => rs.ReleaseId!.Value)
+            .ToList();
+    }
+}
--- src/GovUk.Education.ExploreEducationStatistics.Publisher/Services/PublishingCompletionService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher/Services/PublishingCompletionService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Predicates;
 using GovUk.Education.ExploreEducationStatistics.Content.Services.Interfaces.Cache;
 using GovUk.Education.ExploreEducationStatistics.Publisher.Model;
@@ -125,10 +126,7 @@ public class PublishingCompletionService(
             .SingleAsync(p => p.Id == publicationId);
 
         // Get the publications release id's by the order they appear in the release series
-        var releaseSeriesReleaseIds = publication.ReleaseSeries
-            .Where(rsi => !rsi.IsLegacyLink)
-            .Select(rs => rs.ReleaseId!.Value)
-            .ToList();
+        var releaseSeriesReleaseIds = publication.ReleaseSeries.ReleaseIds();
 
         // Work out the publication's new latest published release version.
         // This is the latest published version of the first release which has a published version
