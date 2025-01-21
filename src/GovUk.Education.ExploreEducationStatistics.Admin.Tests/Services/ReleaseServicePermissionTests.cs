45326e95365f07d0739d2978a66c572c64881872
jack-hive <148866614+jack-hive@users.noreply.github.com>
2025-01-21 14:00:44 +0000

    EES-5639 Moving the `CreateRelease` test out of `ReleaseVersionServicePermissionTests` into `ReleaseServicePermissionTests`

--- src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/ReleaseServicePermissionTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/ReleaseServicePermissionTests.cs
@@ -1,385 +1,64 @@
 #nullable enable
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using GovUk.Education.ExploreEducationStatistics.Admin.Requests;
 using GovUk.Education.ExploreEducationStatistics.Admin.Security;
 using GovUk.Education.ExploreEducationStatistics.Admin.Services;
 using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
-using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces.Public.Data;
-using GovUk.Education.ExploreEducationStatistics.Common.Model;
 using GovUk.Education.ExploreEducationStatistics.Common.Services;
-using GovUk.Education.ExploreEducationStatistics.Common.Services.Interfaces;
 using GovUk.Education.ExploreEducationStatistics.Common.Services.Interfaces.Security;
-using GovUk.Education.ExploreEducationStatistics.Common.Tests.Extensions;
-using GovUk.Education.ExploreEducationStatistics.Common.Tests.Fixtures;
-using GovUk.Education.ExploreEducationStatistics.Common.Tests.Utils;
-using GovUk.Education.ExploreEducationStatistics.Common.Utils;
 using GovUk.Education.ExploreEducationStatistics.Content.Model;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
-using GovUk.Education.ExploreEducationStatistics.Content.Model.Repository.Interfaces;
-using GovUk.Education.ExploreEducationStatistics.Content.Model.Tests.Fixtures;
-using GovUk.Education.ExploreEducationStatistics.Content.Security;
-using GovUk.Education.ExploreEducationStatistics.Content.Services.Interfaces.Cache;
-using GovUk.Education.ExploreEducationStatistics.Data.Model.Database;
-using GovUk.Education.ExploreEducationStatistics.Data.Model.Repository.Interfaces;
 using Moq;
 using static GovUk.Education.ExploreEducationStatistics.Admin.Security.SecurityPolicies;
 using static GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services.DbUtils;
-using static GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services.MapperUtils;
 using static GovUk.Education.ExploreEducationStatistics.Common.Tests.Utils.PermissionTestUtils;
-using IReleaseVersionRepository = GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces.IReleaseVersionRepository;
 
-namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services;
+
+public class ReleaseServicePermissionTests
 {
-    public class ReleaseServicePermissionTests
+    private static readonly Publication Publication = new()
     {
-        private readonly DataFixture _dataFixture = new();
+        Id = Guid.NewGuid()
+    };
 
-        private static readonly Publication Publication = new()
-        {
-            Id = Guid.NewGuid()
-        };
-
-        private readonly ReleaseVersion _releaseVersion = new()
-        {
-            Id = Guid.NewGuid(),
-            PublicationId = Publication.Id,
-            Published = DateTime.Now,
-            TimePeriodCoverage = TimeIdentifier.April
-        };
-
-        private readonly Guid _userId = Guid.NewGuid();
-
-        [Fact]
-        public async Task GetRelease()
-        {
-            await PolicyCheckBuilder<ContentSecurityPolicies>()
-                .SetupResourceCheckToFail(_releaseVersion, ContentSecurityPolicies.CanViewSpecificRelease)
-                .AssertForbidden(
-                    userService =>
-                    {
-                        var service = BuildReleaseService(userService.Object);
-                        return service.GetRelease(_releaseVersion.Id);
-                    }
-                );
-        }
-
-        [Fact]
-        public async Task CreateRelease()
-        {
-            await PolicyCheckBuilder<SecurityPolicies>()
-                .SetupResourceCheckToFail(Publication, CanCreateReleaseForSpecificPublication)
-                .AssertForbidden(
-                    userService =>
-                    {
-                        using var contextDbContext = InMemoryApplicationDbContext();
-                        contextDbContext.Publications.Add(Publication);
-                        contextDbContext.SaveChangesAsync();
-
-                        var service = BuildReleaseService(
-                            context: contextDbContext,
-                            userService: userService.Object);
-
-                        return service.CreateRelease(
-                            new ReleaseCreateRequest
-                            {
-                                PublicationId = Publication.Id,
-                            }
-                        );
-                    }
-                );
-        }
-
-        [Fact]
-        public async Task GetLatestPublishedRelease()
-        {
-            Publication publication = _dataFixture.DefaultPublication()
-                .WithReleases(_ => [_dataFixture.DefaultRelease(publishedVersions: 1)]);
-
-            await PolicyCheckBuilder<SecurityPolicies>()
-                .SetupResourceCheckToFailWithMatcher<Publication>(p => p.Id == publication.Id,
-                    CanViewSpecificPublication)
-                .AssertForbidden(
-                    userService =>
-                    {
-                        using var contextDbContext = InMemoryApplicationDbContext();
-                        contextDbContext.Publications.Add(publication);
-                        contextDbContext.SaveChangesAsync();
-
-                        var service = BuildReleaseService(
-                            context: contextDbContext,
-                            userService: userService.Object);
-
-                        return service.GetLatestPublishedRelease(publication.Id);
-                    }
-                );
-        }
-
-        [Fact]
-        public async Task GetDeleteReleaseVersionPlan()
-        {
-            await PolicyCheckBuilder<SecurityPolicies>()
-                .SetupResourceCheckToFail(_releaseVersion, CanDeleteSpecificRelease)
-                .AssertForbidden(
-                    userService =>
-                    {
-                        var service = BuildReleaseService(userService.Object);
-                        return service.GetDeleteReleaseVersionPlan(_releaseVersion.Id);
-                    }
-                );
-        }
-
-        [Fact]
-        public async Task DeleteRelease()
-        {
-            await PolicyCheckBuilder<SecurityPolicies>()
-                .SetupResourceCheckToFail(_releaseVersion, CanDeleteSpecificRelease)
-                .AssertForbidden(
-                    userService =>
-                    {
-                        using var contextDbContext = InMemoryApplicationDbContext();
-                        contextDbContext.ReleaseVersions.Add(_releaseVersion);
-                        contextDbContext.SaveChangesAsync();
-
-                        var service = BuildReleaseService(
-                            context: contextDbContext,
-                            userService: userService.Object);
-                        
-                        return service.DeleteReleaseVersion(_releaseVersion.Id);
-                    }
-                );
-        }
-
-        [Fact]
-        public async Task DeleteTestRelease()
-        {
-            await PolicyCheckBuilder<SecurityPolicies>()
-                .SetupResourceCheckToFail(_releaseVersion, CanDeleteTestRelease)
-                .AssertForbidden(
-                    userService =>
-                    {
-                        using var contextDbContext = InMemoryApplicationDbContext();
-                        contextDbContext.ReleaseVersions.Add(_releaseVersion);
-                        contextDbContext.SaveChangesAsync();
-
-                        var service = BuildReleaseService(
-                            context: contextDbContext,
-                            userService: userService.Object);
-                        
-                        return service.DeleteTestReleaseVersion(_releaseVersion.Id);
-                    }
-                );
-        }
-        
-        [Fact]
-        public async Task ListReleasesWithStatuses_CanViewAllReleases()
-        {
-            var releaseVersionRepository = new Mock<IReleaseVersionRepository>();
-
-            var list = new List<ReleaseVersion>
-            {
-                new()
+    [Fact]
+    public async Task CreateRelease()
+    {
+        await PolicyCheckBuilder<SecurityPolicies>()
+            .SetupResourceCheckToFail(Publication, CanCreateReleaseForSpecificPublication)
+            .AssertForbidden(
+                userService =>
                 {
-                    Id = Guid.NewGuid(),
-                    ReleaseName = "2000",
-                    TimePeriodCoverage = TimeIdentifier.AcademicYear,
+                    using var contextDbContext = InMemoryApplicationDbContext();
+                    contextDbContext.Publications.Add(Publication);
+                    contextDbContext.SaveChangesAsync();
+
+                    var service = BuildReleaseService(
+                        context: contextDbContext,
+                        userService: userService.Object);
+
+                    return service.CreateRelease(
+                        new ReleaseCreateRequest
+                        {
+                            PublicationId = Publication.Id,
+                        }
+                    );
                 }
-            };
-
-            releaseVersionRepository
-                .Setup(s => s.ListReleases(ReleaseApprovalStatus.Approved))
-                .ReturnsAsync(list);
-
-            await PolicyCheckBuilder<SecurityPolicies>()
-                .SetupCheck(RegisteredUser)
-                .SetupCheck(CanViewAllReleases)
-                .AssertSuccess(
-                    async userService =>
-                    {
-                        userService
-                            .Setup(s => s.MatchesPolicy(list[0], CanAssignPreReleaseUsersToSpecificRelease))
-                            .ReturnsAsync(true);
-
-                        userService
-                            .Setup(s => s.MatchesPolicy(list[0], ContentSecurityPolicies.CanViewSpecificRelease))
-                            .ReturnsAsync(true);
-
-                        userService
-                            .Setup(s => s.MatchesPolicy(list[0], CanUpdateSpecificReleaseVersion))
-                            .ReturnsAsync(true);
-
-                        userService
-                            .Setup(s => s.MatchesPolicy(list[0], CanDeleteSpecificRelease))
-                            .ReturnsAsync(true);
-
-                        userService
-                            .Setup(s => s.MatchesPolicy(list[0], CanMakeAmendmentOfSpecificRelease))
-                            .ReturnsAsync(true);
-
-                        var service = BuildReleaseService(
-                            userService.Object,
-                            releaseVersionRepository: releaseVersionRepository.Object
-                        );
-                        var result = await service.ListReleasesWithStatuses(ReleaseApprovalStatus.Approved);
-
-                        var viewModel = result.AssertRight();
-                        Assert.Single(viewModel);
-                        Assert.Equal(list[0].Id, viewModel[0].Id);
-
-                        return result;
-                    }
-                );
-
-            releaseVersionRepository.Verify(s => s.ListReleases(ReleaseApprovalStatus.Approved));
-            releaseVersionRepository.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ListReleasesWithStatuses_CanViewRelatedReleases()
-        {
-            var repository = new Mock<IReleaseVersionRepository>();
-
-            var list = new List<ReleaseVersion>
-            {
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    ReleaseName = "2000",
-                    TimePeriodCoverage = TimeIdentifier.AcademicYear,
-                }
-            };
-
-            repository
-                .Setup(s => s.ListReleasesForUser(_userId, ReleaseApprovalStatus.Approved))
-                .ReturnsAsync(list);
-
-            await PolicyCheckBuilder<SecurityPolicies>()
-                .SetupCheck(RegisteredUser)
-                .ExpectCheckToFail(CanViewAllReleases)
-                .AssertSuccess(
-                    async userService =>
-                    {
-                        userService
-                            .Setup(s => s.MatchesPolicy(list[0], CanAssignPreReleaseUsersToSpecificRelease))
-                            .ReturnsAsync(true);
-
-                        userService
-                            .Setup(s => s.MatchesPolicy(list[0], ContentSecurityPolicies.CanViewSpecificRelease))
-                            .ReturnsAsync(true);
-
-                        userService
-                            .Setup(s => s.MatchesPolicy(list[0], CanUpdateSpecificReleaseVersion))
-                            .ReturnsAsync(true);
-
-                        userService
-                            .Setup(s => s.MatchesPolicy(list[0], CanDeleteSpecificRelease))
-                            .ReturnsAsync(true);
-
-                        userService
-                            .Setup(s => s.MatchesPolicy(list[0], CanMakeAmendmentOfSpecificRelease))
-                            .ReturnsAsync(true);
-
-                        userService
-                            .Setup(s => s.GetUserId())
-                            .Returns(_userId);
-
-                        var service = BuildReleaseService(
-                            userService.Object,
-                            releaseVersionRepository: repository.Object
-                        );
-                        var result = await service.ListReleasesWithStatuses(ReleaseApprovalStatus.Approved);
-
-                        var viewModel = result.AssertRight();
-                        Assert.Single(viewModel);
-                        Assert.Equal(list[0].Id, viewModel[0].Id);
-
-                        return result;
-                    }
-                );
-
-            repository.Verify(s => s.ListReleasesForUser(_userId, ReleaseApprovalStatus.Approved));
-            repository.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ListReleasesWithStatuses_NoAccessToSystem()
-        {
-            await PolicyCheckBuilder<SecurityPolicies>()
-                .ExpectCheckToFail(RegisteredUser)
-                .AssertForbidden(
-                    async userService =>
-                    {
-                        var service = BuildReleaseService(userService: userService.Object);
-                        return await service.ListReleasesWithStatuses(ReleaseApprovalStatus.Approved);
-                    }
-                );
-        }
-
-        [Fact]
-        public async Task RemoveDataFiles()
-        {
-            await PolicyCheckBuilder<SecurityPolicies>()
-                .SetupResourceCheckToFail(_releaseVersion, CanUpdateSpecificReleaseVersion)
-                .AssertForbidden(
-                    async userService =>
-                    {
-                        var service = BuildReleaseService(userService.Object);
-                        return await service.RemoveDataFiles(_releaseVersion.Id, Guid.NewGuid());
-                    }
-                );
-        }
-
-        [Fact]
-        public async Task UpdateReleasePublished()
-        {
-            await PolicyCheckBuilder<SecurityPolicies>()
-                .ExpectCheckToFail(IsBauUser)
-                .AssertForbidden(
-                    userService =>
-                    {
-                        var service = BuildReleaseService(userService.Object);
-                        return service.UpdateReleasePublished(_releaseVersion.Id,
-                            new ReleasePublishedUpdateRequest());
-                    }
-                );
-        }
-
-        private ReleaseVersionService BuildReleaseService(
-            IUserService userService,
-            ContentDbContext? context = null,
-            IReleaseVersionRepository? releaseVersionRepository = null)
-        {
-            return new ReleaseVersionService(
-                context ?? Mock.Of<ContentDbContext>(),
-                Mock.Of<StatisticsDbContext>(),
-                AdminMapper(),
-                DefaultPersistenceHelperMock().Object,
-                userService,
-                releaseVersionRepository ?? Mock.Of<IReleaseVersionRepository>(),
-                Mock.Of<IReleaseCacheService>(),
-                Mock.Of<IReleaseFileRepository>(),
-                Mock.Of<IReleaseDataFileService>(),
-                Mock.Of<IReleaseFileService>(),
-                Mock.Of<IDataImportService>(),
-                Mock.Of<IFootnoteRepository>(),
-                Mock.Of<IDataBlockService>(),
-                Mock.Of<IReleasePublishingStatusRepository>(),
-                Mock.Of<IReleaseSubjectRepository>(),
-                Mock.Of<IDataSetVersionService>(),
-                Mock.Of<IProcessorClient>(),
-                new SequentialGuidGenerator(),
-                Mock.Of<IBlobCacheService>()
             );
-        }
+    }
 
-        private Mock<IPersistenceHelper<ContentDbContext>> DefaultPersistenceHelperMock()
-        {
-            var mock = MockUtils.MockPersistenceHelper<ContentDbContext, ReleaseVersion>();
-            MockUtils.SetupCall(mock, _releaseVersion.Id, _releaseVersion);
-            MockUtils.SetupCall(mock, Publication.Id, Publication);
-
-            return mock;
-        }
+    private ReleaseService BuildReleaseService(
+        IUserService userService,
+        ContentDbContext? context = null,
+        IReleaseVersionService? releaseVersionService = null)
+    {
+        return new ReleaseService(
+            context: context ?? Mock.Of<ContentDbContext>(),
+            userService: userService,
+            releaseVersionService: releaseVersionService ?? Mock.Of<IReleaseVersionService>(),
+            guidGenerator: new SequentialGuidGenerator()
+        );
     }
 }
