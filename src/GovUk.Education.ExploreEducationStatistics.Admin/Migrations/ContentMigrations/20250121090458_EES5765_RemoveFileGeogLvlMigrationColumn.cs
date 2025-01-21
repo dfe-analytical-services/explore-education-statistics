d751e0d9013cfde5b1fdd71ba7eae3e5422ae713
Mark Youngman <mark@hiveit.co.uk>
2025-01-21 09:09:03 +0000

    EES-5598 Cleanup after removing geog lvls from DataSetFileMeta

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20250121090458_EES5765_RemoveFileGeogLvlMigrationColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    /// <inheritdoc />
+    public partial class EES5765_RemoveFileGeogLvlMigrationColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataSetFileMetaGeogLvlMigrated",
+                table: "Files");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DataSetFileMetaGeogLvlMigrated",
+                table: "Files",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
