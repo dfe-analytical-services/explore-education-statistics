bbd0f0aae6794096cea667d99ab8ea4638ba8c5e
mmoalin <mmoalin@outlook.com>
2025-01-24 12:30:20 +0000

    Unit tests for the wildcard versioning filter query helper

--- src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Tests/UnitTests/WildCardDatasetVersionTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Tests/UnitTests/WildCardDatasetVersionTests.cs
@@ -3,99 +3,133 @@ using GovUk.Education.ExploreEducationStatistics.Common.Utils;
 using GovUk.Education.ExploreEducationStatistics.Public.Data.Model;
 using GovUk.Education.ExploreEducationStatistics.Public.Data.Model.Database;
 using GovUk.Education.ExploreEducationStatistics.Public.Data.Model.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Public.Data.Model.Utils;
 using GovUk.Education.ExploreEducationStatistics.Public.Data.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Moq.EntityFrameworkCore;
 using Semver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Tests.UnitTests;
 
 public class WildCardDatasetVersionTests
 {
-    [Fact]
-    public void TestDataSetVersions()
-    {
-        
-    //    Api.Services.DataSetService dataSetService = new Api.Services.DataSetService(
-    //        new PublicDataDbContext(),
-    //        new MockDataSetVersionPathResolver(),
-    //        userService
-            
-    //        );
-    //    DataSetVersionQueryableExtensions.FindByVersion();
-    //    var versionString = "1.1.*";
-    //    VersionUtils.TryParse(versionString, out var version);
-
-    //    //Assert.True();
-
-
-    //    // Arrange
-    //    var datasetId = Guid.NewGuid();
-    //    var release = new Release()
-    //    {
-    //        DataSetFileId = Guid.NewGuid(),
-    //        ReleaseFileId = Guid.NewGuid(),
-    //        Slug = "test",
-    //        Title = "Test"
-    //    };
-    //    //TODO: Mock dataset and then use this list as an attribute. Then test the service with this provided dataset with different versions.
-    //    var versions = new List<DataSetVersion>
-    //    {
-    //        new() { 
-    //            DataSetId = datasetId , Notes = "hi", Release = release, Status = DataSetVersionStatus.Published,
-    //            VersionMajor = 1, VersionMinor = 0 
-    //        },
-    //        new() { 
-    //            DataSetId = datasetId, Notes = "hi", Release = release, Status = DataSetVersionStatus.Published,   VersionMajor = 1, VersionMinor = 1 
-    //        },
-    //        new() { 
-    //            DataSetId = datasetId, Notes = "hi", Release = release, Status = DataSetVersionStatus.Published,   VersionMajor = 2, VersionMinor = 0 
-    //        },
-    //        new() { 
-    //            DataSetId = datasetId, Notes = "hi", Release = release, Status = DataSetVersionStatus.Published,   VersionMajor = 2, VersionMinor = 1 
-    //        },
-    //        new() { 
-    //            DataSetId = datasetId, Notes = "hi", Release = release, Status = DataSetVersionStatus.Published,   VersionMajor = 1, VersionMinor = 3
-    //        }
-    //    };
-
-    //    // Act
-    //    var sortedVersions = versions.OrderBy(v => v.VersionMajor).ThenBy(v => v.VersionMinor).ToList();
-
-    //    // Assert
-    //    Assert.Equal(1, sortedVersions[0].VersionMajor);
-    //    Assert.Equal(0, sortedVersions[0].VersionMinor);
-    //    Assert.Equal(1, sortedVersions[1].VersionMajor);
-    //    Assert.Equal(1, sortedVersions[1].VersionMinor);
-    //    Assert.Equal(2, sortedVersions[2].VersionMajor);
-    //    Assert.Equal(0, sortedVersions[2].VersionMinor);
-    //    Assert.Equal(2, sortedVersions[3].VersionMajor);
-    //    Assert.Equal(1, sortedVersions[3].VersionMinor);
-    //    Assert.Equal(3, sortedVersions[4].VersionMajor);
-    //    Assert.Equal(0, sortedVersions[4].VersionMinor);
-    }
     [Theory]
-    [InlineData("1.*.*)", 1, 0, 0)]
-    [InlineData("v2.0", 2, 0)]
-    [InlineData("1", 1)]
-    [InlineData("v2", 2)]
-    [InlineData("v2.0.0", 2, 0, 0)]
-    [InlineData("v1.*.*)", 1, 0, 0)]
-    [InlineData("1.*)", 1, 0, 0)]
-    [InlineData("v1.*)", 1, 0, 0)]
-    [InlineData("1.3.*)", 1, 0, 0)]
-    [InlineData("v1.3.*)", 1, 0, 0)]
-    public void ValidVersion_SuccessfullyParsed(string versionString,
+    [InlineData("v*", 5)]
+    [InlineData("*", 5)]
+    [InlineData("2.*.*", 2, 1, 4)]
+    [InlineData("2.*", 2, 1, 4)]
+    [InlineData("2.1.*", 2, 1, 4)]
+    [InlineData("1.*.*", 1, 3, 0)]
+    [InlineData("1.2.*", 1, 2, 0)]
+    [InlineData("1.1.*", 1, 1, 1)]
+    [InlineData("1.*", 1, 3, 0)]
+    [InlineData("v2.*.*", 2, 1, 4)]
+    [InlineData("v2.*", 2, 1, 4)]
+    [InlineData("v2.1.*", 2, 1, 4)]
+    [InlineData("v1.*.*", 1, 3, 0)]
+    [InlineData("v1.2.*", 1, 2, 0)]
+    [InlineData("v1.1.*", 1, 1, 1)]
+    [InlineData("v1.*", 1, 3, 0)]
+    public async Task TestDataSetVersions(string versionString,
             int expectedMajor,
             int expectedMinor = default,
             int expectedPatch = default)
     {
-        //Assert.True(VersionUtils.TryParse(versionString, out var version));
-        //Assert.Equal(expectedMajor, version.Major);
-        //Assert.Equal(expectedMinor, version.Minor);
-        //Assert.Equal(expectedPatch, version.Patch);
+        // Arrange
+        var dataSetId = Guid.NewGuid();
+        var queryable = SetupDataSetVersions(dataSetId);
+
+        // Act
+        var result = await DataSetVersionWildCardQueryHelper.FetchDatasetUsingWildCardVersion(queryable, dataSetId, versionString, CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsRight);
+        Assert.Equal(expectedMajor, result.Right.VersionMajor);
+        Assert.Equal(expectedMinor, result.Right.VersionMinor);
+        Assert.Equal(expectedPatch, result.Right.VersionPatch);
+    }
+
+    private static IQueryable<DataSetVersion> SetupDataSetVersions(Guid dataSetId)
+    {
+        var publicDataDbContextMock = new Mock<PublicDataDbContext>();
+        
+        var release = new Release()
+        {
+            DataSetFileId = Guid.NewGuid(),
+            ReleaseFileId = Guid.NewGuid(),
+            Slug = "test",
+            Title = "Test"
+        };
+        var versions = new List<DataSetVersion>
+            {
+                new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 2, VersionMinor = 0
+                },
+                new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 2, VersionMinor = 1, VersionPatch = 0
+                },
+                new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 2, VersionMinor = 1, VersionPatch = 1
+                },
+                new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 2, VersionMinor = 1, VersionPatch = 2
+                },
+                new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 2, VersionMinor = 1, VersionPatch = 3
+                },
+                new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 2, VersionMinor = 1, VersionPatch = 4
+                },
+
+                new() {
+                    DataSetId = dataSetId , Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 1, VersionMinor = 0
+                },
+                new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 1, VersionMinor = 1
+                },
+                new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 1, VersionMinor = 1, VersionPatch = 1
+                },
+                new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 1, VersionMinor = 2
+                },
+                new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 1, VersionMinor = 3
+                },
+                 new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 3, VersionMinor = 0
+                },
+                  new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 4, VersionMinor = 0
+                },
+                  new() {
+                    DataSetId = dataSetId, Notes = release.Slug, Release = release, Status = DataSetVersionStatus.Published,
+                    VersionMajor = 5, VersionMinor = 0
+                }
+            };
+        
+        publicDataDbContextMock.SetupGet(dbContext => dbContext.DataSetVersions).ReturnsDbSet(versions);
+
+        return publicDataDbContextMock.Object.DataSetVersions.AsNoTracking();
     }
 }
--- src/GovUk.Education.ExploreEducationStatistics.Public.Data.Model/Utils/DataSetVersionWildCardQueryHelper.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Model/Utils/DataSetVersionWildCardQueryHelper.cs
@@ -13,42 +13,19 @@ public static class DataSetVersionWildCardQueryHelper
     public static async Task<Either<ActionResult, DataSetVersion>> FetchDatasetUsingWildCardVersion(IQueryable<DataSetVersion> queryable, Guid dataSetId, string version, CancellationToken cancellationToken)
     {
         var parts = version.Trim('v').Split('.');
-        Task<Either<ActionResult, DataSetVersion>> result;
-        if (parts.Length == 2)
-        {
-            int? major = parts[0] == "*" ? null : int.Parse(parts[0]);
-            int? minor = parts[1] == "*" ? null : int.Parse(parts[1]);
+        int? major = parts[0] == "*" ? null : int.Parse(parts[0]);
+        int? minor = (parts.Length > 1 && parts[1] != "*") ? int.Parse(parts[1]) : null;
+        int? patch = (parts.Length > 2 && parts[2] != "*") ? int.Parse(parts[2]) : null;
 
-            result = queryable
-                .Where(dsv => dsv.DataSetId == dataSetId)
-                .Where(v =>
+        return await queryable
+            .Where(dsv => dsv.DataSetId == dataSetId)
+            .Where(v =>
                 (!major.HasValue || v.VersionMajor == major) &&
-                (!minor.HasValue || v.VersionMinor == minor))
-                .OrderByDescending(v => v.VersionMajor)
-                .ThenByDescending(v => v.VersionMinor)
-                .FirstOrNotFoundAsync(cancellationToken);
-        }
-        else if (parts.Length == 3)//REFACTOR TO REMOVE THIS BRANCHING based on testing
-        {
-            int? major = parts[0] == "*" ? null : int.Parse(parts[0]);
-            int? minor = parts[1] == "*" ? null : int.Parse(parts[1]);
-            int? patch = parts[2] == "*" ? null : int.Parse(parts[2]);
-
-            result = queryable
-                .Where(dsv => dsv.DataSetId == dataSetId)
-                .Where(v =>
-                    (!major.HasValue || v.VersionMajor == major) &&
-                    (!minor.HasValue || v.VersionMinor == minor) &&
-                    (!patch.HasValue || v.VersionPatch == patch))//TODO: find out if this is null if this errors
-                .OrderByDescending(v => v.VersionMajor)
-                .ThenByDescending(v => v.VersionMinor)
-                .ThenByDescending(v => v.VersionPatch)//TODO: find out if this is null if this errors
-                .FirstOrNotFoundAsync(cancellationToken);
-        }
-        else
-        {
-            return new NotFoundResult();
-        }
-        return await result;
+                (!minor.HasValue || v.VersionMinor == minor) &&
+                (!patch.HasValue || v.VersionPatch == patch))
+            .OrderByDescending(v => v.VersionMajor)
+            .ThenByDescending(v => v.VersionMinor)
+            .ThenByDescending(v => v.VersionPatch)
+            .FirstOrNotFoundAsync(cancellationToken);
     }
 }
