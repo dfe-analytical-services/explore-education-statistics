cc3079a78ebb760f603457090632fed0444b7953
Nicholas Tsim <nicholastsim@gmail.com>
2025-02-05 15:16:04 +0000

    EES-5846 Add tests for public API doc URLs in `DataSetFileApiQuickStart`

--- src/explore-education-statistics-frontend/src/modules/data-catalogue/components/__tests__/DataSetFileApiQuickStart.test.tsx
+++ src/explore-education-statistics-frontend/src/modules/data-catalogue/components/__tests__/DataSetFileApiQuickStart.test.tsx
@@ -45,18 +45,52 @@ describe('DataSetFileApiQuickStart', () => {
       screen.getByLabelText('GET data set summary URL'),
     ).toHaveDisplayValue('http://localhost:5050/v1/data-sets/api-data-set-id');
     expect(
+      screen.getByRole('link', { name: 'Guidance: Get data set summary' }),
+    ).toHaveAttribute(
+      'href',
+      'https://dev.statistics.api.education.gov.uk/docs/reference-v1/endpoints/GetDataSet/',
+    );
+
+    expect(
       screen.getByLabelText('GET data set metadata URL'),
     ).toHaveDisplayValue(
       'http://localhost:5050/v1/data-sets/api-data-set-id/meta?dataSetVersion=1.0',
     );
+    expect(
+      screen.getByRole('link', { name: 'Guidance: Get data set metadata' }),
+    ).toHaveAttribute(
+      'href',
+      'https://dev.statistics.api.education.gov.uk/docs/reference-v1/endpoints/GetDataSetMeta/',
+    );
+
     expect(screen.getByLabelText('GET data set query URL')).toHaveDisplayValue(
       'http://localhost:5050/v1/data-sets/api-data-set-id/query?dataSetVersion=1.0',
     );
+    expect(
+      screen.getByRole('link', { name: 'Guidance: Query data set (GET)' }),
+    ).toHaveAttribute(
+      'href',
+      'https://dev.statistics.api.education.gov.uk/docs/reference-v1/endpoints/QueryDataSetGet/',
+    );
+
     expect(screen.getByLabelText('POST data set query URL')).toHaveDisplayValue(
       'http://localhost:5050/v1/data-sets/api-data-set-id/query?dataSetVersion=1.0',
     );
+    expect(
+      screen.getByRole('link', { name: 'Guidance: Query data set (POST)' }),
+    ).toHaveAttribute(
+      'href',
+      'https://dev.statistics.api.education.gov.uk/docs/reference-v1/endpoints/QueryDataSetPost/',
+    );
+
     expect(screen.getByLabelText('GET data set CSV URL')).toHaveDisplayValue(
       'http://localhost:5050/v1/data-sets/api-data-set-id/csv?dataSetVersion=1.0',
+    );
+    expect(
+      screen.getByRole('link', { name: 'Guidance: Download data set as CSV' }),
+    ).toHaveAttribute(
+      'href',
+      'https://dev.statistics.api.education.gov.uk/docs/reference-v1/endpoints/DownloadDataSetCsv/',
     );
   });
 });
