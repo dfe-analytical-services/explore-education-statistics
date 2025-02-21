1d5dd7b9c37d57a50fa6fb0cc1f8cf718195eccd
Lee Oades <lee.oades@gmail.com>
2025-02-21 15:54:46 +0000

    Renamed ToTypeBoost to ToSearchDocumentTypeBoost for extra clarity. Also, metadata will contain the raw ReleaseType value instead of a UI friendly version. We'll let the UI deal with that

--- src/GovUk.Education.ExploreEducationStatistics.Content.ViewModels/Extensions/ReleaseTypeExtensions.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.ViewModels/Extensions/ReleaseTypeExtensions.cs
@@ -4,18 +4,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Content.ViewModels.Extensio
 
 public static class ReleaseTypeExtensions
 {
-    public static string ToDisplayString(this ReleaseType releaseType) => releaseType switch
-    {
-        ReleaseType.AdHocStatistics => "Ad Hoc Statistics",
-        ReleaseType.AccreditedOfficialStatistics => "Accredited Official Statistics",
-        ReleaseType.ExperimentalStatistics => "Experimental Statistics",
-        ReleaseType.ManagementInformation => "Management Information",
-        ReleaseType.OfficialStatistics => "Official Statistics",
-        ReleaseType.OfficialStatisticsInDevelopment => "Official Statistics In Development",
-        _ => throw new ArgumentOutOfRangeException(nameof(releaseType), releaseType, null)
-    };
-
-    public static int ToTypeBoost(this ReleaseType releaseType) => releaseType switch
+    public static int ToSearchDocumentTypeBoost(this ReleaseType releaseType) => releaseType switch
     {
         ReleaseType.AccreditedOfficialStatistics => 6,
         ReleaseType.OfficialStatistics => 5,
