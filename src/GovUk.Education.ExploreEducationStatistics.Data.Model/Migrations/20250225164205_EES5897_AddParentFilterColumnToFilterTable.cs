a5f751aa8cecfc03a8b33afe50c31245c0940b1c
Mark Youngman <mark@hiveit.co.uk>
2025-02-25 16:49:36 +0000

    EES-5897 Create new parent_filter meta file column for filter hierarchies

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20250225164205_EES5897_AddParentFilterColumnToFilterTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class EES5897_AddParentFilterColumnToFilterTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ParentFilter",
+                table: "Filter",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentFilter",
+                table: "Filter");
+        }
+    }
+}
