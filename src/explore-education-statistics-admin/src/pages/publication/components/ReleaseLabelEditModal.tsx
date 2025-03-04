bccc510b98fa1a458adee274c38151970682c811
jack-hive <148866614+jack-hive@users.noreply.github.com>
2025-03-04 17:07:20 +0000

    EES-5640 Refactoring the release label edit modal to use a reusable `FormModal` component

--- src/explore-education-statistics-admin/src/pages/publication/components/ReleaseLabelEditModal.tsx
+++ src/explore-education-statistics-admin/src/pages/publication/components/ReleaseLabelEditModal.tsx
@@ -1,13 +1,8 @@
-import Button from '@common/components/Button';
-import ButtonGroup from '@common/components/ButtonGroup';
-import Form from '@common/components/form/Form';
+import FormModal from '@admin/components/FormModal';
 import FormFieldTextInput from '@common/components/form/FormFieldTextInput';
-import FormProvider from '@common/components/form/FormProvider';
-import Modal from '@common/components/Modal';
-import useMountedRef from '@common/hooks/useMountedRef';
-import useToggle from '@common/hooks/useToggle';
 import Yup from '@common/validation/yup';
 import React, { ReactNode } from 'react';
+import { ObjectSchema } from 'yup';
 
 export type ReleaseLabelFormValues = {
   label?: string;
@@ -19,73 +14,29 @@ interface Props {
   onSubmit: (releaseDetailsFormValues: ReleaseLabelFormValues) => Promise<void>;
 }
 
+const validationSchema: ObjectSchema<ReleaseLabelFormValues> = Yup.object({
+  label: Yup.string().max(
+    20,
+    /* eslint-disable no-template-curly-in-string */
+    'Release label must be no longer than ${max} characters',
+  ),
+});
+
 export default function ReleaseLabelEditModal({
   triggerButton,
   initialValues,
   onSubmit,
 }: Props) {
-  const isMounted = useMountedRef();
-
-  const [open, toggleOpen] = useToggle(false);
-  const [isSaving, toggleSaving] = useToggle(false);
-
-  const handleSubmit = async (values: ReleaseLabelFormValues) => {
-    if (isSaving || !isMounted.current) {
-      return;
-    }
-
-    toggleSaving.on();
-
-    await onSubmit(values);
-
-    if (isMounted.current) {
-      toggleSaving.off();
-      toggleOpen.off();
-    }
-  };
-
   return (
-    <Modal
-      open={open}
-      closeOnOutsideClick={!isSaving}
-      closeOnEsc={!isSaving}
+    <FormModal
+      formId="editReleaseLabelForm"
       title="Edit release label"
       triggerButton={triggerButton}
-      onExit={toggleOpen.off}
-      onToggleOpen={toggleOpen}
+      initialValues={initialValues ?? { label: undefined }}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
     >
-      <FormProvider
-        initialValues={initialValues ?? { label: undefined }}
-        validationSchema={Yup.object({
-          label: Yup.string().max(
-            20,
-            /* eslint-disable no-template-curly-in-string */
-            'Release label must be no longer than ${max} characters',
-          ),
-        })}
-      >
-        <Form id="relatedPageForm" onSubmit={handleSubmit}>
-          <FormFieldTextInput<ReleaseLabelFormValues>
-            label="Label"
-            name="label"
-          />
-          <ButtonGroup>
-            <Button
-              type="submit"
-              className="govuk-button govuk-!-margin-right-1"
-            >
-              Save
-            </Button>
-            <Button
-              className="govuk-button govuk-button--secondary"
-              variant="secondary"
-              onClick={toggleOpen.off}
-            >
-              Cancel
-            </Button>
-          </ButtonGroup>
-        </Form>
-      </FormProvider>
-    </Modal>
+      <FormFieldTextInput<ReleaseLabelFormValues> label="Label" name="label" />
+    </FormModal>
   );
 }
