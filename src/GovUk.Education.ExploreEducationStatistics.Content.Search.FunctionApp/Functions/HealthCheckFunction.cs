aab31d7f2275ef0d3defbd190ec8cbc4997da513
Ben Outram <ben@hiveit.co.uk>
2025-03-06 17:05:26 +0000

    EES-5899 Add HealthCheck function

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Functions/HealthCheckFunction.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Functions;
+
+public class HealthCheckFunction
+{
+    [Function(nameof(HealthCheck))]
+    [Produces("application/json")]
+    public IActionResult HealthCheck(
+#pragma warning disable IDE0060
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get")]
+        HttpRequest request)
+#pragma warning restore IDE0060
+    {
+        var healthCheckResponse = new HealthCheckResponse();
+
+        if (healthCheckResponse.Healthy)
+        {
+            return new OkObjectResult(healthCheckResponse);
+        }
+
+        return new ObjectResult(healthCheckResponse) { StatusCode = StatusCodes.Status500InternalServerError };
+    }
+}
+
+public record HealthCheckResponse
+{
+    public bool Healthy => true;
+}
