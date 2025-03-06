099dd5807c877207e996aa667c141de508a48669
Lee Oades <lee.oades@gmail.com>
2025-03-06 17:45:17 +0000

    Testing Publishing Completion Service
    Happy Path done.
    Fix namespaces
    Methodology tests
    Test that latest release version is saved against the publication
    Content Service tests
    Notification, Redirects cache, and final publishing status tests

---
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher.Tests/Builders/DataSetPublishingServiceBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Publisher.Services.Interfaces;
+using Moq;
+
+namespace GovUk.Education.ExploreEducationStatistics.Publisher.Tests.Builders;
+
+public class DataSetPublishingServiceBuilder
+{
+    private readonly Mock<IDataSetPublishingService> _mock = new(MockBehavior.Strict);
+    public IDataSetPublishingService Build() => _mock.Object;
+    
+    public DataSetPublishingServiceBuilder()
+    {
+        _mock
+            .Setup(m => m.PublishDataSets(It.IsAny<Guid[]>()))
+            .Returns(Task.CompletedTask);
+    }
+}
