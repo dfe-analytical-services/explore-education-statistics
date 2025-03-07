7c4c4a0135af95944f7ec4787070b146d96052c9
Lee Oades <lee.oades@gmail.com>
2025-03-07 11:36:06 +0000

    Add Event raiser service to Publisher

---
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher/Services/Interfaces/IEventRaiserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GovUk.Education.ExploreEducationStatistics.Publisher.Services.Interfaces;
+
+public interface IEventRaiserService
+{
+    Task RaiseReleaseVersionPublishedEvents(
+        IEnumerable<PublishingCompletionService.PublishedReleaseVersionInfo> publicationSlugs);
+}
