f4fa7457d99ffa4f9ed76b3a776bbb7f4b3cc5bc
jack-hive <148866614+jack-hive@users.noreply.github.com>
2025-03-12 12:55:56 +0000

    EES-5640 Adding tests for the new `isUndefinedOrWhitespace` utility function

--- src/explore-education-statistics-common/src/utils/string/isUndefinedOrWhitespace.ts
+++ src/explore-education-statistics-common/src/utils/string/isUndefinedOrWhitespace.ts
@@ -1,6 +1,6 @@
 /**
  * Check if a {@param str} is null or whitespace.
  */
-export default function isNullOrWhitespace(str?: string) {
+export default function isUndefinedOrWhitespace(str?: string) {
   return !str || !str.trim();
 }
