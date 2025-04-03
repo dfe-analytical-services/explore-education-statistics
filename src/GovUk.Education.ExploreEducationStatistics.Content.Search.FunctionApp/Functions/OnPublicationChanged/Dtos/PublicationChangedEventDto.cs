3c4f67663b46b03f5288041b21ea22c336761a6e
Lee Oades <lee.oades@gmail.com>
2025-04-03 15:49:36 +0100

    Add Publication changed event handler function

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Functions/OnPublicationChanged/Dtos/PublicationChangedEventDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Functions.OnPublicationChanged.Dtos;
+
+public class PublicationChangedEventDto
+{
+    public string? Title { get; init; }
+    public string? Summary { get; init; }
+    public string? Slug { get; init; }
+}
