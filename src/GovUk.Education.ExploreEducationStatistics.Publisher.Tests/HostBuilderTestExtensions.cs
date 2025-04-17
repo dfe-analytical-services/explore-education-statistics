ddf990ccfeb2af66d57fdf4cfad5d8c02a555c12
Duncan Watson <duncan@hiveit.co.uk>
2025-04-17 09:56:14 +0100

    EES-XXXX - udpated test connection string variable name to reflect that it is a standard development storage connection string

--- src/GovUk.Education.ExploreEducationStatistics.Publisher.Tests/HostBuilderTestExtensions.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher.Tests/HostBuilderTestExtensions.cs
@@ -6,7 +6,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Publisher.Tests;
 
 public static class HostBuilderTestExtensions
 {
-    private const string StorageConnectionString = 
+    private const string DevelopmentStorageConnectionString = 
         "DefaultEndpointsProtocol=http;AccountName=account;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://data-storage:10000/devstoreaccount1;";
 
     public static IHostBuilder ConfigureTestAppConfiguration(this IHostBuilder hostBuilder)
@@ -16,10 +16,10 @@ public static class HostBuilderTestExtensions
             var configuration = new Dictionary<string, string>
             {
                 { "PublicDataDbExists", "false" },
-                { "App:PrivateStorageConnectionString", StorageConnectionString },
-                { "App:NotifierStorageConnectionString", StorageConnectionString },
-                { "App:PublicStorageConnectionString", StorageConnectionString },
-                { "App:PublisherStorageConnectionString", StorageConnectionString },
+                { "App:PrivateStorageConnectionString", DevelopmentStorageConnectionString },
+                { "App:NotifierStorageConnectionString", DevelopmentStorageConnectionString },
+                { "App:PublicStorageConnectionString", DevelopmentStorageConnectionString },
+                { "App:PublisherStorageConnectionString", DevelopmentStorageConnectionString },
             };
 
             s.AddInMemoryCollection(configuration!).Build();
