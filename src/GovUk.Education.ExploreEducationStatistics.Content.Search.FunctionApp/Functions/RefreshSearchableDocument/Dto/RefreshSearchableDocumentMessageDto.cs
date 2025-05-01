d4795d4a30ea40ccfe741d4601bfc405a52ff179
Ben Outram <ben@hiveit.co.uk>
2025-05-01 17:26:19 +0100

    EES-6104 Add event handling to the Search Function App for restoring archived publications

--- src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Functions/RefreshSearchableDocument/Dto/RefreshSearchableDocumentMessageDto.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Functions/RefreshSearchableDocument/Dto/RefreshSearchableDocumentMessageDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Functions.RefreshSearchableDocument.Dto;
 
-public class RefreshSearchableDocumentMessageDto
+public record RefreshSearchableDocumentMessageDto
 {
     public string? PublicationSlug { get; init; }
 }
