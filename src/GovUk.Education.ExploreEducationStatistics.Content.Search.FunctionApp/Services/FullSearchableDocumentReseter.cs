e9dcee97d7679bcd14549be60c653976add96a97
Lee Oades <lee.oades@gmail.com>
2025-05-12 16:27:56 +0100

    Add FullSearchableDocumentReseter - which coordinates the full reset

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Services/FullSearchableDocumentReseter.cs
@@ -0,0 +1,34 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Clients.ContentApi;
+using Microsoft.Extensions.Logging;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Services;
+
+public interface IFullSearchableDocumentReseter
+{
+    Task<RunResponse> Run(CancellationToken cancellationToken = default);
+}
+
+public class FullSearchableDocumentReseter(
+    IContentApiClient contentApiClient,
+    ISearchableDocumentRemover searchableDocumentRemover,
+    ILogger<FullSearchableDocumentReseter> logger) : IFullSearchableDocumentReseter
+{
+    public async Task<RunResponse> Run(CancellationToken cancellationToken = default)
+    {
+        logger.LogInformation("Executing Full Searchable Document Reset");
+        
+        // Delete all searchable documents
+        await searchableDocumentRemover.RemoveAllSearchableDocuments(cancellationToken);
+        
+        // Get list of all live publications
+        var allPublications = await contentApiClient.GetAllLivePublicationSlugs(cancellationToken);
+        
+        // Return list of slugs to send to the Searchable Document creator
+        return new RunResponse { AllPublicationSlugs = allPublications };
+    }
+}
+
+public class RunResponse
+{
+    public string[] AllPublicationSlugs { get; init; }
+}
