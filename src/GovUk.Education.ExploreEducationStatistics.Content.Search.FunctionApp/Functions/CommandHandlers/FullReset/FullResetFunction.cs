011f1f5c814b301d97a5ec327f0f31a09f4a377b
Lee Oades <lee.oades@gmail.com>
2025-05-12 16:28:33 +0100

    Add new Azure function - command handler - to perform a full reset of the searchable docs

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Functions/CommandHandlers/FullReset/FullResetFunction.cs
@@ -0,0 +1,34 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Functions.RefreshSearchableDocument.Dto;
+using GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Functions.CommandHandlers.FullReset;
+
+public class FullResetFunction(IFullSearchableDocumentReseter fullSearchableDocumentReseter)
+{
+    [Function(nameof(FullSearchableDocumentsReset))]
+    public async Task<FullSearchableDocumentsResetOutput> FullSearchableDocumentsReset(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = nameof(FullSearchableDocumentsReset))]
+        HttpRequest _,
+        FunctionContext context)
+    {
+        var response = await fullSearchableDocumentReseter.Run(context.CancellationToken);
+        
+        return new FullSearchableDocumentsResetOutput
+        {
+            RefreshSearchableDocuments = response.AllPublicationSlugs
+                .Select(publicationSlug => new RefreshSearchableDocumentMessageDto
+                                                    {
+                                                        PublicationSlug = publicationSlug
+                                                    })
+                .ToArray()
+        };
+    }
+}
+
+public class FullSearchableDocumentsResetOutput
+{
+    [QueueOutput("%RefreshSearchableDocumentQueueName%")]
+    public RefreshSearchableDocumentMessageDto[] RefreshSearchableDocuments { get; init; }
+}
