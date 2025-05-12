66baab8cd8bde2d9cffb93bd1a98464962ca578e
Guy Smith <guy@hiveit.co.uk>
2025-05-12 14:31:37 +0100

    Wire up sort functionality

---
+++ src/explore-education-statistics-frontend/src/modules/find-statistics/utils/createAzurePublicationListRequest.ts
@@ -0,0 +1,94 @@
+import { odata } from '@azure/search-documents';
+import { releaseTypes, ReleaseType } from '@common/services/types/releaseType';
+import getFirst from '@common/utils/getFirst';
+import parseNumber from '@common/utils/number/parseNumber';
+import isOneOf from '@common/utils/type-guards/isOneOf';
+import { FindStatisticsPageQuery } from '@frontend/modules/find-statistics/FindStatisticsPage';
+import {
+  PublicationSortOption,
+  publicationSortOptions,
+} from '@frontend/modules/find-statistics/utils/publicationSortOptions';
+import {
+  AzurePublicationListRequest,
+  AzurePublicationOrderByParam,
+} from '@frontend/services/azurePublicationService';
+import omitBy from 'lodash/omitBy';
+
+export default function createAzurePublicationListRequest(
+  query: FindStatisticsPageQuery,
+): AzurePublicationListRequest {
+  const {
+    releaseType,
+    search: searchParam,
+    sortBy,
+    themeId,
+  } = getParamsFromQuery(query);
+
+  const { sort } = getSortParams(sortBy);
+
+  let filter: string | undefined;
+  if (releaseType && themeId) {
+    filter = odata`releaseType eq ${releaseType} and theme eq ${themeId}`;
+  } else if (releaseType) {
+    filter = odata`releaseType eq ${releaseType}`;
+  } else if (themeId) {
+    // TODO amend theme to themeId once EES-6123 merged
+    filter = odata`theme eq ${themeId}`;
+  }
+
+  const minSearchCharacters = 3;
+  const search =
+    searchParam && searchParam.length >= minSearchCharacters ? searchParam : '';
+
+  return omitBy(
+    {
+      filter,
+      page: parseNumber(query.page) ?? 1,
+      releaseType,
+      search,
+      orderBy: sort,
+      themeId,
+    },
+    value => typeof value === 'undefined',
+  );
+}
+
+function getSortParams(sortBy: PublicationSortOption): {
+  sort: AzurePublicationOrderByParam;
+} {
+  if (sortBy === 'relevance') {
+    return {
+      sort: undefined,
+    };
+  }
+  if (sortBy === 'title') {
+    return {
+      sort: 'title asc',
+    };
+  }
+  if (sortBy === 'oldest') {
+    return {
+      sort: 'published asc',
+    };
+  }
+  return {
+    sort: 'published desc',
+  };
+}
+
+export function getParamsFromQuery(query: FindStatisticsPageQuery) {
+  return {
+    page: getFirst(query.page),
+    releaseType:
+      query.releaseType &&
+      isOneOf(query.releaseType, Object.keys(releaseTypes) as ReleaseType[])
+        ? query.releaseType
+        : undefined,
+    search: getFirst(query.search),
+    sortBy:
+      query.sortBy && isOneOf(query.sortBy, publicationSortOptions)
+        ? query.sortBy
+        : 'newest',
+    themeId: getFirst(query.themeId),
+  };
+}
