fc56616be02a1f4bf10bf8d466e743e7b2f44b24
Lee Oades <lee.oades@gmail.com>
2025-05-13 11:15:56 +0100

    Move the services into their own folders. Adjust namespaces.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Tests/Services/ResetSearchableDocuments/FullSearchableDocumentResetterTests.cs
@@ -0,0 +1,6 @@
+﻿namespace GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Tests.Services.ResetSearchableDocuments;
+
+public class FullSearchableDocumentResetterTests
+{
+    
+}
