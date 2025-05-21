f72af36867273c1f7a68d575217b694d4a57645d
mmoalin <mmoalin@outlook.com>
2025-05-21 15:15:51 +0100

    UI changes to Data file details tab to support cancelling a replacement that has api data set attached to it and to show if there is a major version upgrade. Backend changes to support deleting draft data set attached to replacement (WIP)
    Extract and integrate MappingTypesRepository for clean separation
    
    Refactored mapping logic to a dedicated MappingTypesRepository, implementing IMappingTypesRepository for retrieving mapping types and detecting major version changes. Updated relevant services to use the repository, improving modularity and maintainability. Added unit tests to verify the integration and ensure correctness.
    
    Use repository pattern to enable more testability.
    
    Create repository for Mapping types logic & determination of if major version changes are due to indicators, geographic levels or time periods being deleted.
    
    Utilize shared logic for determining if major version changes are due to established business logic.
    
    Moved mapping types queries from DataSetVersionMappingService to a new IMappingTypesRepository implementation. This improves separation of concerns and reduces duplication. Added tests to ensure repository methods function as expected.
    
    Remove unused DataSetVersion retrieval method.
    
    Fix failing UI tests, remove code that is not ready for PR

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/IDataSetVersionRepository.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/IDataSetVersionRepository.cs
@@ -10,8 +10,6 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
 public interface IDataSetVersionRepository
 {
     Task<List<DataSetVersion>> GetDataSetVersions(Guid releaseVersionId);
-    
-    Task<DataSetVersion> GetDataSetVersion(Guid dataSetVersionId);
 
     Task<DataSetVersion> GetDataSetVersion(Guid dataSetId, SemVersion version, CancellationToken cancellationToken = default);
 }
