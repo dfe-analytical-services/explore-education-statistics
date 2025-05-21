1741ed3a58f3b252282ec1e04632dc7036d2a913
Lee Oades <lee.oades@gmail.com>
2025-05-21 15:53:02 +0100

    Extract logging into a separate extension method

--- src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Extensions/HostBuilderExtension.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Extensions/HostBuilderExtension.cs
@@ -34,27 +34,11 @@ public static class HostBuilderExtension
         .ConfigureServices(
             (context, services) =>
                 services
-                    .AddApplicationInsightsTelemetryWorkerService()
-                    .ConfigureFunctionsApplicationInsights()
+                    .ConfigureLogging(context.Configuration)
                     // Config
                     .Configure<AppOptions>(context.Configuration.GetSection(AppOptions.Section))
                     .Configure<ContentApiOptions>(context.Configuration.GetSection(ContentApiOptions.Section))
                     .Configure<AzureSearchOptions>(context.Configuration.GetSection(AzureSearchOptions.Section))
-                    .Configure<LoggerFilterOptions>(options =>
-                    {
-                        // The Application Insights SDK adds a default logging filter that instructs ILogger to capture
-                        // only Warning and more severe logs. Application Insights requires an explicit override.
-                        // Log levels can also be configured using appsettings.json.
-                        // For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
-                        var toRemove = options.Rules.FirstOrDefault(rule =>
-                            rule.ProviderName ==
-                            "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
-
-                        if (toRemove is not null)
-                        {
-                            options.Rules.Remove(toRemove);
-                        }
-                    })
                     // Services
                     .AddTransient<ISearchableDocumentCreator, SearchableDocumentCreator>()
                     .AddTransient<ISearchableDocumentRemover, SearchableDocumentRemover>()
--- src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Extensions/ServiceCollectionExtensions.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Extensions/ServiceCollectionExtensions.cs
@@ -1,9 +1,12 @@
-using GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Clients.AzureBlobStorage;
+﻿using GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Clients.AzureBlobStorage;
 using GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Clients.AzureSearch;
 using GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Clients.ContentApi;
 using GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Functions.HealthChecks.Strategies;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Extensions;
 
@@ -26,4 +29,28 @@ public static class ServiceCollectionExtensions
             .AddTransient<Func<IContentApiClient>>(sp => sp.GetRequiredService<IContentApiClient>)
             .AddTransient<Func<IAzureBlobStorageClient>>(sp => sp.GetRequiredService<IAzureBlobStorageClient>)
             .AddTransient<Func<ISearchIndexerClient>>(sp => sp.GetRequiredService<ISearchIndexerClient>);
+
+    public static IServiceCollection ConfigureLogging(
+        this IServiceCollection serviceCollection,
+        IConfiguration configuration) =>
+        serviceCollection
+            .AddApplicationInsightsTelemetryWorkerService()
+            .ConfigureFunctionsApplicationInsights()
+            .Configure<LoggerFilterOptions>(
+                options =>
+                {
+                    // The Application Insights SDK adds a default logging filter that instructs ILogger to capture
+                    // only Warning and more severe logs. Application Insights requires an explicit override.
+                    // Log levels can also be configured using appsettings.json.
+                    // For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
+                    var toRemove = options.Rules.FirstOrDefault(
+                        rule =>
+                            rule.ProviderName ==
+                            "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+                    if (toRemove is not null)
+                    {
+                        options.Rules.Remove(toRemove);
+                    }
+                });
 }
