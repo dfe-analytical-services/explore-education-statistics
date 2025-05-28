9560a3ab65305ab3cf2753151026255f508fc12f
Lee Oades <lee.oades@gmail.com>
2025-05-28 14:52:08 +0100

    Replace Serilog setup with different approach to try to enable structured logging in App Insights.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Functions/Startup.cs
@@ -0,0 +1,29 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Functions;
+using Microsoft.ApplicationInsights;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+// Copied from Serilog documentation. See bottom of:
+// https://github.com/serilog-contrib/serilog-sinks-applicationinsights?tab=readme-ov-file
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Functions;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddSingleton<ILoggerProvider>((sp) => 
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.ApplicationInsights(
+                    sp.GetRequiredService<TelemetryClient>(), 
+                    TelemetryConverter.Traces)
+                .CreateLogger();
+            return new SerilogLoggerProvider(Log.Logger, true);
+        });
+    }
+}
