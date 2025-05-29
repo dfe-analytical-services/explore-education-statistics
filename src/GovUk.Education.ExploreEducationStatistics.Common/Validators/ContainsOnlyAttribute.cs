a8cbf382ec46a4d4c6c82b8f19853780648a4ebd
Lee Oades <lee.oades@gmail.com>
2025-05-29 16:21:49 +0100

    Cleaning up build errors

--- src/GovUk.Education.ExploreEducationStatistics.Common/Validators/ContainsOnlyAttribute.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Validators/ContainsOnlyAttribute.cs
@@ -7,94 +7,83 @@ using System.Linq;
 using System.Reflection;
 using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 
-namespace GovUk.Education.ExploreEducationStatistics.Common.Validators
+namespace GovUk.Education.ExploreEducationStatistics.Common.Validators;
+
+[AttributeUsage(
+    AttributeTargets.Property | AttributeTargets.Field| AttributeTargets.Parameter,
+    AllowMultiple = true)]
+public class ContainsOnlyAttribute(params object[] allowedValues) : ValidationAttribute
 {
-    [AttributeUsage(
-        AttributeTargets.Property | AttributeTargets.Field| AttributeTargets.Parameter,
-        AllowMultiple = true)]
-    public class ContainsOnlyAttribute : ValidationAttribute
+    public IEnumerable<object> AllowedValues { get; } = allowedValues;
+
+    public string? AllowedValuesProvider { get; set; }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        public IEnumerable<object> AllowedValues { get; }
+        var allowedValues = GetAllowedValues(validationContext);
 
-        public string? AllowedValuesProvider { get; set; }
-
-        public ContainsOnlyAttribute(params object[] allowedValues)
+        if (value is not IEnumerable values)
         {
-            AllowedValues = allowedValues;
+            throw new ArgumentException(
+                $"Validated value must implement {nameof(IEnumerable)}",
+                validationContext.MemberName
+            );
         }
 
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        if (!allowedValues.Any())
         {
-            var allowedValues = GetAllowedValues(validationContext);
-
-            if (value is not IEnumerable values)
-            {
-                throw new ArgumentException(
-                    $"Validated value must implement {nameof(IEnumerable)}",
-                    validationContext?.MemberName
-                );
-            }
-
-            if (!allowedValues.Any())
-            {
-                return ValidationResult.Success;
-            }
-
-            if (!values.Cast<object>().All(allowedValues.Contains))
-            {
-                return new ValidationResult(
-                    ErrorMessage ?? $"Field must contain only values from: {allowedValues.JoinToString(", ")}");
-            }
-
             return ValidationResult.Success;
         }
 
-        private HashSet<object> GetAllowedValues(ValidationContext validationContext)
+        if (!values.Cast<object>().All(allowedValues.Contains))
         {
-            if (AllowedValuesProvider is null)
-            {
-                return AllowedValues.ToHashSet();
-            }
-
-            var instance = validationContext.ObjectInstance;
-            var type = instance.GetType();
-            var member = type.GetMember(
-                    AllowedValuesProvider,
-                    MemberTypes.Field | MemberTypes.Property | MemberTypes.Method,
-                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static
-                )
-                .FirstOrDefault();
-
-            if (member is null)
-            {
-                throw new ArgumentException(
-                    $"{nameof(AllowedValuesProvider)} must reference a field or method in {type.FullName}",
-                    AllowedValuesProvider
-                );
-            }
-
-            var providedValues = member.MemberType switch
-            {
-                MemberTypes.Field => ((FieldInfo)member).GetValue(instance),
-                MemberTypes.Property => ((PropertyInfo)member).GetValue(instance),
-                MemberTypes.Method => ((MethodInfo)member).Invoke(instance, new object?[] { }),
-                _ => throw new ArgumentOutOfRangeException(
-                    $"{nameof(AllowedValuesProvider)} is not a valid member on {type.FullName}",
-                    AllowedValuesProvider
-                )
-            };
-
-            if (providedValues is not IEnumerable<object> providedAllowedValues)
-            {
-                throw new ArgumentException(
-                    $"{nameof(AllowedValuesProvider)} on {type.FullName} must implement {nameof(IEnumerable)}",
-                    AllowedValuesProvider
-                );
-            }
-
-            return AllowedValues
-                .Concat(providedAllowedValues)
-                .ToHashSet();
+            return new ValidationResult(
+                ErrorMessage ?? $"Field must contain only values from: {allowedValues.JoinToString(", ")}");
         }
+
+        return ValidationResult.Success;
+    }
+
+    private HashSet<object> GetAllowedValues(ValidationContext validationContext)
+    {
+        if (AllowedValuesProvider is null)
+        {
+            return AllowedValues.ToHashSet();
+        }
+
+        var instance = validationContext.ObjectInstance;
+        var type = instance.GetType();
+        var member = type.GetMember(
+                AllowedValuesProvider,
+                MemberTypes.Field | MemberTypes.Property | MemberTypes.Method,
+                BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static
+            )
+            .FirstOrDefault() 
+                     ?? throw new ArgumentException(
+                            $"{nameof(AllowedValuesProvider)} must reference a field or method in {type.FullName}",
+                            AllowedValuesProvider
+                     );
+        var providedValues = member.MemberType switch
+        {
+            MemberTypes.Field => ((FieldInfo)member).GetValue(instance),
+            MemberTypes.Property => ((PropertyInfo)member).GetValue(instance),
+            MemberTypes.Method => ((MethodInfo)member).Invoke(instance, []),
+            _ => throw new ArgumentOutOfRangeException(
+                $"{nameof(AllowedValuesProvider)} is not a valid member on {type.FullName}",
+                AllowedValuesProvider
+            )
+        };
+
+        if (providedValues is not IEnumerable<object> providedAllowedValues)
+        {
+            throw new ArgumentException(
+                $"{nameof(AllowedValuesProvider)} on {type.FullName} must implement {nameof(IEnumerable)}",
+                AllowedValuesProvider
+            );
+        }
+
+        return AllowedValues
+            .Concat(providedAllowedValues)
+            .ToHashSet();
     }
 }
