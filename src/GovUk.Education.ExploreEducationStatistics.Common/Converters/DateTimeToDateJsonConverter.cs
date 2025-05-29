3bfd9bbb57b011ad2346ac9d6eef1fd0aedb2266
Lee Oades <lee.oades@gmail.com>
2025-05-29 16:31:50 +0100

    Tiny improvements

--- src/GovUk.Education.ExploreEducationStatistics.Common/Converters/DateTimeToDateJsonConverter.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Converters/DateTimeToDateJsonConverter.cs
@@ -1,15 +1,15 @@
-﻿using Newtonsoft.Json.Converters;
+﻿using System;
+using Newtonsoft.Json.Converters;
 
-namespace GovUk.Education.ExploreEducationStatistics.Common.Converters
+namespace GovUk.Education.ExploreEducationStatistics.Common.Converters;
+
+/**
+ * Converts a <see cref="DateTime"/> to and from the format yyyy-MM-dd (e.g. <c>"2008-04-12"</c>).
+ */
+public class DateTimeToDateJsonConverter : IsoDateTimeConverter
 {
-    /**
-     * Converts a <see cref="DateTime"/> to and from the format yyyy-MM-dd (e.g. <c>"2008-04-12"</c>).
-     */
-    public class DateTimeToDateJsonConverter : IsoDateTimeConverter
+    public DateTimeToDateJsonConverter()
     {
-        public DateTimeToDateJsonConverter()
-        {
-            DateTimeFormat = "yyyy-MM-dd";
-        }
+        DateTimeFormat = "yyyy-MM-dd";
     }
 }
