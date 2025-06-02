319acb7f802e737041b9f0910549d5c0a5827a7d
Lee Oades <lee.oades@gmail.com>
2025-06-02 12:36:02 +0100

    Admin startup assumes connection strings exist and does not check them. Ditto OpenId authority url. Other minor fixes.
    Late evaluate connection strings so that if they aren't required (e.g. in tests) then we don't throw. Original behaviour evaluated a null early and closed around the value in the factory method anyway so the change in behaviour is that we fail early if the config is missing.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+#nullable enable
+using System;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Extensions;
+
+public static class StringExtensions
+{
+    public static string ThrowIfBlank(this string? value, string description) =>
+        string.IsNullOrWhiteSpace(value)
+            ? throw new ArgumentNullException($"Value is missing:{description}")
+            : value;
+}
