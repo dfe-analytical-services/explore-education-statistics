d6f80215e1153e9d75fb8c61673b39127d14e5ff
Duncan Watson <duncan@hiveit.co.uk>
2025-06-04 12:22:32 +0100

    EES-6085 - refactored IRequestFileProcessor implementations to have their common workflow injected by DI as per the recent changes to using a common workflow in the Public API writer strategies.

--- src/GovUk.Education.ExploreEducationStatistics.Analytics.Consumer/ProcessorHostBuilder.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Analytics.Consumer/ProcessorHostBuilder.cs
@@ -1,6 +1,7 @@
 using GovUk.Education.ExploreEducationStatistics.Analytics.Consumer.Options;
 using GovUk.Education.ExploreEducationStatistics.Analytics.Consumer.Services;
 using GovUk.Education.ExploreEducationStatistics.Analytics.Consumer.Services.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Analytics.Consumer.Services.Workflow;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,7 +44,8 @@ public static class ProcessorHostBuilder
                     .AddTransient<IRequestFileProcessor, PublicApiDataSetCallsProcessor>()
                     .AddTransient<IRequestFileProcessor, PublicApiDataSetVersionCallsProcessor>()
                     .AddTransient<IRequestFileProcessor, PublicApiQueriesProcessor>()
-                    .AddTransient<IRequestFileProcessor, PublicZipDownloadsProcessor>();
+                    .AddTransient<IRequestFileProcessor, PublicZipDownloadsProcessor>()
+                    .AddTransient<IProcessRequestFilesWorkflow, ProcessRequestFilesWorkflow>();
             });
     }
 }
