a86ff31055a6f0518d60618607acd47bb6930a59
Lee Oades <lee.oades@gmail.com>
2025-06-10 14:32:18 +0100

    It is not helpful for Error message to be null. Code paths don't expect it to be null either.

--- src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Exceptions/UnableToGetReleasesForPublicationException.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Exceptions/UnableToGetReleasesForPublicationException.cs
@@ -3,7 +3,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.
 /// <summary>
 /// Exception thrown when the call to the Content API to retrieve the releases for a publication fails.
 /// </summary>
-public class UnableToGetReleasesForPublicationException(string publicationSlug, string? errorMessage)
+public class UnableToGetReleasesForPublicationException(string publicationSlug, string errorMessage)
     : Exception($"""
                  Unable to get releases for publication "{publicationSlug}". Error: "{errorMessage}"
                  """);
@@ -11,7 +11,7 @@ public class UnableToGetReleasesForPublicationException(string publicationSlug, 
 /// <summary>
 /// Exception thrown when the call to the Content API to retrieve the release summary for a publication fails.
 /// </summary>
-public class UnableToGetReleaseSummaryForPublicationException(string publicationSlug, string releaseSlug, string? errorMessage)
+public class UnableToGetReleaseSummaryForPublicationException(string publicationSlug, string releaseSlug, string errorMessage)
     : Exception($"""
                  Unable to get release summary for release "{releaseSlug}" for publication "{publicationSlug}". Error: "{errorMessage}"
                  """);
