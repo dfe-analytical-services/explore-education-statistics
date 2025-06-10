04ad758222fa7c093833b1bf54c354500c5015dd
Tom Jones <tomjones@hiveit.co.uk>
2025-06-10 10:16:14 +0100

    EES-5999: Add tabs to upload modal, show screener results.

--- src/explore-education-statistics-admin/src/pages/release/data/components/DataFilesTableUploadsRow.tsx
+++ src/explore-education-statistics-admin/src/pages/release/data/components/DataFilesTableUploadsRow.tsx
@@ -9,9 +9,14 @@ import ButtonText from '@common/components/ButtonText';
 import Modal from '@common/components/Modal';
 import React from 'react';
 import { generatePath } from 'react-router';
-import styles from './DataFilesTable.module.scss';
-import { terminalImportStatuses } from './ImporterStatus';
+import Tabs from '@common/components/Tabs';
+import TabsSection from '@common/components/TabsSection';
+import WarningMessage from '@common/components/WarningMessage';
 import DataSetUploadSummaryList from './DataSetUploadSummaryList';
+import dataSetUploadTabIds from '../utils/dataSetUploadTabIds';
+import ScreenerResultSummaryList from './ScreenerResultSummaryList';
+import { terminalImportStatuses } from './ImporterStatus';
+import styles from './DataFilesTable.module.scss';
 
 interface Props {
   canUpdateRelease?: boolean;
@@ -26,6 +31,18 @@ export default function DataFilesTableUploadRow({
   publicationId,
   releaseVersionId,
 }: Props) {
+  const hasFailures = dataSetUpload.screenerResult.testResults.some(
+    result => result.testResult === 'FAIL',
+  );
+  const hasWarnings = dataSetUpload.screenerResult.testResults.some(
+    result => result.testResult === 'WARNING',
+  );
+  let tabTitle = '';
+
+  if (hasFailures && hasWarnings) tabTitle = 'Failures & warnings';
+  if (hasFailures && !hasWarnings) tabTitle = 'Failures';
+  if (!hasFailures && hasWarnings) tabTitle = 'Warnings';
+
   return (
     <tr key={dataSetUpload.dataSetTitle}>
       <td data-testid="Title" className={styles.title}>
@@ -42,7 +59,54 @@ export default function DataFilesTableUploadRow({
             title="Data file details"
             triggerButton={<ButtonText>View details</ButtonText>}
           >
-            <DataSetUploadSummaryList dataSetUpload={dataSetUpload} />
+            <Tabs id="data-and-files-tabs">
+              <TabsSection
+                id={dataSetUploadTabIds.screenerFailuresAndWarnings}
+                title={tabTitle}
+              >
+                {dataSetUpload.screenerResult.result === 'Failed' ? (
+                  <>
+                    <h3>Screener test failures</h3>
+                    <WarningMessage>
+                      You will need to delete this file, fix the failed tests
+                      and upload again
+                    </WarningMessage>
+                  </>
+                ) : (
+                  <>
+                    <h3>Screener test warnings</h3>
+                    <WarningMessage>
+                      You will need to review each warning before continuing the
+                      file upload
+                    </WarningMessage>
+                  </>
+                )}
+                <ScreenerResultSummaryList
+                  screenerResult={dataSetUpload.screenerResult}
+                  showAll={false}
+                />
+              </TabsSection>
+              <TabsSection
+                id={dataSetUploadTabIds.screenerResults}
+                title="All tests"
+              >
+                <h3>
+                  Full breakdown of{' '}
+                  {dataSetUpload.screenerResult.testResults.length} tests
+                  checked against this file
+                </h3>
+                <ScreenerResultSummaryList
+                  screenerResult={dataSetUpload.screenerResult}
+                  showAll
+                />
+              </TabsSection>
+              <TabsSection
+                id={dataSetUploadTabIds.fileDetails}
+                title="File details"
+              >
+                <DataSetUploadSummaryList dataSetUpload={dataSetUpload} />
+              </TabsSection>
+            </Tabs>
           </Modal>
           {canUpdateRelease &&
             terminalImportStatuses.includes(dataSetUpload.status) && (
