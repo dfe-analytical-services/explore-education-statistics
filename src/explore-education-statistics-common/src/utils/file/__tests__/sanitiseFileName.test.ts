c4d883f5861c9dfca9ebbbf9bd3c79ab6f6c754c
Stuart Bennett <stuart@hiveit.co.uk>
2025-06-13 13:45:13 +0100

    EES-6059 no longer remove fullstops from file names in sanitiseFileName

--- src/explore-education-statistics-common/src/utils/file/__tests__/sanitiseFileName.test.ts
+++ src/explore-education-statistics-common/src/utils/file/__tests__/sanitiseFileName.test.ts
@@ -3,11 +3,14 @@ import sanitiseFileName from '../sanitiseFileName';
 describe('sanitiseFileName', () => {
   test('removes disallowed characters from the file name', () => {
     // eslint-disable-next-line no-useless-escape
-    expect(sanitiseFileName(`file<>:"/\|?.*name`)).toBe('filename');
+    expect(sanitiseFileName(`file<>:"/\|?.*name`)).toBe('file.name');
+    expect(sanitiseFileName(`data-further-education-and-skills.meta.csv`)).toBe(
+      'data-further-education-and-skills.meta.csv',
+    );
   });
 
   test('trims the file name', () => {
     // eslint-disable-next-line no-useless-escape
-    expect(sanitiseFileName(` file<>:"/\|?*.name `)).toBe('filename');
+    expect(sanitiseFileName(` file<>:"/\|?*.name `)).toBe('file.name');
   });
 });
