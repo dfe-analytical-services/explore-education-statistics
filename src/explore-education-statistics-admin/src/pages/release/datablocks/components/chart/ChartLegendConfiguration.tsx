1072eac7526a5c1af90190d71da844fafb6a509b
Guy Smith <guy@hiveit.co.uk>
2025-06-16 16:56:45 +0100

    EES-6141 increase label y offset range

--- src/explore-education-statistics-admin/src/pages/release/datablocks/components/chart/ChartLegendConfiguration.tsx
+++ src/explore-education-statistics-admin/src/pages/release/datablocks/components/chart/ChartLegendConfiguration.tsx
@@ -140,12 +140,13 @@ const ChartLegendConfiguration = ({
             params.path as string,
           )}`,
       ),
+      labelColour: Yup.boolean().optional(),
       symbol: Yup.string<ChartSymbol>().optional(),
       lineStyle: Yup.string<LineStyle>().optional(),
       inlinePosition: Yup.string<LegendInlinePosition>().optional(),
       inlinePositionOffset: Yup.number()
-        .min(-30, 'Offset must be between -30 and 30')
-        .max(30, 'Offset must be between -30 and 30')
+        .min(-100, 'Offset must be between -100 and 100')
+        .max(100, 'Offset must be between -100 and 100')
         .optional(),
     });
 
