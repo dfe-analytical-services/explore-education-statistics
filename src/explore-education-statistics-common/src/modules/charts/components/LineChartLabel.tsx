a5750f1d6441292f144325c6789e6630cf079e24
Guy Smith <guy@hiveit.co.uk>
2025-06-17 17:16:39 +0100

    WIP trying to get legend robot tests matching

--- src/explore-education-statistics-common/src/modules/charts/components/LineChartLabel.tsx
+++ src/explore-education-statistics-common/src/modules/charts/components/LineChartLabel.tsx
@@ -90,6 +90,7 @@ export default function LineChartLabel({
         textAnchor={isPositionRight ? 'start' : 'end'}
         x={x}
         y={y}
+        data-testid="inline-legend-label"
       >
         {isPositionRight ? (
           // SVG <text> does not line wrap automatically :(
