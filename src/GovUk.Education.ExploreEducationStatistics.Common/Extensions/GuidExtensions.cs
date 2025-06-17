6c8d38d6b7613b17c86d6f7afdad32df540973e1
Lee Oades <lee.oades@gmail.com>
2025-06-17 15:17:57 +0100

    Fixed little bug

--- src/GovUk.Education.ExploreEducationStatistics.Common/Extensions/GuidExtensions.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Extensions/GuidExtensions.cs
@@ -5,6 +5,6 @@ namespace GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 
 public static class GuidExtensions
 {
-    public static bool IsBlank([NotNullWhen(true)]this Guid? guid) =>
+    public static bool IsBlank([NotNullWhen(false)]this Guid? guid) =>
         !guid.HasValue || guid.Value == Guid.Empty;
 }
