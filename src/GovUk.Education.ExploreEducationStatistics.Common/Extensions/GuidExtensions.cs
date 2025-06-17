47af65ea4c4b8163e34494d91c342d8fcd74df63
Lee Oades <lee.oades@gmail.com>
2025-06-17 13:57:05 +0100

    Move the equivalent Guid.IsBlank to Common.Extensions

---
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Extensions/GuidExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+
+public static class GuidExtensions
+{
+    public static bool IsBlank([NotNullWhen(true)]this Guid? guid) =>
+        !guid.HasValue || guid.Value == Guid.Empty;
+}
