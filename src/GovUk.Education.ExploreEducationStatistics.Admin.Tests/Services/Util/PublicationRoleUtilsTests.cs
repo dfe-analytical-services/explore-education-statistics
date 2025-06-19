b62ea088ef2b2b8efd8ecc5edfd634536978166d
jack-hive <148866614+jack-hive@users.noreply.github.com>
2025-06-19 18:28:33 +0100

    EES-6148 Adding some Publication Role utility functions

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/Util/PublicationRoleUtilsTests.cs
@@ -0,0 +1,51 @@
+#nullable enable
+using GovUk.Education.ExploreEducationStatistics.Admin.Services.Util;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services.Util;
+
+public class PublicationRoleUtilsTests
+{
+    [Theory]
+    [InlineData(PublicationRole.Owner, PublicationRole.Drafter)]
+    [InlineData(PublicationRole.Allower, PublicationRole.Approver)]
+    [InlineData(PublicationRole.Drafter, PublicationRole.Drafter)]
+    [InlineData(PublicationRole.Approver, PublicationRole.Approver)]
+    public void ConvertToNewPermissionsSystemPublicationRole(
+        PublicationRole publicationRoleToConvert,
+        PublicationRole expectedPublicationRole)
+    {
+        var convertedPublicationRole = publicationRoleToConvert.ConvertToNewPermissionsSystemPublicationRole();
+
+        Assert.Equal(expectedPublicationRole, convertedPublicationRole);
+    }
+
+    [Theory]
+    [InlineData(ReleaseRole.PrereleaseViewer, null, false)]
+    [InlineData(ReleaseRole.Contributor, PublicationRole.Drafter, true)]
+    [InlineData(ReleaseRole.Approver, PublicationRole.Approver, true)]
+    public void TryConvertToNewPermissionsSystemPublicationRole(
+        ReleaseRole releaseRoleToConvert,
+        PublicationRole? expectedPublicationRole,
+        bool expectedConversionSuccessful)
+    {
+        var conversionSuccessful = releaseRoleToConvert.TryConvertToNewPermissionsSystemPublicationRole(out var convertedPublicationRole);
+
+        Assert.Equal(expectedConversionSuccessful, conversionSuccessful);
+        Assert.Equal(expectedPublicationRole, convertedPublicationRole);
+    }
+
+    [Theory]
+    [InlineData(PublicationRole.Owner, false)]
+    [InlineData(PublicationRole.Allower, false)]
+    [InlineData(PublicationRole.Drafter, true)]
+    [InlineData(PublicationRole.Approver, true)]
+    public void IsNewPermissionsSystemPublicationRole(
+        PublicationRole publicationRole,
+        bool expectedResult)
+    {
+        var isNewPermissionsSystemPublicationRole = publicationRole.IsNewPermissionsSystemPublicationRole();
+
+        Assert.Equal(expectedResult, isNewPermissionsSystemPublicationRole);
+    }
+}
