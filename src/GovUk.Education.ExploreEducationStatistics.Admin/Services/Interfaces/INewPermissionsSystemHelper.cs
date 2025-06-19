6c95e41729db551ab109dcba72b46410504016d2
jack-hive <148866614+jack-hive@users.noreply.github.com>
2025-06-19 18:32:31 +0100

    EES-6148 adding an helper class to help with the creation and deletion of publication roles in the NEW permissions system
    This class, or some form of it, will probably stick around even after EES-6196, but will most likely be amended slightly in EES-6196, once we stop the ability for users to create/delete release roles and publication roles in the OLD permissions system.
    
    It contains the logic used to determine when to UPGRADE one of the NEW roles from `Drafter` to `Approver`, when to create any NEW roles at all, and when NOT to create any NEW role, based on the role passed in (which is the role a user is trying to create)

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/INewPermissionsSystemHelper.cs
@@ -0,0 +1,14 @@
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
+
+public interface INewPermissionsSystemHelper
+{
+    Task<(PublicationRole? newSystemPublicationRoleToRemove, PublicationRole? newSystemPublicationRoleToCreate)>
+        DetermineNewPermissionsSystemChanges(PublicationRole publicationRoleToCreate, Guid userId, Guid publicationId);
+
+    Task<(PublicationRole? newSystemPublicationRoleToRemove, PublicationRole? newSystemPublicationRoleToCreate)>
+        DetermineNewPermissionsSystemChanges(ReleaseRole releaseRoleToCreate, Guid userId, Guid publicationId);
+}
