1ec4502682c9db10c94ddcb7a23df427f53815ec
Stuart Bennett <stuart@hiveit.co.uk>
2025-07-02 14:06:30 +0100

    EES-6259 table tool publication meta changes

--- src/explore-education-statistics-frontend/src/modules/table-tool/TableToolPage.tsx
+++ src/explore-education-statistics-frontend/src/modules/table-tool/TableToolPage.tsx
@@ -50,7 +50,20 @@ const TableToolPage: NextPage<TableToolPageProps> = ({
   const router = useRouter();
   const [loadingFastTrack, setLoadingFastTrack] = useState(false);
   const [currentStep, setCurrentStep] = useState<number | undefined>(undefined);
-  const [pageTitle, setPageTitle] = useState<string>(defaultPageTitle);
+  const [basePageTitle, setBasePageTitle] = useState<string>(
+    selectedPublication?.title
+      ? `${defaultPageTitle} on ${selectedPublication?.title.toLocaleLowerCase()}`
+      : defaultPageTitle,
+  );
+  const [pageTitle, setPageTitle] = useState<string>(basePageTitle);
+
+  useEffect(() => {
+    const newBasePageTitle = selectedPublication?.title
+      ? `${defaultPageTitle} on ${selectedPublication?.title.toLocaleLowerCase()}`
+      : defaultPageTitle;
+    setBasePageTitle(newBasePageTitle);
+    setPageTitle(newBasePageTitle);
+  }, [selectedPublication]);
 
   useEffect(() => {
     // Intercept the back button and activate the appropriate step
@@ -145,9 +158,14 @@ const TableToolPage: NextPage<TableToolPageProps> = ({
   return (
     <Page
       // Don't include the default meta title after intitial step to prevent too much screen reader noise.
-      includeDefaultMetaTitle={pageTitle === defaultPageTitle}
+      includeDefaultMetaTitle={pageTitle === basePageTitle}
       metaTitle={pageTitle}
-      title={defaultPageTitle}
+      title={basePageTitle}
+      description={
+        selectedPublication?.title
+          ? `Create and download your own custom data tables by choosing your areas of interest using filters to build your table from ${selectedPublication.title}`
+          : undefined
+      }
       caption="Table Tool"
       wide
     >
@@ -229,7 +247,7 @@ const TableToolPage: NextPage<TableToolPageProps> = ({
         }}
         onStepSubmit={({ nextStepNumber, nextStepTitle }) =>
           setPageTitle(
-            `Step ${nextStepNumber}: ${nextStepTitle} - ${defaultPageTitle}`,
+            `Step ${nextStepNumber}: ${nextStepTitle} - ${basePageTitle}`,
           )
         }
         onSubjectFormSubmit={async ({ publication, release, subjectId }) => {
