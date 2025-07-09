b68cabf289436c768b3517e16e6490466951effe
Lee Oades <lee.oades@gmail.com>
2025-07-09 08:57:52 +0100

    Change ReleaseVersionId to a GUID

--- src/GovUk.Education.ExploreEducationStatistics.Data.Services/Analytics/CaptureTableToolDownloadCall.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Services/Analytics/CaptureTableToolDownloadCall.cs
@@ -6,7 +6,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Analytics;
 
 public record CaptureTableToolDownloadCall : IAnalyticsCaptureRequest
 {
-    public string ReleaseVersionId { get; init; }
+    public Guid ReleaseVersionId { get; init; }
     public string PublicationName { get; init; }
     public string ReleasePeriodAndLabel { get; init; }
     public Guid SubjectId { get; init; }
