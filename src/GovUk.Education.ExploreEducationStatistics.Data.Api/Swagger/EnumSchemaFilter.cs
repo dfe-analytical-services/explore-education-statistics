405a76f5808b76f0b365471442da67141bea0703
Lee Oades <lee.oades@gmail.com>
2025-07-09 10:36:56 +0100

    When a controller binding object has an enum on in, instead of the default behaviour of accepting the int value (which is largely meaningless) it accepts the string name instead.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api/Swagger/EnumSchemaFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Swagger;
+
+/// <summary>
+/// Emit the Enum string names in the Swagger Documentation instead of their int values.
+/// </summary>
+public class EnumSchemaFilter : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        if (context.Type.IsEnum)
+        {
+            schema.Enum.Clear();
+            schema.Enum.AddRange(Enum.GetNames(context.Type).Select(name => new OpenApiString($"{name}")));;
+        }
+    }
+}
