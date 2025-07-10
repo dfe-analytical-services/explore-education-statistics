ce4ec3a28346136473e9e3211672eb20e98a71b9
Lee Oades <lee.oades@gmail.com>
2025-07-10 12:00:29 +0100

    Analytics Controller tests added.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api.Tests/Builders/AnalyticsManagerMockBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Analytics.Common.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Common.Tests.Extensions;
+using Moq;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Tests.Builders;
+
+public class AnalyticsManagerMockBuilder
+{
+    private readonly Mock<IAnalyticsManager> _mock = new(MockBehavior.Strict);
+    
+    public IAnalyticsManager Build()
+    {
+        _mock
+            .Setup(m => m.Add(It.IsAny<IAnalyticsCaptureRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+        
+        return _mock.Object;
+    }
+
+    public Asserter Assert => new(_mock);
+    
+    public class Asserter(Mock<IAnalyticsManager> mock)
+    {
+        public void RequestAdded(IAnalyticsCaptureRequest expected)
+        {
+            mock.Verify(m => m.Add(ItIs.DeepEqualTo(expected), It.IsAny<CancellationToken>()), Times.Once);
+        } 
+    }
+}
---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api.Tests/Controllers/AnalyticsControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Common.Tests.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Data.Api.Controllers;
+using GovUk.Education.ExploreEducationStatistics.Data.Api.Tests.Builders;
+using GovUk.Education.ExploreEducationStatistics.Data.Api.Tests.Requests;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Tests.Controllers;
+
+public class AnalyticsControllerTests
+{
+    private readonly AnalyticsManagerMockBuilder _analyticsManager = new();
+
+    private AnalyticsController GetSut() => new(
+        _analyticsManager.Build(),
+        new NullLogger<AnalyticsController>());
+
+    [Fact]
+    public void Can_instantiate_Sut() => Assert.NotNull(GetSut());
+
+    [Fact]
+    public async Task
+        GivenValidRecordTableToolDownloadRequestBindingModel_WhenCallToRecordDownload_ThenInformationPassedToAnalyticsManager()
+    {
+        // ARRANGE
+        var bindingModel = new RecordTableToolDownloadRequestBindingModelBuilder().Build();
+        var sut = GetSut();
+
+        // ACT
+        var response = await sut.RecordDownload(bindingModel);
+        
+        // ASSERT
+        response.AssertOkResult();
+        var expected = bindingModel.ToModel();
+        _analyticsManager.Assert.RequestAdded(expected);
+    }
+}
