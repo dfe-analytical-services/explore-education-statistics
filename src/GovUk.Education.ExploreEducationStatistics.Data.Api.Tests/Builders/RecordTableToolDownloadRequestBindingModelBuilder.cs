e50891c05fcd2e0543f739c0ff497fd4720416bb
Lee Oades <lee.oades@gmail.com>
2025-07-10 12:00:29 +0100

    Analytics Controller tests added.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api.Tests/Builders/RecordTableToolDownloadRequestBindingModelBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using GovUk.Education.ExploreEducationStatistics.Common.Model;
+using GovUk.Education.ExploreEducationStatistics.Common.Model.Data.Query;
+using GovUk.Education.ExploreEducationStatistics.Common.Requests;
+using GovUk.Education.ExploreEducationStatistics.Data.Api.Requests;
+using GovUk.Education.ExploreEducationStatistics.Data.Services.Analytics;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Tests.Builders;
+
+public class RecordTableToolDownloadRequestBindingModelBuilder
+{
+    public RecordTableToolDownloadRequestBindingModel Build() =>
+        new()
+        {
+            DataSetName = "data set name",
+            DownloadFormat = TableDownloadFormat.ODS,
+            PublicationName = "Publication Name",
+            ReleasePeriodAndLabel = "release period label",
+            ReleaseVersionId = Guid.NewGuid(),
+            SubjectId = Guid.NewGuid(),
+            Query = new FullTableQueryRequest
+            {
+                SubjectId = Guid.NewGuid(),
+                Filters = [Guid.NewGuid(), Guid.NewGuid()],
+                Indicators = [Guid.NewGuid(), Guid.NewGuid()],
+                LocationIds = [Guid.NewGuid(), Guid.NewGuid()],
+                TimePeriod =
+                    new TimePeriodQuery
+                    {
+                        StartYear = 2025,
+                        StartCode = TimeIdentifier.July,
+                        EndYear = 2026,
+                        EndCode = TimeIdentifier.November
+                    },
+                FilterHierarchiesOptions = new Dictionary<Guid, List<FilterHierarchyOption>>
+                {
+                    { Guid.NewGuid(), [new FilterHierarchyOption([Guid.NewGuid(), Guid.NewGuid()])] },
+                    { Guid.NewGuid(), [new FilterHierarchyOption([Guid.NewGuid(), Guid.NewGuid()])] },
+                }
+            }
+        };
+}
---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Api.Tests/Requests/RecordTableToolDownloadRequestBindingModelTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using GovUk.Education.ExploreEducationStatistics.Data.Api.Requests;
+using GovUk.Education.ExploreEducationStatistics.Data.Api.Tests.Builders;
+using GovUk.Education.ExploreEducationStatistics.Data.Services.Analytics;
+using Xunit;
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Api.Tests.Requests;
+
+public class RecordTableToolDownloadRequestBindingModelTests
+{
+    [Theory]
+    [MemberData(nameof(InvalidBindingModels))]
+    public void GivenInvalidBindingModel_WhenValidated_ThenResultIsInvalid(RecordTableToolDownloadRequestBindingModel invalidModel)
+    {
+        // ARRANGE
+        var validator = new RecordTableToolDownloadRequestBindingModel.Validator();
+        
+        // ACT
+        var result = validator.Validate(invalidModel);
+        
+        // ASSERT
+        Assert.NotNull(result);
+        Assert.False(result.IsValid);
+    }
+
+    [Fact]
+    public void GivenValidBindingModel_WhenValidated_ThenResultIsValid()
+    {
+        // ARRANGE
+        var validator = new RecordTableToolDownloadRequestBindingModel.Validator();
+        
+        // ACT
+        var result = validator.Validate(FullyPopulatedModel());
+        
+        // ASSERT
+        Assert.NotNull(result);
+        Assert.True(result.IsValid);
+    }
+    
+    public static TheoryData<RecordTableToolDownloadRequestBindingModel> InvalidBindingModels() =>
+        new(
+            new RecordTableToolDownloadRequestBindingModel(),
+            FullyPopulatedModel() with { DataSetName = null },
+            FullyPopulatedModel() with { DataSetName = string.Empty },
+            FullyPopulatedModel() with { DownloadFormat = null },
+            FullyPopulatedModel() with { DownloadFormat = (TableDownloadFormat)99 },
+            FullyPopulatedModel() with { PublicationName = null },
+            FullyPopulatedModel() with { PublicationName = string.Empty },
+            FullyPopulatedModel() with { ReleasePeriodAndLabel = null },
+            FullyPopulatedModel() with { ReleasePeriodAndLabel = string.Empty },
+            FullyPopulatedModel() with { ReleaseVersionId = null },
+            FullyPopulatedModel() with { ReleaseVersionId = Guid.Empty },
+            FullyPopulatedModel() with { SubjectId = null },
+            FullyPopulatedModel() with { SubjectId = Guid.Empty },
+            FullyPopulatedModel() with { Query = null }
+        );
+
+    private static RecordTableToolDownloadRequestBindingModel FullyPopulatedModel() =>
+        new RecordTableToolDownloadRequestBindingModelBuilder().Build();
+}
