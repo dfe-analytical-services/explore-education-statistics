e9c4759271a8b50e32845b7413c9a4a9c3e180ad
mmoalin <mmoalin@outlook.com>
2025-07-10 13:26:29 +0100

    Further clean up post rebase & merge conflict resolution

--- src/explore-education-statistics-admin/src/pages/release/data/components/DataFileReplacementPlan.tsx
+++ src/explore-education-statistics-admin/src/pages/release/data/components/DataFileReplacementPlan.tsx
@@ -32,6 +32,7 @@ import sanitizeHtml from '@common/utils/sanitizeHtml';
 import { useAuthContext } from '@admin/contexts/AuthContext';
 import Link from '@admin/components/Link';
 import { useConfig } from '@admin/contexts/ConfigContext';
+import isPatchVersion from '@common/utils/isPatchVersion';
 
 interface Props {
   cancelButton: ReactNode;
@@ -160,9 +161,8 @@ const DataFileReplacementPlan = ({
     </Tag>
   );
 
-  const versionParts = plan?.apiDataSetVersionPlan?.version?.split('.') || [];
   const isPatch = isNewReplaceDsvFeatureEnabled
-    ? versionParts?.length === 3 && parseInt(versionParts[2], 10) > 0
+    ? isPatchVersion(plan?.apiDataSetVersionPlan?.version)
     : false;
 
   const linkToApiDetailsTab = apiDataSetsTabRoute && (
