9caf019a67ac064576c2a4c358c85810ad9cda14
Lee Oades <lee.oades@gmail.com>
2025-07-11 18:18:02 +0100

    Marked the 2 new capture calls as nullable enabled. This meant marking the properties as required. Updated the tests that were instantiating the calls without setting the properties. Consolidated the creation of the calls into builders.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Services/Analytics/Dtos/CapturePermaLinkTableDownloadCall.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Services/Analytics/Dtos/CapturePermaLinkTableDownloadCall.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using GovUk.Education.ExploreEducationStatistics.Analytics.Common.Interfaces;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Analytics.Dtos;
 
 public record CapturePermaLinkTableDownloadCall : IAnalyticsCaptureRequest
 {
-    public string PermalinkTitle { get; init; }
-    public Guid PermalinkId { get; init; }
-    public TableDownloadFormat DownloadFormat { get; init; }
+    public required string PermalinkTitle { get; init; }
+    public required Guid PermalinkId { get; init; }
+    public required TableDownloadFormat DownloadFormat { get; init; }
 }
