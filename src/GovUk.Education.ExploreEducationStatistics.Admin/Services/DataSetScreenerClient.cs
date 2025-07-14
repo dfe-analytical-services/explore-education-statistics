26841fc07f3d05ba10bc8232cb66e32da3f0b0cd
Duncan Watson <duncan@hiveit.co.uk>
2025-07-14 15:02:45 +0100

    EES-6023 - added Screener Easy Auth components to Admin and its DataSetScreenerClient. Abstracted out the work to add authentication to HTTP requests from Admin via its Clients.

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/DataSetScreenerClient.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/DataSetScreenerClient.cs
@@ -1,6 +1,3 @@
-using GovUk.Education.ExploreEducationStatistics.Admin.Requests;
-using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
-using GovUk.Education.ExploreEducationStatistics.Common.Model;
 using System;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -8,10 +5,17 @@ using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Admin.Options;
+using GovUk.Education.ExploreEducationStatistics.Admin.Requests;
+using GovUk.Education.ExploreEducationStatistics.Admin.Services.Authentication;
+using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Common.Model;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Services;
 
-public class DataSetScreenerClient(HttpClient client) : IDataSetScreenerClient
+public class DataSetScreenerClient(
+    HttpClient httpClient,
+    IHttpClientAzureAuthenticationManager<DataScreenerClientOptions> authenticationManager) : IDataSetScreenerClient
 {
     private readonly JsonSerializerOptions _jsonSerializerOptions = new()
     {
@@ -22,11 +26,13 @@ public class DataSetScreenerClient(HttpClient client) : IDataSetScreenerClient
         DataSetScreenerRequest dataSetRequest,
         CancellationToken cancellationToken)
     {
+        await authenticationManager.AddAuthentication(httpClient, cancellationToken);
+        
         var json = JsonSerializer.Serialize(dataSetRequest, _jsonSerializerOptions);
         var content = new StringContent(json, Encoding.UTF8, "application/json");
 
         // TODO (EES-5353): Add cancellation token handling logic to terminate Azure Function processes
-        var response = await client.PostAsync(client.BaseAddress, content, CancellationToken.None);
+        var response = await httpClient.PostAsync(httpClient.BaseAddress, content, CancellationToken.None);
 
         return response.IsSuccessStatusCode
             ? await response.Content.ReadFromJsonAsync<DataSetScreenerResponse>(cancellationToken)
