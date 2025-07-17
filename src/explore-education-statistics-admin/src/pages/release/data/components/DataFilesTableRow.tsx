c809ca62c37fd732000a638f13df294fd95d1400
mmoalin <mmoalin@outlook.com>
2025-07-17 11:15:33 +0100

    EES-5779/EES-6306 Block replacement of draft data file linked to draft API version. (#6126)
    * Block users from replacing a data file which is attached to a draft api version
    
    * Add tests to new functionality. Prevent UI bug if publicApiDataSetId is undefined
    
    * Fix broken tests for ReleaseDataUploadsSection
    
    * Changes after PR feedback
    
    * Tidy up based on PR comments

--- src/explore-education-statistics-admin/src/pages/release/data/components/DataFilesTableRow.tsx
+++ src/explore-education-statistics-admin/src/pages/release/data/components/DataFilesTableRow.tsx
@@ -22,6 +22,7 @@ import ButtonText from '@common/components/ButtonText';
 import Modal from '@common/components/Modal';
 import React from 'react';
 import { generatePath } from 'react-router';
+import { useReleaseVersionContext } from '@admin/pages/release/contexts/ReleaseVersionContext';
 import styles from './DataFilesTable.module.scss';
 import DeleteDataFileModal from './DeleteDataFileModal';
 
@@ -48,10 +49,10 @@ export default function DataFilesTableRow({
   const {
     enableReplacementOfPublicApiDataSets: isNewReplaceDsvFeatureEnabled,
   } = useConfig();
+  const { releaseVersion } = useReleaseVersionContext();
   const allowReplacementOfDataFile = isNewReplaceDsvFeatureEnabled
     ? true
     : dataFile.publicApiDataSetId == null;
-
   return (
     <tr key={dataFile.title}>
       <td data-testid="Title" className={styles.title}>
@@ -99,46 +100,17 @@ export default function DataFilesTableRow({
                     >
                       Edit title
                     </Link>
-                    {!allowReplacementOfDataFile ? (
-                      <Modal
-                        showClose
-                        title="Cannot replace data"
-                        triggerButton={<ButtonText>Replace data</ButtonText>}
-                      >
-                        <p>
-                          This data file has an API data set linked to it.
-                          Please remove the API data set before replacing the
-                          data.
-                        </p>
-                        <p>
-                          <Link
-                            to={generatePath<ReleaseDataSetRouteParams>(
-                              releaseApiDataSetDetailsRoute.path,
-                              {
-                                publicationId,
-                                releaseVersionId,
-                                dataSetId: dataFile.publicApiDataSetId ?? '',
-                              },
-                            )}
-                          >
-                            Go to API data set
-                          </Link>
-                        </p>
-                      </Modal>
-                    ) : (
-                      <Link
-                        to={generatePath<ReleaseDataFileReplaceRouteParams>(
-                          releaseDataFileReplaceRoute.path,
-                          {
-                            publicationId,
-                            releaseVersionId,
-                            fileId: dataFile.id,
-                          },
-                        )}
-                      >
-                        Replace data
-                      </Link>
-                    )}
+                    <ReplaceDataButtonOrModal
+                      allowReplacementOfDataFile={allowReplacementOfDataFile}
+                      releaseIsNotAmendmentAndIsLinkedToApi={
+                        !releaseVersion.amendment &&
+                        dataFile.publicApiDataSetId != null
+                      }
+                      publicationId={publicationId}
+                      dataFileId={dataFile.id}
+                      publicApiDataSetId={dataFile.publicApiDataSetId}
+                      releaseVersionId={releaseVersionId}
+                    />
                   </>
                 )}
                 {dataFile.publicApiDataSetId ? (
@@ -186,5 +158,95 @@ export default function DataFilesTableRow({
         </ButtonGroup>
       </td>
     </tr>
+  );
+}
+
+function CannotReplaceDataModal({ children }: { children: React.ReactNode }) {
+  return (
+    <Modal
+      showClose
+      title="Cannot replace data"
+      triggerButton={<ButtonText>Replace data</ButtonText>}
+    >
+      {children}
+    </Modal>
+  );
+}
+
+function ReplaceDataButtonOrModal({
+  allowReplacementOfDataFile,
+  releaseIsNotAmendmentAndIsLinkedToApi,
+  dataFileId,
+  publicApiDataSetId,
+  publicationId,
+  releaseVersionId,
+}: {
+  allowReplacementOfDataFile: boolean;
+  releaseIsNotAmendmentAndIsLinkedToApi: boolean;
+  dataFileId: string;
+  publicApiDataSetId: string | undefined;
+  publicationId: string;
+  releaseVersionId: string;
+}) {
+  if (!allowReplacementOfDataFile) {
+    return (
+      <CannotReplaceDataModal>
+        <p>
+          This data file has an API data set linked to it. Please remove the API
+          data set before replacing the data.
+        </p>
+        <p>
+          <Link
+            to={
+              publicApiDataSetId
+                ? generatePath<ReleaseDataSetRouteParams>(
+                    releaseApiDataSetDetailsRoute.path,
+                    {
+                      publicationId,
+                      releaseVersionId,
+                      dataSetId: publicApiDataSetId,
+                    },
+                  )
+                : {}
+            }
+          >
+            Go to API data set
+          </Link>
+        </p>
+      </CannotReplaceDataModal>
+    );
+  }
+
+  if (releaseIsNotAmendmentAndIsLinkedToApi) {
+    return (
+      <CannotReplaceDataModal>
+        <p>
+          This data replacement can not be completed as it is targeting an
+          existing draft API data set.
+        </p>
+        <p>
+          Please contact the explore statistics team at{' '}
+          <a href="mailto:explore.statistics@education.gov.uk">
+            explore.statistics@education.gov.uk
+          </a>{' '}
+          for support on completing this replacement.
+        </p>
+      </CannotReplaceDataModal>
+    );
+  }
+
+  return (
+    <Link
+      to={generatePath<ReleaseDataFileReplaceRouteParams>(
+        releaseDataFileReplaceRoute.path,
+        {
+          publicationId,
+          releaseVersionId,
+          fileId: dataFileId,
+        },
+      )}
+    >
+      Replace data
+    </Link>
   );
 }
