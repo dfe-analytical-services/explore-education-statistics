f5879c820a9326453ad98be221374cd9edd36d37
Ben Outram <ben@hiveit.co.uk>
2025-07-18 17:08:28 +0100

    EES-6320 Allow setting publishing organisations when creating releases and updating/unsetting publishing organisations when updating release versions

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/MockBuilders/OrganisationsValidatorMockBuilder.cs
@@ -0,0 +1,68 @@
+#nullable enable
+using System;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Admin.Validators;
+using GovUk.Education.ExploreEducationStatistics.Common.Model;
+using GovUk.Education.ExploreEducationStatistics.Common.ViewModels;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ValidationUtils = GovUk.Education.ExploreEducationStatistics.Common.Validators.ValidationUtils;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.MockBuilders;
+
+public class OrganisationsValidatorMockBuilder
+{
+    private readonly Mock<IOrganisationsValidator> _mock = new(MockBehavior.Strict);
+
+    private Organisation[]? _organisations;
+    private ErrorViewModel[]? _validationErrors;
+
+    private static readonly Expression<Func<IOrganisationsValidator, Task<Either<ActionResult, Organisation[]>>>>
+        ValidateOrganisations =
+            m => m.ValidateOrganisations(
+                It.IsAny<Guid[]?>(),
+                It.IsAny<string>(),
+                It.IsAny<CancellationToken>());
+
+    public IOrganisationsValidator Build()
+    {
+        _mock.Setup(ValidateOrganisations)
+            .ReturnsAsync(_validationErrors?.Length > 0
+                ? ValidationUtils.ValidationResult(_validationErrors)
+                : _organisations ?? []);
+
+        return _mock.Object;
+    }
+
+    public OrganisationsValidatorMockBuilder WhereHasOrganisations(Organisation[] organisations)
+    {
+        _organisations = organisations;
+        return this;
+    }
+
+    public OrganisationsValidatorMockBuilder WhereHasValidationErrors(ErrorViewModel[] validationErrors)
+    {
+        _validationErrors = validationErrors;
+        return this;
+    }
+
+    public Asserter Assert => new(_mock);
+
+    public class Asserter(Mock<IOrganisationsValidator> mock)
+    {
+        public void ValidateOrganisationsWasCalled(
+            Guid[]? expectedOrganisationIds = null,
+            string? expectedPath = null) =>
+            mock.Verify(m => m.ValidateOrganisations(
+                    It.Is<Guid[]?>(organisationIds =>
+                        expectedOrganisationIds == null || organisationIds == expectedOrganisationIds),
+                    It.Is<string?>(path => expectedPath == null || path == expectedPath),
+                    It.IsAny<CancellationToken>()),
+                Times.Once);
+
+        public void ValidateOrganisationsWasNotCalled() => mock.Verify(ValidateOrganisations, Times.Never);
+    }
+}
---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Validators/OrganisationsValidatorTests.cs
@@ -0,0 +1,199 @@
+﻿#nullable enable
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using GovUk.Education.ExploreEducationStatistics.Admin.Validators;
+using GovUk.Education.ExploreEducationStatistics.Common.Tests.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Common.Tests.Fixtures;
+using GovUk.Education.ExploreEducationStatistics.Common.Validators.ErrorDetails;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Tests.Fixtures;
+using Moq;
+using static GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services.DbUtils;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Validators;
+
+public abstract class OrganisationsValidatorTests
+{
+    private readonly DataFixture _dataFixture = new();
+
+    public class ValidateOrganisationsTests : OrganisationsValidatorTests
+    {
+        [Fact]
+        public async Task WhenOrganisationIdsIsNull_ReturnsEmpty()
+        {
+            // Arrange
+            await using var context = InMemoryApplicationDbContext();
+            var sut = BuildService(context);
+
+            // Act
+            var result = await sut.ValidateOrganisations(organisationIds: null);
+
+            // Assert
+            var actualOrganisations = result.AssertRight();
+            Assert.Empty(actualOrganisations);
+        }
+
+        [Fact]
+        public async Task WhenOrganisationIdsIsEmpty_ReturnsEmpty()
+        {
+            // Arrange
+            await using var context = InMemoryApplicationDbContext();
+            var sut = BuildService(context);
+
+            // Act
+            var result = await sut.ValidateOrganisations(organisationIds: []);
+
+            // Assert
+            var actualOrganisations = result.AssertRight();
+            Assert.Empty(actualOrganisations);
+        }
+
+        [Fact]
+        public async Task WhenOrganisationIdsExist_ReturnsOrganisations()
+        {
+            // Arrange
+            var organisations = _dataFixture.DefaultOrganisation()
+                .GenerateArray(2);
+            var organisationIds = GetOrganisationIds(organisations);
+
+            var contextId = Guid.NewGuid().ToString();
+            await using (var context = InMemoryApplicationDbContext(contextId))
+            {
+                context.Organisations.AddRange(organisations);
+                await context.SaveChangesAsync();
+            }
+
+            await using (var context = InMemoryApplicationDbContext(contextId))
+            {
+                var sut = BuildService(context);
+
+                // Act
+                var result = await sut.ValidateOrganisations(organisationIds);
+
+                // Assert
+                var actualOrganisations = result.AssertRight();
+                Assert.Equal(organisations.Length, actualOrganisations.Length);
+                Assert.All(organisations, organisation => Assert.Contains(organisation, actualOrganisations));
+            }
+        }
+
+        [Fact]
+        public async Task WhenOrganisationIdsDoNotExist_ReturnsValidationErrors()
+        {
+            // Arrange
+            var organisations = _dataFixture.DefaultOrganisation()
+                .GenerateArray(2);
+            Guid[] organisationIdsNotExisting = [Guid.NewGuid(), Guid.NewGuid()];
+            const string errorPath = "testPath";
+
+            var contextId = Guid.NewGuid().ToString();
+            await using (var context = InMemoryApplicationDbContext(contextId))
+            {
+                context.Organisations.AddRange(organisations);
+                await context.SaveChangesAsync();
+            }
+
+            await using (var context = InMemoryApplicationDbContext(contextId))
+            {
+                var sut = BuildService(context);
+
+                // Act
+                var result = await sut.ValidateOrganisations(
+                    organisationIds: organisationIdsNotExisting,
+                    path: errorPath);
+
+                // Assert
+                var validationProblem = result.AssertBadRequestWithValidationProblem();
+                Assert.Equal(organisationIdsNotExisting.Length, validationProblem.Errors.Count);
+                Assert.All(organisationIdsNotExisting,
+                    organisationId =>
+                    {
+                        validationProblem.AssertHasError(
+                            expectedCode: ValidationMessages.OrganisationNotFound.Code,
+                            expectedMessage: ValidationMessages.OrganisationNotFound.Message,
+                            expectedPath: errorPath,
+                            expectedDetail: new InvalidErrorDetail<Guid>(organisationId));
+                    });
+            }
+        }
+
+        [Fact]
+        public async Task WhenOrganisationIdsPartiallyExist_ReturnsValidationErrors()
+        {
+            // Arrange
+            var organisations = _dataFixture.DefaultOrganisation()
+                .GenerateArray(2);
+            var organisationIds = GetOrganisationIds(organisations);
+            var organisationIdNotExisting = Guid.NewGuid();
+            const string errorPath = "testPath";
+
+            var contextId = Guid.NewGuid().ToString();
+            await using (var context = InMemoryApplicationDbContext(contextId))
+            {
+                context.Organisations.AddRange(organisations);
+                await context.SaveChangesAsync();
+            }
+
+            await using (var context = InMemoryApplicationDbContext(contextId))
+            {
+                var sut = BuildService(context);
+
+                // Act
+                var result = await sut.ValidateOrganisations(
+                    organisationIds: [.. organisationIds, organisationIdNotExisting],
+                    path: errorPath);
+
+                // Assert
+                var validationProblem = result.AssertBadRequestWithValidationProblem();
+                Assert.Single(validationProblem.Errors);
+                validationProblem.AssertHasError(
+                    expectedCode: ValidationMessages.OrganisationNotFound.Code,
+                    expectedMessage: ValidationMessages.OrganisationNotFound.Message,
+                    expectedPath: errorPath,
+                    expectedDetail: new InvalidErrorDetail<Guid>(organisationIdNotExisting));
+            }
+        }
+
+        [Fact]
+        public async Task WhenOrganisationIdsHasDuplicates_ReturnsUniqueOrganisations()
+        {
+            // Arrange
+            var organisations = _dataFixture.DefaultOrganisation()
+                .GenerateArray(2);
+            var organisationIds = GetOrganisationIds(organisations);
+            Guid[] organisationIdsWithDuplicates = [.. organisationIds, .. organisationIds];
+
+            var contextId = Guid.NewGuid().ToString();
+            await using (var context = InMemoryApplicationDbContext(contextId))
+            {
+                context.Organisations.AddRange(organisations);
+                await context.SaveChangesAsync();
+            }
+
+            await using (var context = InMemoryApplicationDbContext(contextId))
+            {
+                var sut = BuildService(context);
+
+                // Act
+                var result = await sut.ValidateOrganisations(organisationIdsWithDuplicates);
+
+                // Assert
+                var actualOrganisations = result.AssertRight();
+                Assert.Equal(organisations.Length, actualOrganisations.Length);
+                Assert.All(organisations, organisation => Assert.Contains(organisation, actualOrganisations));
+            }
+        }
+    }
+
+    private static Guid[] GetOrganisationIds(Organisation[] organisations) =>
+        organisations.Select(o => o.Id).ToArray();
+
+    private static OrganisationsValidator BuildService(ContentDbContext? context = null)
+    {
+        return new OrganisationsValidator(
+            context ?? Mock.Of<ContentDbContext>(MockBehavior.Strict)
+        );
+    }
+}
