c0a1772a45bae1e35ee203ab5f14fc3d929bfe3f
mmoalin <mmoalin@outlook.com>
2025-07-24 20:44:52 +0100

    Rendering patch changes inside DataSetFileApiChangelog

--- src/explore-education-statistics-frontend/src/modules/data-catalogue/components/DataSetFileApiChangelog.tsx
+++ src/explore-education-statistics-frontend/src/modules/data-catalogue/components/DataSetFileApiChangelog.tsx
@@ -1,3 +1,4 @@
+import SectionBreak from '@common/components/SectionBreak';
 import ApiDataSetChangelog from '@common/modules/data-catalogue/components/ApiDataSetChangelog';
 import { ApiDataSetVersionChanges } from '@common/services/types/apiDataSetChanges';
 import DataSetFilePageSection from '@frontend/modules/data-catalogue/components/DataSetFilePageSection';
@@ -9,12 +10,14 @@ interface Props {
   guidanceNotes: string;
   version: string;
   renderHeading?: boolean;
+  patchHistory: ApiDataSetVersionChanges[];
 }
 
 export default function DataSetFileApiChangelog({
   changes,
   guidanceNotes,
   version,
+  patchHistory,
   renderHeading = true,
 }: Props) {
   const { majorChanges, minorChanges } = changes;
@@ -22,7 +25,6 @@ export default function DataSetFileApiChangelog({
   if (!Object.keys(majorChanges).length && !Object.keys(minorChanges).length) {
     return null;
   }
-
   return (
     <DataSetFilePageSection
       heading={renderHeading ?? true ? pageSections.apiChangelog : ''}
@@ -36,6 +38,32 @@ export default function DataSetFileApiChangelog({
         minorChanges={changes.minorChanges}
         version={version}
       />
+      <SectionBreak size="xl" />
+
+      {patchHistory &&
+        patchHistory.map(
+          (patch, idx) =>
+            !(
+              !Object.keys(patch.majorChanges).length &&
+              !Object.keys(patch.minorChanges).length
+            ) && (
+              <>
+                {(patch.notes ?? '').length > 0 && (
+                  <p data-testid="public-guidance-notes">{patch.notes}</p>
+                )}
+                <ApiDataSetChangelog
+                  majorChanges={patch.majorChanges}
+                  minorChanges={patch.minorChanges}
+                  version={
+                    patch.versionNumber.patch > 0
+                      ? `${patch.versionNumber.major}.${patch.versionNumber.minor}.${patch.versionNumber.patch}`
+                      : `${patch.versionNumber.major}.${patch.versionNumber.minor}`
+                  }
+                />
+                {idx !== patchHistory.length - 1 && <SectionBreak size="xl" />}
+              </>
+            ),
+        )}
     </DataSetFilePageSection>
   );
 }
