92d6689886ec198a12cf146886d9eca12ba75779
Mark Youngman <mark@hiveit.co.uk>
2025-08-05 09:42:59 +0100

    EES-6377 Add ListLatestPages endpoint

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/EducationInNumbersService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/EducationInNumbersService.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GovUk.Education.ExploreEducationStatistics.Admin.Extensions;
@@ -8,13 +9,14 @@ using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Common.Model;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Services;
 
 public class EducationInNumbersService(
     ContentDbContext contentDbContext) : IEducationInNumbersService
 {
-    public async Task<Either<ActionResult, EducationInNumbersPageViewModel>> GetPage(
+    public async Task<Either<ActionResult, EducationInNumbersPageViewModel>> GetPage( // @MarkFix tests?
         string? slug,
         bool? published = null)
     {
@@ -25,5 +27,17 @@ public class EducationInNumbersService(
             .OrderByDescending(page => page.Version) // @MarkFix descending correct?
             .FirstOrNotFoundAsync()
             .OnSuccess(page => page.ToViewModel());
+    }
+
+    public async Task<Either<ActionResult, List<EducationInNumbersPageViewModel>>> ListLatestPages() // @MarkFix tests?
+    {
+        return await contentDbContext.EducationInNumbersPages
+            .AsNoTracking()
+            .GroupBy(page => page.Slug)
+            .Select(group => group
+                .OrderByDescending(p => p.Version)
+                .First())
+            .Select(page => page.ToViewModel()) // @MarkFix might want to be a summary view model
+            .ToListAsync();
     }
 }
