090e26e0f579ef2f2daeb453479cce36b687e773
Mark Youngman <mark@hiveit.co.uk>
2025-08-05 08:43:21 +0100

    EES-6377 Create EducationInNumbersPages table

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/EducationInNumbersPage.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Model;
+
+public class EducationInNumbersPage
+{
+    public Guid Id { get; set; }
+
+    [MaxLength(255)] public string Title { get; set; } = string.Empty;
+
+    [MaxLength(255)] public string? Slug { get; set; } = string.Empty;
+
+    [MaxLength(2047)] // @MarkFix length ok?
+    public string Description { get; set; } = string.Empty;
+
+    public int Version { get; set; }
+
+    public int Order { get; set; }
+
+    public DateTimeOffset? Published { get; set; }
+
+    public DateTimeOffset Created { get; set; }
+
+    public Guid CreatedById { get; set; }
+
+    public DateTimeOffset? Updated { get; set; }
+
+    public Guid? UpdatedById { get; set; }
+}
