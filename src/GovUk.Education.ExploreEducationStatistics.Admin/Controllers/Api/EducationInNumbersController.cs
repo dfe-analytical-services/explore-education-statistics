3db375a1916faa2ef0f646b5f81330b7020b67ff
Mark Youngman <mark@hiveit.co.uk>
2025-08-05 14:43:31 +0100

    EES-6377 Add CreateAmendment endpoint

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/EducationInNumbersController.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/EducationInNumbersController.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using GovUk.Education.ExploreEducationStatistics.Admin.Models;
@@ -45,6 +46,23 @@ public class EducationInNumbersController(
         [FromQuery] CreateEducationInNumbersPageRequest request)
     {
         return await einService.CreatePage(request)
+            .HandleFailuresOrOk();
+    }
+
+    [HttpPost("education-in-numbers/{id:Guid}/amendment")]
+    public async Task<ActionResult<EducationInNumbersPageViewModel>> CreateAmendment(
+        [FromRoute] Guid id)
+    {
+        return await einService.CreateAmendment(id)
+            .HandleFailuresOrOk();
+    }
+
+    [HttpPut("education-in-numbers/{id:Guid}")]
+    public async Task<ActionResult<EducationInNumbersPageViewModel>> UpdatePage(
+        [FromRoute] Guid id,
+        [FromQuery] UpdateEducationInNumbersPageRequest request)
+    {
+        return await einService.UpdatePage(id, request)
             .HandleFailuresOrOk();
     }
 }
