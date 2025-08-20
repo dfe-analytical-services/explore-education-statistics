c12e300201a500e1ced0c8129e8f5f1b438a2155
jack-hive <148866614+jack-hive@users.noreply.github.com>
2025-08-20 16:20:10 +0100

    EES-6429 Adding migration to remove all ACCEPTED UserPublicationInvites and UserReleaseInvites (#6239)

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20250820103403_EES6429_RemovingAcceptedPublicationAndReleaseInvites.cs
@@ -0,0 +1,27 @@
+using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations;
+
+/// <inheritdoc />
+public partial class EES6429_RemovingAcceptedPublicationAndReleaseInvites : Migration
+{
+    private const string MigrationId = "20250820103403";
+
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        // Delete all Accepted UserPublicationInvites and UserReleaseInvites
+        migrationBuilder.SqlFromFile(
+            MigrationConstants.ContentMigrationsPath,
+            $"{MigrationId}_{nameof(EES6429_RemovingAcceptedPublicationAndReleaseInvites)}.sql");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+
+    }
+}
