720b93268e6163daa1a904202774fcee2c16c864
Ben Outram <ben@hiveit.co.uk>
2025-09-12 15:57:29 +0100

    EES-6404 New Content API endpoint to get Release version summary

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Services/Releases/ReleaseVersionsService.cs
@@ -0,0 +1,60 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Common.Model;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
+using GovUk.Education.ExploreEducationStatistics.Content.Model.Predicates;
+using GovUk.Education.ExploreEducationStatistics.Content.Services.Releases.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Services.Releases;
+
+public class ReleaseVersionsService(ContentDbContext contentDbContext) : IReleaseVersionsService
+{
+    public async Task<Either<ActionResult, ReleaseVersionSummaryDto>> GetReleaseVersionSummary(
+        string publicationSlug,
+        string releaseSlug,
+        CancellationToken cancellationToken = default) =>
+        await GetPublicationBySlug(publicationSlug, cancellationToken)
+            .OnSuccess(publication => GetLatestPublishedReleaseVersionByReleaseSlug(
+                publication,
+                releaseSlug,
+                cancellationToken))
+            .OnSuccess(async releaseVersion =>
+            {
+                var isLatestRelease = releaseVersion.Id ==
+                                      releaseVersion.Release.Publication.LatestPublishedReleaseVersionId;
+                var updateCount = await GetUpdateCount(releaseVersion.Id, cancellationToken);
+
+                return ReleaseVersionSummaryDto.FromModel(
+                    releaseVersion: releaseVersion,
+                    isLatestRelease: isLatestRelease,
+                    updateCount: updateCount);
+            });
+
+    private Task<Either<ActionResult, Publication>> GetPublicationBySlug(
+        string publicationSlug,
+        CancellationToken cancellationToken) =>
+        contentDbContext.Publications
+            .AsNoTracking()
+            .Where(p => p.Slug == publicationSlug && p.LatestPublishedReleaseVersionId.HasValue)
+            .SingleOrNotFoundAsync(cancellationToken);
+
+    private Task<Either<ActionResult, ReleaseVersion>> GetLatestPublishedReleaseVersionByReleaseSlug(
+        Publication publication,
+        string releaseSlug,
+        CancellationToken cancellationToken) =>
+        contentDbContext.ReleaseVersions
+            .AsNoTracking()
+            .Include(rv => rv.Release.Publication)
+            .Include(rv => rv.PublishingOrganisations)
+            .LatestReleaseVersions(publication.Id, releaseSlug, publishedOnly: true)
+            .SingleOrNotFoundAsync(cancellationToken);
+
+    private async Task<int> GetUpdateCount(
+        Guid releaseVersionId,
+        CancellationToken cancellationToken) =>
+        await contentDbContext.Update
+            .Where(u => u.ReleaseVersionId == releaseVersionId)
+            .CountAsync(cancellationToken);
+}
