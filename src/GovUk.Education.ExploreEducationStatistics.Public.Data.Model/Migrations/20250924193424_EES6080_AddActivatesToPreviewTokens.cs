0382cf0998335ea8f3235c0dee3ba11dc1e8b54e
mmoalin <mmoalin@outlook.com>
2025-09-26 09:37:29 +0100

    Add missing migration

---
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Model/Migrations/20250924193424_EES6080_AddActivatesToPreviewTokens.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Public.Data.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class EES6080_AddActivatesToPreviewTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Activates",
+                table: "PreviewTokens",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Activates",
+                table: "PreviewTokens");
+        }
+    }
+}
