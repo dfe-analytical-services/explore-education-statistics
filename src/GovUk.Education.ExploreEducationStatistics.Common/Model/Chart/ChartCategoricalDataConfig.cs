374a6051748e93d35117b43d245f7127912e3279
Tom Jones <tomjones@hiveit.co.uk>
2025-10-09 17:02:35 +0100

    EES-6232: Add BAU endpoints to rename/delete ZIP-related blob directories.

--- src/GovUk.Education.ExploreEducationStatistics.Common/Model/Chart/ChartCategoricalDataConfig.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common/Model/Chart/ChartCategoricalDataConfig.cs
@@ -1,7 +1,4 @@
 #nullable enable
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
 namespace GovUk.Education.ExploreEducationStatistics.Common.Model.Chart;
 
 public class ChartCategoricalDataConfig
