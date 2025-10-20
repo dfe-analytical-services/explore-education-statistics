ecf226d98034a39ab1114828c10d1c75ebcb3151
amyb-hiveit <81572860+amyb-hiveit@users.noreply.github.com>
2025-10-20 15:11:10 +0100

    EES-6597 move reorder table headers button to the right (#6396)

--- src/explore-education-statistics-common/src/modules/table-tool/components/TimePeriodDataTable.tsx
+++ src/explore-education-statistics-common/src/modules/table-tool/components/TimePeriodDataTable.tsx
@@ -10,7 +10,7 @@ import DataTableCaption from '@common/modules/table-tool/components/DataTableCap
 import FixedMultiHeaderDataTable from '@common/modules/table-tool/components/FixedMultiHeaderDataTable';
 import mapTableToJson from '@common/modules/table-tool/utils/mapTableToJson';
 import TableExportMenu from '@common/modules/find-statistics/components/TableExportMenu';
-import React, { forwardRef, memo, RefObject } from 'react';
+import React, { forwardRef, memo, ReactNode, RefObject } from 'react';
 
 interface Props {
   captionTitle?: string;
@@ -22,6 +22,7 @@ interface Props {
   releaseVersionId?: string;
   source?: string;
   tableHeadersConfig: TableHeadersConfig;
+  tableHeadersForm?: ReactNode;
   onError?: (message: string) => void;
 }
 
@@ -37,6 +38,7 @@ const TimePeriodDataTable = forwardRef<HTMLElement, Props>(
       releaseVersionId,
       source,
       tableHeadersConfig,
+      tableHeadersForm,
       onError,
     }: Props,
     dataTableRef,
@@ -103,6 +105,7 @@ const TimePeriodDataTable = forwardRef<HTMLElement, Props>(
             ref={dataTableRef}
             footnotes={subjectMeta.footnotes}
             source={source}
+            tableHeadersForm={tableHeadersForm}
           />
         </>
       );
