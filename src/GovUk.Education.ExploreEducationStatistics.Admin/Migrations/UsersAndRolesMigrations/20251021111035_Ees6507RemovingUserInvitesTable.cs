20020b5eaf9ece4ef4f67bca1aa7859afdbe39a8
jack-hive <148866614+jack-hive@users.noreply.github.com>
2025-10-21 12:13:56 +0100

    EES-6507 Removing the `UserInvites` entity and database table

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/UsersAndRolesMigrations/20251021111035_Ees6507RemovingUserInvitesTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.UsersAndRolesMigrations
+{
+    /// <inheritdoc />
+    public partial class Ees6507RemovingUserInvitesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "UserInvites");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserInvites",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CreatedById = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Accepted = table.Column<bool>(type: "bit", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInvites", x => x.Email);
+                    table.ForeignKey(
+                        name: "FK_UserInvites_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade
+                    );
+                    table.ForeignKey(
+                        name: "FK_UserInvites_AspNetUsers_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull
+                    );
+                }
+            );
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInvites_CreatedById",
+                table: "UserInvites",
+                column: "CreatedById"
+            );
+
+            migrationBuilder.CreateIndex(name: "IX_UserInvites_RoleId", table: "UserInvites", column: "RoleId");
+        }
+    }
+}
