8b915b5862b1f2f1ab582ffe0382f7d0f4a775e0
Guy Smith <guy@hiveit.co.uk>
2025-11-06 16:19:11 +0000

    EES-6673 remove redundant RouteLeavingGuard

--- src/explore-education-statistics-admin/src/pages/release/content/components/ReleaseContentRedesign.tsx
+++ src/explore-education-statistics-admin/src/pages/release/content/components/ReleaseContentRedesign.tsx
@@ -1,41 +1,17 @@
 import Link from '@admin/components/Link';
-import RouteLeavingGuard from '@admin/components/RouteLeavingGuard';
-import { useEditingContext } from '@admin/contexts/EditingContext';
 import { useReleaseContentState } from '@admin/pages/release/content/contexts/ReleaseContentContext';
 import VisuallyHidden from '@common/components/VisuallyHidden';
 import ReleaseSummaryBlock from '@common/modules/release/components/ReleaseSummaryBlock';
-import React, { Fragment, useMemo } from 'react';
+import React, { Fragment } from 'react';
 import ReleasePageTitle from './ReleasePageTitle';
 
 const ReleaseContent = () => {
-  const { unsavedBlocks, unsavedCommentDeletions } = useEditingContext();
   const { release } = useReleaseContentState();
-
-  const blockRouteChange = useMemo(() => {
-    if (unsavedBlocks.length > 0) {
-      return true;
-    }
-
-    const blocksWithCommentDeletions = Object.entries(unsavedCommentDeletions)
-      .filter(blockWithDeletions => blockWithDeletions[1].length)
-      .map(blockWithDeletions => blockWithDeletions[0]);
-
-    return blocksWithCommentDeletions.length > 0;
-  }, [unsavedBlocks, unsavedCommentDeletions]);
 
   const { publication } = release;
 
   return (
     <>
-      <RouteLeavingGuard
-        blockRouteChange={blockRouteChange}
-        title="There are unsaved changes"
-      >
-        <p>
-          Clicking away from this tab will result in the changes being lost.
-        </p>
-      </RouteLeavingGuard>
-
       <ReleasePageTitle
         publicationSummary={publication.summary || ''}
         publicationTitle={publication.title}
