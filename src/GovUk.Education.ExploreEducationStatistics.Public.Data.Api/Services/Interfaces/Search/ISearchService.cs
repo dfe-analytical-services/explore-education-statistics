29942300e7a48ea97bf8c23177656eabf91e8849
Ben Outram <ben@hiveit.co.uk>
2025-11-07 11:13:58 +0000

    EES-6470 Change Public API to use Azure Search instead of Content API for searching publications

---
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api/Services/Interfaces/Search/ISearchService.cs
@@ -0,0 +1,17 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Model;
+using GovUk.Education.ExploreEducationStatistics.Common.ViewModels;
+using GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Services.Search;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Services.Interfaces.Search;
+
+public interface ISearchService
+{
+    Task<Either<ActionResult, PaginatedListViewModel<PublicationSearchResult>>> SearchPublications(
+        int page,
+        int pageSize,
+        IEnumerable<Guid> publicationIds,
+        string? searchText = null,
+        CancellationToken cancellationToken = default
+    );
+}
