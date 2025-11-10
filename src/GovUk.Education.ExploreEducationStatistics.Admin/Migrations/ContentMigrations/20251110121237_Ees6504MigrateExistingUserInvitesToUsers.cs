b38241408ed0bff530dd3828ab615d5a1ca44ad5
jack-hive <148866614+jack-hive@users.noreply.github.com>
2025-11-10 13:25:13 +0000

    EES-6504 Migrating any EXISTING PENDING (Unaccepted) `UserInvites` --> a corresponding `Users` entity (#6463)

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20251110121237_Ees6504MigrateExistingUserInvitesToUsers.cs
@@ -0,0 +1,26 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    /// <inheritdoc />
+    public partial class Ees6504MigrateExistingUserInvitesToUsers : Migration
+    {
+        private const string MigrationId = "20251110121237";
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Migrate existing UNACCEPTED UserInvites to Users
+            migrationBuilder.SqlFromFile(
+                MigrationConstants.ContentMigrationsPath,
+                $"{MigrationId}_{nameof(Ees6504MigrateExistingUserInvitesToUsers)}.sql"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) { }
+    }
+}
---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20251110121237_Ees6504MigrateExistingUserInvitesToUsers.sql
@@ -0,0 +1,26 @@
+﻿-- Migrate existing UNACCEPTED UserInvites to Users
+INSERT INTO Users (
+    Id,
+    FirstName,
+    LastName,
+    Email,
+    DeletedById,
+    SoftDeleted,
+    Active,
+    RoleId,
+    Created,
+    CreatedById
+)
+SELECT
+    NEWID() AS Id,      
+    NULL AS FirstName,    
+    NULL AS LastName,       
+    ui.Email,               
+    NULL AS DeletedById,   
+    NULL AS SoftDeleted,   
+    0 AS Active,           
+    ui.RoleId,             
+    ui.Created,          
+    ui.CreatedById    
+FROM UserInvites ui
+WHERE ui.Accepted = 0;
