1c2bad818ab354508d32020802d7851b906924ea
mmoalin <mmoalin@outlook.com>
2025-11-14 07:14:01 +0000

    EES-6644 Add pfa_name & pfa_code to locations table

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/20251113142225_Ees6644AddPoliceForceAreaToLocation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Data.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ees6644AddPoliceForceAreaToLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PoliceForceArea_Code",
+                table: "Location",
+                type: "nvarchar(max)",
+                nullable: true
+            );
+
+            migrationBuilder.AddColumn<string>(
+                name: "PoliceForceArea_Name",
+                table: "Location",
+                type: "nvarchar(max)",
+                nullable: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "PoliceForceArea_Code", table: "Location");
+
+            migrationBuilder.DropColumn(name: "PoliceForceArea_Name", table: "Location");
+        }
+    }
+}
