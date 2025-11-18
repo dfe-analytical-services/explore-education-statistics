6f2bd213a20f55e42bbf26ad1a7944a4bd674976
Ben Outram <ben@hiveit.co.uk>
2025-11-18 12:06:09 +0000

    refactor: simplify ReleaseNoteController by using expression-bodied members and primary constructor

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/ManageContent/ReleaseNoteController.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/ManageContent/ReleaseNoteController.cs
@@ -9,46 +9,33 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Controllers.Api.Manag
 [Route("api")]
 [ApiController]
 [Authorize]
-public class ReleaseNoteController : ControllerBase
+public class ReleaseNoteController(IReleaseNoteService releaseNoteService) : ControllerBase
 {
-    private readonly IReleaseNoteService _releaseNoteService;
-
-    public ReleaseNoteController(IReleaseNoteService releaseNoteService)
-    {
-        _releaseNoteService = releaseNoteService;
-    }
-
     [HttpPost("release/{releaseVersionId:guid}/content/release-note")]
     public async Task<ActionResult<List<ReleaseNoteViewModel>>> AddReleaseNote(
         ReleaseNoteSaveRequest saveRequest,
         Guid releaseVersionId
-    )
-    {
-        return await _releaseNoteService
+    ) =>
+        await releaseNoteService
             .AddReleaseNote(releaseVersionId, saveRequest)
             .HandleFailuresOr(result => Created(HttpContext.Request.Path, result));
-    }
 
     [HttpPut("release/{releaseVersionId:guid}/content/release-note/{releaseNoteId:guid}")]
     public async Task<ActionResult<List<ReleaseNoteViewModel>>> UpdateReleaseNote(
         ReleaseNoteSaveRequest saveRequest,
         Guid releaseVersionId,
         Guid releaseNoteId
-    )
-    {
-        return await _releaseNoteService
+    ) =>
+        await releaseNoteService
             .UpdateReleaseNote(releaseVersionId: releaseVersionId, releaseNoteId: releaseNoteId, saveRequest)
             .HandleFailuresOrOk();
-    }
 
     [HttpDelete("release/{releaseVersionId:guid}/content/release-note/{releaseNoteId:guid}")]
     public async Task<ActionResult<List<ReleaseNoteViewModel>>> DeleteReleaseNote(
         Guid releaseVersionId,
         Guid releaseNoteId
-    )
-    {
-        return await _releaseNoteService
+    ) =>
+        await releaseNoteService
             .DeleteReleaseNote(releaseVersionId: releaseVersionId, releaseNoteId: releaseNoteId)
             .HandleFailuresOrOk();
-    }
 }
