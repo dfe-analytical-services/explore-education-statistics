3fc41bac9c86bf475421cbfd9d8687de5e078abd
Duncan Watson <duncan@hiveit.co.uk>
2025-11-20 09:18:01 +0000

    EES-6450 - created new simplified integration test hierarchy for tests that require HttpClients and PSQL TestContainers. Focused on reducing time-consuming processes like DB "Migrate" calls, looking up common resources from the WebApplicationFactory e.g. DbContexts, where they can be reused between tests.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Fixture/Optimised/OptimisedHttpClientExtensions.cs
@@ -0,0 +1,50 @@
+#nullable enable
+using System.Security.Claims;
+using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Common.Services.Security;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Fixture.Optimised;
+
+/// <summary>
+/// Some convenience methods for <see cref="HttpClient"/> to be used within the test classes.
+/// </summary>
+public static class OptimisedHttpClientExtensions
+{
+    /// <summary>
+    ///
+    /// Set the user who will be making the call via the HttpClient. This will make the user available to the
+    /// <see cref="OptimisedTestAuthHandler"/> which can then look up the user from the <see cref="OptimisedTestUserPool"/>.
+    ///
+    /// In order to find the appropriate user in the pool, the user must be registered in the pool prior to
+    /// using the HttpClient to make any calls. This can be done via the appropriate Collection fixture's
+    /// "RegisterTestUser" method.
+    ///
+    /// </summary>
+    public static HttpClient WithUser(this HttpClient client, ClaimsPrincipal user)
+    {
+        return client.WithOptionalHeader(OptimisedTestAuthHandler.TestUserId, user.GetUserId().ToString());
+    }
+
+    private static HttpClient WithOptionalHeader(this HttpClient client, string headerName, string? headerValue)
+    {
+        if (headerValue != null)
+        {
+            client.WithAdditionalHeaders(new Dictionary<string, string> { { headerName, headerValue } });
+        }
+
+        return client;
+    }
+
+    // TODO EES-6450 - remove these disables once we're able to without warnings.
+    // ReSharper disable once MemberCanBePrivate.Global
+    // ReSharper disable once UnusedMethodReturnValue.Global
+    public static HttpClient WithAdditionalHeaders(
+        this HttpClient client,
+        Dictionary<string, string>? additionalHeaders
+    )
+    {
+        additionalHeaders?.ForEach(header => client.DefaultRequestHeaders.Add(header.Key, header.Value));
+
+        return client;
+    }
+}
