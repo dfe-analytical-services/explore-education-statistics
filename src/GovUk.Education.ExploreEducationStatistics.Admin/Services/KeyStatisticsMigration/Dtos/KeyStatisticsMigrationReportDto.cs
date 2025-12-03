455c2436a286c47e1f5eb7328a8579cc6309df5c
Ben Outram <ben@hiveit.co.uk>
2025-12-03 18:47:04 +0000

    EES-6745 Add migration to remove Markdown from Key Statistics Guidance text (#6551)
    * EES-6745 Add migration to remove Markdown from Key Statistics Guidance text
    
    * EES-6745 Fixing failing unit test
    
    * EES-6745 Fix spelling mistake

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/KeyStatisticsMigration/Dtos/KeyStatisticsMigrationReportDto.cs
@@ -0,0 +1,77 @@
+﻿#nullable enable
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.KeyStatisticsMigration.Dtos;
+
+/// <summary>
+/// TODO EES-6747 Remove after the Key Statistics migration is complete.
+/// </summary>
+public record KeyStatisticsMigrationReportDto
+{
+    public required bool DryRun { get; init; }
+
+    public int KeyStatisticsCount { get; init; }
+
+    public int UpdatedKeyStatisticsCount => MigrationResults.Count;
+
+    public required List<KeyStatisticsMigrationReportKeyStatisticUpdateDto> MigrationResults { get; init; }
+
+    public required List<KeyStatisticsMigrationReportPublicationDto> Publications { get; init; }
+}
+
+public record KeyStatisticsMigrationReportPublicationDto
+{
+    public required Guid PublicationId { get; init; }
+
+    public required string Slug { get; init; }
+
+    public required string Title { get; init; }
+
+    public required string Theme { get; init; }
+
+    public required List<KeyStatisticsMigrationReportReleaseDto> Releases { get; init; }
+}
+
+public record KeyStatisticsMigrationReportReleaseDto
+{
+    public required Guid ReleaseId { get; init; }
+
+    public required string Slug { get; init; }
+
+    public required string Title { get; init; }
+
+    public required List<KeyStatisticsMigrationReportReleaseVersionDto> ReleaseVersions { get; init; }
+}
+
+public record KeyStatisticsMigrationReportReleaseVersionDto
+{
+    public required Guid ReleaseVersionId { get; init; }
+
+    public bool IsDraft => !Published.HasValue;
+
+    public required List<KeyStatisticsMigrationReportKeyStatisticDto> KeyStatistics { get; init; }
+
+    public required DateTime? Published { get; init; }
+
+    public required int Version { get; init; }
+}
+
+public record KeyStatisticsMigrationReportKeyStatisticDto
+{
+    public required Guid KeyStatisticId { get; init; }
+
+    public required string? GuidanceTextOriginal { get; init; }
+
+    public required string? GuidanceTextPlain { get; init; }
+
+    public required int Order { get; init; }
+
+    public bool HasGuidanceTextChanged => GuidanceTextOriginal != GuidanceTextPlain;
+}
+
+public record KeyStatisticsMigrationReportKeyStatisticUpdateDto
+{
+    public required Guid KeyStatisticId { get; init; }
+
+    public required string GuidanceTextOriginal { get; init; }
+
+    public required string GuidanceTextPlain { get; init; }
+}
