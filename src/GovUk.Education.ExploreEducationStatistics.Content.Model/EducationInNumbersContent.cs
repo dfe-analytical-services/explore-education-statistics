9b0ead13a548ceec1ce6cf8666602101ce385b4e
Mark Youngman <mark@youngman.info>
2025-12-11 17:00:57 +0000

    EES-6752 Saving work

--- src/GovUk.Education.ExploreEducationStatistics.Content.Model/EducationInNumbersContent.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/EducationInNumbersContent.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 using System.ComponentModel.DataAnnotations;
+using Semver;
 
 namespace GovUk.Education.ExploreEducationStatistics.Content.Model;
 
@@ -54,6 +55,7 @@ public enum EinTileType
 {
     // NOTE: Update ContentDbContext.ConfigureEinTile if you add a new type!
     FreeTextStatTile,
+    ApiQueryTile,
 }
 
 public class EinFreeTextStatTile : EinTile
@@ -64,3 +66,49 @@ public class EinFreeTextStatTile : EinTile
     public string? LinkUrl { get; set; }
     public string? LinkText { get; set; }
 }
+
+public class EinApiQueryStatTile : EinTile
+{
+    public string Title { get; set; } = string.Empty;
+
+    public Guid DataSetVersionId { get; set; }
+
+    public SemVersion Version { get; set; } = null!;
+
+    public string Query { get; set; } = string.Empty;
+
+    // @MarkFix do we need the whole meta query result, or just the indicator type (i.e. num decimal places etc.)
+    public string MetaQueryResult { get; set; } = string.Empty;
+
+    public string QueryResult { get; set; } = string.Empty;
+
+    public bool PreviousYearComparison { get; set; } = false;
+}
+
+// @MarkFix Public API query for EiN page
+// - query must return multiple results
+// -- results for previous year for comparison
+// -- results for multiple locations if they want a chart displaying with other locations
+// -- presumably some kind of warning if the query doesn't return these? Or if there is more than one result per location?
+// -- we default to returning to NAT for location?
+//
+// - also need meta query to properly display results
+//
+// - can display one stat, a table, or even a chart
+// -- one stat for now, but needs to be prepared for tables/charts
+//
+// - what happens if an api data set gets updated?
+// -- cache the results, update the results once a day or something?
+// -- what if the query fails after the api data sets updates
+// -- what if the latest year results have changed - need the latest and previous years
+// -- or maybe just some kind of alert to update the EiN page?
+//
+// MVP
+// - add dataSetVersionId and query json body for Papi stat
+// - filters by NAT
+// -- gets the results for the latest year
+// -- checks there is one result
+
+// Post MVP
+// - if Tile's new bool PreviousYearComparison == true, checks there is one result for the previous year
+// - auto update EiN page when new api data set version is published (just do it when the new release is published?)
