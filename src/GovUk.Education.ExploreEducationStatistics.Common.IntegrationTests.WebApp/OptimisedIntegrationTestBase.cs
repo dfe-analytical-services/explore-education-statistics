349c20111b5a034059cc9ba90f604b99e2e85534
Duncan Watson <duncan@hiveit.co.uk>
2025-12-12 15:32:17 +0000

    EES-6762 - improved comments around new integration test support code

--- src/GovUk.Education.ExploreEducationStatistics.Common.IntegrationTests.WebApp/OptimisedIntegrationTestBase.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common.IntegrationTests.WebApp/OptimisedIntegrationTestBase.cs
@@ -2,6 +2,12 @@ using Xunit;
 
 namespace GovUk.Education.ExploreEducationStatistics.Common.IntegrationTests.WebApp;
 
+/// <summary>
+/// A convenience base class that is simply used to allow us to reduce the boilerplate of calling
+/// <see cref="OptimisedIntegrationTestFixtureBase{TStartup}.BeforeEachTest"/> before each test in
+/// a test Collection. The alternative would be to make all child test Collection classes implement
+/// <see cref="IAsyncLifetime.InitializeAsync"/> and call "fixture.BeforeEachTest()".
+/// </summary>
 public abstract class OptimisedIntegrationTestBase<TStartup>(OptimisedIntegrationTestFixtureBase<TStartup> fixture)
     : IAsyncLifetime
     where TStartup : class
