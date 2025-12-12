c004ce2a4468b0bcaff18231fb94214da3ade2a8
Duncan Watson <duncan@hiveit.co.uk>
2025-12-12 11:19:06 +0000

    EES-6762 - migrated RequestTimeoutTests to optimised setup

--- src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Tests/RequestTimeoutTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Tests/RequestTimeoutTests.cs
@@ -1,21 +1,44 @@
 using Asp.Versioning;
+using GovUk.Education.ExploreEducationStatistics.Common.IntegrationTests.WebApp;
 using GovUk.Education.ExploreEducationStatistics.Common.Tests.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Options;
-using GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Tests.Fixture;
+using GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Tests.Fixture.Optimised;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace GovUk.Education.ExploreEducationStatistics.Public.Data.Api.Tests;
 
-public class RequestTimeoutTests(TestApplicationFactory testApp) : IntegrationTestFixture(testApp)
+// ReSharper disable once ClassNeverInstantiated.Global
+public class RequestTimeoutTestsFixture : OptimisedPublicApiCollectionFixture
+{
+    protected override void ConfigureServicesAndConfiguration(
+        OptimisedServiceAndConfigModifications serviceModifications
+    )
+    {
+        base.ConfigureServicesAndConfiguration(serviceModifications);
+
+        serviceModifications
+            .AddController(typeof(TestController))
+            .AddInMemoryCollection(
+                new Dictionary<string, string?>
+                {
+                    { $"{RequestTimeoutOptions.Section}:{nameof(RequestTimeoutOptions.TimeoutMilliseconds)}", "100" },
+                }
+            )
+            .ConfigureService<ApiVersioningOptions>(options => options.AssumeDefaultVersionWhenUnspecified = true);
+    }
+}
+
+[CollectionDefinition(nameof(RequestTimeoutTestsFixture))]
+public class RequestTimeoutTestsCollection : ICollectionFixture<RequestTimeoutTestsFixture>;
+
+[Collection(nameof(RequestTimeoutTestsFixture))]
+public class RequestTimeoutTests(RequestTimeoutTestsFixture fixture)
 {
     [Fact]
     public async Task TestGet()
     {
-        var client = BuildApp().CreateClient();
+        var client = fixture.CreateClient();
 
         var response = await client.GetAsync(requestUri: nameof(TestController.TestGet));
 
@@ -25,7 +48,7 @@ public class RequestTimeoutTests(TestApplicationFactory testApp) : IntegrationTe
     [Fact]
     public async Task TestPost()
     {
-        var client = BuildApp().CreateClient();
+        var client = fixture.CreateClient();
 
         var response = await client.PostAsync(requestUri: nameof(TestController.TestPost), null);
 
@@ -35,7 +58,7 @@ public class RequestTimeoutTests(TestApplicationFactory testApp) : IntegrationTe
     [Fact]
     public async Task TestPut()
     {
-        var client = BuildApp().CreateClient();
+        var client = fixture.CreateClient();
 
         var response = await client.PutAsync(requestUri: nameof(TestController.TestPut), null);
 
@@ -45,7 +68,7 @@ public class RequestTimeoutTests(TestApplicationFactory testApp) : IntegrationTe
     [Fact]
     public async Task TestPatch()
     {
-        var client = BuildApp().CreateClient();
+        var client = fixture.CreateClient();
 
         var response = await client.PatchAsync(requestUri: nameof(TestController.TestPatch), null);
 
@@ -55,76 +78,51 @@ public class RequestTimeoutTests(TestApplicationFactory testApp) : IntegrationTe
     [Fact]
     public async Task TestDelete()
     {
-        var client = BuildApp().CreateClient();
+        var client = fixture.CreateClient();
 
         var response = await client.DeleteAsync(requestUri: nameof(TestController.TestDelete));
 
         response.AssertGatewayTimeout();
     }
+}
 
-    [ApiController]
-    private class TestController(IOptions<RequestTimeoutOptions> requestTimeoutOptions) : ControllerBase
+[ApiController]
+public class TestController(IOptions<RequestTimeoutOptions> requestTimeoutOptions) : ControllerBase
+{
+    [HttpGet(nameof(TestGet))]
+    public async Task<ActionResult> TestGet(CancellationToken _)
     {
-        [HttpGet(nameof(TestGet))]
-        public async Task<ActionResult> TestGet(CancellationToken _)
-        {
-            await Task.Delay(_requestTimeout + 100, HttpContext.RequestAborted);
-            return Ok();
-        }
-
-        [HttpPost(nameof(TestPost))]
-        public async Task<ActionResult> TestPost(CancellationToken _)
-        {
-            await Task.Delay(_requestTimeout + 100, HttpContext.RequestAborted);
-            return Ok();
-        }
-
-        [HttpPut(nameof(TestPut))]
-        public async Task<ActionResult> TestPut(CancellationToken _)
-        {
-            await Task.Delay(_requestTimeout + 100, HttpContext.RequestAborted);
-            return Ok();
-        }
-
-        [HttpPatch(nameof(TestPatch))]
-        public async Task<ActionResult> TestPatch(CancellationToken _)
-        {
-            await Task.Delay(_requestTimeout + 100, HttpContext.RequestAborted);
-            return Ok();
-        }
-
-        [HttpDelete(nameof(TestDelete))]
-        public async Task<ActionResult> TestDelete(CancellationToken _)
-        {
-            await Task.Delay(_requestTimeout + 100, HttpContext.RequestAborted);
-            return NoContent();
-        }
-
-        private readonly int _requestTimeout = requestTimeoutOptions.Value.TimeoutMilliseconds;
+        await Task.Delay(_requestTimeout + 100, HttpContext.RequestAborted);
+        return Ok();
     }
 
-    private WebApplicationFactory<Startup> BuildApp()
+    [HttpPost(nameof(TestPost))]
+    public async Task<ActionResult> TestPost(CancellationToken _)
     {
-        return TestApp
-            .WithWebHostBuilder(builder =>
-            {
-                builder.WithAdditionalControllers(typeof(TestController));
-                builder.ConfigureAppConfiguration(
-                    (_, config) =>
-                        config.AddInMemoryCollection(
-                            new Dictionary<string, string?>
-                            {
-                                {
-                                    $"{RequestTimeoutOptions.Section}:{nameof(RequestTimeoutOptions.TimeoutMilliseconds)}",
-                                    "100"
-                                },
-                            }
-                        )
-                );
-            })
-            .ConfigureServices(s =>
-            {
-                s.Configure<ApiVersioningOptions>(options => options.AssumeDefaultVersionWhenUnspecified = true);
-            });
+        await Task.Delay(_requestTimeout + 100, HttpContext.RequestAborted);
+        return Ok();
     }
+
+    [HttpPut(nameof(TestPut))]
+    public async Task<ActionResult> TestPut(CancellationToken _)
+    {
+        await Task.Delay(_requestTimeout + 100, HttpContext.RequestAborted);
+        return Ok();
+    }
+
+    [HttpPatch(nameof(TestPatch))]
+    public async Task<ActionResult> TestPatch(CancellationToken _)
+    {
+        await Task.Delay(_requestTimeout + 100, HttpContext.RequestAborted);
+        return Ok();
+    }
+
+    [HttpDelete(nameof(TestDelete))]
+    public async Task<ActionResult> TestDelete(CancellationToken _)
+    {
+        await Task.Delay(_requestTimeout + 100, HttpContext.RequestAborted);
+        return NoContent();
+    }
+
+    private readonly int _requestTimeout = requestTimeoutOptions.Value.TimeoutMilliseconds;
 }
