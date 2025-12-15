5ef7e31fbe26dd9242919626deb401ec6f08e128
jack-hive <148866614+jack-hive@users.noreply.github.com>
2025-12-15 15:51:10 +0000

    EES-6511 Adding missing `using` introduced from rebase

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/ReleaseInviteService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/ReleaseInviteService.cs
@@ -2,6 +2,7 @@
 using GovUk.Education.ExploreEducationStatistics.Admin.Services.Enums;
 using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
 using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces.Security;
+using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Common.Model;
 using GovUk.Education.ExploreEducationStatistics.Common.Services.Interfaces.Security;
 using GovUk.Education.ExploreEducationStatistics.Common.Utils;
