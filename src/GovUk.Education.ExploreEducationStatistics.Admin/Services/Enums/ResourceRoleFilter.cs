cafc75873f56b8f7e1662c73b1ba50d375369b4b
jack-hive <148866614+jack-hive@users.noreply.github.com>
2025-12-19 13:53:02 +0000

    EES-6511 - Adding some XML documentation comments for `ResourceRoleFilter.cs`

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Enums/ResourceRoleFilter.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Enums/ResourceRoleFilter.cs
@@ -1,9 +1,29 @@
-﻿namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Enums;
+﻿using GovUk.Education.ExploreEducationStatistics.Content.Model;
 
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Enums;
+
+/// <summary>
+/// Represents the possible statuses for a resource role (<see cref="UserPublicationRole"/>/<see cref="UserReleaseRole"/>).
+/// </summary>
 public enum ResourceRoleFilter
 {
+    /// <summary>
+    /// Only include roles associated with ACTIVE Users.
+    /// </summary>
     ActiveOnly,
+
+    /// <summary>
+    /// Only include roles associated with Users with PENDING invites
+    /// </summary>
     PendingOnly,
+
+    /// <summary>
+    /// Include all roles EXCEPT those associated with Users with EXPIRED invites.
+    /// </summary>
     AllButExpired,
+
+    /// <summary>
+    /// Include all roles, regardless of status.
+    /// </summary>
     All,
 }
