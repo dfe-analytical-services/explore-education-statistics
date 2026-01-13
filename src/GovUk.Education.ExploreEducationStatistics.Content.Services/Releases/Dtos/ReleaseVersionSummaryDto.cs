857d31818514d7bda0ba4e4b3b895069b2982e09
Ben Outram <ben@hiveit.co.uk>
2026-01-13 10:20:29 +0000

    EES-6823 Update ReleaseVersionSummaryDto to set last updated date from latest release update

--- src/GovUk.Education.ExploreEducationStatistics.Content.Services/Releases/Dtos/ReleaseVersionSummaryDto.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Services/Releases/Dtos/ReleaseVersionSummaryDto.cs
@@ -39,6 +39,7 @@ public record ReleaseVersionSummaryDto
     public static ReleaseVersionSummaryDto FromReleaseVersion(
         ReleaseVersion releaseVersion,
         bool isLatestRelease,
+        DateTimeOffset lastUpdated,
         PublishingOrganisationDto[] publishingOrganisations,
         int updateCount
     ) =>
@@ -48,7 +49,7 @@ public record ReleaseVersionSummaryDto
             ReleaseId = releaseVersion.ReleaseId,
             IsLatestRelease = isLatestRelease,
             Label = releaseVersion.Release.Label,
-            LastUpdated = releaseVersion.Published ?? throw new ArgumentException("ReleaseVersion must be published"),
+            LastUpdated = lastUpdated,
             // TODO EES-6414 'Published' should be the published display date
             Published = releaseVersion.Published.Value,
             PublishingOrganisations = publishingOrganisations,
