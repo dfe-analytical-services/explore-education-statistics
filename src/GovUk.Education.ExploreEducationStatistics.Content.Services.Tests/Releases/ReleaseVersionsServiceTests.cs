0362941f362c9b9a9ab662db7a0f94cc83087f19
Ben Outram <ben@hiveit.co.uk>
2026-01-28 10:59:39 +0000

    EES-6833 Update ReleaseVersionSummaryDto to set last updated date from the actual published date.

--- src/GovUk.Education.ExploreEducationStatistics.Content.Services.Tests/Releases/ReleaseVersionsServiceTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Services.Tests/Releases/ReleaseVersionsServiceTests.cs
@@ -199,64 +199,8 @@ public abstract class ReleaseVersionsServiceTests
                 // Assert
                 var result = outcome.AssertRight();
                 Assert.Equal(release.Versions[2].Id, result.Id);
-            }
-        }
-
-        [Fact]
-        public async Task WhenReleaseHasMultiplePublishedVersions_ReturnsLastUpdatedFromLatestReleaseUpdate()
-        {
-            // Arrange
-            Publication publication = _dataFixture
-                .DefaultPublication()
-                .WithReleases(_ => [_dataFixture.DefaultRelease(publishedVersions: 3)]);
-            var release = publication.Releases[0];
-
-            // Ensure the generated release versions have different published dates
-            Assert.True(
-                release.Versions[0].Published < release.Versions[1].Published,
-                "Version 0 should be published before version 1"
-            );
-
-            Assert.True(
-                release.Versions[1].Published < release.Versions[2].Published,
-                "Version 1 should be published before version 2"
-            );
-
-            release.Versions[1].Updates = _dataFixture
-                .DefaultUpdate()
-                .WithReleaseVersionId(release.Versions[1].Id)
-                .GenerateList(1);
-
-            release.Versions[2].Updates = _dataFixture
-                .DefaultUpdate()
-                .WithReleaseVersionId(release.Versions[2].Id)
-                .ForIndex(0, s => s.SetOn(new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Local)))
-                .ForIndex(1, s => s.SetOn(new DateTime(2026, 6, 1, 0, 0, 0, DateTimeKind.Local)))
-                .GenerateList(2);
-
-            var contextId = Guid.NewGuid().ToString();
-            await using (var context = InMemoryContentDbContext(contextId))
-            {
-                context.Publications.Add(publication);
-                await context.SaveChangesAsync();
-            }
-
-            await using (var context = InMemoryContentDbContext(contextId))
-            {
-                var sut = BuildService(context);
-
-                // Act
-                var outcome = await sut.GetReleaseVersionSummary(
-                    publicationSlug: publication.Slug,
-                    releaseSlug: release.Slug
-                );
-
-                // Assert
-                var result = outcome.AssertRight();
-
-                // Expect last updated to be mapped from the latest release update on the latest published release version
-                Assert.Equal(new DateTimeOffset(release.Versions[2].Updates[1].On), result.LastUpdated);
-                Assert.Equal(TimeSpan.Zero, result.LastUpdated.Offset);
+                Assert.Equal(release.Versions[2].Published, result.LastUpdated);
+                Assert.Equal(release.Versions[2].PublishedDisplayDate, result.Published);
             }
         }
 
