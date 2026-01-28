185b264a25cb185c3305999b7b72ef74101bded7
Ben Outram <ben@hiveit.co.uk>
2026-01-28 14:59:47 +0000

    EES-6885 Remove all changes related to the EES-6830 Release Versions migration.

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/ReleaseVersionsMigration/Dtos/ReleaseVersionsMigrationReportDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.ReleaseVersionsMigration.Dtos;
-
-/// <summary>
-/// TODO EES-6885 Remove after the Release Versions migration is complete.
-/// </summary>
-public record ReleaseVersionsMigrationReportDto
-{
-    public required bool DryRun { get; init; }
-}
