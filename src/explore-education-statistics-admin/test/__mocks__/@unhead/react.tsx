b9a4d73fe598fb6af5c155beb598d9a30cae366b
Guy Smith <guy@hiveit.co.uk>
2026-02-03 09:25:13 +0000

    Downgrade unhead to stable version, add mock

---
+++ src/explore-education-statistics-admin/test/__mocks__/@unhead/react.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const Head = ({ children }: any) => {
+  return <>{children}</>;
+};
