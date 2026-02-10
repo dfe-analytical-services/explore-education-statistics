d4c7ebb621404a0c17063a43d27d9d8f58a027a2
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-02-10 15:42:51 +0000

    EES-6918 - Hotfix - Fixing duplicate publications appearing on the Dashboard (#6723)

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/PublicationRepository.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/PublicationRepository.cs
@@ -1,11 +1,9 @@
 #nullable enable
 using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
-using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Content.Model;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Queries;
 using Microsoft.EntityFrameworkCore;
-using static GovUk.Education.ExploreEducationStatistics.Admin.Services.PublicationService;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Services;
 
@@ -22,75 +20,55 @@ public class PublicationRepository(
 
     public async Task<List<Publication>> ListPublicationsForUser(Guid userId, Guid? themeId = null)
     {
-        var publicationsGrantedByPublicationRoleQueryable = userPublicationRoleRepository
+        var authorizedPublicationIds = await GetAuthorizedPublicationIds(userId, themeId);
+
+        if (authorizedPublicationIds.Count == 0)
+        {
+            return [];
+        }
+
+        return await context
+            .Publications.AsNoTracking()
+            .Where(p => authorizedPublicationIds.Contains(p.Id))
+            .Include(p => p.Theme)
+            .ToListAsync();
+    }
+
+    private async Task<HashSet<Guid>> GetAuthorizedPublicationIds(Guid userId, Guid? themeId = null)
+    {
+        var publicationIdsFromPublicationRolesQueryable = userPublicationRoleRepository
             .Query()
-            .AsNoTracking()
             .WhereForUser(userId)
             .WhereRolesIn([PublicationRole.Owner, PublicationRole.Allower]);
 
-        if (themeId.HasValue)
-        {
-            publicationsGrantedByPublicationRoleQueryable = publicationsGrantedByPublicationRoleQueryable.Where(
-                userPublicationRole => userPublicationRole.Publication.ThemeId == themeId.Value
-            );
-        }
-
-        var publicationsGrantedByPublicationRole = await publicationsGrantedByPublicationRoleQueryable
-            .Select(userPublicationRole => userPublicationRole.Publication)
-            .ToListAsync();
-
-        var publicationIdsGrantedByPublicationRole = publicationsGrantedByPublicationRole
-            .Select(publication => publication.Id)
-            .ToList();
-
-        var releasesGrantedByReleaseRolesQueryable = userReleaseRoleRepository
+        var publicationIdsFromReleaseRolesQueryable = userReleaseRoleRepository
             .Query()
-            .AsNoTracking()
-            .Include(urr => urr.ReleaseVersion)
-                .ThenInclude(rv => rv.Release)
-                    .ThenInclude(r => r.Publication)
             .WhereForUser(userId)
             .WhereRolesNotIn(ReleaseRole.PrereleaseViewer);
 
         if (themeId.HasValue)
         {
-            releasesGrantedByReleaseRolesQueryable = releasesGrantedByReleaseRolesQueryable.Where(userReleaseRole =>
-                userReleaseRole.ReleaseVersion.Release.Publication.ThemeId == themeId.Value
+            publicationIdsFromPublicationRolesQueryable = publicationIdsFromPublicationRolesQueryable.Where(upr =>
+                upr.Publication.ThemeId == themeId.Value
+            );
+
+            publicationIdsFromReleaseRolesQueryable = publicationIdsFromReleaseRolesQueryable.Where(urr =>
+                urr.ReleaseVersion.Release.Publication.ThemeId == themeId.Value
             );
         }
 
-        var releasesGrantedByReleaseRoles = await releasesGrantedByReleaseRolesQueryable
-            .Select(userReleaseRole => userReleaseRole.ReleaseVersion)
+        var publicationIdsFromPublicationRoles = publicationIdsFromPublicationRolesQueryable.Select(upr =>
+            upr.PublicationId
+        );
+
+        var publicationIdsFromReleaseRoles = publicationIdsFromReleaseRolesQueryable.Select(urr =>
+            urr.ReleaseVersion.Release.PublicationId
+        );
+
+        var allDistinctPublicationIds = await publicationIdsFromPublicationRoles
+            .Union(publicationIdsFromReleaseRoles)
             .ToListAsync();
 
-        var publications = new List<Publication>();
-
-        // Add publication view models for the Publications granted directly via Publication roles
-        publications.AddRange(
-            await publicationsGrantedByPublicationRole.SelectAsync(async publication =>
-                await HydratePublication(context.Publications).FirstAsync(p => p.Id == publication.Id)
-            )
-        );
-
-        // Add publication view models for the Publications granted indirectly via Release roles
-        publications.AddRange(
-            await releasesGrantedByReleaseRoles
-                .GroupBy(releaseVersion => releaseVersion.Release.Publication)
-                .Where(publicationWithReleases =>
-                {
-                    // Don't include a publication that's already been included by Publication roles
-                    var publication = publicationWithReleases.Key;
-                    return !publicationIdsGrantedByPublicationRole.Contains(publication.Id);
-                })
-                .SelectAsync(async publicationWithReleases =>
-                {
-                    var publication = publicationWithReleases.Key;
-                    return await HydratePublication(context.Publications)
-                        .AsNoTracking()
-                        .FirstAsync(p => p.Id == publication.Id);
-                })
-        );
-
-        return publications;
+        return [.. allDistinctPublicationIds];
     }
 }
