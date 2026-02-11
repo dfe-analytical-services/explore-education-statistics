9d93e80d1fa58b7c6333b794cc592dc53b873f7c
Ben Outram <ben@hiveit.co.uk>
2026-02-11 12:16:41 +0000

    EES-6916 Add unit test for ReleaseVersionsMigrationReportWarningsDto.HasWarnings to check that it is covering all the individual warning properties.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/ReleaseVersionsMigration/Dtos/ReleaseVersionsMigrationReportWarningsDtoTests.cs
@@ -0,0 +1,84 @@
+﻿#nullable enable
+using GovUk.Education.ExploreEducationStatistics.Admin.Services.ReleaseVersionsMigration.Dtos;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Tests.Services.ReleaseVersionsMigration.Dtos;
+
+/// <summary>
+/// TODO EES-6885 Remove after the Release Versions migration is complete.
+/// </summary>
+public class ReleaseVersionsMigrationReportWarningsDtoTests
+{
+    public static TheoryData<ReleaseVersionsMigrationReportWarningsDto> ReportsWithSingleWarnings
+    {
+        get
+        {
+            var baseReport = new ReleaseVersionsMigrationReportWarningsDto
+            {
+                NoSuccessfulPublishingAttempts = null,
+                MultipleSuccessfulPublishingAttempts = null,
+                UpdatesCountDoesNotMatchVersion = null,
+                ProposedPublishedDateDoesNotMatchLatestUpdateNoteDate = null,
+                ProposedPublishedDateIsNotSimilarToScheduledTriggerDate = null,
+                ScheduledTriggerDateIsNotMidnightUkLocalTime = null,
+            };
+
+            return
+            [
+                baseReport with
+                {
+                    NoSuccessfulPublishingAttempts = true,
+                },
+                baseReport with
+                {
+                    MultipleSuccessfulPublishingAttempts = true,
+                },
+                baseReport with
+                {
+                    UpdatesCountDoesNotMatchVersion = true,
+                },
+                baseReport with
+                {
+                    ProposedPublishedDateDoesNotMatchLatestUpdateNoteDate = true,
+                },
+                baseReport with
+                {
+                    ProposedPublishedDateIsNotSimilarToScheduledTriggerDate = true,
+                },
+                baseReport with
+                {
+                    ScheduledTriggerDateIsNotMidnightUkLocalTime = true,
+                },
+            ];
+        }
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData(false)]
+    public void WhenReportHasNullWarnings_ThenHasWarningsIsFalse(bool? warning)
+    {
+        var report = new ReleaseVersionsMigrationReportWarningsDto
+        {
+            NoSuccessfulPublishingAttempts = warning,
+            MultipleSuccessfulPublishingAttempts = warning,
+            UpdatesCountDoesNotMatchVersion = warning,
+            ProposedPublishedDateDoesNotMatchLatestUpdateNoteDate = warning,
+            ProposedPublishedDateIsNotSimilarToScheduledTriggerDate = warning,
+            ScheduledTriggerDateIsNotMidnightUkLocalTime = warning,
+        };
+
+        Assert.False(report.HasWarnings);
+    }
+
+    [Theory]
+    [MemberData(nameof(ReportsWithSingleWarnings))]
+    public void WhenReportHasAnyWarnings_ThenHasWarningsIsTrue(ReleaseVersionsMigrationReportWarningsDto report)
+    {
+        // The member data provides a set of Warning reports with each one having an individual warning property set to
+        // true, with the remaining warnings all set to null.
+
+        // This test ensures HasWarnings is true when any of the warnings are true,
+        // and that it is covering all the individual warning properties.
+        Assert.True(report.HasWarnings);
+    }
+}
