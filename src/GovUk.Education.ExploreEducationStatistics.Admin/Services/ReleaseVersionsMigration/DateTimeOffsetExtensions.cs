d095b7acd6632a782074cd5843a232486e9aba6d
Ben Outram <ben@hiveit.co.uk>
2026-02-11 18:45:17 +0000

    EES-6916 Refactor DateTimeOffsetExtensions.AdjustUkLocalMidnightTo0930 to allow using it in unit tests.

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/ReleaseVersionsMigration/DateTimeOffsetExtensions.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/ReleaseVersionsMigration/DateTimeOffsetExtensions.cs
@@ -8,25 +8,19 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.ReleaseVersi
 /// </summary>
 public static class DateTimeOffsetExtensions
 {
-    public static DateTimeOffset? AdjustUkLocalMidnightTo0930(this DateTimeOffset? dateTimeOffset)
+    public static DateTimeOffset? AdjustUkLocalMidnightTo0930(this DateTimeOffset? dateTimeOffset) =>
+        dateTimeOffset?.AdjustUkLocalMidnightTo0930();
+
+    public static DateTimeOffset AdjustUkLocalMidnightTo0930(this DateTimeOffset dateTimeOffset) =>
+        dateTimeOffset.IsUkLocalMidnight() ? dateTimeOffset.AddHours(9).AddMinutes(30) : dateTimeOffset;
+
+    public static bool IsUkLocalMidnight(this DateTimeOffset? dateTimeOffset) =>
+        dateTimeOffset.HasValue && dateTimeOffset.Value.IsUkLocalMidnight();
+
+    private static bool IsUkLocalMidnight(this DateTimeOffset dateTimeOffset)
     {
-        if (dateTimeOffset == null)
-        {
-            return null;
-        }
-
-        return dateTimeOffset.IsUkLocalMidnight() ? dateTimeOffset.Value.AddHours(9).AddMinutes(30) : dateTimeOffset;
-    }
-
-    public static bool IsUkLocalMidnight(this DateTimeOffset? dateTimeOffset)
-    {
-        if (!dateTimeOffset.HasValue)
-        {
-            return false;
-        }
-
         // Convert the DateTimeOffset to UK local time
-        var ukLocalDateTimeOffset = TimeZoneInfo.ConvertTime(dateTimeOffset.Value, TimeZoneUtils.GetUkTimeZone());
+        var ukLocalDateTimeOffset = TimeZoneInfo.ConvertTime(dateTimeOffset, TimeZoneUtils.GetUkTimeZone());
 
         // Return true if the time element is exactly 00:00:00
         return ukLocalDateTimeOffset.TimeOfDay == TimeSpan.Zero;
