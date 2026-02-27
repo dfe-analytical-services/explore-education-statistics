ad2f6721dbbe18b0fcbf145a2e28e6d25d80ab65
Ben Outram <ben@hiveit.co.uk>
2026-02-27 15:16:35 +0000

    EES-6940 Insert content sections for existing release versions.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20260302153355_Ees6940InsertContentSections.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations;
+
+/// <inheritdoc />
+[ExcludeFromCodeCoverage]
+public partial class Ees6940InsertContentSections : Migration
+{
+    private const string MigrationId = "20260302153355";
+
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        // Insert content sections for existing release versions
+        migrationBuilder.SqlFromFile(
+            MigrationConstants.ContentMigrationsPath,
+            $"{MigrationId}_{nameof(Ees6940InsertContentSections)}.sql"
+        );
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) { }
+}
