9c8e646456817c30e99155b52878ca736b96c938
Duncan Watson <duncan@hiveit.co.uk>
2026-03-10 18:32:29 +0000

    EES-6764 - added indicator mapping progress to standard data replacement screen.

--- src/explore-education-statistics-admin/src/services/dataReplacementService.ts
+++ src/explore-education-statistics-admin/src/services/dataReplacementService.ts
@@ -95,7 +95,8 @@ export interface MappingStatus {
   locationsHaveMajorChange: boolean;
   filtersComplete: boolean;
   filtersHaveMajorChange: boolean;
-  // TODO EES-6764 - add indicators here?
+  indicatorsComplete: boolean;
+  indicatorsHaveMajorChange: boolean;
   isMajorVersionUpdate: boolean;
 }
 
