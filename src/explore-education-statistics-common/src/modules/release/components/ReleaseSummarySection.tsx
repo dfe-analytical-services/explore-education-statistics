6853d88aea26872f018ea27d860a18935f379e47
Guy Smith <guy@hiveit.co.uk>
2026-03-16 15:19:32 +0000

    EES-6942 Split out release notes from last updated
    They are no longer explicitly linked, so split the display of them into
    two list items

--- src/explore-education-statistics-common/src/modules/release/components/ReleaseSummarySection.tsx
+++ src/explore-education-statistics-common/src/modules/release/components/ReleaseSummarySection.tsx
@@ -94,9 +94,16 @@ export default function ReleaseSummarySection({
 
         {(isEditing || lastUpdated) && (
           <SummaryListItem term="Last updated">
-            {lastUpdated && <FormattedDate>{lastUpdated}</FormattedDate>}
-            {renderReleaseNotes}
+            {lastUpdated ? (
+              <FormattedDate>{lastUpdated}</FormattedDate>
+            ) : (
+              <p>TBA</p>
+            )}
           </SummaryListItem>
+        )}
+
+        {renderReleaseNotes && (
+          <SummaryListItem term="Updates">{renderReleaseNotes}</SummaryListItem>
         )}
 
         <SummaryListItem term="Release type">
