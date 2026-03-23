4cfcfeca787527135b1c109d3696ce103779309e
Ben Outram <ben@hiveit.co.uk>
2026-03-23 17:40:15 +0000

    EES-7016: Add GetUkEndOfDayUtc to DateOnlyExtensions

--- src/GovUk.Education.ExploreEducationStatistics.Common.Tests/Extensions/DateOnlyExtensionsTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Common.Tests/Extensions/DateOnlyExtensionsTests.cs
@@ -6,7 +6,7 @@ namespace GovUk.Education.ExploreEducationStatistics.Common.Tests.Extensions;
 // ReSharper disable once ClassNeverInstantiated.Global
 public abstract class DateOnlyExtensionsTests
 {
-    public class GetUkStartOfDayUtc : DateOnlyExtensionsTests
+    public class GetUkStartOfDayUtcTests : DateOnlyExtensionsTests
     {
         [Theory]
         [InlineData("2025-01-01", "2025-01-01T00:00:00 +00:00")] // Day within GMT
@@ -21,6 +21,55 @@ public abstract class DateOnlyExtensionsTests
             var expectedDateTimeOffset = DateTimeOffset.Parse(expectedDateTimeOffsetString);
 
             var actual = dateOnly.GetUkStartOfDayUtc();
+
+            Assert.Equal(TimeSpan.Zero, actual.Offset);
+            Assert.Equal(expectedDateTimeOffset, actual);
+        }
+    }
+
+    public class GetUkEndOfDayUtcTests : DateOnlyExtensionsTests
+    {
+        [Theory]
+        [InlineData("2025-01-01", "2025-01-01T23:59:59.9999999 +00:00")] // Day within GMT
+        [InlineData("2025-06-01", "2025-06-01T22:59:59.9999999 +00:00")] // Day within BST
+        [InlineData("2025-03-29", "2025-03-29T23:59:59.9999999 +00:00")] // Day before BST starts (end of day is within GMT)
+        [InlineData("2025-03-30", "2025-03-30T22:59:59.9999999 +00:00")] // BST starts on this day (end of day is within BST)
+        [InlineData("2025-03-31", "2025-03-31T22:59:59.9999999 +00:00")] // Day after BST starts (end of day is within BST)
+        [InlineData("2025-10-25", "2025-10-25T22:59:59.9999999 +00:00")] // Day before BST ends (end of day is within BST)
+        [InlineData("2025-10-26", "2025-10-26T23:59:59.9999999 +00:00")] // BST ends on this day (end of day is within GMT)
+        [InlineData("2025-10-27", "2025-10-27T23:59:59.9999999 +00:00")] // Day after BST ends (end of day is within GMT)
+        public void WhenIncludeMillisecondsIsTrue_ReturnsTimeAtEndOfUkDayWithMilliseconds(
+            string dateOnlyString,
+            string expectedDateTimeOffsetString
+        )
+        {
+            var dateOnly = DateOnly.Parse(dateOnlyString);
+            var expectedDateTimeOffset = DateTimeOffset.Parse(expectedDateTimeOffsetString);
+
+            var actual = dateOnly.GetUkEndOfDayUtc();
+
+            Assert.Equal(TimeSpan.Zero, actual.Offset);
+            Assert.Equal(expectedDateTimeOffset, actual);
+        }
+
+        [Theory]
+        [InlineData("2025-01-01", "2025-01-01T23:59:59 +00:00")] // Day within GMT
+        [InlineData("2025-06-01", "2025-06-01T22:59:59 +00:00")] // Day within BST
+        [InlineData("2025-03-29", "2025-03-29T23:59:59 +00:00")] // Day before BST starts (end of day is within GMT)
+        [InlineData("2025-03-30", "2025-03-30T22:59:59 +00:00")] // BST starts on this day (end of day is within BST)
+        [InlineData("2025-03-31", "2025-03-31T22:59:59 +00:00")] // Day after BST starts (end of day is within BST)
+        [InlineData("2025-10-25", "2025-10-25T22:59:59 +00:00")] // Day before BST ends (end of day is within BST)
+        [InlineData("2025-10-26", "2025-10-26T23:59:59 +00:00")] // BST ends on this day (end of day is within GMT)
+        [InlineData("2025-10-27", "2025-10-27T23:59:59 +00:00")] // Day after BST ends (end of day is within GMT)
+        public void WhenIncludeMillisecondsIsFalse_ReturnsTimeAtEndOfUkDayWithoutMilliseconds(
+            string dateOnlyString,
+            string expectedDateTimeOffsetString
+        )
+        {
+            var dateOnly = DateOnly.Parse(dateOnlyString);
+            var expectedDateTimeOffset = DateTimeOffset.Parse(expectedDateTimeOffsetString);
+
+            var actual = dateOnly.GetUkEndOfDayUtc(includeFractionalSeconds: false);
 
             Assert.Equal(TimeSpan.Zero, actual.Offset);
             Assert.Equal(expectedDateTimeOffset, actual);
