ee41c2fd866b1b325aa72bd361012fe7a2ffb78f
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-03-30 17:05:57 +0100

    EES-7041 - Refactoring the UserPrereleaseRoleRepository to be purely used for pre-release roles

--- src/GovUk.Education.ExploreEducationStatistics.Content.Model/Queries/UserReleaseRoleQueries.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/Queries/UserReleaseRoleQueries.cs
@@ -1,6 +1,4 @@
-﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
-
-namespace GovUk.Education.ExploreEducationStatistics.Content.Model.Queries;
+﻿namespace GovUk.Education.ExploreEducationStatistics.Content.Model.Queries;
 
 public static class UserReleaseRoleQueries
 {
@@ -16,32 +14,6 @@ public static class UserReleaseRoleQueries
         this IQueryable<UserReleaseRole> query,
         Guid publicationId
     ) => query.Where(urr => urr.ReleaseVersion.Release.PublicationId == publicationId);
-
-    public static IQueryable<UserReleaseRole> WhereRolesIn(
-        this IQueryable<UserReleaseRole> query,
-        params ReleaseRole[] roles
-    )
-    {
-        if (roles.IsNullOrEmpty())
-        {
-            throw new ArgumentException($"{nameof(roles)} should not be empty or NULL.");
-        }
-
-        return query.Where(urr => roles.Contains(urr.Role));
-    }
-
-    public static IQueryable<UserReleaseRole> WhereRolesNotIn(
-        this IQueryable<UserReleaseRole> query,
-        params ReleaseRole[] roles
-    )
-    {
-        if (roles.IsNullOrEmpty())
-        {
-            throw new ArgumentException($"{nameof(roles)} should not be empty or NULL.");
-        }
-
-        return query.Where(urr => !roles.Contains(urr.Role));
-    }
 
     public static IQueryable<UserReleaseRole> WhereEmailNotSent(this IQueryable<UserReleaseRole> query) =>
         query.Where(urr => urr.EmailSent == null);
