68d6d290a03e18d8740f77aebd6829d6760d93f8
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-03-30 17:05:57 +0100

    EES-7041 - Refactoring the UserPrereleaseRoleRepository to be purely used for pre-release roles

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/IUserPrereleaseRoleRepository.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/IUserPrereleaseRoleRepository.cs
@@ -1,6 +1,7 @@
 #nullable enable
 using GovUk.Education.ExploreEducationStatistics.Admin.Services.Enums;
 using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using static GovUk.Education.ExploreEducationStatistics.Admin.Services.UserPrereleaseRoleRepository;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
 
@@ -9,23 +10,21 @@ public interface IUserPrereleaseRoleRepository
     Task<UserReleaseRole> Create(
         Guid userId,
         Guid releaseVersionId,
-        ReleaseRole role,
         Guid createdById,
         DateTime? createdDate = null,
         CancellationToken cancellationToken = default
     );
 
     Task<List<UserReleaseRole>> CreateManyIfNotExists(
-        IReadOnlyList<UserReleaseRole> userReleaseRolesToCreate,
+        IReadOnlyList<UserPrereleaseRoleCreateDto> userPrereleaseRolesToCreate,
         CancellationToken cancellationToken = default
     );
 
-    Task<UserReleaseRole?> GetById(Guid userReleaseRoleId, CancellationToken cancellationToken = default);
+    Task<UserReleaseRole?> GetById(Guid userPrereleaseRoleId, CancellationToken cancellationToken = default);
 
     Task<UserReleaseRole?> GetByCompositeKey(
         Guid userId,
         Guid releaseVersionId,
-        ReleaseRole role,
         CancellationToken cancellationToken = default
     );
 
@@ -37,45 +36,30 @@ public interface IUserPrereleaseRoleRepository
     /// <param name="resourceRoleFilter">Filter resource roles by their status (see <see cref="ResourceRoleFilter"/>).</param>
     IQueryable<UserReleaseRole> Query(ResourceRoleFilter resourceRoleFilter = ResourceRoleFilter.ActiveOnly);
 
-    Task<bool> RemoveById(Guid userReleaseRoleId, CancellationToken cancellationToken = default);
+    Task<bool> RemoveById(Guid userPrereleaseRoleId, CancellationToken cancellationToken = default);
 
-    Task<bool> RemoveByCompositeKey(
-        Guid userId,
-        Guid releaseVersionId,
-        ReleaseRole role,
-        CancellationToken cancellationToken = default
-    );
+    Task<bool> RemoveByCompositeKey(Guid userId, Guid releaseVersionId, CancellationToken cancellationToken = default);
 
-    Task RemoveMany(HashSet<Guid> userReleaseRoleIds, CancellationToken cancellationToken = default);
+    Task RemoveMany(HashSet<UserReleaseRole> userPrereleaseRoleIds, CancellationToken cancellationToken = default);
 
     Task RemoveForUser(Guid userId, CancellationToken cancellationToken = default);
 
-    Task<bool> UserHasRoleOnReleaseVersion(
+    Task<bool> UserHasPrereleaseRole(
         Guid userId,
         Guid releaseVersionId,
-        ReleaseRole role,
         ResourceRoleFilter resourceRoleFilter = ResourceRoleFilter.ActiveOnly,
         CancellationToken cancellationToken = default
     );
 
-    Task<bool> UserHasAnyRoleOnReleaseVersion(
-        Guid userId,
-        Guid releaseVersionId,
-        ResourceRoleFilter resourceRoleFilter = ResourceRoleFilter.ActiveOnly,
-        CancellationToken cancellationToken = default,
-        params ReleaseRole[] rolesToInclude
-    );
-
-    Task<bool> UserHasAnyRoleOnPublication(
+    Task<bool> UserHasPrereleaseRoleOnPublication(
         Guid userId,
         Guid publicationId,
         ResourceRoleFilter resourceRoleFilter = ResourceRoleFilter.ActiveOnly,
-        CancellationToken cancellationToken = default,
-        params ReleaseRole[] rolesToInclude
+        CancellationToken cancellationToken = default
     );
 
     Task MarkEmailAsSent(
-        Guid userReleaseRoleId,
+        Guid userPrereleaseRoleId,
         DateTimeOffset? dateSent = null,
         CancellationToken cancellationToken = default
     );
