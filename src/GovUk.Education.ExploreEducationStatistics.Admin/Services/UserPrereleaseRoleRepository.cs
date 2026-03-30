c3be5699bccd0e69f8d2a87144c17f4f481ad49c
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-03-30 17:05:57 +0100

    EES-7041 - Refactoring the UserPrereleaseRoleRepository to be purely used for pre-release roles

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/UserPrereleaseRoleRepository.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/UserPrereleaseRoleRepository.cs
@@ -1,8 +1,6 @@
 #nullable enable
 using GovUk.Education.ExploreEducationStatistics.Admin.Services.Enums;
 using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
-using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
-using GovUk.Education.ExploreEducationStatistics.Common.Utils;
 using GovUk.Education.ExploreEducationStatistics.Content.Model;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Database;
 using GovUk.Education.ExploreEducationStatistics.Content.Model.Queries;
@@ -10,20 +8,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GovUk.Education.ExploreEducationStatistics.Admin.Services;
 
-public class UserPrereleaseRoleRepository(
-    ContentDbContext contentDbContext,
-    INewPermissionsSystemHelper newPermissionsSystemHelper,
-    IUserPublicationRoleRepository userPublicationRoleRepository,
-    UserReleaseRoleQueryRepository userReleaseRoleQueryRepository,
-    IUserRepository userRepository
-) : IUserPrereleaseRoleRepository
+public class UserPrereleaseRoleRepository(ContentDbContext contentDbContext) : IUserPrereleaseRoleRepository
 {
-    // This method will remain but be amended slightly in EES-6196, when we no longer have to cater for
-    // the old roles.
     public async Task<UserReleaseRole> Create(
         Guid userId,
         Guid releaseVersionId,
-        ReleaseRole role,
         Guid createdById,
         DateTime? createdDate = null,
         CancellationToken cancellationToken = default
@@ -31,88 +20,33 @@ public class UserPrereleaseRoleRepository(
     {
         createdDate ??= createdDate?.ToUniversalTime() ?? DateTime.UtcNow;
 
-        var publicationId = await GetPublicationId(releaseVersionId, cancellationToken);
-
-        var allUserPublicationRolesForUserAndPublicationByRole = await userPublicationRoleRepository
-            .Query(ResourceRoleFilter.All, includeNewPermissionsSystemRoles: true)
-            .WhereForUser(userId)
-            .WhereForPublication(publicationId)
-            .ToDictionaryAsync(upr => upr.Role, cancellationToken);
-
-        var allPublicationRolesForUserAndPublication =
-            allUserPublicationRolesForUserAndPublicationByRole.Keys.ToHashSet();
-
-        var allReleaseRolesForUserAndPublication = (
-            await Query(ResourceRoleFilter.All)
-                .WhereForUser(userId)
-                .WhereForPublication(publicationId)
-                .Select(urr => urr.Role)
-                .ToListAsync(cancellationToken)
-        ).ToHashSet();
-
-        var (newSystemPublicationRoleToRemove, newSystemPublicationRoleToCreate) =
-            newPermissionsSystemHelper.DetermineNewPermissionsSystemChangesForRoleCreation(
-                existingSetOfPublicationRolesForPublication: allPublicationRolesForUserAndPublication,
-                existingSetOfReleaseRolesForPublication: allReleaseRolesForUserAndPublication,
-                releaseRoleToCreate: role
-            );
-
-        if (newSystemPublicationRoleToRemove.HasValue)
-        {
-            var userPublicationRole = allUserPublicationRolesForUserAndPublicationByRole[
-                newSystemPublicationRoleToRemove.Value
-            ];
-
-            await userPublicationRoleRepository.RemoveRole(userPublicationRole!, cancellationToken);
-        }
-
-        if (newSystemPublicationRoleToCreate.HasValue)
-        {
-            await userPublicationRoleRepository.CreateRole(
-                userId: userId,
-                publicationId: publicationId,
-                role: newSystemPublicationRoleToCreate.Value,
-                createdById: createdById,
-                createdDate: createdDate!.Value,
-                cancellationToken: cancellationToken
-            );
-        }
-
-        var newUserReleaseRole = new UserReleaseRole
+        var newUserPrereleaseRole = new UserReleaseRole
         {
             UserId = userId,
             ReleaseVersionId = releaseVersionId,
-            Role = role,
+            Role = ReleaseRole.PrereleaseViewer,
             Created = createdDate!.Value,
             CreatedById = createdById,
         };
 
-        contentDbContext.UserReleaseRoles.Add(newUserReleaseRole);
+        contentDbContext.UserReleaseRoles.Add(newUserPrereleaseRole);
         await contentDbContext.SaveChangesAsync(cancellationToken);
 
-        return newUserReleaseRole;
+        return newUserPrereleaseRole;
     }
 
     public async Task<List<UserReleaseRole>> CreateManyIfNotExists(
-        // I have purposefully not created a DTO for this method, to match the equivalent method in IUserPublicationRoleRepository.
-        // Reason being, eventually the only release role which will exist is the pre-release role, which does not map to any NEW permissions system publication role.
-        // At that point, this method will no longer need to call the Create method (which contains the logic to determine which OLD/NEW roles to create),
-        // and will simply be adding UserReleaseRole entities to the DbContext directly.
-        // I suspect this will be the case in EES-6196.
-        // By leaving this as accepting a list of UserReleaseRole entities for now, we can avoid having to create yet another DTO which will only be used temporarily,
-        // and avoid having to refactor the code/tests accordingly.
-        IReadOnlyList<UserReleaseRole> userReleaseRolesToCreate,
+        IReadOnlyList<UserPrereleaseRoleCreateDto> userPrereleaseRolesToCreate,
         CancellationToken cancellationToken = default
     )
     {
-        return await userReleaseRolesToCreate
+        return await userPrereleaseRolesToCreate
             .ToAsyncEnumerable()
             .Where(
                 async (urr, ct) =>
-                    !await UserHasRoleOnReleaseVersion(
+                    !await UserHasPrereleaseRole(
                         userId: urr.UserId,
                         releaseVersionId: urr.ReleaseVersionId,
-                        role: urr.Role,
                         resourceRoleFilter: ResourceRoleFilter.All,
                         cancellationToken: ct
                     )
@@ -122,110 +56,108 @@ public class UserPrereleaseRoleRepository(
                     await Create(
                         userId: urr.UserId,
                         releaseVersionId: urr.ReleaseVersionId,
-                        role: urr.Role,
-                        // Need to check if all database values are non-null now. Maybe I can migrate this field to be non-nullable?
-                        createdById: urr.CreatedById!.Value,
-                        createdDate: urr.Created,
+                        createdById: urr.CreatedById,
+                        createdDate: urr.CreatedDate,
                         cancellationToken: ct
                     )
             )
             .ToListAsync(cancellationToken);
     }
 
-    public async Task<UserReleaseRole?> GetById(Guid userReleaseRoleId, CancellationToken cancellationToken = default)
+    public async Task<UserReleaseRole?> GetById(
+        Guid userPrereleaseRoleId,
+        CancellationToken cancellationToken = default
+    )
     {
         return await Query(ResourceRoleFilter.All)
-            .SingleOrDefaultAsync(urr => urr.Id == userReleaseRoleId, cancellationToken);
+            .SingleOrDefaultAsync(urr => urr.Id == userPrereleaseRoleId, cancellationToken);
     }
 
     public async Task<UserReleaseRole?> GetByCompositeKey(
         Guid userId,
         Guid releaseVersionId,
-        ReleaseRole role,
         CancellationToken cancellationToken = default
     )
     {
         return await Query(ResourceRoleFilter.All)
             .WhereForUser(userId)
             .WhereForReleaseVersion(releaseVersionId)
-            .WhereRolesIn(role)
             .SingleOrDefaultAsync(cancellationToken);
     }
 
     public IQueryable<UserReleaseRole> Query(ResourceRoleFilter resourceRoleFilter = ResourceRoleFilter.ActiveOnly) =>
-        userReleaseRoleQueryRepository.Query(resourceRoleFilter);
+        resourceRoleFilter switch
+        {
+            ResourceRoleFilter.ActiveOnly => contentDbContext.UserReleaseRoles.WhereUserIsActive(),
+            ResourceRoleFilter.PendingOnly => contentDbContext.UserReleaseRoles.WhereUserHasPendingInvite(),
+            ResourceRoleFilter.AllButExpired => contentDbContext.UserReleaseRoles.WhereUserIsActiveOrHasPendingInvite(),
+            ResourceRoleFilter.All => contentDbContext.UserReleaseRoles,
+            _ => throw new ArgumentOutOfRangeException(nameof(resourceRoleFilter), resourceRoleFilter, null),
+        };
 
-    // This method will remain but be amended slightly in EES-6196, when we no longer have to cater
-    // for the old roles.
-    public async Task<bool> RemoveById(Guid userReleaseRoleId, CancellationToken cancellationToken = default)
+    public async Task<bool> RemoveById(Guid userPrereleaseRoleId, CancellationToken cancellationToken = default)
     {
-        var userReleaseRole = await GetById(userReleaseRoleId, cancellationToken);
+        var userPrereleaseRole = await GetById(userPrereleaseRoleId, cancellationToken);
 
-        if (userReleaseRole is null)
+        if (userPrereleaseRole is null)
         {
             return false;
         }
 
-        await Remove(userReleaseRole, cancellationToken);
+        await Remove(userPrereleaseRole, cancellationToken);
 
         return true;
     }
 
-    // This method will remain but be amended slightly in EES-6196, when we no longer have to cater
-    // for the old roles.
     public async Task<bool> RemoveByCompositeKey(
         Guid userId,
         Guid releaseVersionId,
-        ReleaseRole role,
         CancellationToken cancellationToken = default
     )
     {
-        var userReleaseRole = await GetByCompositeKey(
+        var userPrereleaseRole = await GetByCompositeKey(
             userId: userId,
             releaseVersionId: releaseVersionId,
-            role: role,
             cancellationToken: cancellationToken
         );
 
-        if (userReleaseRole is null)
+        if (userPrereleaseRole is null)
         {
             return false;
         }
 
-        await Remove(userReleaseRole, cancellationToken);
+        await Remove(userPrereleaseRole, cancellationToken);
 
         return true;
     }
 
-    // This will be reverted back to removing the role entities from the DbContext directly in EES-6196, when
-    // we no longer have to cater for the NEW publication roles within this repository (when only pre-release roles will exist).
-    public async Task RemoveMany(HashSet<Guid> userReleaseRoleIds, CancellationToken cancellationToken = default)
+    public async Task RemoveMany(
+        HashSet<UserReleaseRole> userPrereleaseRoles,
+        CancellationToken cancellationToken = default
+    )
     {
-        if (!userReleaseRoleIds.Any())
+        if (!userPrereleaseRoles.Any())
         {
             return;
         }
 
-        foreach (var userReleaseRoleId in userReleaseRoleIds)
-        {
-            await RemoveById(userReleaseRoleId, cancellationToken);
-        }
+        contentDbContext.UserReleaseRoles.RemoveRange(userPrereleaseRoles);
+        await contentDbContext.SaveChangesAsync(cancellationToken);
     }
 
     public async Task RemoveForUser(Guid userId, CancellationToken cancellationToken = default)
     {
-        var userReleaseRoles = await Query(ResourceRoleFilter.All)
+        var userPrereleaseRoles = await Query(ResourceRoleFilter.All)
             .Where(urr => urr.UserId == userId)
             .ToListAsync(cancellationToken);
 
-        contentDbContext.UserReleaseRoles.RemoveRange(userReleaseRoles);
+        contentDbContext.UserReleaseRoles.RemoveRange(userPrereleaseRoles);
         await contentDbContext.SaveChangesAsync(cancellationToken);
     }
 
-    public async Task<bool> UserHasRoleOnReleaseVersion(
+    public async Task<bool> UserHasPrereleaseRole(
         Guid userId,
         Guid releaseVersionId,
-        ReleaseRole role,
         ResourceRoleFilter resourceRoleFilter = ResourceRoleFilter.ActiveOnly,
         CancellationToken cancellationToken = default
     )
@@ -233,118 +165,53 @@ public class UserPrereleaseRoleRepository(
         return await Query(resourceRoleFilter)
             .WhereForUser(userId)
             .WhereForReleaseVersion(releaseVersionId)
-            .WhereRolesIn(role)
             .AnyAsync(cancellationToken);
     }
 
-    public async Task<bool> UserHasAnyRoleOnReleaseVersion(
-        Guid userId,
-        Guid releaseVersionId,
-        ResourceRoleFilter resourceRoleFilter = ResourceRoleFilter.ActiveOnly,
-        CancellationToken cancellationToken = default,
-        params ReleaseRole[] rolesToInclude
-    )
-    {
-        var rolesToCheck = rolesToInclude.IsNullOrEmpty() ? EnumUtil.GetEnumsArray<ReleaseRole>() : rolesToInclude;
-
-        return await Query(resourceRoleFilter)
-            .WhereForUser(userId)
-            .WhereForReleaseVersion(releaseVersionId)
-            .WhereRolesIn(rolesToCheck)
-            .AnyAsync(cancellationToken);
-    }
-
-    public async Task<bool> UserHasAnyRoleOnPublication(
+    public async Task<bool> UserHasPrereleaseRoleOnPublication(
         Guid userId,
         Guid publicationId,
         ResourceRoleFilter resourceRoleFilter = ResourceRoleFilter.ActiveOnly,
-        CancellationToken cancellationToken = default,
-        params ReleaseRole[] rolesToInclude
+        CancellationToken cancellationToken = default
     )
     {
-        var rolesToCheck = rolesToInclude.IsNullOrEmpty() ? EnumUtil.GetEnumsArray<ReleaseRole>() : rolesToInclude;
-
         return await Query(resourceRoleFilter)
             .WhereForUser(userId)
             .WhereForPublication(publicationId)
-            .WhereRolesIn(rolesToCheck)
             .AnyAsync(cancellationToken);
     }
 
     public async Task MarkEmailAsSent(
-        Guid userReleaseRoleId,
+        Guid userPrereleaseRoleId,
         DateTimeOffset? dateSent = null,
         CancellationToken cancellationToken = default
     )
     {
-        var userReleaseRole = await GetById(userReleaseRoleId: userReleaseRoleId, cancellationToken: cancellationToken);
+        var userPrereleaseRole = await GetById(
+            userPrereleaseRoleId: userPrereleaseRoleId,
+            cancellationToken: cancellationToken
+        );
 
-        if (userReleaseRole is null)
+        if (userPrereleaseRole is null)
         {
-            throw new InvalidOperationException($"No User Release Role found with ID {userReleaseRoleId}.");
+            throw new InvalidOperationException($"No User Release Role found with ID {userPrereleaseRoleId}.");
         }
 
-        userReleaseRole.EmailSent = dateSent ?? DateTimeOffset.UtcNow;
+        userPrereleaseRole.EmailSent = dateSent ?? DateTimeOffset.UtcNow;
 
         await contentDbContext.SaveChangesAsync(cancellationToken);
     }
 
-    private async Task Remove(UserReleaseRole userReleaseRole, CancellationToken cancellationToken = default)
+    private async Task Remove(UserReleaseRole userPrereleaseRole, CancellationToken cancellationToken = default)
     {
-        var publicationId = await GetPublicationId(userReleaseRole.ReleaseVersionId, cancellationToken);
-
-        var allReleaseRolesForUserAndPublication = await Query(ResourceRoleFilter.All)
-            .WhereForUser(userReleaseRole.UserId)
-            .WhereForPublication(publicationId)
-            .Select(urr => urr.Role)
-            .ToListAsync(cancellationToken);
-
-        var allUserPublicationRolesForUserAndPublicationByRole = await userPublicationRoleRepository
-            .Query(ResourceRoleFilter.All, includeNewPermissionsSystemRoles: true)
-            .WhereForUser(userReleaseRole.UserId)
-            .WhereForPublication(publicationId)
-            .ToDictionaryAsync(upr => upr.Role, cancellationToken);
-
-        var allPublicationRolesForUserAndPublication =
-            allUserPublicationRolesForUserAndPublicationByRole.Keys.ToHashSet();
-
-        var (newSystemPublicationRoleToRemove, newSystemPublicationRoleToCreate) =
-            newPermissionsSystemHelper.DetermineNewPermissionsSystemChangesForRoleRemoval(
-                existingSetOfPublicationRolesForPublication: allPublicationRolesForUserAndPublication,
-                allExistingReleaseRolesForPublication: allReleaseRolesForUserAndPublication,
-                releaseRoleToRemove: userReleaseRole.Role
-            );
-
-        contentDbContext.UserReleaseRoles.Remove(userReleaseRole);
+        contentDbContext.UserReleaseRoles.Remove(userPrereleaseRole);
         await contentDbContext.SaveChangesAsync(cancellationToken);
-
-        if (newSystemPublicationRoleToRemove.HasValue)
-        {
-            var newSystemUserPublicationRoleToRemove = allUserPublicationRolesForUserAndPublicationByRole[
-                newSystemPublicationRoleToRemove.Value
-            ];
-
-            await userPublicationRoleRepository.RemoveRole(newSystemUserPublicationRoleToRemove, cancellationToken);
-        }
-
-        if (newSystemPublicationRoleToCreate.HasValue)
-        {
-            var deletedUserPlaceholder = await userRepository.FindDeletedUserPlaceholder(cancellationToken);
-
-            await userPublicationRoleRepository.CreateRole(
-                userId: userReleaseRole.UserId,
-                publicationId: publicationId,
-                role: newSystemPublicationRoleToCreate.Value,
-                createdById: deletedUserPlaceholder.Id,
-                createdDate: DateTime.UtcNow,
-                cancellationToken: cancellationToken
-            );
-        }
     }
 
-    private async Task<Guid> GetPublicationId(Guid releaseVersionId, CancellationToken cancellationToken) =>
-        await contentDbContext
-            .ReleaseVersions.Where(r => r.Id == releaseVersionId)
-            .Select(r => r.Release.PublicationId)
-            .SingleAsync(cancellationToken);
+    public record UserPrereleaseRoleCreateDto(
+        Guid UserId,
+        Guid ReleaseVersionId,
+        Guid CreatedById,
+        DateTime? CreatedDate = null
+    );
 }
