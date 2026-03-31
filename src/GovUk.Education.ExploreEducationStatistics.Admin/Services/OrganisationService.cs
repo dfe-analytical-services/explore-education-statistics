184445b21b6a70ad3441d0a2e0fc3f0049d12044
mmoalin <mmoalin@outlook.com>
2026-03-31 10:32:47 +0100

    EES-6991 - Temporarily hide specific organisations ("Ofqual", "Ofsted") from results in `GetAllOrganisations`. (#6872)

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Services/OrganisationService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/OrganisationService.cs
@@ -8,6 +8,11 @@ namespace GovUk.Education.ExploreEducationStatistics.Admin.Services;
 
 public class OrganisationService(ContentDbContext contentDbContext) : IOrganisationService
 {
+    private readonly List<string> _hiddenOrganisations = ["Ofqual", "Ofsted"]; // TODO: Temporarily hiding these orgs until EES-6991 is done
+
     public async Task<Organisation[]> GetAllOrganisations(CancellationToken cancellationToken = default) =>
-        await contentDbContext.Organisations.OrderBy(o => o.Title).ToArrayAsync(cancellationToken);
+        await contentDbContext
+            .Organisations.Where(o => !_hiddenOrganisations.Contains(o.Title))
+            .OrderBy(o => o.Title)
+            .ToArrayAsync(cancellationToken);
 }
