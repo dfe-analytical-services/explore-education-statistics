44c6286718d8eb28cf9b65b71ba4a773e17a966b
Ben Outram <ben@hiveit.co.uk>
2026-04-15 12:17:44 +0100

    EES-7022 Content API tidy up

--- src/GovUk.Education.ExploreEducationStatistics.Content.Services/Cache/ReleaseCacheService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Services/Cache/ReleaseCacheService.cs
@@ -1,49 +1,14 @@
 using GovUk.Education.ExploreEducationStatistics.Common;
-using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Common.Model;
 using GovUk.Education.ExploreEducationStatistics.Common.Services;
 using GovUk.Education.ExploreEducationStatistics.Common.Services.Interfaces;
-using GovUk.Education.ExploreEducationStatistics.Content.Services.Interfaces;
 using GovUk.Education.ExploreEducationStatistics.Content.Services.Interfaces.Cache;
-using GovUk.Education.ExploreEducationStatistics.Content.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace GovUk.Education.ExploreEducationStatistics.Content.Services.Cache;
 
-public class ReleaseCacheService(
-    IReleaseService releaseService,
-    IPublicBlobStorageService publicBlobStorageService,
-    IPublicBlobCacheService publicBlobCacheService,
-    ILogger<ReleaseCacheService> logger
-) : IReleaseCacheService
+public class ReleaseCacheService(IPublicBlobStorageService publicBlobStorageService) : IReleaseCacheService
 {
-    public Task<Either<ActionResult, ReleaseCacheViewModel>> GetRelease(
-        string publicationSlug,
-        string? releaseSlug = null
-    )
-    {
-        return publicBlobCacheService.GetOrCreateAsync(
-            cacheKey: new ReleaseCacheKey(publicationSlug: publicationSlug, releaseSlug: releaseSlug),
-            createIfNotExistsFn: () => releaseService.GetRelease(publicationSlug, releaseSlug),
-            logger: logger
-        );
-    }
-
-    public Task<Either<ActionResult, ReleaseCacheViewModel>> UpdateRelease(
-        Guid releaseVersionId,
-        string publicationSlug,
-        string? releaseSlug = null,
-        DateTimeOffset? expectedPublishDate = null
-    )
-    {
-        return publicBlobCacheService.Update(
-            cacheKey: new ReleaseCacheKey(publicationSlug: publicationSlug, releaseSlug: releaseSlug),
-            createFn: () => releaseService.GetRelease(releaseVersionId, expectedPublishDate),
-            logger: logger
-        );
-    }
-
     public async Task<Either<ActionResult, Unit>> RemoveRelease(string publicationSlug, string releaseSlug)
     {
         await publicBlobStorageService.DeleteBlobs(
--- src/GovUk.Education.ExploreEducationStatistics.Content.Services/Mappings/MappingProfiles.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Services/Mappings/MappingProfiles.cs
@@ -1,4 +1,3 @@
-using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
 using GovUk.Education.ExploreEducationStatistics.Common.Mappings;
 using GovUk.Education.ExploreEducationStatistics.Content.Model;
 using GovUk.Education.ExploreEducationStatistics.Content.ViewModels;
@@ -12,7 +11,13 @@ public class MappingProfiles : CommonMappingProfile
 {
     public MappingProfiles()
     {
-        CreateContentBlockMap();
+        CreateMap<MethodologyVersion, MethodologyVersionSummaryViewModel>();
+        MapTypesForMethodologyVersionViewModel();
+    }
+
+    private void MapTypesForMethodologyVersionViewModel()
+    {
+        // The following configs are all required to map MethodologyVersion to MethodologyVersionViewModel
 
         CreateMap<ContentSection, ContentSectionViewModel>()
             .ForMember(
@@ -21,8 +26,6 @@ public class MappingProfiles : CommonMappingProfile
             );
 
         CreateMap<MethodologyNote, MethodologyNoteViewModel>();
-
-        CreateMap<MethodologyVersion, MethodologyVersionSummaryViewModel>();
 
         CreateMap<MethodologyVersion, MethodologyVersionViewModel>()
             .ForMember(
@@ -47,47 +50,7 @@ public class MappingProfiles : CommonMappingProfile
                     )
             );
 
-        CreateMap<Publication, PublicationSummaryViewModel>();
-
-        CreateMap<KeyStatisticDataBlock, KeyStatisticDataBlockViewModel>();
-        CreateMap<KeyStatisticText, KeyStatisticTextViewModel>();
-        CreateMap<KeyStatistic, KeyStatisticViewModel>().IncludeAllDerived();
-
-        CreateMap<ReleaseVersion, ReleaseCacheViewModel>()
-            .ForMember(dest => dest.CoverageTitle, m => m.MapFrom(rv => rv.Release.TimePeriodCoverage.GetEnumLabel()))
-            .ForMember(dest => dest.ReleaseName, m => m.MapFrom(rv => rv.Release.Year.ToString()))
-            .ForMember(dest => dest.Slug, m => m.MapFrom(rv => rv.Release.Slug))
-            .ForMember(dest => dest.Title, m => m.MapFrom(rv => rv.Release.Title))
-            .ForMember(dest => dest.YearTitle, m => m.MapFrom(rv => rv.Release.YearTitle))
-            .ForMember(dest => dest.Published, m => m.MapFrom(releaseVersion => releaseVersion.PublishedDisplayDate))
-            .ForMember(
-                dest => dest.PublishingOrganisations,
-                m => m.MapFrom(rv => rv.PublishingOrganisations.OrderBy(o => o.Title))
-            )
-            .ForMember(dest => dest.Updates, m => m.MapFrom(rv => rv.Updates.OrderByDescending(update => update.On)))
-            .ForMember(dest => dest.Content, m => m.MapFrom(rv => rv.GenericContent.OrderBy(s => s.Order)))
-            .ForMember(dest => dest.KeyStatistics, m => m.MapFrom(rv => rv.KeyStatistics.OrderBy(ks => ks.Order)));
-
-        CreateMap<Link, LinkViewModel>();
-
-        CreateMap<Organisation, OrganisationViewModel>();
-
-        CreateMap<Update, ReleaseNoteViewModel>();
-    }
-
-    private void CreateContentBlockMap()
-    {
         CreateMap<ContentBlock, IContentBlockViewModel>().IncludeAllDerived();
-
-        // EES-4640 - we include an AfterMap configuration here to ensure that any time we create a
-        // DataBlockViewModel from a plain DataBlock, we also include the DataBlockParentId on the
-        // destination DataBlockViewModel that the DataBlock itself does not contain. When DataBlock is
-        // removed from the ContentBlock model, this can go too.
-        CreateMap<DataBlock, DataBlockViewModel>().AfterMap<DataBlockViewModelPostMappingAction>();
-
-        CreateMap<EmbedBlockLink, EmbedBlockLinkViewModel>()
-            .ForMember(dest => dest.Title, m => m.MapFrom(embedBlockLink => embedBlockLink.EmbedBlock.Title))
-            .ForMember(dest => dest.Url, m => m.MapFrom(embedBlockLink => embedBlockLink.EmbedBlock.Url));
 
         CreateMap<HtmlBlock, HtmlBlockViewModel>();
     }
