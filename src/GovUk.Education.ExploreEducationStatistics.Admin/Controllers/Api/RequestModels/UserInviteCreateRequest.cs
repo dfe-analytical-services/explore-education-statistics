270f4835345601db100fa6f692200752857aa754
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-04-15 16:11:28 +0100

    EES-7041 - Refactoring/reconfiguring the endpoints in the UserManagementController/PreReleaseController/ReleasesController/PrereleaseUsersController/UserInvitesController to try to make them a little more consistent/intuitive
    We have purposefully not tried to introduce any new tests; only move tests around. Adding more tests would technically be out of scope for what we're trying to test here. So if certain methods before had no tests covering them, they still won't

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/RequestModels/UserInviteCreateRequest.cs
@@ -0,0 +1,20 @@
+#nullable enable
+using System.ComponentModel.DataAnnotations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Controllers.Api.RequestModels;
+
+public record UserInviteCreateRequest
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; } = null!;
+
+    [Required(AllowEmptyStrings = false)]
+    public string RoleId { get; init; } = null!;
+
+    public DateTimeOffset? CreatedDate { get; init; }
+
+    public IReadOnlyList<UserPrereleaseRoleCreateRequest> UserPrereleaseRoles { get; init; } = [];
+
+    public IReadOnlyList<UserPublicationRoleCreateRequest> UserPublicationRoles { get; init; } = [];
+}
---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/UserManagement/PublicationRolesController.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+using GovUk.Education.ExploreEducationStatistics.Admin.Controllers.Api.RequestModels;
+using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Common.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Controllers.Api.UserManagement;
+
+[ApiController]
+[Route("api")]
+[Authorize]
+public class PublicationRolesController(IUserRoleService userRoleService) : ControllerBase
+{
+    [HttpPost("users/{userId:guid}/publication-roles")]
+    [ProducesResponseType(200)]
+    public async Task<ActionResult<Unit>> AddPublicationRole(Guid userId, UserPublicationRoleCreateRequest request)
+    {
+        return await userRoleService
+            .AddPublicationRole(userId, request.PublicationId, request.PublicationRole)
+            .HandleFailuresOrOk();
+    }
+
+    [HttpDelete("users/publication-roles/{id:guid}")]
+    [ProducesResponseType(200)]
+    public async Task<ActionResult<Unit>> DeleteUserPublicationRole(Guid id)
+    {
+        return await userRoleService.RemoveUserPublicationRole(id).HandleFailuresOrOk();
+    }
+}
