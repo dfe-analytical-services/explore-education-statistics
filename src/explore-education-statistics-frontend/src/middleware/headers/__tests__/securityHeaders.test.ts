bbc8326d428b985fcbbe8050c0434b3e167df788
Duncan Watson <duncan@hiveit.co.uk>
2026-04-20 17:08:57 +0100

    EES-7038 - removed _next URL exclusion from security header middleware as these are not processed by middleware

--- src/explore-education-statistics-frontend/src/middleware/headers/__tests__/securityHeaders.test.ts
+++ src/explore-education-statistics-frontend/src/middleware/headers/__tests__/securityHeaders.test.ts
@@ -2,15 +2,8 @@ import securityHeaders from '@frontend/middleware/headers/securityHeaders';
 import runMiddleware from '@frontend/middleware/__tests__/util/runMiddleware';
 
 describe('securityHeaders', () => {
-  test('ignores any requests to /_next/* paths', async () => {
-    const response = await getResponseForUrl('https://my-env/_next/a-url');
-    expect(response.headers.get('X-Content-Type-Options')).toBeNull();
-  });
-
-  test('applies an X-Content-Type-Options header for all other requests', async () => {
-    const response = await getResponseForUrl(
-      'https://my-env/any-other-request',
-    );
+  test('applies an X-Content-Type-Options header to requests', async () => {
+    const response = await getResponseForUrl('https://my-env/any-request');
     expect(response.headers.get('X-Content-Type-Options')).toBe('nosniff');
   });
 
--- src/explore-education-statistics-frontend/src/middleware/headers/securityHeaders.ts
+++ src/explore-education-statistics-frontend/src/middleware/headers/securityHeaders.ts
@@ -15,13 +15,6 @@ export default async function securityHeaders(
     return null;
   }
 
-  const { pathname } = req.nextUrl;
-
-  // Exclude altering any requests to /_next/* in any way. Next will handle these separately.
-  if (pathname.startsWith('/_next/')) {
-    return res;
-  }
-
   res.headers.set('X-Content-Type-Options', 'nosniff');
   return res;
 }
