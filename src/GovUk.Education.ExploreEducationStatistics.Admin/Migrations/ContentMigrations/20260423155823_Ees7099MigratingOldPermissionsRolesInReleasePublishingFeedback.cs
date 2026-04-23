b74b27d1d00db268640c87a7c4d2500d56b8c86f
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-04-23 17:25:00 +0100

    EES-7099 - Adding a migration to migrate OLD permissions system roles to NEW ones in the ReleasePublishingFeedback table

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20260423155823_Ees7099MigratingOldPermissionsRolesInReleasePublishingFeedback.cs
@@ -0,0 +1,28 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations;
+
+/// <inheritdoc />
+public partial class Ees7099MigratingOldPermissionsRolesInReleasePublishingFeedback : Migration
+{
+    private const string MigrationId = "20260423155823";
+
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        // Migrate all existing UserPublicationRole entries in the ReleasePublishingFeedback table
+        // from the OLD permissions system roles to the corresponding NEW ones:
+        // - `Allower` to `Approver`
+        // - `Owner` to `Drafter`
+        migrationBuilder.SqlFromFile(
+            MigrationConstants.ContentMigrationsPath,
+            $"{MigrationId}_{nameof(Ees7099MigratingOldPermissionsRolesInReleasePublishingFeedback)}.sql"
+        );
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) { }
+}
