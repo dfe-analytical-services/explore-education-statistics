d4d7e5fd4e4f5a271b768187d468899d3ca232fe
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-04-23 17:25:00 +0100

    EES-7099 - Adding a migration to migrate OLD permissions system roles to NEW ones in the ReleasePublishingFeedback table

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20260423155823_Ees7099MigratingOldPermissionsRolesInReleasePublishingFeedback.sql
@@ -0,0 +1,13 @@
+﻿-- Migrate all existing UserPublicationRole entries in the ReleasePublishingFeedback table
+-- from the OLD permissions system roles to the corresponding NEW ones:
+-- - `Allower` to `Approver`
+-- - `Owner` to `Drafter` 
+UPDATE rpf
+SET rpf.UserPublicationRole = 'Approver'
+FROM ReleasePublishingFeedback rpf
+WHERE rpf.UserPublicationRole = 'Allower'
+
+UPDATE rpf
+SET rpf.UserPublicationRole = 'Drafter'
+FROM ReleasePublishingFeedback rpf
+WHERE rpf.UserPublicationRole = 'Owner'
