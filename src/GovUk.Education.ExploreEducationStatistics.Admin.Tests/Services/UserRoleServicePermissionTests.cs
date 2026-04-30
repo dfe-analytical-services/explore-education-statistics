c35a8d2972d3e16f7dd51034da52f4312b6855a8
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-04-30 20:24:20 +0100

    EES-7042 - Adding missing `UpdatePublicationDrafters` BE endpoint + moving the `ListPublicationRoles` endpoint

--- src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/UserRoleServicePermissionTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin.Tests/Services/UserRoleServicePermissionTests.cs
@@ -99,6 +99,38 @@ public class UserRoleServicePermissionTests
     }
 
     [Fact]
+    public async Task InviteDrafter()
+    {
+        await PolicyCheckBuilder<SecurityPolicies>()
+            .SetupResourceCheckToFail(_publication, CanUpdateDrafters)
+            .AssertForbidden(async userService =>
+            {
+                await using var contentDbContext = InMemoryApplicationDbContext();
+                await contentDbContext.AddAsync(_publication);
+                await contentDbContext.SaveChangesAsync();
+
+                var service = SetupService(contentDbContext: contentDbContext, userService: userService.Object);
+                return await service.InviteDrafter("test@test.com", _publication.Id);
+            });
+    }
+
+    [Fact]
+    public async Task UpdatePublicationDrafters()
+    {
+        await PolicyCheckBuilder<SecurityPolicies>()
+            .SetupResourceCheckToFail(_publication, CanUpdateDrafters)
+            .AssertForbidden(async userService =>
+            {
+                await using var contentDbContext = InMemoryApplicationDbContext();
+                await contentDbContext.AddAsync(_publication);
+                await contentDbContext.SaveChangesAsync();
+
+                var service = SetupService(contentDbContext: contentDbContext, userService: userService.Object);
+                return await service.UpdatePublicationDrafters(_publication.Id, []);
+            });
+    }
+
+    [Fact]
     public async Task RemoveUserPublicationRole()
     {
         await PolicyCheckBuilder<SecurityPolicies>()
