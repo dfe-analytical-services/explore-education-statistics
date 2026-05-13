3c8bbd819672d996558f5671e7e47636322cb01f
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-05-13 09:29:13 +0100

    EES-7041 - Re-adding the `DeleteAllUserResourceRoles` endpoint which is currently used by some robot tests
    Added it so a robot-test specific namespace

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Controllers/Api/RobotTests/RobotTestsController.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces;
+using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Common.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Controllers.Api.RobotTests;
+
+[ApiController]
+[Route("api")]
+[Authorize]
+public class RobotTestsController(IUserRoleService userRoleService) : ControllerBase
+{
+    /// <summary>
+    /// Removes all user resource roles for the given user.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    [HttpDelete("users/{userId:guid}/resource-roles")]
+    [ProducesResponseType(204)]
+    public async Task<ActionResult<Unit>> DeleteAllUserResourceRoles(Guid userId)
+    {
+        return await userRoleService.RemoveAllUserResourceRoles(userId).HandleFailuresOrNoContent();
+    }
+}
