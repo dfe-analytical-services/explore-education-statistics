62e01f38e670ecd1e9a93500f114776365b594df
Ben Outram <ben@hiveit.co.uk>
2026-05-18 13:36:51 +0100

    🐛 EES-7133 Remove Publication cache when publication slug changes (#6988)
    * EES-7133 Move IReleaseCacheService/ReleaseCacheService from Content.Services to Admin
    
    * EES-7133 Change return type of RemoveRelease from Task<Either<ActionResult, Unit>> to Task
    
    * EES-7133 Remove publication cache when publication slug changes
    
    * EES-7133 Remove unused method ReleasePublishingStatusService.GetLatest
    
    * EES-7133 Append slash to blob name prefix in RemoveRelease
    
    * EES-7133 Add PublicationCacheServiceTests
    
    * EES-7133 Add ReleaseCacheServiceTests
    
    * EES-7133 Update PublicationCacheServiceTests
    
    * EES-7133 Tidy up usages of mock builders in UpdatePublication unit test assertions

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Services/Interfaces/Cache/IPublicationCacheService.cs
@@ -0,0 +1,8 @@
+﻿#nullable enable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Services.Interfaces.Cache;
+
+public interface IPublicationCacheService
+{
+    Task RemovePublication(string publicationSlug);
+}
