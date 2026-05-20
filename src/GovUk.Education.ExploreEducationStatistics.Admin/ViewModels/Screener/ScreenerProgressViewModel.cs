bb1a6b97671b0f9059b399a204482e22d9412e8c
Tom Jones <tomjones@hiveit.co.uk>
2026-05-20 14:00:02 +0100

    wip change handler, ignore cache files

--- src/GovUk.Education.ExploreEducationStatistics.Admin/ViewModels/Screener/ScreenerProgressViewModel.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/ViewModels/Screener/ScreenerProgressViewModel.cs
@@ -1,7 +1,11 @@
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+
 namespace GovUk.Education.ExploreEducationStatistics.Admin.ViewModels.Screener;
 
 public record ScreenerProgressViewModel
 {
+    public DataSetUploadStatus Status { get; set; }
+
     public int PercentageComplete { get; set; }
 
     public string Stage { get; set; } = null!;
