3f473b491e508c8f90ec0790dcf458be07ebf367
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-05-27 16:32:47 +0100

    EES-7099 - Reordering migration

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20260527152901_Ees7099MigratingOldPermissionsRolesInReleasePublishingFeedback.sql
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20260527152901_Ees7099MigratingOldPermissionsRolesInReleasePublishingFeedback.sql
@@ -1,7 +1,7 @@
 ﻿-- Migrate all existing UserPublicationRole entries in the ReleasePublishingFeedback table
 -- from the OLD permissions system roles to the corresponding NEW ones:
 -- - `Allower` to `Approver`
--- - `Owner` to `Drafter` 
+-- - `Owner` to `Drafter`
 UPDATE ReleasePublishingFeedback
 SET UserPublicationRole = 'Approver'
 WHERE UserPublicationRole = 'Allower';
