a57af07a870cce07e375b4a2d5bbc87c1c984e5b
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-06-22 10:10:43 +0100

    EES-7225 - Adding migration to remove historical redundant pre-release roles which are assigned to releases which are already associated with more powerful publication-level roles

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20260622084716_Ees7225RemovingRedundantPreReleaseRoles.cs
@@ -0,0 +1,27 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    /// <inheritdoc />
+    public partial class Ees7225RemovingRedundantPreReleaseRoles : Migration
+    {
+        private const string MigrationId = "20260622084716";
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Remove any redundant pre-release roles that have been created for users who already have a
+            // publication-level role for the same release.
+            migrationBuilder.SqlFromFile(
+                MigrationConstants.ContentMigrationsPath,
+                $"{MigrationId}_{nameof(Ees7225RemovingRedundantPreReleaseRoles)}.sql"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) { }
+    }
+}
