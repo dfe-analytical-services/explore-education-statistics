688c0a8fb33e77d10dae78f0620d440bfceebb39
Ben Outram <ben@hiveit.co.uk>
2026-07-03 16:16:33 +0100

    EES-7343 Tidy-up TimePeriodServiceTests.

--- src/GovUk.Education.ExploreEducationStatistics.Data.Services.Tests/TimePeriodServiceTests.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Services.Tests/TimePeriodServiceTests.cs
@@ -6,241 +6,247 @@ using static GovUk.Education.ExploreEducationStatistics.Data.Model.Tests.Utils.S
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Services.Tests;
 
-public class TimePeriodServiceTests
+public abstract class TimePeriodServiceTests
 {
-    [Fact]
-    public async Task GetTimePeriods_Subject_CorrectlyOrderWeeklyTimeIdentifiers()
+    public class GetTimePeriodsTests : TimePeriodServiceTests
     {
-        var subject = new Subject
+        [Fact]
+        public async Task SubjectHasWeeklyTimeIdentifiers_CorrectlyOrdered()
         {
-            Observations = new List<Observation>
+            var subject = new Subject
             {
-                new() { Year = 2001, TimeIdentifier = Week1 },
-                new() { Year = 2000, TimeIdentifier = Week20 },
-                new() { Year = 2000, TimeIdentifier = Week2 },
-                new() { Year = 2000, TimeIdentifier = Week1 },
-                new() { Year = 2000, TimeIdentifier = Week10 },
-            },
-        };
+                Observations =
+                [
+                    new Observation { Year = 2001, TimeIdentifier = Week1 },
+                    new Observation { Year = 2000, TimeIdentifier = Week20 },
+                    new Observation { Year = 2000, TimeIdentifier = Week2 },
+                    new Observation { Year = 2000, TimeIdentifier = Week1 },
+                    new Observation { Year = 2000, TimeIdentifier = Week10 },
+                ],
+            };
 
-        var statisticsDbContextId = Guid.NewGuid().ToString();
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
-        {
-            await statisticsDbContext.AddAsync(subject);
-            await statisticsDbContext.SaveChangesAsync();
-        }
-
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
-        {
-            var service = new TimePeriodService(statisticsDbContext);
-            var result = await service.GetTimePeriods(subject.Id);
-
-            Assert.Equal(5, result.Count);
-            Assert.Equal((2000, Week1), result[0]);
-            Assert.Equal((2000, Week2), result[1]);
-            Assert.Equal((2000, Week10), result[2]);
-            Assert.Equal((2000, Week20), result[3]);
-            Assert.Equal((2001, Week1), result[4]);
-        }
-    }
-
-    [Fact]
-    public async Task GetTimePeriods_Subject_CorrectlyOrderMonthlyTimeIdentifiers()
-    {
-        var subject = new Subject
-        {
-            Observations = new List<Observation>
+            var statisticsDbContextId = Guid.NewGuid().ToString();
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
             {
-                new() { Year = 2001, TimeIdentifier = January },
-                new() { Year = 2000, TimeIdentifier = February },
-                new() { Year = 2000, TimeIdentifier = December },
-                new() { Year = 2000, TimeIdentifier = October },
-                new() { Year = 2000, TimeIdentifier = March },
-            },
-        };
+                statisticsDbContext.Add(subject);
+                await statisticsDbContext.SaveChangesAsync();
+            }
 
-        var statisticsDbContextId = Guid.NewGuid().ToString();
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
-        {
-            await statisticsDbContext.AddAsync(subject);
-            await statisticsDbContext.SaveChangesAsync();
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                var service = new TimePeriodService(statisticsDbContext);
+                var result = await service.GetTimePeriods(subject.Id);
+
+                Assert.Equal(5, result.Count);
+                Assert.Equal((2000, Week1), result[0]);
+                Assert.Equal((2000, Week2), result[1]);
+                Assert.Equal((2000, Week10), result[2]);
+                Assert.Equal((2000, Week20), result[3]);
+                Assert.Equal((2001, Week1), result[4]);
+            }
         }
 
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+        [Fact]
+        public async Task SubjectHasMonthlyTimeIdentifiers_CorrectlyOrdered()
         {
-            var service = new TimePeriodService(statisticsDbContext);
-            var result = await service.GetTimePeriods(subject.Id);
+            var subject = new Subject
+            {
+                Observations =
+                [
+                    new Observation { Year = 2001, TimeIdentifier = January },
+                    new Observation { Year = 2000, TimeIdentifier = February },
+                    new Observation { Year = 2000, TimeIdentifier = December },
+                    new Observation { Year = 2000, TimeIdentifier = October },
+                    new Observation { Year = 2000, TimeIdentifier = March },
+                ],
+            };
 
-            Assert.Equal(5, result.Count);
-            Assert.Equal((2000, February), result[0]);
-            Assert.Equal((2000, March), result[1]);
-            Assert.Equal((2000, October), result[2]);
-            Assert.Equal((2000, December), result[3]);
-            Assert.Equal((2001, January), result[4]);
-        }
-    }
+            var statisticsDbContextId = Guid.NewGuid().ToString();
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                statisticsDbContext.Add(subject);
+                await statisticsDbContext.SaveChangesAsync();
+            }
 
-    [Fact]
-    public async Task GetTimePeriods_Observations_CorrectlyOrderWeeklyTimeIdentifiers()
-    {
-        var obs1 = new Observation { Year = 2001, TimeIdentifier = Week1 };
-        var obs2 = new Observation { Year = 2000, TimeIdentifier = Week20 };
-        var obs3 = new Observation { Year = 2000, TimeIdentifier = Week2 };
-        var obs4 = new Observation { Year = 2000, TimeIdentifier = Week1 };
-        var obs5 = new Observation { Year = 2000, TimeIdentifier = Week10 };
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                var service = new TimePeriodService(statisticsDbContext);
+                var result = await service.GetTimePeriods(subject.Id);
 
-        var statisticsDbContextId = Guid.NewGuid().ToString();
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
-        {
-            await statisticsDbContext.AddRangeAsync(obs1, obs2, obs3, obs4, obs5);
-            await statisticsDbContext.SaveChangesAsync();
-        }
-
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
-        {
-            var service = new TimePeriodService(statisticsDbContext);
-            var queryableObservations = statisticsDbContext.Observation.AsQueryable();
-            var result = await service.GetTimePeriods(queryableObservations);
-
-            Assert.Equal(5, result.Count);
-            Assert.Equal((2000, Week1), result[0]);
-            Assert.Equal((2000, Week2), result[1]);
-            Assert.Equal((2000, Week10), result[2]);
-            Assert.Equal((2000, Week20), result[3]);
-            Assert.Equal((2001, Week1), result[4]);
-        }
-    }
-
-    [Fact]
-    public async Task GetTimePeriodLabels()
-    {
-        var releaseVersion = new ReleaseVersion();
-
-        var subject = new Subject();
-
-        var releaseSubject = new ReleaseSubject { ReleaseVersion = releaseVersion, Subject = subject };
-
-        var subjectObservation1 = new Observation
-        {
-            Subject = subject,
-            Year = 2030,
-            TimeIdentifier = AcademicYearQ3,
-        };
-
-        var subjectObservation2 = new Observation
-        {
-            Subject = subject,
-            Year = 2020,
-            TimeIdentifier = AcademicYearQ4,
-        };
-
-        var subjectObservation3 = new Observation
-        {
-            Subject = subject,
-            Year = 2021,
-            TimeIdentifier = AcademicYearQ1,
-        };
-
-        var statisticsDbContextId = Guid.NewGuid().ToString();
-
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
-        {
-            statisticsDbContext.ReleaseVersion.Add(releaseVersion);
-            statisticsDbContext.Subject.Add(subject);
-            statisticsDbContext.ReleaseSubject.Add(releaseSubject);
-            statisticsDbContext.Observation.AddRange(subjectObservation1, subjectObservation2, subjectObservation3);
-            await statisticsDbContext.SaveChangesAsync();
+                Assert.Equal(5, result.Count);
+                Assert.Equal((2000, February), result[0]);
+                Assert.Equal((2000, March), result[1]);
+                Assert.Equal((2000, October), result[2]);
+                Assert.Equal((2000, December), result[3]);
+                Assert.Equal((2001, January), result[4]);
+            }
         }
 
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+        [Fact]
+        public async Task ObservationsHaveWeeklyTimeIdentifiers_CorrectlyOrdered()
         {
-            var service = new TimePeriodService(statisticsDbContext);
+            var obs1 = new Observation { Year = 2001, TimeIdentifier = Week1 };
+            var obs2 = new Observation { Year = 2000, TimeIdentifier = Week20 };
+            var obs3 = new Observation { Year = 2000, TimeIdentifier = Week2 };
+            var obs4 = new Observation { Year = 2000, TimeIdentifier = Week1 };
+            var obs5 = new Observation { Year = 2000, TimeIdentifier = Week10 };
 
-            var result = await service.GetTimePeriodLabels(subject.Id);
+            var statisticsDbContextId = Guid.NewGuid().ToString();
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                statisticsDbContext.AddRange(obs1, obs2, obs3, obs4, obs5);
+                await statisticsDbContext.SaveChangesAsync();
+            }
 
-            Assert.Equal("2020/21 Q4", result.From);
-            Assert.Equal("2030/31 Q3", result.To);
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                var service = new TimePeriodService(statisticsDbContext);
+                var queryableObservations = statisticsDbContext.Observation.AsQueryable();
+                var result = await service.GetTimePeriods(queryableObservations);
+
+                Assert.Equal(5, result.Count);
+                Assert.Equal((2000, Week1), result[0]);
+                Assert.Equal((2000, Week2), result[1]);
+                Assert.Equal((2000, Week10), result[2]);
+                Assert.Equal((2000, Week20), result[3]);
+                Assert.Equal((2001, Week1), result[4]);
+            }
         }
     }
 
-    [Fact]
-    public async Task GetTimePeriodLabels_CorrectlyOrderWeeks()
+    public class GetTimePeriodLabelsTests : TimePeriodServiceTests
     {
-        var releaseVersion = new ReleaseVersion();
-
-        var subject = new Subject();
-
-        var releaseSubject = new ReleaseSubject { ReleaseVersion = releaseVersion, Subject = subject };
-
-        var subjectObservation1 = new Observation
+        [Fact]
+        public async Task SubjectHasQuarterlyObservations_ReturnsCorrectFromAndToLabels()
         {
-            Subject = subject,
-            Year = 2020,
-            TimeIdentifier = Week9,
-        };
+            var releaseVersion = new ReleaseVersion();
 
-        var subjectObservation2 = new Observation
-        {
-            Subject = subject,
-            Year = 2020,
-            TimeIdentifier = Week37,
-        };
+            var subject = new Subject();
 
-        var subjectObservation3 = new Observation
-        {
-            Subject = subject,
-            Year = 2020,
-            TimeIdentifier = Week8,
-        };
+            var releaseSubject = new ReleaseSubject { ReleaseVersion = releaseVersion, Subject = subject };
 
-        var statisticsDbContextId = Guid.NewGuid().ToString();
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
-        {
-            statisticsDbContext.ReleaseVersion.Add(releaseVersion);
-            statisticsDbContext.Subject.Add(subject);
-            statisticsDbContext.ReleaseSubject.Add(releaseSubject);
-            statisticsDbContext.Observation.AddRange(subjectObservation1, subjectObservation2, subjectObservation3);
-            await statisticsDbContext.SaveChangesAsync();
+            var subjectObservation1 = new Observation
+            {
+                Subject = subject,
+                Year = 2030,
+                TimeIdentifier = AcademicYearQ3,
+            };
+
+            var subjectObservation2 = new Observation
+            {
+                Subject = subject,
+                Year = 2020,
+                TimeIdentifier = AcademicYearQ4,
+            };
+
+            var subjectObservation3 = new Observation
+            {
+                Subject = subject,
+                Year = 2021,
+                TimeIdentifier = AcademicYearQ1,
+            };
+
+            var statisticsDbContextId = Guid.NewGuid().ToString();
+
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                statisticsDbContext.ReleaseVersion.Add(releaseVersion);
+                statisticsDbContext.Subject.Add(subject);
+                statisticsDbContext.ReleaseSubject.Add(releaseSubject);
+                statisticsDbContext.Observation.AddRange(subjectObservation1, subjectObservation2, subjectObservation3);
+                await statisticsDbContext.SaveChangesAsync();
+            }
+
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                var service = new TimePeriodService(statisticsDbContext);
+
+                var result = await service.GetTimePeriodLabels(subject.Id);
+
+                Assert.Equal("2020/21 Q4", result.From);
+                Assert.Equal("2030/31 Q3", result.To);
+            }
         }
 
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+        [Fact]
+        public async Task SubjectHasWeeklyObservations_ReturnsCorrectFromAndToLabels()
         {
-            var service = new TimePeriodService(statisticsDbContext);
+            var releaseVersion = new ReleaseVersion();
 
-            var result = await service.GetTimePeriodLabels(subject.Id);
+            var subject = new Subject();
 
-            Assert.Equal("2020 Week 8", result.From);
-            Assert.Equal("2020 Week 37", result.To);
+            var releaseSubject = new ReleaseSubject { ReleaseVersion = releaseVersion, Subject = subject };
+
+            var subjectObservation1 = new Observation
+            {
+                Subject = subject,
+                Year = 2020,
+                TimeIdentifier = Week9,
+            };
+
+            var subjectObservation2 = new Observation
+            {
+                Subject = subject,
+                Year = 2020,
+                TimeIdentifier = Week37,
+            };
+
+            var subjectObservation3 = new Observation
+            {
+                Subject = subject,
+                Year = 2020,
+                TimeIdentifier = Week8,
+            };
+
+            var statisticsDbContextId = Guid.NewGuid().ToString();
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                statisticsDbContext.ReleaseVersion.Add(releaseVersion);
+                statisticsDbContext.Subject.Add(subject);
+                statisticsDbContext.ReleaseSubject.Add(releaseSubject);
+                statisticsDbContext.Observation.AddRange(subjectObservation1, subjectObservation2, subjectObservation3);
+                await statisticsDbContext.SaveChangesAsync();
+            }
+
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                var service = new TimePeriodService(statisticsDbContext);
+
+                var result = await service.GetTimePeriodLabels(subject.Id);
+
+                Assert.Equal("2020 Week 8", result.From);
+                Assert.Equal("2020 Week 37", result.To);
+            }
         }
-    }
 
-    [Fact]
-    public async Task GetTimePeriodLabels_NoObservations()
-    {
-        var releaseVersion = new ReleaseVersion();
-
-        var subject = new Subject();
-
-        var releaseSubject1 = new ReleaseSubject { ReleaseVersion = releaseVersion, Subject = subject };
-
-        var statisticsDbContextId = Guid.NewGuid().ToString();
-
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+        [Fact]
+        public async Task SubjectHasNoObservations_ReturnsEmptyLabels()
         {
-            statisticsDbContext.ReleaseVersion.Add(releaseVersion);
-            statisticsDbContext.Subject.Add(subject);
-            statisticsDbContext.ReleaseSubject.Add(releaseSubject1);
-            await statisticsDbContext.SaveChangesAsync();
-        }
+            var releaseVersion = new ReleaseVersion();
 
-        await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
-        {
-            var service = new TimePeriodService(statisticsDbContext);
+            var subject = new Subject();
 
-            var result = await service.GetTimePeriodLabels(subject.Id);
+            var releaseSubject1 = new ReleaseSubject { ReleaseVersion = releaseVersion, Subject = subject };
 
-            Assert.Empty(result.From);
-            Assert.Empty(result.To);
+            var statisticsDbContextId = Guid.NewGuid().ToString();
+
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                statisticsDbContext.ReleaseVersion.Add(releaseVersion);
+                statisticsDbContext.Subject.Add(subject);
+                statisticsDbContext.ReleaseSubject.Add(releaseSubject1);
+                await statisticsDbContext.SaveChangesAsync();
+            }
+
+            await using (var statisticsDbContext = InMemoryStatisticsDbContext(statisticsDbContextId))
+            {
+                var service = new TimePeriodService(statisticsDbContext);
+
+                var result = await service.GetTimePeriodLabels(subject.Id);
+
+                Assert.Empty(result.From);
+                Assert.Empty(result.To);
+            }
         }
     }
 }
