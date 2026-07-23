ea750bec2bb91ff0fc0d82caf62b6d947299cdd7
Tom Jones <77351705+tomjonesdev@users.noreply.github.com>
2026-07-23 10:20:13 +0100

    EES-6891: Retrieve subject meta based on matched observations, rather than the table query. (#7195)
    Co-authored-by: Tom Jones <tomjones@hiveit.co.uk>

--- src/GovUk.Education.ExploreEducationStatistics.Data.Model/Repository/Interfaces/IFilterItemRepository.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Repository/Interfaces/IFilterItemRepository.cs
@@ -41,4 +41,17 @@ public interface IFilterItemRepository
     /// <returns>a set of <see cref="FilterItem" />s, their <see cref="FilterGroup" />s and
     /// <see cref="Filter" />s</returns>
     Task<IList<FilterItem>> GetFilterItems(IEnumerable<Guid> filterItemIds);
+
+    /// <summary>
+    /// Method to retrieve a set of <see cref="FilterItem" />s, their <see cref="FilterGroup" />s and
+    /// <see cref="Filter" />s that are present on a given set of Observations.
+    ///
+    /// Note that this method is for use when there is a set of Observations that has been fetched into memory
+    /// already, as it is optimised for this scenario.
+    /// </summary>
+    ///
+    /// <param name="observations">A set of Observations that have been already fetched into memory.</param>
+    /// <returns>a set of <see cref="FilterItem" />s, their <see cref="FilterGroup" />s and
+    /// <see cref="Filter" />s</returns>
+    Task<IList<FilterItem>> GetFilterItemsFromObservations(IEnumerable<Observation> observations);
 }
