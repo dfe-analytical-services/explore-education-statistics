c43a867c0a65f1e0c3a622fdadcd1b120bd6bbab
Mark Youngman <mark@youngman.info>
2026-08-10 15:50:55 +0100

    EES-6779 AI: Fix bug where ApiQueryStatTile version not being passed

--- src/GovUk.Education.ExploreEducationStatistics.Admin/Extensions/EducationInNumbersContentExtensions.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Extensions/EducationInNumbersContentExtensions.cs
@@ -71,6 +71,7 @@ public static class EducationInNumbersContentExtensions
                 DecimalPlaces = statTile.DecimalPlaces,
                 QueryResult = statTile.QueryResult,
                 ReleaseId = statTile.ReleaseId,
+                Version = statTile.Version,
             },
             _ => throw new Exception($"{nameof(EinTile)} type {tile.GetType()} not found"),
         };
