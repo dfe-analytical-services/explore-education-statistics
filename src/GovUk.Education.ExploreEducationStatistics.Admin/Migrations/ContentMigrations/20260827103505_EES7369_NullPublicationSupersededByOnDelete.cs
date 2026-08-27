98e8044e29ce0022aba4792dd4612f8c36705b13
Tom Jones <tomjones@hiveit.co.uk>
2026-08-27 11:40:45 +0100

    EES-7368: Set Publication.SupersededBy to null on delete.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20260827103505_EES7369_NullPublicationSupersededByOnDelete.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    /// <inheritdoc />
+    public partial class EES7369_NullPublicationSupersededByOnDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_Publications_Publications_SupersededById", table: "Publications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Publications_Publications_SupersededById",
+                table: "Publications",
+                column: "SupersededById",
+                principalTable: "Publications",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_Publications_Publications_SupersededById", table: "Publications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Publications_Publications_SupersededById",
+                table: "Publications",
+                column: "SupersededById",
+                principalTable: "Publications",
+                principalColumn: "Id"
+            );
+        }
+    }
+}
