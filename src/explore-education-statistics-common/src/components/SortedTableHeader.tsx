66fa66af792362cf513e70fb9de19f12b2df70f5
Guy Smith <guy@hiveit.co.uk>
2026-09-04 12:05:48 +0100

    EES-7545 abstract out ChevronIcon

--- src/explore-education-statistics-common/src/components/SortedTableHeader.tsx
+++ src/explore-education-statistics-common/src/components/SortedTableHeader.tsx
@@ -1,4 +1,5 @@
 import ButtonText from '@common/components/ButtonText';
+import ChevronIcon from '@common/components/ChevronIcon';
 import styles from '@common/components/SortedTableHeader.module.scss';
 import classNames from 'classnames';
 import React from 'react';
@@ -42,27 +43,11 @@ export default function SortedTableHeader<TColumn extends string>({
       >
         {label}
 
-        <svg
-          aria-hidden
+        <ChevronIcon
           className={styles.icon}
-          focusable="false"
-          viewBox="0 0 22 22"
+          direction={sortedDirection}
           width="1.2em"
-          fill="currentColor"
-        >
-          {sortedDirection === 'descending' && (
-            <path d="M15.4375 7L11 15.8687L6.5625 7L15.4375 7Z" />
-          )}
-          {sortedDirection === 'ascending' && (
-            <path d="M6.5625 15L11 6.1313L15.4375 15L6.5625 15Z" />
-          )}
-          {!sortedDirection && (
-            <>
-              <path d="M8.1875 9.5L10.9609 3.95703L13.7344 9.5H8.1875Z" />
-              <path d="M13.7344 12.0781L10.9609 17.6211L8.1875 12.0781H13.7344Z" />
-            </>
-          )}
-        </svg>
+        />
       </ButtonText>
     </th>
   );
