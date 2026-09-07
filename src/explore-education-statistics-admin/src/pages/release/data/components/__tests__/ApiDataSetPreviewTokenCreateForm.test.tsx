8ff7c3798e7ed571ef008887397854cc5d08ffa4
jack-hive <148866614+jack-hive@users.noreply.github.com>
2026-09-07 18:11:45 +0100

    EES-7389 - Adding robot tests

--- src/explore-education-statistics-admin/src/pages/release/data/components/__tests__/ApiDataSetPreviewTokenCreateForm.test.tsx
+++ src/explore-education-statistics-admin/src/pages/release/data/components/__tests__/ApiDataSetPreviewTokenCreateForm.test.tsx
@@ -273,6 +273,17 @@ describe('ApiDataSetPreviewTokenCreateForm', () => {
         selector: '#apiDataSetTokenCreateForm-selectionMethod-presetDays',
       }),
     ).not.toBeInTheDocument();
+
+    expect(
+      screen.getByText(
+        /If you would like to extend the period that this new token is valid for, please contact/,
+      ),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', {
+        name: 'explore.statistics@education.gov.uk',
+      }),
+    ).toHaveAttribute('href', 'mailto:explore.statistics@education.gov.uk');
   });
 });
 
