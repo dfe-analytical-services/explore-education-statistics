e722792f4d1f39bddbb46eed059f9b40bfcff0c0
Ben Outram <ben@hiveit.co.uk>
2026-09-14 16:59:09 +0100

    EES-7632 Update ReleaseSearchableDocument to add OrderedEquality annotation for PublishingOrganisations, and add unit tests to verify equality.

--- src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Domain/ReleaseSearchableDocument.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp/Domain/ReleaseSearchableDocument.cs
@@ -1,25 +1,43 @@
+using Generator.Equals;
+
 namespace GovUk.Education.ExploreEducationStatistics.Content.Search.FunctionApp.Domain;
 
-public record ReleaseSearchableDocument
+[Equatable]
+public partial record ReleaseSearchableDocument
 {
     public required Guid ReleaseId { get; init; }
+
     public required string ReleaseSlug { get; init; }
+
     public required Guid ReleaseVersionId { get; init; }
+
     public required Guid PublicationId { get; init; }
+
     public required string PublicationSlug { get; init; }
+
     public required string Summary { get; init; }
+
     public required string PublicationTitle { get; init; }
+
     public required DateTimeOffset Published { get; init; }
+
     public required Guid ThemeId { get; init; }
+
     public required string ThemeTitle { get; init; }
+
     public required string ReleaseType { get; init; }
+
     public required int TypeBoost { get; init; }
+
+    [OrderedEquality]
     public required PublishingOrganisation[] PublishingOrganisations { get; init; }
+
     public required string HtmlContent { get; init; }
 }
 
 public record PublishingOrganisation
 {
     public required Guid Id { get; init; }
+
     public required string Title { get; init; }
 }
