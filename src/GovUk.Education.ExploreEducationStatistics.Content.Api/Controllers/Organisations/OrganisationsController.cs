37ee7df4a8e66183c22b84b3b8fb4a84183367b8
Ben Outram <ben@hiveit.co.uk>
2026-09-21 13:42:49 +0100

    EES-7668 Add endpoint to get all organisations to the Content API. Also moves PublishingOrganisationDto out of ReleaseVersionSummaryDtos into separate Organisations.Dtos.OrganisationDto for reuse.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Controllers/Organisations/OrganisationsController.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using GovUk.Education.ExploreEducationStatistics.Content.Services.Organisations;
+using GovUk.Education.ExploreEducationStatistics.Content.Services.Organisations.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Controllers.Organisations;
+
+[Route("api")]
+[ApiController]
+public class OrganisationsController(IOrganisationsService organisationsService) : ControllerBase
+{
+    [HttpGet("organisations")]
+    public async Task<OrganisationDto[]> GetAllOrganisations(CancellationToken cancellationToken = default) =>
+        await organisationsService.GetAllOrganisations(cancellationToken);
+}
---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Services/Organisations/Dtos/OrganisationDto.cs
@@ -0,0 +1,29 @@
+﻿using GovUk.Education.ExploreEducationStatistics.Content.Model;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Services.Organisations.Dtos;
+
+public record OrganisationDto
+{
+    public required Guid Id { get; init; }
+
+    public required string? GISLogoHexCode { get; init; }
+
+    public required string LogoFileName { get; init; }
+
+    public required string Title { get; init; }
+
+    public required string Url { get; init; }
+
+    public required bool UseGISLogo { get; init; }
+
+    public static OrganisationDto FromOrganisation(Organisation organisation) =>
+        new()
+        {
+            Id = organisation.Id,
+            GISLogoHexCode = organisation.GISLogoHexCode,
+            LogoFileName = organisation.LogoFileName,
+            Title = organisation.Title,
+            Url = organisation.Url,
+            UseGISLogo = organisation.UseGISLogo,
+        };
+}
